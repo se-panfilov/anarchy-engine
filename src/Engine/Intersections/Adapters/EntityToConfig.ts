8a1014d29e2c8650eeecb15f539a5c741a348351
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:50:25 +0100

    [Serialize] Intersections

--- src/Engine/Intersections/Adapters/EntityToConfig.ts
+++ src/Engine/Intersections/Adapters/EntityToConfig.ts
@@ -1,3 +1,4 @@
+import type { TActor } from '@/Engine/Actor';
 import type { TIntersectionsWatcher, TIntersectionsWatcherConfig } from '@/Engine/Intersections/Models';
 import { extractSerializableRegistrableFields } from '@/Engine/Mixins';
 import { filterOutEmptyFields } from '@/Engine/Utils';
@@ -5,7 +6,7 @@ import { filterOutEmptyFields } from '@/Engine/Utils';
 export function intersectionsToConfig(entity: TIntersectionsWatcher): TIntersectionsWatcherConfig {
   return filterOutEmptyFields({
     cameraName: entity.getCamera().name,
-    actorNames: entity.getActors().map((actor) => actor.name),
+    actorNames: entity.getActors().map((actor: TActor): string => actor.name),
     isAutoStart: entity.isAutoStart,
     intersectionsLoop: entity.getIntersectionsLoop()?.name,
     ...extractSerializableRegistrableFields(entity)
