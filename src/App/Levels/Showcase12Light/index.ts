90f5db9d9489740a5e9b2fcef7641be55866848f
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:59:10 +0100

    light showcase (directional)

--- src/App/Levels/Showcase12Light/index.ts
+++ src/App/Levels/Showcase12Light/index.ts
@@ -1,8 +1,7 @@
 import GUI from 'lil-gui';
 
 import type { IShowcase } from '@/App/Levels/Models';
-import type { IAppCanvas, ILevel, ILevelConfig } from '@/Engine';
-import { buildLevelFromConfig } from '@/Engine';
+import { buildLevelFromConfig, envMapService, IAppCanvas, IDirectionalLightWrapper, ILevel, ILevelConfig, isNotDefined } from '@/Engine';
 
 import levelConfig from './showcase-12-light.json';
 
@@ -10,10 +9,19 @@ import levelConfig from './showcase-12-light.json';
 export function showcaseLevel(canvas: IAppCanvas): IShowcase {
   const gui: GUI = new GUI();
   const level: ILevel = buildLevelFromConfig(canvas, levelConfig as ILevelConfig);
-  const { actorRegistry } = level.entities;
+  const { lightRegistry } = level.entities;
 
-  async function init(): Promise<void> {
-    // gui.add(coordsUI, 'x').listen();
+  // void envMapService.load('/Showcase/hdr/urban_alley_01_4k.hdr');
+
+  function init(): void {
+    const directionalLight: IDirectionalLightWrapper | undefined = lightRegistry.getUniqByTag('directional') as IDirectionalLightWrapper | undefined;
+    if (isNotDefined(directionalLight)) throw new Error('Directional light not found');
+    const directionalFolder: GUI = gui.addFolder('Directional light');
+    directionalFolder.add(directionalLight.entity.position, 'x').min(-50).max(50).step(0.5);
+    directionalFolder.add(directionalLight.entity.position, 'y').min(-50).max(50).step(0.5);
+    directionalFolder.add(directionalLight.entity.position, 'z').min(-50).max(50).step(0.5);
+    directionalFolder.addColor(directionalLight.entity, 'color');
+    directionalFolder.add(directionalLight.entity, 'intensity').min(0).max(10).step(0.1);
   }
 
   function start(): void {
