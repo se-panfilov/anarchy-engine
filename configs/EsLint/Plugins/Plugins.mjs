91c2839d26ac42e4606dca53a7d70bed36c62803
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:30:01 +0100

    Lint: fixed plugins

--- configs/EsLint/Plugins/Plugins.mjs
+++ configs/EsLint/Plugins/Plugins.mjs
@@ -1,8 +1,12 @@
 import sortExportAll from 'eslint-plugin-sort-export-all';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import functionalPlugin from 'eslint-plugin-functional';
+
 export const plugins = {
-  // '@stylistic': stylistic,
-  simpleImportSort,
-  sortExportAll
+  'simple-import-sort': simpleImportSort,
+  '@typescript-eslint': tsPlugin,
+  functional: functionalPlugin,
+  'sort-export-all': sortExportAll
 };
--- configs/EsLint/Rules/Imports/SimpleImportRules.mjs
+++ configs/EsLint/Rules/Imports/SimpleImportRules.mjs
@@ -1,5 +1,4 @@
 export const SimpleImportRules = {
-  // TODO 10-1-0: EsLint fix:  simple-import-sort/imports
-  // 'simple-import-sort/imports': 'error',
-  // 'simple-import-sort/exports': 'error',
+  'simple-import-sort/imports': 'error',
+  'simple-import-sort/exports': 'error'
 };
