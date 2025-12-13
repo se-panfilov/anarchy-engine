9b6b20e06273cf55caec5d6244159369cd9fd640
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:51:19 +0100

    showcase 1

--- src/App/index.ts
+++ src/App/index.ts
@@ -1,6 +1,7 @@
 import '@App/style.css';
 
-import { showcaseLevel1 as showcase } from '@App/Levels/ShowcaseLevel1';
+// import { showcaseLevel1 as showcase } from '@App/Levels/ShowcaseLevel1';
+import { showcaseLevel2 as showcase } from '@App/Levels/ShowcaseLevel2';
 
 import type { IAppCanvas } from '@/Engine';
 import { ambientContext, isNotDefined } from '@/Engine';
@@ -8,13 +9,3 @@ import { ambientContext, isNotDefined } from '@/Engine';
 const canvas: IAppCanvas | null = ambientContext.container.getCanvasElement('#app');
 if (isNotDefined(canvas)) throw new Error('Canvas is not defined');
 showcase(canvas);
-
-// START Experiment1: animations ---------------
-// level.actor.factory.initial.create({
-//   type: ActorType.cube,
-//   position: Vector3Wrapper({ x: 0, y: 3, z: 0 }).entity,
-//   castShadow: true,
-//   materialParams: { color: '#5177ff' },
-//   tags: [ActorTag.Intersectable]
-// } satisfies IActorParams);
-// // END Experiment1: animations ---------------
