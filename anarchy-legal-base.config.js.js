34dfe1ecf6253d02137e92bdc6c983f87bb8bbed
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:07:05 +0100

    [LEGAL] added base config

---
+++ anarchy-legal-base.config.js.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    type: 'GENERIC',
+    messages: {
+      HAS_DPO: false,
+      HAS_EU_REP: false,
+      HAS_CHINA_REP: false,
+      PROHIBIT_HIGH_RISK_USE: true,
+      SBOM_AVAILABLE: false,
+      REGION_CN: true,
+      EU_CHILD_CONSENT_AGE: 16,
+      US_CHILD_AGE: 13,
+      CN_CHILD_AGE: 14
+    }
+  }
+];
