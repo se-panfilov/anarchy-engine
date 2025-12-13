7572270e17f2b75825621e659945cfded41ec9d0
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:17:16 +0100

    [APP] more data

--- apps/showcases-core/src/env-schema.ts
+++ apps/showcases-core/src/env-schema.ts
@@ -1,10 +1,11 @@
 import { toBool } from '@Anarchy/Shared/Utils';
-import { object } from 'valibot';
+import { object, string } from 'valibot';
 
 export const runtimeSchema = object({
   VITE_APP_SHOW_DEBUG_INFO: toBool,
   VITE_APP_SHOW_DEV_NAV: toBool,
-  VITE_SHOW_EXIT_GAME_MENU_BTN: toBool
+  VITE_SHOW_EXIT_GAME_MENU_BTN: toBool,
+  VITE_SENTRY_DSN: string()
   // VITE_APP_DRACO_DECODER_PATH: optional(string())
 });
 
