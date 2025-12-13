da30bf01450291ed9f324c122053e26290dc0f38
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:13:52 +0100

    [e2e] fix

--- packages/anarchy-i18n/src/Utils/LocalesUtils.spec.ts
+++ packages/anarchy-i18n/src/Utils/LocalesUtils.spec.ts
@@ -8,12 +8,9 @@ describe('LocalesUtils', (): void => {
   const enCA: TLocaleId = 'en-CA';
   const enGB: TLocaleId = 'en-GB';
   const nlNl: TLocaleId = 'nl-NL';
-  // eslint-disable-next-line spellcheck/spell-checker
   const srCyrlRs: TLocaleId = 'sr-Cyrl-RS';
   const zhHansCN: TLocaleId = 'zh-Hans-CN';
-  // eslint-disable-next-line spellcheck/spell-checker
   const zhHantHk: TLocaleId = 'zh-Hant-HK';
-  // eslint-disable-next-line spellcheck/spell-checker
   const zhHantTw: TLocaleId = 'zh-Hant-TW';
 
   describe('getPreferLocaleId', (): void => {
@@ -26,27 +23,20 @@ describe('LocalesUtils', (): void => {
 
       it('should return preferred localeId (locales with script)', (): void => {
         expect(getPreferLocaleId([zhHansCN, nlNl], [enUS, zhHansCN], enUS)).toBe(zhHansCN);
-        // eslint-disable-next-line spellcheck/spell-checker
         expect(getPreferLocaleId([srCyrlRs, nlNl], [srCyrlRs, enUS], enUS)).toBe(srCyrlRs);
-        // eslint-disable-next-line spellcheck/spell-checker
         expect(getPreferLocaleId([zhHansCN, zhHantHk], [enUS, nlNl, zhHantHk], enUS)).toBe(zhHantHk);
-        // eslint-disable-next-line spellcheck/spell-checker
         expect(getPreferLocaleId([zhHansCN, zhHantHk], [zhHansCN, nlNl, zhHantHk], enUS)).toBe(zhHansCN);
       });
     });
 
     describe('Partial match (lang only)', (): void => {
       it('should return localeId by lang', (): void => {
-        // eslint-disable-next-line spellcheck/spell-checker
         expect(getPreferLocaleId([enCA], [zhHantHk, enUS, nlNl], nlNl)).toBe(enUS);
-
         expect(getPreferLocaleId([enCA, enGB], [nlNl, zhHansCN, enUS], nlNl)).toBe(enUS);
       });
 
       it('should return localeId by lang (locales with script)', (): void => {
-        // eslint-disable-next-line spellcheck/spell-checker
         expect(getPreferLocaleId([zhHantHk, zhHantTw], [zhHansCN, enUS, nlNl], nlNl)).toBe(zhHansCN);
-        // eslint-disable-next-line spellcheck/spell-checker
         expect(getPreferLocaleId([enGB, zhHantHk], [nlNl, zhHantHk, zhHantTw], nlNl)).toBe(zhHantHk);
       });
     });
