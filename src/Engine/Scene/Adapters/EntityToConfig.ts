0b78686acbc2b8fc180aa623699b987a89c98313
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:41:29 +0100

    Serialization: Color

--- src/Engine/Scene/Adapters/EntityToConfig.ts
+++ src/Engine/Scene/Adapters/EntityToConfig.ts
@@ -1,13 +1,16 @@
 import type { TColor } from '@/Engine/Color';
 import { extractSerializableRegistrableFields } from '@/Engine/Mixins';
 import type { TSceneConfig, TSceneWrapper } from '@/Engine/Scene/Models';
-import { filterOutEmptyFields } from '@/Engine/Utils';
+import { filterOutEmptyFields, isDefined } from '@/Engine/Utils';
 
 // TODO 15-0-0: validate result
 export function sceneToConfig(entity: TSceneWrapper): TSceneConfig {
+  const background: TColor | undefined = entity.getBackground() as TColor | undefined;
+  // TODO we cannot serialize other backgrounds than Color at the moment
+  const hexString: string | undefined = isDefined(background) ? '#' + background.getHexString() : undefined;
+
   return filterOutEmptyFields({
-    // TODO we cannot serialize other backgrounds than Color at the moment
-    background: '#' + (entity.getBackground() as TColor).getHexString(),
+    background: hexString,
     isActive: entity.isActive(),
     ...extractSerializableRegistrableFields(entity)
   });
