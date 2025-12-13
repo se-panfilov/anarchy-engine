f7ed66b9d5837450094aad6b5bdaec3a704e55b2
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:28:12 +0100

    animations from config

--- src/Engine/Models3d/Models/TModel3dConfig.ts
+++ src/Engine/Models3d/Models/TModel3dConfig.ts
@@ -6,8 +6,7 @@ import type { TModel3dProps } from './TModel3dProps';
 
 export type TModel3dConfig = Readonly<{
   model3dSource: string | PrimitiveModel3dType;
-  // TODO 9.0.0. RESOURCES: implement animations loading in resources (shall animations be loaded separately from models?)
-  // animationsSource: ReadonlyArray<string>;
+  animationsSource?: ReadonlyArray<string>;
   materialSource?: string;
 }> &
   Omit<TModel3dProps, 'scale' | 'position' | 'rotation'> &
