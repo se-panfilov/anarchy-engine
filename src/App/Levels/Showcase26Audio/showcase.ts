e699ea5d230d9d4b95b33f315af6d621b3b1af70
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:30:40 +0100

    showcase: added initial audio showcase

---
+++ src/App/Levels/Showcase26Audio/showcase.ts
@@ -0,0 +1,93 @@
+import { distinctUntilChanged } from 'rxjs';
+import type { AnimationAction } from 'three';
+
+import type { TShowcase } from '@/App/Levels/Models';
+import { addGizmo } from '@/App/Levels/Utils';
+import type { TActor, TAppCanvas, TEngine, TFsmStates, TFsmWrapper, TModel3d, TSpace, TSpaceConfig, TSpaceServices } from '@/Engine';
+import { ambientContext, Engine, isNotDefined, spaceService } from '@/Engine';
+
+import spaceConfig from './showcase.json';
+
+export async function showcase(canvas: TAppCanvas): Promise<TShowcase> {
+  const space: TSpace = await spaceService.buildSpaceFromConfig(canvas, spaceConfig as TSpaceConfig);
+  const engine: TEngine = Engine(space);
+
+  function init(): void {
+    addGizmo(space.services, ambientContext.screenSizeWatcher, space.loops, { placement: 'bottom-left' });
+    const fadeDuration = 0.3;
+
+    const solder1AnimFsm: TFsmWrapper = initSolder1('solder_actor_1', fadeDuration, space.services);
+  }
+
+  function start(): void {
+    engine.start();
+    void init();
+  }
+
+  return { start, space };
+}
+
+function initSolder1(actorName: string, fadeDuration: number, { animationsService, fsmService, actorService }: TSpaceServices): TFsmWrapper {
+  const actor: TActor | undefined = actorService.getRegistry().findByName(actorName);
+  if (isNotDefined(actor)) throw new Error(`Actor "${actorName}" is not found`);
+
+  const model3d: TModel3d = actor.model3d;
+  const actions = animationsService.startAutoUpdateMixer(model3d).actions;
+
+  enum AnimationActions {
+    Run = 'Run',
+    Walk = 'Walk',
+    Idle = 'Idle'
+    // TPose = 'TPose'
+  }
+
+  const { Run, Walk, Idle } = AnimationActions;
+
+  const runAction: AnimationAction = actions[Run];
+  const walkAction: AnimationAction = actions[Walk];
+  const idleAction: AnimationAction = actions[Idle];
+  // const tPoseAction: AnimationAction = actions['TPose'];
+
+  const solderAnimFsm: TFsmWrapper = fsmService.create({
+    name: 'solder_anim_fsm',
+    type: 'animation',
+    initial: Idle,
+    transitions: [
+      [Idle, Run, Run],
+      [Idle, Walk, Walk],
+      [Walk, Idle, Idle],
+      [Walk, Run, Run],
+      [Run, Walk, Walk],
+      [Run, Idle, Idle]
+    ]
+  });
+
+  actor.setAnimationsFsm(solderAnimFsm);
+
+  const { animationsFsm } = actor.states;
+  if (isNotDefined(animationsFsm)) throw new Error('Animations FSM is not defined');
+
+  animationsFsm.changed$.pipe(distinctUntilChanged()).subscribe((state: TFsmStates): void => {
+    switch (state) {
+      case Idle:
+        walkAction.fadeOut(fadeDuration);
+        runAction.fadeOut(fadeDuration);
+        idleAction.reset().fadeIn(fadeDuration).play();
+        break;
+      case Walk:
+        idleAction.fadeOut(fadeDuration);
+        runAction.fadeOut(fadeDuration);
+        walkAction.reset().fadeIn(fadeDuration).play();
+        break;
+      case Run:
+        idleAction.fadeOut(fadeDuration);
+        walkAction.fadeOut(fadeDuration);
+        runAction.reset().fadeIn(fadeDuration).play();
+        break;
+      default:
+        throw new Error(`Unknown state: ${String(state)}`);
+    }
+  });
+
+  return animationsFsm;
+}
