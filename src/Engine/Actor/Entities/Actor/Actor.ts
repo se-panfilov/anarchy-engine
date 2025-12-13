7cd99fa5c6496324ab0ff87a1c37964bd0848840
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:25:02 +0100

    [WIP] todo

--- src/Engine/Actor/Entities/Actor/Actor.ts
+++ src/Engine/Actor/Entities/Actor/Actor.ts
@@ -27,7 +27,7 @@ export function Actor(
   const driveToTargetConnector: TDriveToTargetConnector = DriveToTargetConnector(drive, model3d.getRawModel3d());
 
   // TODO CWP:
-  // TODO 8.0.0. MODELS: Make sure, rotations are working (adjust showcase)
+  // TODO 8.0.0. MODELS: Make sure, rotations are working for kinematics (add some methods to move to a point, rotate to a point)
   // TODO 8.0.0. MODELS: Perhaps models need to have an offset (rotation, position). That offset should adjust rotation of the model (maybe in DriveToTargetConnector)
   // TODO 8.0.0. MODELS: Rigid body immediately created (should be created only when physics drive is initialized, maybe in hook)
   // TODO 8.0.0. MODELS: Fix Physics agent: should respect external change of position, rotation, scale
