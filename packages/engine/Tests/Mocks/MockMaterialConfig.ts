49a6aea757a27b76426d2698e9e5f838de72f1d2
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:52:42 +0100

    [MONOREPO] Removed old tests

--- packages/engine/Tests/Mocks/MockMaterialConfig.ts
+++
@@ -1,9 +0,0 @@
-import type { TMaterialConfig } from '@/Material';
-import { MaterialType } from '@/Material';
-
-export const mockMaterialName = 'mock_material';
-
-export const mockMaterialConfig: TMaterialConfig = {
-  type: MaterialType.Standard,
-  name: mockMaterialName
-};
