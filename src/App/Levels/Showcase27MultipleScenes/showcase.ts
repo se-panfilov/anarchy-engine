0c5ed00aed672b1c11900fd711d39cd194301c71
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:35:44 +0100

    Showcase: fixes

--- src/App/Levels/Showcase27MultipleScenes/showcase.ts
+++ src/App/Levels/Showcase27MultipleScenes/showcase.ts
@@ -1,9 +1,9 @@
+import type { Subscription } from 'rxjs';
 import { combineLatest } from 'rxjs';
 import { Clock } from 'three';
 
-import { addGizmo } from '@/App/Levels/Utils';
-import type { TActor, TActorRegistry, TMilliseconds, TSpace, TSpaceConfig } from '@/Engine';
-import { ambientContext, asRecord, isNotDefined, spaceService } from '@/Engine';
+import type { TActor, TActorRegistry, TActorService, TMilliseconds, TSpace, TSpaceConfig, TTransformLoop } from '@/Engine';
+import { asRecord, isNotDefined, spaceService } from '@/Engine';
 
 import spaceAlphaConfigJson from './spaceAlpha.json';
 import spaceBetaConfigJson from './spaceBeta.json';
@@ -11,7 +11,6 @@ import spaceBetaConfigJson from './spaceBeta.json';
 const spaceAlphaConfig: TSpaceConfig = spaceAlphaConfigJson as TSpaceConfig;
 const spaceBetaConfig: TSpaceConfig = spaceBetaConfigJson as TSpaceConfig;
 
-// TODO 14-0-0: Do not allow create spaces with the same name (apply the same for the all entities, add validation)
 export function start(): void {
   const spaces: Record<string, TSpace> = asRecord('name', spaceService.createFromConfig([spaceAlphaConfig, spaceBetaConfig]));
   const spaceAlpha: TSpace = spaces[spaceAlphaConfig.name];
@@ -23,22 +22,38 @@ export function start(): void {
 }
 
 export function showcase(space: TSpace): void {
-  const { actorService } = space.services;
-  const { transformLoop } = space.loops;
+  switch (space.name) {
+    case spaceAlphaConfig.name:
+      showcaseAlpha(space);
+      break;
+    case spaceBetaConfig.name:
+      showcaseBeta(space);
+      break;
+    default:
+      throw new Error(`Showcase: Space "${space.name}" is not defined`);
+  }
+}
 
+export function showcaseAlpha(space: TSpace): void {
+  moveCircle('sphere_actor', space.services.actorService, space.loops.transformLoop, new Clock());
+
+  space.start$.next(true);
+}
+
+export function showcaseBeta(space: TSpace): void {
+  moveCircle('box_actor', space.services.actorService, space.loops.transformLoop, new Clock());
+
+  space.start$.next(true);
+}
+
+function moveCircle(actorName: string, actorService: TActorService, transformLoop: TTransformLoop, clock: Clock): Subscription {
   const actorRegistry: TActorRegistry = actorService.getRegistry();
+  const actor: TActor | undefined = actorRegistry.findByName(actorName);
+  if (isNotDefined(actor)) throw new Error(`Actor "${actorName}" is not defined`);
 
-  addGizmo(space.services, ambientContext.screenSizeWatcher, space.loops, { placement: 'bottom-left' });
-
-  const actor: TActor | undefined = actorRegistry.findByName('sphere_actor');
-  if (isNotDefined(actor)) throw new Error('Actor is not defined');
-
-  const clock: Clock = new Clock();
-  transformLoop.tick$.subscribe((): void => {
+  return transformLoop.tick$.subscribe((): void => {
     const elapsedTime: TMilliseconds = clock.getElapsedTime() as TMilliseconds;
     actor.drive.default.setX(Math.sin(elapsedTime) * 8);
     actor.drive.default.setZ(Math.cos(elapsedTime) * 8);
   });
-
-  space.start$.next(true);
 }
