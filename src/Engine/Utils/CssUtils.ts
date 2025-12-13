f01b9b3c94d3b7ca9b01a78123feca7973602deb
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:43:53 +0100

    [Utils]: Css utils

--- src/Engine/Utils/CssUtils.ts
+++ src/Engine/Utils/CssUtils.ts
@@ -13,6 +13,14 @@ export function toPx(value: string | number | undefined): string {
   return getSizeInPx(value);
 }
 
+export function toRem(value: string | number | undefined): string {
+  const defaultBaseTextSize: number = 16;
+  const baseTextSize: number = getBaseTextSize() || defaultBaseTextSize;
+  if (isNotDefined(value)) return `${baseTextSize / 16}rem`;
+  if (typeof value === 'number') return `${value / baseTextSize}rem`;
+  return getSizeInRem(value);
+}
+
 function getSizeInPx(size: string): string {
   const units: string = getUnitsFromCssValue(size);
   const value: number = stripUnits(size);
@@ -32,11 +40,30 @@ function getSizeInPx(size: string): string {
   }
 }
 
+function getSizeInRem(size: string): string {
+  const units: string = getUnitsFromCssValue(size);
+  const value: number = stripUnits(size);
+  switch (units) {
+    case CssUnits.Px:
+      return `${value / getBaseTextSize()}rem`;
+    case CssUnits.Em:
+      return `${value}rem`; // Em is relative to the font size of the element, so it remains the same
+    case CssUnits.Rem:
+      return `${value}rem`;
+    case CssUnits.Percent:
+      return `${(value / 100) * (getBaseTextSize() / 16)}rem`;
+    case CssUnits.Pt:
+      return `${(value * 4) / (3 * getBaseTextSize())}rem`; // 1pt = 1.3333px
+    default:
+      return size;
+  }
+}
+
 export function stripUnits(value: string): number {
-  return parseInt(value.replace(/[^\d]/g, ''));
+  return parseFloat(value);
 }
 
 export function getUnitsFromCssValue(value: string): string {
-  const match = value.match(/[a-zA-Z%]+/);
-  return match ? match[0] : '';
+  const match = value.match(/([a-zA-Z%]+)$/);
+  return match ? match[1] : '';
 }
