7bf7f1343f9d6f57d50fce9218aaad149562ba05
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:01:26 +0100

    [DESKTOP] handling events

--- apps/showcases-desktop/src/Services/AppToPlatformMessagesService.ts
+++ apps/showcases-desktop/src/Services/AppToPlatformMessagesService.ts
@@ -1,14 +1,19 @@
+import { PlatformActions } from '@Desktop/Constants';
 import type { IpcMainInvokeEvent } from 'electron';
 
 // TODO DESKTOP: fix handling of the events
-// TODO DESKTOP: any
-export function handleAppRequest(_event: IpcMainInvokeEvent, ...args: ReadonlyArray<any>): Promise<any> | any {
-  console.log('XXX event', args);
+export function handleAppRequest(_event: IpcMainInvokeEvent, ...args: [PlatformActions, unknown]): Promise<any> | any {
+  const type: PlatformActions = args[0];
+  const payload: unknown = args[1];
 
-  return 'pong';
-  //   ping
-  //   readConfig
-  //   node
-  //   chrome
-  //   electron
+  switch (type) {
+    case PlatformActions.SaveAppSettings:
+      // TODO DESKTOP: implement saving app settings
+      return Promise.resolve();
+    case PlatformActions.LoadAppSettings:
+      // TODO DESKTOP: implement loading app settings
+      return Promise.resolve({});
+    default:
+      throw new Error(`Unknown platform action: ${type}`);
+  }
 }
