fb14586daba5050f30967738a10a761e6a3fca0c
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:52:27 +0100

    ai refactoring

--- src/Engine/Domains/Abstract/Wrapper/AbstractWrapper.ts
+++ src/Engine/Domains/Abstract/Wrapper/AbstractWrapper.ts
@@ -4,19 +4,10 @@ import type { WrapperType } from '@/Engine/Domains/Abstract';
 import type { IWrapper } from '@/Engine/Domains/Abstract/Models';
 import { destroyableMixin } from '@/Engine/Domains/Mixins';
 
-export function AbstractWrapper<T>(entity: T, type: string, params?: Readonly<{ tags: ReadonlyArray<string> }>): IWrapper<T> {
-  const id: string = type + '_' + nanoid();
-
-  const wrapper: IWrapper<T> = {} as IWrapper<T>;
-
+export function AbstractWrapper<T>(entity: T, type: WrapperType | string, params?: Readonly<{ tags: ReadonlyArray<string> }>): IWrapper<T> {
   return {
-    ...wrapper,
-    get id(): string {
-      return id;
-    },
-    get type(): WrapperType | string {
-      return type;
-    },
+    id: type + '_' + nanoid(),
+    type,
     entity,
     tags: params?.tags ?? [],
     isRegistrable: true,
