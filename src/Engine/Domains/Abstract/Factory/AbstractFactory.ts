efbc1687cbdbb5dcdcafa1f39baf237711eb874b
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:52:28 +0100

    ai refactoring

--- src/Engine/Domains/Abstract/Factory/AbstractFactory.ts
+++ src/Engine/Domains/Abstract/Factory/AbstractFactory.ts
@@ -3,14 +3,5 @@ import type { IFactory } from '@Engine/Domains/Abstract/Models';
 import { nanoid } from 'nanoid';
 
 export function AbstractFactory<T, P>(type: FactoryType | string): Omit<IFactory<T, P>, 'create'> {
-  const id: string = type + '_factory_' + nanoid();
-
-  return {
-    get id(): string {
-      return id;
-    },
-    get type(): FactoryType | string {
-      return type;
-    }
-  };
+  return { id: type + '_factory_' + nanoid(), type };
 }
