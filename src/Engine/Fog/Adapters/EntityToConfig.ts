8fbf2e33371ea4adc0291999c6c274ed092cdf1c
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:40:03 +0100

    Serialization: og

--- src/Engine/Fog/Adapters/EntityToConfig.ts
+++ src/Engine/Fog/Adapters/EntityToConfig.ts
@@ -5,13 +5,12 @@ import { extractSerializableRegistrableFields } from '@/Engine/Mixins';
 import { filterOutEmptyFields } from '@/Engine/Utils';
 
 export function fogToConfig(entity: TFogWrapper): TFogConfig {
-  // TODO 15-0-0: implement
-  console.log('XXX entity', entity);
-
   const json: FogJSON = entity.entity.toJSON();
 
-  // TODO 15-0-0: fix any
   return filterOutEmptyFields({
+    color: entity.entity.color.getHexString(),
+    near: json.near,
+    far: json.far,
     ...extractSerializableRegistrableFields(entity)
-  }) as any;
+  });
 }
