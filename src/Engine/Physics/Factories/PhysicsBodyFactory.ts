89fa17d505a560624ecf7fc430639a1677a0fc34
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:22:56 +0100

    [HEAVY WIP] fixed import

--- src/Engine/Physics/Factories/PhysicsBodyFactory.ts
+++ src/Engine/Physics/Factories/PhysicsBodyFactory.ts
@@ -1,8 +1,7 @@
-import { PhysicsBody } from 'src/Engine/Physics/Entities';
-
 import type { TReactiveFactoryWithDependencies } from '@/Engine/Abstract';
 import { FactoryType, ReactiveFactoryWithDependencies } from '@/Engine/Abstract';
 import { configToParamsBody as configToParams } from '@/Engine/Physics/Adapters';
+import { PhysicsBody } from '@/Engine/Physics/Entities';
 import type { TPhysicsBody, TPhysicsBodyFactory, TPhysicsBodyParams, TPhysicsDependencies } from '@/Engine/Physics/Models';
 
 const factory: TReactiveFactoryWithDependencies<TPhysicsBody, TPhysicsBodyParams, TPhysicsDependencies> = ReactiveFactoryWithDependencies(FactoryType.PhysicsBody, PhysicsBody);
