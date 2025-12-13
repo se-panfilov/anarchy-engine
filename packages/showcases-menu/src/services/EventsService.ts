87bba436767004e085c507a55e381d3ddab107e5
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:01:20 +0100

    [MENU] refactor

--- packages/showcases-menu/src/services/EventsService.ts
+++ packages/showcases-menu/src/services/EventsService.ts
@@ -9,20 +9,22 @@ function EventsService(): TEventsService {
 
   const setBus = (bus: Subject<TMenuEvent>): void => void (bus$ = bus);
 
+  const noBusError = '[EventsService]: bus$ is not defined. Call setBus() first.';
+
   function emitCloseMenu(): void | never {
-    if (isNotDefined(bus$)) throw new Error('[EventsService]: bus$ is not defined. Call setBus() first.');
+    if (isNotDefined(bus$)) throw new Error(noBusError);
     console.log('[EventsService]: emitCloseMenu');
     bus$.next({ type: MenuEvents.CloseMenu });
   }
 
   function emitSaveMenuSettings(settings: TShowcaseGameSettings): void | never {
-    if (isNotDefined(bus$)) throw new Error('[EventsService]: bus$ is not defined. Call setBus() first.');
+    if (isNotDefined(bus$)) throw new Error(noBusError);
     console.log('[EventsService]: emitSaveMenuSettings');
     bus$.next({ type: MenuEvents.SaveSettings, payload: settings });
   }
 
   function emitLoadMenuSettings(): void | never {
-    if (isNotDefined(bus$)) throw new Error('[EventsService]: bus$ is not defined. Call setBus() first.');
+    if (isNotDefined(bus$)) throw new Error(noBusError);
     console.log('[EventsService]: emitLoadMenuSettings');
     // TODO DESKTOP: Implement, make receive settings and apply to store
     console.warn(`[EventsService]: emitLoadMenuSettings is not implemented yet.`);
@@ -30,19 +32,19 @@ function EventsService(): TEventsService {
   }
 
   function emitStartNewGame(): void | never {
-    if (isNotDefined(bus$)) throw new Error('[EventsService]: bus$ is not defined. Call setBus() first.');
+    if (isNotDefined(bus$)) throw new Error(noBusError);
     console.log('[EventsService]: emitStartNewGame');
     bus$.next({ type: MenuEvents.StartNewGame });
   }
 
   function emitContinueGame(): void | never {
-    if (isNotDefined(bus$)) throw new Error('[EventsService]: bus$ is not defined. Call setBus() first.');
+    if (isNotDefined(bus$)) throw new Error(noBusError);
     console.log('[EventsService]: emitContinueGame');
     bus$.next({ type: MenuEvents.ContinueGame });
   }
 
   function emitLoadGame(): void | never {
-    if (isNotDefined(bus$)) throw new Error('[EventsService]: bus$ is not defined. Call setBus() first.');
+    if (isNotDefined(bus$)) throw new Error(noBusError);
     console.log('[EventsService]: emitLoadGame');
     bus$.next({ type: MenuEvents.LoadGame });
   }
