07365a260f9a813de6f8dac434a149872eb3b72e
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:17:05 +0100

    [HEAVY WIP] showcase: fixes for showcase 10

--- src/Engine/Material/Models/TMaterialProps.ts
+++ src/Engine/Material/Models/TMaterialProps.ts
@@ -1,5 +1,5 @@
-import type { TWithNameRequired } from '@/Engine/Mixins';
+import type { TWithNameRequired, TWithReadonlyTags } from '@/Engine/Mixins';
 
 import type { TWithMaterialType } from './TWithMaterialType';
-
-export type TMaterialProps = Readonly<{ options?: Record<string, any> }> & TWithMaterialType & TWithNameRequired;
+// TODO 9.0.0. RESOURCES: Check all entities in registries: should have names, tags and etc.
+export type TMaterialProps = Readonly<{ options?: Record<string, any> }> & TWithMaterialType & TWithNameRequired & TWithReadonlyTags;
