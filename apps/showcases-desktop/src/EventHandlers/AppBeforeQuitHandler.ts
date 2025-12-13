9a1693db95c82e6b2ea7aca364019c261358cbae
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:11:05 +0100

    [DESKTOP] close/restart app (fix)

--- apps/showcases-desktop/src/EventHandlers/AppBeforeQuitHandler.ts
+++ apps/showcases-desktop/src/EventHandlers/AppBeforeQuitHandler.ts
@@ -1,11 +1,11 @@
 import type { TDesktopAppService } from '@Showcases/Desktop/Models';
-import type { App } from 'electron';
+import type { App, Event } from 'electron';
 
 export function appBeforeQuitHandler(app: App, desktopAppService: TDesktopAppService): void {
-  app.on('before-quit', (e): void => {
+  app.on('before-quit', (event: Event): void => {
     /// Using a single point of exiting the app
     if (!desktopAppService.isExiting()) {
-      e.preventDefault();
+      event.preventDefault();
       desktopAppService.closeApp();
     }
   });
