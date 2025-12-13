214527776553a3ec23bad4b53c8251e33484cabc
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:53:21 +0100

    fixes

--- src/Engine/Domains/Loop/Services/LoopService.ts
+++ src/Engine/Domains/Loop/Services/LoopService.ts
@@ -3,37 +3,42 @@ import { Clock } from 'three';
 
 import type { ILoopService, ILoopTimes } from '@/Engine/Domains/Loop/Models';
 
+type ILoopServiceState = { isLooping: boolean };
+
 export function LoopService(): ILoopService {
   const tick$: Subject<ILoopTimes> = new Subject<ILoopTimes>();
+  const state: ILoopServiceState = {
+    isLooping: false
+  };
 
-  let isLooping: boolean = false;
-
-  const loopFn = getLoopFn(tick$, isLooping);
+  const loopFn = getLoopFn(tick$, state);
 
   function start(): void {
-    isLooping = true;
+    // eslint-disable-next-line functional/immutable-data
+    state.isLooping = true;
     requestAnimationFrame(loopFn);
   }
 
   function stop(): void {
-    isLooping = false;
+    // eslint-disable-next-line functional/immutable-data
+    state.isLooping = false;
   }
 
   return {
     start,
     stop,
     tick$: tick$.asObservable(),
-    getIsLooping: (): boolean => isLooping
+    getIsLooping: (): boolean => state.isLooping
   };
 }
 
-function getLoopFn(tick$: Subject<ILoopTimes>, isLooping: boolean): (time: number) => void {
+function getLoopFn(tick$: Subject<ILoopTimes>, state: ILoopServiceState): (time: number) => void {
   const clock: Clock = new Clock();
   let lastElapsedTime: number = 0;
 
   function loopFn(frameTime: number): void {
     // (fpsGraph as any).begin();
-    if (!isLooping) return;
+    if (!state.isLooping) return;
     const elapsedTime: number = clock.getElapsedTime();
     const delta: number = elapsedTime - lastElapsedTime;
     lastElapsedTime = elapsedTime;
