c7f06b9e6c3d33c217837f1e1fdd94bdf4f9301b
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:12:53 +0100

    [SHOWCASES] only used locales

--- packages/showcases-shared/src/Models/TShowcaseLocales.ts
+++ packages/showcases-shared/src/Models/TShowcaseLocales.ts
@@ -1,8 +1,9 @@
 import type { TLocale } from '@Anarchy/i18n';
 
-import type { TShowcaseLocaleIds } from './TShowcaseLocaleIds';
+import type { TFullLocaleIds, TShowcaseLocaleIds } from './TShowcaseLocaleIds';
 
 //Locale with enforced font property
 export type TLocaleWithFont = Omit<TLocale, 'font'> & Readonly<{ font: string }>;
 
 export type TShowcaseLocales = Record<TShowcaseLocaleIds, TLocaleWithFont>;
+export type TFullLocales = Record<TFullLocaleIds, TLocaleWithFont>;
