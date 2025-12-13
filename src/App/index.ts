3fe6379e4cf042e4cf7e47d780fb331e61078104
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:08:31 +0100

    Refactored showcase

--- src/App/index.ts
+++ src/App/index.ts
@@ -21,7 +21,8 @@ import '@/App/style.css';
 // import { showcase } from '@/App/Levels/Showcase19Physics';
 // import { showcase } from '@/App/Levels/Showcase20PhysicsSyncWithModelsTest';
 // import { showcase } from '@/App/Levels/Showcase21PhysicsManualStep';
-import { showcase } from '@/App/Levels/Showcase22PhysicsComplex';
+import { showcase } from 'src/App/Levels/Showcase22PhysicsShooter';
+
 import type { TAppCanvas } from '@/Engine';
 import { ambientContext, isNotDefined } from '@/Engine';
 
