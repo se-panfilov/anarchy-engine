3e7250a743099151cf9bf1c0564320a106bb2459
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:21:34 +0100

    [ENGINE] todo

--- packages/anarchy-engine/src/Space/Utils/PrepareServicesUtils.ts
+++ packages/anarchy-engine/src/Space/Utils/PrepareServicesUtils.ts
@@ -116,6 +116,7 @@ export function buildEntitiesServices(
     materialService,
     models3dService,
     animationsService,
+    // TODO CWP  USE canvas instead of a global container (window, document, etc.) to handle mouse events (otherwise we can't prevent click through UI elements)
     mouseService: MouseService(container, MouseClickWatcherFactory(), MouseClickWatcherRegistry(), MousePositionWatcherFactory(), MousePositionWatcherRegistry(), loops),
     particlesService: ParticlesService(ParticlesFactory(), ParticlesRegistry(), { transformDriveService, materialService }, sceneW),
     physicsBodyService,
