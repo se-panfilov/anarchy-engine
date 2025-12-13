03ea4d99996ab66dda69a741dddd3d13aa5635c5
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:23:01 +0100

    [ENGINE] removed comments

--- packages/anarchy-engine/src/Keyboard/Constants/KeysExtra.ts
+++ packages/anarchy-engine/src/Keyboard/Constants/KeysExtra.ts
@@ -1,23 +1,23 @@
 export enum KeysExtra {
-  PauseBreak = 'PauseBreak', //19
-  Space = 'Space', //32
-  TAB = 'TAB', //9
-  Enter = 'Enter', //13
-  Escape = 'Escape', //27
-  Backspace = 'Backspace', //8
-  Delete = 'Delete', //46
-  Insert = 'Insert', //45 ?????????
-  Home = 'Home', //36
-  End = 'End', //35
-  PageUp = 'PageUp', //33
-  PageDown = 'PageDown', //34
-  Shift = 'Shift', //16 ?????????
+  PauseBreak = 'PauseBreak',
+  Space = 'Space',
+  TAB = 'TAB',
+  Enter = 'Enter',
+  Escape = 'Escape',
+  Backspace = 'Backspace',
+  Delete = 'Delete',
+  Insert = 'Insert',
+  Home = 'Home',
+  End = 'End',
+  PageUp = 'PageUp',
+  PageDown = 'PageDown',
+  Shift = 'Shift',
 
-  ArrowLeft = 'ArrowLeft', //37
-  ArrowUp = 'ArrowUp', //38
-  ArrowRight = 'ArrowRight', //39
-  ArrowDown = 'ArrowDown', //40
+  ArrowLeft = 'ArrowLeft',
+  ArrowUp = 'ArrowUp',
+  ArrowRight = 'ArrowRight',
+  ArrowDown = 'ArrowDown',
 
-  NumLock = 'NumLock', //144
-  ScrollLock = 'ScrollLock' //145
+  NumLock = 'NumLock',
+  ScrollLock = 'ScrollLock'
 }
