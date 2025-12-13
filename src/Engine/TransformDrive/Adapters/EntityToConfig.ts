10810a8a388eeb1260213e26fcd596fa6656234f
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:39:22 +0100

    [WIP] Serialization: todo

--- src/Engine/TransformDrive/Adapters/EntityToConfig.ts
+++ src/Engine/TransformDrive/Adapters/EntityToConfig.ts
@@ -1,6 +1,7 @@
 import type { TTransformDrive, TTransformDriveSerializedData } from '@/Engine/TransformDrive/Models';
 
 export function entityToConfig({ position$, rotation$, scale$, agent$ }: TTransformDrive<any>): TTransformDriveSerializedData {
+  // TODO 15-0-0: Agent's data also should be serializable and available via config
   return {
     position: position$.value,
     rotation: rotation$.value,
