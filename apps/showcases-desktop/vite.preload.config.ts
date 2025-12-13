cf0f4614ea530818376af945871eaf8d7f3980c3
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:02:57 +0100

    [MENU] rename package

--- apps/showcases-desktop/vite.preload.config.ts
+++ apps/showcases-desktop/vite.preload.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     alias: {
       ...sharedAliases,
       '@Desktop': path.resolve(__dirname, './src'),
-      '@ShowcasesShared': path.resolve(__dirname, '../../packages/showcases_shared/src')
+      '@ShowcasesShared': path.resolve(__dirname, '../../packages/showcases-shared/src')
     }
   },
   build: {
