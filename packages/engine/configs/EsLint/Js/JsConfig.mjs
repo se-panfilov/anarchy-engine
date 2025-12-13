6a50123bd6b7832cfaeba6ecafe342695a845f08
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:54:21 +0100

    [ESLINT]

--- packages/engine/configs/EsLint/Js/JsConfig.mjs
+++ packages/engine/configs/EsLint/Js/JsConfig.mjs
@@ -1,4 +1,4 @@
 import { JsConfig } from '../../../../../configs/EsLint/index.mjs';
 import { JsOverrides as overrides } from './JsOverrides.mjs';
 
-export const EngineJsConfig = [...BaseJsConfig, ...overrides];
+export const EngineJsConfig = [...JsConfig, ...overrides];
