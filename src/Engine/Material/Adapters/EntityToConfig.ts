d59c65460092197a832ad3b0e5e98bd4ad101307
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:41:43 +0100

    Serialization: Materials

--- src/Engine/Material/Adapters/EntityToConfig.ts
+++ src/Engine/Material/Adapters/EntityToConfig.ts
@@ -6,12 +6,13 @@ import { filterOutEmptyFields } from '@/Engine/Utils';
 
 export function materialToConfig(entity: TMaterialWrapper): TMaterialConfig {
   // TODO 15-0-0: implement
-  console.log('XXX entity', entity);
+  console.log('XXX entity material', entity);
   const json: MaterialJSON = entity.entity.toJSON();
   console.log('XXX json', json);
 
   // TODO 15-0-0: fix any
   return filterOutEmptyFields({
+    type: json.type,
     ...extractSerializableRegistrableFields(entity)
   }) as any;
 }
