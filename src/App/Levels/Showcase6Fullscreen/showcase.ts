48d464be0b184b19d45757e4158ca175ad1bf1a1
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:16:55 +0100

    [HEAVY WIP] showcase: fixed showcase 6

--- src/App/Levels/Showcase6Fullscreen/showcase.ts
+++ src/App/Levels/Showcase6Fullscreen/showcase.ts
@@ -1,5 +1,5 @@
 import type { TShowcase } from '@/App/Levels/Models';
-import type { TActorRegistry, TActorWrapper, TAppCanvas, TEngine, TSpace, TSpaceConfig } from '@/Engine';
+import type { TActorRegistry, TActorWrapper, TAppCanvas, TEngine, TModel3dFacade, TModel3dRegistry, TSceneWrapper, TSpace, TSpaceConfig } from '@/Engine';
 import { Engine, isNotDefined, screenService, spaceService } from '@/Engine';
 
 import spaceConfig from './showcase.json';
@@ -8,18 +8,28 @@ export async function showcase(canvas: TAppCanvas): Promise<TShowcase> {
   const space: TSpace = await spaceService.buildSpaceFromConfig(canvas, spaceConfig as TSpaceConfig);
   const engine: TEngine = Engine(space);
 
-  const { actorService, loopService, mouseService } = space.services;
+  const { actorService, loopService, models3dService, mouseService, scenesService } = space.services;
+  const models3dRegistry: TModel3dRegistry = models3dService.getRegistry();
   const actorRegistry: TActorRegistry = actorService.getRegistry();
 
   function init(): void {
-    const actor: TActorWrapper | undefined = actorRegistry.findByTag('sphere');
-    if (isNotDefined(actor)) throw new Error('Actor not found');
+    const sceneW: TSceneWrapper | undefined = scenesService.findActive();
+    if (isNotDefined(sceneW)) throw new Error('Scene is not defined');
+
+    const planeModel3dF: TModel3dFacade | undefined = models3dRegistry.findByName('surface_model');
+    if (isNotDefined(planeModel3dF)) throw new Error('Plane model is not defined');
+
+    sceneW.addModel3d(planeModel3dF.getModel());
+
+    const actor: TActorWrapper | undefined = actorRegistry.findByName('sphere_actor');
+    if (isNotDefined(actor)) throw new Error('Actor is not found');
     actor.setY(2);
 
     mouseService.clickLeftRelease$.subscribe(() => {
       void screenService.toggleFullScreen();
     });
 
+    //Better to move actors via kinematic (or physics), but for a simple example we can just set coords
     loopService.tick$.subscribe(({ elapsedTime }) => {
       actor.setX(Math.sin(elapsedTime) * 8);
       actor.setZ(Math.cos(elapsedTime) * 8);
