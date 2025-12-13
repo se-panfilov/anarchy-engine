57b6cc13d0a15c2059125d3457d538895b3ec08e
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:35:28 +0100

    ShowcaseL destroy showcase

---
+++ src/App/Levels/Showcase27MultipleScenes/showcase.ts
@@ -0,0 +1,43 @@
+import { combineLatest } from 'rxjs';
+import { Clock } from 'three';
+
+import { addGizmo } from '@/App/Levels/Utils';
+import type { TActor, TActorRegistry, TMilliseconds, TSpace, TSpaceConfig } from '@/Engine';
+import { ambientContext, asRecord, isNotDefined, spaceService } from '@/Engine';
+
+import spaceAlphaConfigJson from './spaceAlpha.json';
+import spaceBetaConfigJson from './spaceBeta.json';
+
+const spaceAlphaConfig: TSpaceConfig = spaceAlphaConfigJson as TSpaceConfig;
+const spaceBetaConfig: TSpaceConfig = spaceBetaConfigJson as TSpaceConfig;
+
+export function start(): void {
+  const spaces: Record<string, TSpace> = asRecord('name', spaceService.createFromConfig([spaceAlphaConfig, spaceBetaConfig]));
+  const spaceAlpha: TSpace = spaces[spaceAlphaConfig.name];
+  const spaceBeta: TSpace = spaces[spaceBetaConfig.name];
+  if (isNotDefined(spaceAlpha)) throw new Error(`Showcase "${spaceAlphaConfig.name}": Space is not defined`);
+  if (isNotDefined(spaceBeta)) throw new Error(`Showcase "${spaceBetaConfig.name}": Space is not defined`);
+
+  combineLatest([spaceAlpha.built$, spaceBeta.built$]).subscribe(() => showcase(spaceAlpha));
+}
+
+export function showcase(space: TSpace): void {
+  const { actorService } = space.services;
+  const { transformLoop } = space.loops;
+
+  const actorRegistry: TActorRegistry = actorService.getRegistry();
+
+  addGizmo(space.services, ambientContext.screenSizeWatcher, space.loops, { placement: 'bottom-left' });
+
+  const actor: TActor | undefined = actorRegistry.findByName('sphere_actor');
+  if (isNotDefined(actor)) throw new Error('Actor is not defined');
+
+  const clock: Clock = new Clock();
+  transformLoop.tick$.subscribe((): void => {
+    const elapsedTime: TMilliseconds = clock.getElapsedTime() as TMilliseconds;
+    actor.drive.default.setX(Math.sin(elapsedTime) * 8);
+    actor.drive.default.setZ(Math.cos(elapsedTime) * 8);
+  });
+
+  space.start$.next(true);
+}
