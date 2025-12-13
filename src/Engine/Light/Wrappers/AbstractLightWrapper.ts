04c97d66dc6553d15f6e81b52337073e6b6c77b5
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:12:20 +0100

    refactoring

--- src/Engine/Light/Wrappers/AbstractLightWrapper.ts
+++ src/Engine/Light/Wrappers/AbstractLightWrapper.ts
@@ -1,6 +1,7 @@
 import { AbstractWrapper } from '@/Engine/Abstract';
 import type { TAbstractLightWrapper, TLight, TLightParams } from '@/Engine/Light/Models';
 import { getWrapperType } from '@/Engine/Light/Utils';
+import { applyShadowParams } from '@/Engine/Light/Wrappers/LightWrapperHelper';
 import { withMoveBy3dMixin, withObject3d, withRotationByXyzMixin } from '@/Engine/Mixins';
 import { applyObject3dParams, applyPosition, applyRotation } from '@/Engine/Utils';
 
@@ -13,6 +14,7 @@ export function AbstractLightWrapper<T extends TLight>(entity: T, params: TLight
     entity
   };
 
+  applyShadowParams(params, result.entity);
   applyPosition(result, params.position);
   applyRotation(result, params.rotation);
   applyObject3dParams(result, params);
--- src/Engine/Light/Wrappers/DirectionalLightWrapper.ts
+++ src/Engine/Light/Wrappers/DirectionalLightWrapper.ts
@@ -3,12 +3,8 @@ import { DirectionalLight } from 'three';
 import type { TAbstractLightWrapper, TDirectionalLight, TDirectionalLightParams } from '@/Engine/Light/Models';
 
 import { AbstractLightWrapper } from './AbstractLightWrapper';
-import { applyShadowParams } from './LightWrapperHelper';
 
 export function DirectionalLightWrapper(params: TDirectionalLightParams): TAbstractLightWrapper<TDirectionalLight> {
   const entity: TDirectionalLight = new DirectionalLight(params.color, params.intensity);
-
-  applyShadowParams(params, entity);
-
   return AbstractLightWrapper(entity, params);
 }
