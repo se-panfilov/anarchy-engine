47753231ae1a2e38eed2cd5e61455278afdce92c
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:01:22 +0100

    removed extra

--- src/Engine/Utils/SchemasUtils.ts
+++ src/Engine/Utils/SchemasUtils.ts
@@ -6,7 +6,18 @@ import ISpaceConfigSchema from '@/Engine/Space/Schemas/ISpaceConfig.json';
 const ajv: Ajv = new Ajv();
 
 export function validLevelConfig(config: ISpaceConfig): { isValid: boolean; errors?: ReadonlyArray<any> | null | undefined } {
+  const validationResult = validateJsonSchema(config);
+  return validationResult;
+}
+
+function validateJsonSchema(config: ISpaceConfig): { isValid: boolean; errors?: ReadonlyArray<any> | null | undefined } {
   const validate = ajv.compile(ISpaceConfigSchema);
   const isValid: boolean = validate(config);
   return { isValid, errors: validate.errors };
+}
+
+function validateData(): boolean {
+  //at least one active entity for withActiveEntities
+  //no multiple active entities for withActiveEntities
+  //at least one scene
 }
