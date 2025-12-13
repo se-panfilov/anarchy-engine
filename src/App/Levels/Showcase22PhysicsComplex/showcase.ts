9f52ffacb89804986ea5ba077f892dcc70edb382
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:08:14 +0100

    fixes

--- src/App/Levels/Showcase22PhysicsComplex/showcase.ts
+++ src/App/Levels/Showcase22PhysicsComplex/showcase.ts
@@ -1,4 +1,5 @@
 import type { Vector3 } from 'three';
+import { GridHelper } from 'three';
 import { Line2 } from 'three/examples/jsm/lines/Line2';
 import { LineGeometry } from 'three/examples/jsm/lines/LineGeometry';
 import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial';
@@ -28,9 +29,11 @@ import {
   getPushCoordsFrom3dAzimuth,
   isDefined,
   isNotDefined,
+  KeyCode,
   KeysExtra,
   MaterialType,
   mouseService,
+  mpsSpeed,
   RigidBodyTypesNames,
   Vector3Wrapper
 } from '@/Engine';
@@ -42,20 +45,31 @@ export function showcase(canvas: TAppCanvas): TShowcase {
   const space: TSpace = buildSpaceFromConfig(canvas, spaceConfig as TSpaceConfig);
   const engine: TEngine = Engine(space);
   const { keyboardService } = engine.services;
-  const { physicsLoopService, cameraService, actorService, loopService, controlsService, intersectionsWatcherService } = space.services;
+  const { physicsLoopService, cameraService, controlsService, actorService, loopService, intersectionsWatcherService } = space.services;
+  const { onKey } = keyboardService;
 
   async function init(): Promise<void> {
     // physicsWorldService.getDebugRenderer(loopService).start();
     physicsLoopService.shouldAutoUpdate(false);
 
-    controlsService.findActive()?.entity.target.set(0, 10, 0);
     const cameraW: TCameraWrapper | undefined = cameraService.findActive();
     if (isNotDefined(cameraW)) throw new Error(`Cannot find active camera`);
 
-    const ballActorW: TActorWrapperWithPhysicsAsync | TActorWrapperAsync | undefined = await actorService.getRegistry().findByNameAsync('ball');
-    if (isNotDefined(ballActorW)) throw new Error(`Cannot find "ball" actor`);
+    const heroW: TActorWrapperWithPhysicsAsync | TActorWrapperAsync | undefined = await actorService.getRegistry().findByNameAsync('hero');
+    if (isNotDefined(heroW)) throw new Error(`Cannot find "hero" actor`);
+
+    onKey(KeyCode.W).pressing$.subscribe(({ delta }): void => void heroW.addZ(mpsSpeed(-10, delta.delta)));
+    onKey(KeyCode.A).pressing$.subscribe(({ delta }): void => void heroW.addX(mpsSpeed(-10, delta.delta)));
+    onKey(KeyCode.S).pressing$.subscribe(({ delta }): void => void heroW.addZ(mpsSpeed(10, delta.delta)));
+    onKey(KeyCode.D).pressing$.subscribe(({ delta }): void => void heroW.addX(mpsSpeed(10, delta.delta)));
 
     const sceneWrapper: TSceneWrapper = actorService.getScene();
+
+    //helper grid
+    const size: number = 200;
+    const divisions: number = 200;
+    const gridHelper: GridHelper = new GridHelper(size, divisions);
+    sceneWrapper.entity.add(gridHelper);
 
     const blocks = await buildTower(actorService, 10, 10, 20);
 
@@ -68,31 +82,33 @@ export function showcase(canvas: TAppCanvas): TShowcase {
       tags: []
     });
 
-    let mouseLineIntersectionsCoords: Vector3 | undefined = undefined;
+    const mouseLineIntersectionsCoords: Vector3 | undefined = undefined;
     mouseLineIntersectionsWatcher.value$.subscribe((intersection: TIntersectionEvent) => {
-      mouseLineIntersectionsCoords = intersection.point;
+      // mouseLineIntersectionsCoords = intersection.point;
     });
 
     const line: Line2 = createLine();
     sceneWrapper.entity.add(line);
 
-    let coords: Readonly<{ azimuth: number; elevation: number }> = {
+    const coords: Readonly<{ azimuth: number; elevation: number }> = {
       azimuth: 0,
       elevation: 0
     };
 
     loopService.tick$.subscribe(() => {
-      if (isDefined(mouseLineIntersectionsCoords)) {
-        const ballCoords: TWithCoordsXYZ = ballActorW.getPosition().getCoords();
-        coords = get3DAzimuth(ballCoords, mouseLineIntersectionsCoords);
-        line.geometry.setPositions([ballCoords.x, ballCoords.y, ballCoords.z, mouseLineIntersectionsCoords.x, mouseLineIntersectionsCoords.y, mouseLineIntersectionsCoords.z]);
-        line.computeLineDistances();
-      }
+      cameraFollowingActor(cameraW, heroW);
+
+      // if (isDefined(mouseLineIntersectionsCoords)) {
+      //   const ballCoords: TWithCoordsXYZ = ballActorW.getPosition().getCoords();
+      //   coords = get3DAzimuth(ballCoords, mouseLineIntersectionsCoords);
+      //   line.geometry.setPositions([ballCoords.x, ballCoords.y, ballCoords.z, mouseLineIntersectionsCoords.x, mouseLineIntersectionsCoords.y, mouseLineIntersectionsCoords.z]);
+      //   line.computeLineDistances();
+      // }
     });
 
     mouseService.clickLeftRelease$.subscribe(() => {
-      if (isNotDefined(ballActorW)) throw new Error(`Cannot find "ball" actor`);
-      ballActorW.physicsBody?.getRigidBody()?.applyImpulse(getPushCoordsFrom3dAzimuth(coords.azimuth, coords.elevation, 100), true);
+      // if (isNotDefined(ballActorW)) throw new Error(`Cannot find "ball" actor`);
+      // ballActorW.physicsBody?.getRigidBody()?.applyImpulse(getPushCoordsFrom3dAzimuth(coords.azimuth, coords.elevation, 100), true);
     });
 
     physicsLoopService.shouldAutoUpdate(true);
@@ -187,4 +203,10 @@ function createLine(): Line2 {
   geometry.setPositions([0, 0, 0, 0, 0, 0]);
 
   return new Line2(geometry, material);
+}
+
+function cameraFollowingActor(cameraW: TCameraWrapper, actorW: TActorWrapperAsync): void {
+  const actorCoords: TWithCoordsXYZ = actorW.getPosition().getCoords();
+  cameraW.setPosition(Vector3Wrapper({ x: actorCoords.x, y: actorCoords.y + 10, z: actorCoords.z + 10 }));
+  cameraW.lookAt(Vector3Wrapper(actorCoords));
 }
