67540c1da5ab552b6699434037ea68138c66973d
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:55:50 +0100

    fixes

--- src/Engine/Domains/Text/Models/IText2dRenderer.ts
+++ src/Engine/Domains/Text/Models/IText2dRenderer.ts
@@ -1,6 +1,6 @@
 import type { CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer';
 
-import type { IScreenSizeValues } from '@/Engine';
+import type { IScreenSizeValues } from '@/Engine/Domains/Screen';
 
 export type IText2dRenderer = {
   renderer: CSS2DRenderer;
