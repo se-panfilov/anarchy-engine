33d97f840417889723d23c551b73977ea1dad027
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:56:38 +0100

    [DESKTOP][BUILD] todo

--- apps/showcases/desktop/electron-main.ts
+++ apps/showcases/desktop/electron-main.ts
@@ -52,4 +52,21 @@ ipcMain.handle('ping', async () => {
   return 'pong';
 });
 
-app.whenReady().then((): void => createWindow(windowWidth, windowHeight));
+app.whenReady().then((): void => {
+  createWindow(windowWidth, windowHeight);
+
+  // TODO DESKTOP: test this case
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow(windowWidth, windowHeight);
+  });
+
+  // TODO DESKTOP: test this case
+  app.on('window-all-closed', function () {
+    // Quit when all windows are closed, except on macOS. There, it's common
+    // for applications and their menu bar to stay active until the user quits
+    // explicitly with Cmd + Q.
+    if (process.platform !== 'darwin') app.quit();
+  });
+});
