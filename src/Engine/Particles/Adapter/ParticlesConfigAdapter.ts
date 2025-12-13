0dac3a06eef642390a735a15eabe0f303a32c276
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:03:54 +0100

    fixes

--- src/Engine/Particles/Adapter/ParticlesConfigAdapter.ts
+++ src/Engine/Particles/Adapter/ParticlesConfigAdapter.ts
@@ -1,11 +1,13 @@
+import { configToParams as materialConfigToParams } from '@/Engine/Material/Adapter';
 import type { IParticlesConfig, IParticlesParams } from '@/Engine/Particles/Models';
 import { configToParamsObject3d } from '@/Engine/ThreeLib';
 
 export function configToParams(config: IParticlesConfig): IParticlesParams {
-  const { position, rotation, layers, animations, scale, ...rest } = config;
+  const { position, rotation, layers, animations, scale, material, ...rest } = config;
 
   return {
     ...rest,
+    material: materialConfigToParams({ ...material.params, type: material.type }),
     ...configToParamsObject3d({ position, rotation, scale, layers, animations })
   };
 }
--- src/Engine/Particles/Models/IParticlesConfig.ts
+++ src/Engine/Particles/Models/IParticlesConfig.ts
@@ -1,6 +1,12 @@
+import type { IMaterialPackConfig, IMaterialTexturePack } from '@/Engine/MaterialTexturePack';
 import type { IWithReadonlyTags } from '@/Engine/Mixins';
 import type { IObject3DPropConfig } from '@/Engine/ThreeLib';
 
 import type { IParticlesProps } from './IParticlesProps';
 
-export type IParticlesConfig = IParticlesProps & IObject3DPropConfig & IWithReadonlyTags;
+export type IParticlesConfig = Omit<IParticlesProps, 'material'> &
+  Readonly<{
+    material: IMaterialPackConfig<IMaterialTexturePack>;
+  }> &
+  IObject3DPropConfig &
+  IWithReadonlyTags;
