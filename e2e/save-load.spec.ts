4b1508f076cebe065d540bfebd892c87219e1567
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:45:26 +0100

    [Serialization]: Controls (fps)

--- e2e/save-load.spec.ts
+++ e2e/save-load.spec.ts
@@ -14,7 +14,7 @@ test.beforeEach(async ({ page }) => {
   await waitUntilReady(page);
 });
 
-const scenes: ReadonlyArray<string> = ['SpaceBasic', 'SpaceCustomModels', 'SpaceTexts', 'SpaceLight', 'SpaceMaterials', 'SpaceOrbitControls'];
+const scenes: ReadonlyArray<string> = ['SpaceBasic', 'SpaceCustomModels', 'SpaceTexts', 'SpaceLight', 'SpaceMaterials', 'SpaceOrbitControls', 'SpaceFpsControls'];
 
 test.describe('Space save/load persistence', () => {
   const thresholds = {
