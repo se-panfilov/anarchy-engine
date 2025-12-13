fc663466bd9e1d9afd758d472ff28ddff666f9ae
gr-olga <gr-olga@ya.ru>
2025-12-13 01:46:56 +0100

    [POC] awillix

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,30 @@
-import * as sceneConfig from '@Engine/Launcher/debug-scene.config.json';
-import { launch } from '@Engine/Launcher';
-import type { SceneConfig } from '@Engine/Launcher/Models';
+// import * as sceneConfig from '@Engine/Launcher/debug-scene.config.json';
+import { asClass, createContainer, InjectionMode } from 'awilix';
+// import { launch } from '@Engine/Launcher';
+// import type { SceneConfig } from '@Engine/Launcher/Models';
+import TestService from './TestService';
+import DependentService from './DependentService';
 
 interface ICradle {
   testService: TestService;
-  depService: DependentService;
+  // depService: DependentService;
 }
 
 // Create the container
 const container = createContainer<ICradle>({
-  injectionMode: InjectionMode.CLASSIC
+  injectionMode: InjectionMode.PROXY
 });
 
+// Register the classes
+container.register({
+  testService: asClass(TestService)
+  // depService: asClass(DependentService)
+});
+
+const dependentService = new DependentService();
+console.log(dependentService);
+// let dep1 = container.cradle.depService
+// console.log(dep1.getInnerData());
+
 // TODO (S.Panfilov) for a production we need a runtime validation
-launch(sceneConfig as unknown as SceneConfig);
+// launch(sceneConfig as unknown as SceneConfig);
