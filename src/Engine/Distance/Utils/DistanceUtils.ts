5f88f8f2197c04ab4205fb3ea19a29bdf63abf79
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:03:05 +0100

    fixes

--- src/Engine/Distance/Utils/DistanceUtils.ts
+++ src/Engine/Distance/Utils/DistanceUtils.ts
@@ -1,9 +1,7 @@
-import { SECOND } from '@/Engine/Measurements';
+import { HOUR, SECOND } from '@/Engine/Measurements';
 
 //move with speed in meters per second
 export const mpsSpeed = (mps: number, delta: number): number => mps * delta;
 
-// TODO (S.Panfilov) check formulas
-// TODO (S.Panfilov) extract constants (second, etc)
 //move with speed in kilometers per hour
-export const kmphSpeed = (kmph: number, delta: number): number => ((kmph * SECOND) / 3600) * delta;
+export const kmphSpeed = (kmph: number, delta: number): number => ((kmph * SECOND) / HOUR) * delta;
