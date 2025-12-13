076daee9505e6013f0d46b6efcbe543f85bfdad6
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:38:45 +0100

    Names: fixes

--- src/Engine/Abstract/Models/TEntityParams.ts
+++ src/Engine/Abstract/Models/TEntityParams.ts
@@ -1,4 +1,4 @@
-import type { TWithId, TWithNameOptional, TWithTags } from '@/Engine/Mixins';
+import type { TWithId, TWithName, TWithTags } from '@/Engine/Mixins';
 import type { TOptional } from '@/Engine/Utils';
 
-export type TEntityParams = TWithTags & TWithNameOptional & TOptional<TWithId>;
+export type TEntityParams = TWithTags & TWithName & TOptional<TWithId>;
