cf36967b8b617a5a61f59bfe642b99d9a650bac8
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:09:38 +0100

    cwp

--- src/App/Levels/Showcase22PhysicsShooter/utils/Collisions.ts
+++ src/App/Levels/Showcase22PhysicsShooter/utils/Collisions.ts
@@ -3,7 +3,11 @@ import type { Mesh, Object3D } from 'three';
 import type { TCollisionsService, TSceneWrapper } from '@/Engine';
 
 export function enableCollisions(scene: TSceneWrapper, collisionsService: TCollisionsService): void {
-  //enable collisions
+  // TODO (S.Panfilov) CWP 1. Visualize grids
+  // TODO (S.Panfilov) CWP 2. Visualize raycasting
+  // TODO (S.Panfilov) CWP 3. Fix collision detection
+  // TODO (S.Panfilov) CWP 4. make sure raycasting is working inside the grid only
+  // TODO (S.Panfilov) CWP 5. make bullets and actors can travel among grids
   scene.entity.traverse((object: Object3D): void => {
     if ((object as Mesh).isMesh) {
       // collisionsService.initializeRaycastBvh(object as Mesh);
