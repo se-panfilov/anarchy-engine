4b842b34c5c02eed3f0824a5cde05d6b6d85f0f6
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:43:39 +0100

    [Utils]: kebabToCamel

--- src/Engine/Utils/StringUtils.spec.ts
+++ src/Engine/Utils/StringUtils.spec.ts
@@ -1,28 +1,62 @@
-import { camelToKebab } from './StringUtils';
+import { camelToKebab, kebabToCamel } from './StringUtils';
 
-describe('camelToKebab', () => {
-  it('converts simple camelCase to kebab-case', () => {
-    expect(camelToKebab('camelCase')).toBe('camel-case');
+describe('StringUtils', () => {
+  describe('camelToKebab', () => {
+    it('converts simple camelCase to kebab-case', () => {
+      expect(camelToKebab('camelCase')).toBe('camel-case');
+    });
+
+    it('handles strings with multiple uppercase letters correctly', () => {
+      expect(camelToKebab('camelCaseExample')).toBe('camel-case-example');
+      expect(camelToKebab('camelCaseExampleSomeOtherThen')).toBe('camel-case-example-some-other-then');
+    });
+
+    it('leaves strings without uppercase letters unchanged', () => {
+      expect(camelToKebab('kebab')).toBe('kebab');
+      expect(camelToKebab('Kebab')).toBe('kebab');
+      expect(camelToKebab('Kebab-one')).toBe('kebab-one');
+    });
+
+    it('handles strings with numbers correctly', () => {
+      expect(camelToKebab('version1Update')).toBe('version1-update');
+      expect(camelToKebab('234version1Update')).toBe('234version1-update');
+    });
+
+    it('handles mixed strings with numbers and uppercase letters', () => {
+      expect(camelToKebab('version1Update2Feature')).toBe('version1-update2-feature');
+      expect(camelToKebab('version122Update233Feature')).toBe('version122-update233-feature');
+    });
   });
 
-  it('handles strings with multiple uppercase letters correctly', () => {
-    expect(camelToKebab('camelCaseExample')).toBe('camel-case-example');
-    expect(camelToKebab('camelCaseExampleSomeOtherThen')).toBe('camel-case-example-some-other-then');
-  });
+  describe('kebabToCamel', () => {
+    it('converts simple kebab-case to camelCase', () => {
+      expect(kebabToCamel('kebab-case')).toBe('kebabCase');
+    });
 
-  it('leaves strings without uppercase letters unchanged', () => {
-    expect(camelToKebab('kebab')).toBe('kebab');
-    expect(camelToKebab('Kebab')).toBe('kebab');
-    expect(camelToKebab('Kebab-one')).toBe('kebab-one');
-  });
+    it('converts UPPER-kebab-case to camelCase', () => {
+      expect(kebabToCamel('KEBAB-CASE')).toBe('kebabCase');
+    });
 
-  it('handles strings with numbers correctly', () => {
-    expect(camelToKebab('version1Update')).toBe('version1-update');
-    expect(camelToKebab('234version1Update')).toBe('234version1-update');
-  });
+    it('handles strings with multiple hyphens correctly', () => {
+      expect(kebabToCamel('kebab-case-example')).toBe('kebabCaseExample');
+      expect(kebabToCamel('kebab-case-example-some-other-then')).toBe('kebabCaseExampleSomeOtherThen');
+    });
 
-  it('handles mixed strings with numbers and uppercase letters', () => {
-    expect(camelToKebab('version1Update2Feature')).toBe('version1-update2-feature');
-    expect(camelToKebab('version122Update233Feature')).toBe('version122-update233-feature');
+    it('leaves strings without hyphens unchanged', () => {
+      expect(kebabToCamel('camelCase')).toBe('camelCase');
+      expect(kebabToCamel('CamelCase')).toBe('CamelCase');
+      // eslint-disable-next-line spellcheck/spell-checker
+      expect(kebabToCamel('nocaseatall')).toBe('nocaseatall');
+    });
+
+    it('handles strings with numbers correctly', () => {
+      expect(kebabToCamel('version1-update')).toBe('version1Update');
+      expect(kebabToCamel('234-version1-update')).toBe('234Version1Update');
+    });
+
+    it('handles mixed strings with numbers and hyphens', () => {
+      expect(kebabToCamel('version1-update2-feature')).toBe('version1Update2Feature');
+      expect(kebabToCamel('version122-update233-feature')).toBe('version122Update233Feature');
+    });
   });
 });
--- src/Engine/Utils/StringUtils.ts
+++ src/Engine/Utils/StringUtils.ts
@@ -1,3 +1,13 @@
 export function camelToKebab(str: string): string {
   return str.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
 }
+
+export function kebabToCamel(input: string): string {
+  if (!input.includes('-')) return input;
+
+  return input
+    .toLowerCase()
+    .split('-')
+    .map((part: string, index: number): string => (index === 0 ? part : part.charAt(0).toUpperCase() + part.slice(1)))
+    .join('');
+}
