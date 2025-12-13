ac456a1d2fe2ceb70a16bbbf71e83fcf5e87549c
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:40:09 +0100

    todo

--- src/Engine/Kinematic/Adapters/EntityToConfig.ts
+++ src/Engine/Kinematic/Adapters/EntityToConfig.ts
@@ -6,6 +6,7 @@ import { filterOutEmptyFields } from '@/Engine/Utils';
 
 export function kinematicToConfig<T extends Readonly<{ kinematic?: TOptional<TKinematicParams> } & TRegistrable>>(entity: T): TKinematicConfig {
   // TODO 15-0-0: implement
+  // TODO 15-0-0: Add possibility to setup kinematic's state via params/config and serialize it
   console.log('XXX entity', entity);
 
   // TODO 15-0-0: fix any
