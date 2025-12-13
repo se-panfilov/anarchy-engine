49edd8a7995dadfa909b4bf449f77d501a49a164
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:39:58 +0100

    Serialization fixes

--- src/Engine/Mixins/Generics/ExtractSerializableRegistrableFields.ts
+++ src/Engine/Mixins/Generics/ExtractSerializableRegistrableFields.ts
@@ -1,4 +1,4 @@
 import type { TRegistrable } from '@/Engine/Mixins/Generics/Models';
 
 export const extractSerializableRegistrableFields = <T extends TRegistrable>({ name, tags }: T): Omit<TRegistrable, 'id'> => ({ name, tags });
-// export const extractRegistrableFields = <T extends TRegistrable>({ name, id, tags }: T): TRegistrable => ({ name, tags, id });
+export const extractRegistrableFields = <T extends TRegistrable>({ name, id, tags }: T): TRegistrable => ({ name, tags, id });
