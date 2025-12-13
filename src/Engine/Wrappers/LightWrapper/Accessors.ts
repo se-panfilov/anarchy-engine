d7c4b61dab3d094cf03df2d34211e8ab7c4388b1
gr-olga <gr-olga@ya.ru>
2025-12-13 01:47:45 +0100

    fixes

--- src/Engine/Wrappers/LightWrapper/Accessors.ts
+++ src/Engine/Wrappers/LightWrapper/Accessors.ts
@@ -9,7 +9,7 @@ export function getAccessors(entity: Writeable<IAmbientLight | IDirectionalLight
   const setCastShadow = (value: boolean): boolean => (entity.castShadow = value);
   const setControls = (x: number, y: number, z: number): IVector3 => entity.position.set(x, y, z);
   // eslint-disable-next-line functional/immutable-data
-  const setFar = (far: number): number => ((entity.shadow.camera as IOrthographicCamera).far = far);
+  const setFar = (far: number): number => ((entity.shadow.camera as Writeable<IOrthographicCamera>).far = far);
   const setShadowMapSize = (x: number, y: number): IVector2 => entity.shadow.mapSize.set(x, y);
   // eslint-disable-next-line functional/immutable-data
   const setNormalBias = (val: number): number => (entity.shadow.normalBias = val);
