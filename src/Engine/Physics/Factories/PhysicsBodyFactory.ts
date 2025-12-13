3e276d16e2cf2dd1212733c5f3d209baf31a5799
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:07:01 +0100

    fix

--- src/Engine/Physics/Factories/PhysicsBodyFactory.ts
+++ src/Engine/Physics/Factories/PhysicsBodyFactory.ts
@@ -1,9 +1,10 @@
-import type { TReactiveFactory } from '@/Engine/Abstract';
+import type { TCreateEntityFactoryFn, TReactiveFactory } from '@/Engine/Abstract';
 import { FactoryType, ReactiveFactory } from '@/Engine/Abstract';
 import { configToParamsBody as configToParams } from '@/Engine/Physics/Adapters';
 import type { TPhysicsBodyFacade, TPhysicsBodyFactory, TPhysicsBodyParams } from '@/Engine/Physics/Models';
 import { PhysicsBodyFacade } from '@/Engine/Physics/Wrappers';
 
-const factory: TReactiveFactory<TPhysicsBodyFacade, TPhysicsBodyParams> = { ...ReactiveFactory(FactoryType.PhysicsBody, PhysicsBodyFacade) };
-// const factory: TReactiveFactory<TPhysicsBodyFacade, TPhysicsBodyParams> = { ...ReactiveFactory(FactoryType.PhysicsBody, PhysicsBodyFacade as TCreateEntityFactoryFn<TPhysicsBodyFacade, TPhysicsBodyParams>) };
+const factory: TReactiveFactory<TPhysicsBodyFacade, TPhysicsBodyParams> = {
+  ...ReactiveFactory(FactoryType.PhysicsBody, PhysicsBodyFacade as TCreateEntityFactoryFn<TPhysicsBodyFacade, TPhysicsBodyParams>)
+};
 export const PhysicsBodyFactory = (): TPhysicsBodyFactory => ({ ...factory, configToParams });
