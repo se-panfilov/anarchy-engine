5ff60fd2ca1230c9ea3a2c92f6432a8100e1002b
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:00:23 +0100

    fix

--- src/Engine/Scene/Models/ISceneProps.ts
+++ src/Engine/Scene/Models/ISceneProps.ts
@@ -1,7 +1,9 @@
 import type { IColor } from '@/Engine/Color';
 import type { ICubeTexture, ITexture } from '@/Engine/Texture';
+import type { IActive } from '@/Engine/Mixins';
 
 export type ISceneProps = Readonly<{
   name: string;
   background?: string | IColor | ITexture | ICubeTexture;
-}>;
+}> &
+  IActive;
