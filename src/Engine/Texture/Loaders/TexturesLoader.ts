acd4dd900f5e02dc1aa868252778379b90406c82
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:15:11 +0100

    [HEAVY WIP] fixes for textures loader

--- src/Engine/Texture/Loaders/TexturesLoader.ts
+++ src/Engine/Texture/Loaders/TexturesLoader.ts
@@ -1,24 +1,25 @@
-import { EquirectangularReflectionMapping } from 'three';
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
+import { TextureLoader } from 'three';
 
 import type { TAbstractLoader } from '@/Engine/Abstract';
 import { AbstractLoader, LoaderType } from '@/Engine/Abstract';
-import type { TEnvMapLoader, TEnvMapResourceConfig, TEnvMapTexture, TEnvMapTextureAsyncRegistry, TEnvMapTextureParams } from '@/Engine/EnvMap/Models';
+import type { TTexture, TTextureAsyncRegistry, TTextureParams, TTextureResourceConfig, TTexturesLoader } from '@/Engine/Texture/Models';
+import { applyFilters, applyTextureParams } from '@/Engine/Texture/Utils';
 import type { TWriteable } from '@/Engine/Utils';
-import { isDefined } from '@/Engine/Utils';
 
-export function TexturesLoader(registry: TEnvMapTextureAsyncRegistry): TEnvMapLoader {
-  const envMapLoader: RGBELoader = new RGBELoader();
-  const loader: TAbstractLoader<TEnvMapTexture, TEnvMapResourceConfig> = AbstractLoader(envMapLoader, registry, LoaderType.EnvMap);
+export function TexturesLoader(registry: TTextureAsyncRegistry): TTexturesLoader {
+  const textureLoader: TextureLoader = new TextureLoader();
+  const loader: TAbstractLoader<TTexture, TTextureResourceConfig> = AbstractLoader(textureLoader, registry, LoaderType.Texture);
 
-  function applyParamsOnLoaded(loaded: TWriteable<TEnvMapTexture>, p?: TEnvMapTextureParams): TEnvMapTexture {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,functional/immutable-data
-    loaded.mapping = isDefined(p?.mapping) ? p.mapping : EquirectangularReflectionMapping;
+  function applyParamsOnLoaded(loaded: TWriteable<TTexture>, params?: TTextureParams): TTexture {
+    applyTextureParams(loaded, params);
+    // TODO 9.0.0. RESOURCES: Applying of colorSpace is turned off for now (find out how to work with it properly. Can we add it toTTextureParams?)
+    // if (isDefined(colorSpace)) applyColorSpace(colorSpace as TMaterialPackKeys, loaded, params);
+    applyFilters(loaded, params);
     return loaded;
   }
 
   return {
     ...loader,
-    loadAsync: (config: TEnvMapResourceConfig): Promise<TEnvMapTexture> => loader.loadAsync(config, applyParamsOnLoaded)
+    loadAsync: (config: TTextureResourceConfig): Promise<TTexture> => loader.loadAsync(config, applyParamsOnLoaded)
   };
 }
