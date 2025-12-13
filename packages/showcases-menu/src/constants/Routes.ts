ab7e859d21d998b1f97698e3c8afaed7a3d27d72
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:10:05 +0100

    [MENU] routes

--- packages/showcases-menu/src/constants/Routes.ts
+++ packages/showcases-menu/src/constants/Routes.ts
@@ -1,6 +1,7 @@
 import Audio from '@Showcases/Menu/views/Audio.vue';
 import Graphics from '@Showcases/Menu/views/Graphics.vue';
 import Home from '@Showcases/Menu/views/Home.vue';
+import Legal from '@Showcases/Menu/views/Legal.vue';
 import Localization from '@Showcases/Menu/views/Localization.vue';
 import Settings from '@Showcases/Menu/views/Settings.vue';
 
@@ -8,15 +9,16 @@ export enum Routes {
   Home = '/',
   Audio = '/audio',
   Graphics = '/graphics',
+  Legal = '/legal',
   Localization = '/localization',
   Settings = '/settings'
 }
 
-// TODO DESKTOP: any?
 export const menuRouteMap: Record<Routes, any> = {
   [Routes.Home]: Home,
   [Routes.Audio]: Audio,
   [Routes.Graphics]: Graphics,
   [Routes.Localization]: Localization,
+  [Routes.Legal]: Legal,
   [Routes.Settings]: Settings
 };
