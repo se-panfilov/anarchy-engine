532e66231d2c10a47222911bdbc02c1a0fb28ffa
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:16:43 +0100

    [HEAVY WIP] materials: fixed schema

--- src/Engine/Space/Models/TSpaceConfigResources.ts
+++ src/Engine/Space/Models/TSpaceConfigResources.ts
@@ -1,11 +1,11 @@
 import type { TEnvMapResourceConfig } from '@/Engine/EnvMap';
-import type { TMaterialConfig, TMaterialTexturePack } from '@/Engine/Material';
+import type { TMaterialConfig } from '@/Engine/Material';
 import type { TModel3dResourceConfig } from '@/Engine/Models3d';
 import type { TTextureResourceConfig } from '@/Engine/Texture';
 
 export type TSpaceConfigResources = Readonly<{
   envMaps: ReadonlyArray<TEnvMapResourceConfig>;
-  materials: ReadonlyArray<TMaterialConfig<TMaterialTexturePack>>;
+  materials: ReadonlyArray<TMaterialConfig>;
   models3d: ReadonlyArray<TModel3dResourceConfig>;
   textures: ReadonlyArray<TTextureResourceConfig>;
 }>;
