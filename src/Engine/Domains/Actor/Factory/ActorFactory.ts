aa906504a27438a7448e4de9c9daf348b5b2cc0b
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:50:43 +0100

    rx factory

--- src/Engine/Domains/Actor/Factory/ActorFactory.ts
+++ src/Engine/Domains/Actor/Factory/ActorFactory.ts
@@ -1,11 +1,27 @@
-import type { IFactory } from '@Engine/Domains/Abstract';
+import type { IFactory, IReactiveFactory } from '@Engine/Domains/Abstract';
 import { AbstractFactory } from '@Engine/Domains/Abstract';
 import { destroyableMixin } from '@Engine/Domains/Mixins';
+import { Subject } from 'rxjs';
 
 import { getParams } from '../Adapter';
 import type { IActorFactory, IActorParams, IActorWrapper } from '../Models';
 import { ActorWrapper } from '../Wrapper';
 
-const create = (params: IActorParams): IActorWrapper => ActorWrapper(params);
-const factory: IFactory<IActorWrapper, IActorParams> = { ...AbstractFactory('actor'), create };
+const abstractFactory: Omit<IFactory<IActorWrapper, IActorParams>, 'create'> = { ...AbstractFactory('actor') };
+
+const reactiveFactory: Omit<IFactory<IActorWrapper, IActorParams> & IReactiveFactory<IActorWrapper>, 'create'> = {
+  ...abstractFactory,
+  entityCreated$: new Subject<IActorWrapper>(),
+  destroyed$: new Subject<void>()
+};
+
+const factory: IFactory<IActorWrapper, IActorParams> & IReactiveFactory<IActorWrapper> = {
+  ...reactiveFactory,
+  create: (params: IActorParams): IActorWrapper => {
+    const wrapper: IActorWrapper = ActorWrapper(params);
+    reactiveFactory.entityCreated$.next(wrapper);
+    return wrapper;
+  }
+};
+
 export const ActorFactory = (): IActorFactory => ({ ...factory, getParams, ...destroyableMixin(factory) });
