a1fcb0ea263eb2e7325c425c86c041a22332e286
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:48:35 +0100

    fixes

--- src/Engine/Wrappers/LightWrapper/Accessors.ts
+++ src/Engine/Wrappers/LightWrapper/Accessors.ts
@@ -12,11 +12,32 @@ export function getAccessors(entity: Writeable<IAmbientLight | IDirectionalLight
   // eslint-disable-next-line functional/immutable-data
   const setCastShadow = (value: boolean): boolean => (entity.castShadow = value);
   const setControls = (x: number, y: number, z: number): IVector3 => entity.position.set(x, y, z);
-  // eslint-disable-next-line functional/immutable-data
-  const setFar = (far: number): number => ((entity.shadow.camera as Writeable<IOrthographicCamera>).far = far);
-  const setShadowMapSize = (x: number, y: number): IVector2 => entity.shadow.mapSize.set(x, y);
-  // eslint-disable-next-line functional/immutable-data
-  const setNormalBias = (val: number): number => (entity.shadow.normalBias = val);
+
+  function setFar(far: number): number | never {
+    if (isAmbientLight(entity)) throw new Error('Cannot call "setFar" for IAmbientLight');
+    // eslint-disable-next-line functional/immutable-data
+    return ((entity.shadow.camera as Writeable<IOrthographicCamera>).far = far);
+  }
+
+  function setShadowMapSize(x: number, y: number): IVector2 {
+    if (isAmbientLight(entity)) throw new Error('Cannot call "setShadowMapSize" for IAmbientLight');
+    // eslint-disable-next-line functional/immutable-data
+    return entity.shadow.mapSize.set(x, y);
+  }
+
+  function setNormalBias(val: number): number {
+    if (isAmbientLight(entity)) throw new Error('Cannot call "setNormalBias" for IAmbientLight');
+    // eslint-disable-next-line functional/immutable-data
+    return (entity.shadow.normalBias = val);
+  }
 
   return { setPosition, setCastShadow, setControls, setFar, setShadowMapSize, setNormalBias };
+}
+
+// function isDirectionalLight(light: IAmbientLight | IDirectionalLight): light is IDirectionalLight {
+//   return Boolean((light as IDirectionalLight).isDirectionalLight);
+// }
+
+function isAmbientLight(light: IAmbientLight | IDirectionalLight): light is IAmbientLight {
+  return Boolean((light as IAmbientLight).isAmbientLight);
 }
