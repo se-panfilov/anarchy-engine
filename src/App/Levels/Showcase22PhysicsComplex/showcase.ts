d10db9b18104604a73162a6e4ed57ca4ba50622c
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:07:57 +0100

    keva tower

--- src/App/Levels/Showcase22PhysicsComplex/showcase.ts
+++ src/App/Levels/Showcase22PhysicsComplex/showcase.ts
@@ -1,11 +1,10 @@
-import type { RigidBody, World } from '@dimforge/rapier3d';
-import { ColliderDesc, RigidBodyDesc } from '@dimforge/rapier3d';
+import type { World } from '@dimforge/rapier3d';
 import type { Vector } from '@dimforge/rapier3d/math';
 import { Vector3 } from 'three';
 
 import type { TShowcase } from '@/App/Levels/Models';
-import type { TAppCanvas, TEngine, TSpace, TSpaceConfig } from '@/Engine';
-import { buildSpaceFromConfig, Engine, isNotDefined, KeysExtra } from '@/Engine';
+import type { TActorService, TActorWrapperAsync, TAppCanvas, TEngine, TSpace, TSpaceConfig } from '@/Engine';
+import { ActorType, buildSpaceFromConfig, CollisionShape, Engine, isNotDefined, KeysExtra, MaterialType, RigidBodyTypesNames, Vector3Wrapper } from '@/Engine';
 
 import spaceConfig from './showcase.json';
 
@@ -13,14 +12,14 @@ export function showcase(canvas: TAppCanvas): TShowcase {
   const space: TSpace = buildSpaceFromConfig(canvas, spaceConfig as TSpaceConfig);
   const engine: TEngine = Engine(space);
   const { keyboardService } = engine.services;
-  const { physicsLoopService, physicsWorldService, loopService } = space.services;
+  const { physicsLoopService, physicsWorldService, physicsBodyService, actorService, loopService } = space.services;
 
-  async function init(): Promise<void> {
+  function init(): void {
     physicsWorldService.getDebugRenderer(loopService).start();
 
     const wold: World | undefined = physicsWorldService.getWorld();
     if (isNotDefined(wold)) throw new Error('World is not defined');
-    buildKevaTower(wold);
+    buildKevaTower(wold, actorService);
 
     keyboardService.onKey(KeysExtra.Space).pressed$.subscribe((): void => physicsLoopService.shouldAutoUpdate(!physicsLoopService.isAutoUpdate()));
   }
@@ -33,7 +32,7 @@ export function showcase(canvas: TAppCanvas): TShowcase {
   return { start, space };
 }
 
-function buildKevaTower(world: World): void {
+function buildKevaTower(world: World, actorService: TActorService): void {
   const halfExtents: Vector3 = new Vector3(0.1, 0.5, 2.0);
   let blockHeight: number = 0.0;
   // These should only be set to odd values otherwise
@@ -46,12 +45,12 @@ function buildKevaTower(world: World): void {
     const numy: number = numyArr[i];
     const numz: number = numx * 3 + 1;
     const blockWidth: number = numx * halfExtents.z * 2.0;
-    buildBlock(world, halfExtents, new Vector3(-blockWidth / 2.0, blockHeight, -blockWidth / 2.0), numx, numy, numz);
+    buildBlock(world, halfExtents, new Vector3(-blockWidth / 2.0, blockHeight, -blockWidth / 2.0), numx, numy, numz, actorService);
     blockHeight += numy * halfExtents.y * 2.0 + halfExtents.x * 2.0;
   }
 }
 
-function buildBlock(world: World, halfExtents: Vector, shift: Vector, numX: number, numY: number, numZ: number): void {
+async function buildBlock(world: World, halfExtents: Vector, shift: Vector, numX: number, numY: number, numZ: number, actorService: TActorService): void {
   const half_extents_zyx: Vector = {
     x: halfExtents.z,
     y: halfExtents.y,
@@ -76,12 +75,36 @@ function buildBlock(world: World, halfExtents: Vector, shift: Vector, numX: numb
       const x: number = i % 2 == 0 ? spacing * j * 2.0 : dim.x * j * 2.0;
 
       for (k = 0; k < numZ; ++k) {
+        // once = false;
         const z: number = i % 2 == 0 ? dim.z * k * 2.0 : spacing * k * 2.0;
         // Build the rigid body.
-        const bodyDesc: RigidBodyDesc = RigidBodyDesc.dynamic().setTranslation(x + dim.x + shift.x, y + dim.y + shift.y, z + dim.z + shift.z);
-        const body: RigidBody = world.createRigidBody(bodyDesc);
-        const colliderDesc: ColliderDesc = ColliderDesc.cuboid(dim.x, dim.y, dim.z);
-        world.createCollider(colliderDesc, body);
+
+        const actor: TActorWrapperAsync = await actorService.createAsync({
+          name: `block-${i}-${j}`,
+          type: ActorType.Cube,
+          width: dim.x * 2,
+          height: dim.y * 2,
+          depth: dim.z * 2,
+          material: { type: MaterialType.Standard, params: { color: '#8FAA8F' } },
+          physics: {
+            type: RigidBodyTypesNames.Dynamic,
+            collisionShape: CollisionShape.Cuboid,
+            shapeParams: {
+              hx: dim.x,
+              hy: dim.y,
+              hz: dim.z
+            }
+          },
+          castShadow: true,
+          tags: []
+        });
+
+        actor.setPosition(Vector3Wrapper({ x: x + dim.x + shift.x, y: y + dim.y + shift.y, z: z + dim.z + shift.z }));
+        actor.physicsBody?.getRigidBody()?.setTranslation({ x: x + dim.x + shift.x, y: y + dim.y + shift.y, z: z + dim.z + shift.z } satisfies Vector, true);
+        // const bodyDesc: RigidBodyDesc = RigidBodyDesc.dynamic().setTranslation(x + dim.x + shift.x, y + dim.y + shift.y, z + dim.z + shift.z);
+        // const body: RigidBody = world.createRigidBody(bodyDesc);
+        // const colliderDesc: ColliderDesc = ColliderDesc.cuboid(dim.x, dim.y, dim.z);
+        // world.createCollider(colliderDesc, body);
       }
     }
   }
@@ -92,10 +115,37 @@ function buildBlock(world: World, halfExtents: Vector, shift: Vector, numX: numb
   for (i = 0; i < blockWidth / (dim.x * 2.0); ++i) {
     for (j = 0; j < blockWidth / (dim.z * 2.0); ++j) {
       // Build the rigid body.
-      const bodyDesc: RigidBodyDesc = RigidBodyDesc.dynamic().setTranslation(i * dim.x * 2.0 + dim.x + shift.x, dim.y + shift.y + blockHeight, j * dim.z * 2.0 + dim.z + shift.z);
-      const body: RigidBody = world.createRigidBody(bodyDesc);
-      const colliderDesc: ColliderDesc = ColliderDesc.cuboid(dim.x, dim.y, dim.z);
-      world.createCollider(colliderDesc, body);
+      const actor2 = await actorService.createAsync({
+        name: `block-${i}-${j}`,
+        type: ActorType.Cube,
+        width: dim.x * 2,
+        height: dim.y * 2,
+        depth: dim.z * 2,
+        material: { type: MaterialType.Standard, params: { color: '#8FAA8F' } },
+        physics: {
+          type: RigidBodyTypesNames.Dynamic,
+          collisionShape: CollisionShape.Cuboid,
+          shapeParams: {
+            hx: dim.x,
+            hy: dim.y,
+            hz: dim.z
+          }
+          // position: Vector3Wrapper({ x: -4, y: 4, z: 3 }),
+          // rotation: Vector4Wrapper({ x: 0, y: 0, z: 0, w: 1 })
+        },
+        // position: Vector3Wrapper({ x: -4, y: 4, z: 3 }),
+        // rotation: EulerWrapper({ x: 0, y: 0, z: 0 }),
+        castShadow: true,
+        tags: []
+      });
+
+      actor2.setPosition(Vector3Wrapper({ x: i * dim.x * 2.0 + dim.x + shift.x, y: dim.y + shift.y + blockHeight, z: j * dim.z * 2.0 + dim.z + shift.z }));
+      actor2.physicsBody?.getRigidBody()?.setTranslation({ x: i * dim.x * 2.0 + dim.x + shift.x, y: dim.y + shift.y + blockHeight, z: j * dim.z * 2.0 + dim.z + shift.z } satisfies Vector, true);
+
+      // const bodyDesc: RigidBodyDesc = RigidBodyDesc.dynamic().setTranslation(i * dim.x * 2.0 + dim.x + shift.x, dim.y + shift.y + blockHeight, j * dim.z * 2.0 + dim.z + shift.z);
+      // const body: RigidBody = world.createRigidBody(bodyDesc);
+      // const colliderDesc: ColliderDesc = ColliderDesc.cuboid(dim.x, dim.y, dim.z);
+      // world.createCollider(colliderDesc, body);
     }
   }
 }
