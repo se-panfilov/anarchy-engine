0a5d6eab61cbc48c85ee94b942e9467cdfbad972
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:31:41 +0100

    fixes for ts-json-schema-generator

--- src/Engine/ThreeLib/Models/TReadonlyQuaternion.ts
+++ src/Engine/ThreeLib/Models/TReadonlyQuaternion.ts
@@ -1,3 +1,25 @@
 import type { Quaternion } from 'three';
 
-export type TReadonlyQuaternion = Readonly<Pick<Quaternion, 'x' | 'y' | 'z' | 'w' | 'clone' | 'equals' | 'dot' | 'toArray' | 'angleTo'>>;
+// TODO Better to use Pick instead of Omit, but it is not working with ts-json-schema-generator
+// We are allowing to use only immutable methods and props (if you need all methods, use .clone() first)
+// export type TReadonlyQuaternion = Readonly<Pick<Quaternion, 'x' | 'y' | 'z' | 'w' | 'clone' | 'equals' | 'dot' | 'toArray' | 'angleTo'>>;
+
+// We are allowing to use only immutable methods and props (if you need all methods, use .clone() first)
+export type TReadonlyQuaternion = Readonly<
+  Omit<
+    Quaternion,
+    | 'copy'
+    | 'invert'
+    | 'multiply'
+    | 'multiplyQuaternions'
+    | 'normalize'
+    | 'premultiply'
+    | 'set'
+    | 'setFromAxisAngle'
+    | 'setFromEuler'
+    | 'setFromRotationMatrix'
+    | 'setFromUnitVectors'
+    | 'slerp'
+    | 'slerpQuaternions'
+  >
+>;
