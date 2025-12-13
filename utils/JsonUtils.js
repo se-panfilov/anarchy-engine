09223d4efb9289dbfca41d839980be52d74fee8f
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:47:32 +0100

    added script

--- utils/JsonUtils.js
+++ utils/JsonUtils.js
@@ -1,29 +1,30 @@
-// import * as tsConfig from '../../tsconfig.json';
 const fs = require('fs');
-const path = require('path');
 const tsj = require('ts-json-schema-generator');
 
-// const inputFile = fs.readFileSync(path.resolve(file));
-// const inputFile = fs.readFileSync(path.resolve('../../tsconfig.json'));
-
 /** @type {import('ts-json-schema-generator/dist/src/Config').Config} */
-const config = {
-  path: '../src/Engine/Launcher/Models/ISceneConfig.ts',
-  tsconfig: '../tsconfig.json',
+const baseConfig = {
+  tsconfig: './tsconfig.json',
   type: '*', // Or <type-name> if you want to generate schema for that one type only,
   topRef: false
 };
 
-// TODO (S.Panfilov) docs: https://github.com/vega/ts-json-schema-generator
+function generate(config) {
+  const schema = tsj.createGenerator(config).createSchema(config.type);
+  return JSON.stringify(schema, null, 2);
+}
 
-const schema = tsj.createGenerator(config).createSchema(config.type);
-const schemaString = JSON.stringify(schema, null, 2);
-const output_path = '../src/Engine/Schemas/ISceneConfig.json';
+function writeSchema(outputPath, schemaString) {
+  // TODO (S.Panfilov) should create an output file if it's not exist
+  fs.writeFile(outputPath, schemaString, (err) => {
+    if (err) throw err;
+  });
+}
 
-fs.writeFile(output_path, schemaString, (err) => {
-  if (err) throw err;
-});
+function generateSceneConfigSchema() {
+  const config = { ...baseConfig, path: './src/Engine/Launcher/Models/ISceneConfig.ts' };
+  const schemaString = generate(config);
+  const outputPath = './src/Engine/Schemas/ISceneConfig.json';
+  writeSchema(outputPath, schemaString);
+}
 
-// export function generateSchemaForInterface(file, tsConfig) {}
-
-// npx ts-json-schema-generator --path 'ISceneConfig.ts' --type 'ISceneConfig' --tsconfig '../../../../tsconfig.json'
+generateSceneConfigSchema();
