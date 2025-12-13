bdd432c35638b348d799a48a59e422b6896d8b05
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:31:36 +0100

    Readonly entities: fix to overcome ts-json-schema-generator bug

--- src/Engine/ThreeLib/Models/TReadonlyEuler.ts
+++ src/Engine/ThreeLib/Models/TReadonlyEuler.ts
@@ -1,3 +1,8 @@
 import type { Euler } from 'three';
 
-export type TReadonlyEuler = Readonly<Pick<Euler, 'x' | 'y' | 'z' | 'order' | 'clone' | 'equals' | 'toArray'>>;
+// TODO Better to use Pick instead of Omit, but it is not working with ts-json-schema-generator
+// We are allowing to use only immutable methods and props (if you need all methods, use .clone() first)
+// export type TReadonlyEuler = Readonly<Pick<Euler, 'x' | 'y' | 'z' | 'order' | 'clone' | 'equals' | 'toArray'>>;
+
+// We are allowing to use only immutable methods and props (if you need all methods, use .clone() first)
+export type TReadonlyEuler = Readonly<Omit<Euler, 'set' | 'copy' | 'reorder' | 'setFromQuaternion' | 'setFromRotationMatrix'>>;
--- src/Engine/ThreeLib/Models/TReadonlyVector3.ts
+++ src/Engine/ThreeLib/Models/TReadonlyVector3.ts
@@ -1,6 +1,68 @@
 import type { Vector3 } from 'three';
 
+// TODO Better to use Pick instead of Omit, but it is not working with ts-json-schema-generator
+// We are allowing to use only immutable methods and props (if you need all methods, use .clone() first)
+// export type TReadonlyVector3 = Readonly<
+//   Pick<Vector3, 'x' | 'y' | 'z' | 'clone' | 'length' | 'lengthSq' | 'manhattanLength' | 'distanceTo' | 'distanceToSquared' | 'manhattanDistanceTo' | 'angleTo' | 'dot' | 'equals'>
+// >;
+
 // We are allowing to use only immutable methods and props (if you need all methods, use .clone() first)
 export type TReadonlyVector3 = Readonly<
-  Pick<Vector3, 'x' | 'y' | 'z' | 'clone' | 'length' | 'lengthSq' | 'manhattanLength' | 'distanceTo' | 'distanceToSquared' | 'manhattanDistanceTo' | 'angleTo' | 'dot' | 'equals'>
+  Omit<
+    Vector3,
+    | 'add'
+    | 'addScalar'
+    | 'addScaledVector'
+    | 'addVectors'
+    | 'applyAxisAngle'
+    | 'applyEuler'
+    | 'applyMatrix3'
+    | 'applyMatrix4'
+    | 'applyQuaternion'
+    | 'ceil'
+    | 'clamp'
+    | 'clampLength'
+    | 'clampScalar'
+    | 'cross'
+    | 'crossVectors'
+    | 'divide'
+    | 'divideScalar'
+    | 'floor'
+    | 'lerp'
+    | 'lerpVectors'
+    | 'max'
+    | 'min'
+    | 'multiply'
+    | 'multiplyScalar'
+    | 'multiplyVectors'
+    | 'negate'
+    | 'normalize'
+    | 'project'
+    | 'projectOnPlane'
+    | 'projectOnVector'
+    | 'reflect'
+    | 'round'
+    | 'roundToZero'
+    | 'set'
+    | 'setComponent'
+    | 'setFromCylindrical'
+    | 'setFromCylindricalCoords'
+    | 'setFromEuler'
+    | 'setFromMatrix3Column'
+    | 'setFromMatrixColumn'
+    | 'setFromMatrixPosition'
+    | 'setFromMatrixScale'
+    | 'setFromSpherical'
+    | 'setFromSphericalCoords'
+    | 'setLength'
+    | 'setScalar'
+    | 'setX'
+    | 'setY'
+    | 'setZ'
+    | 'sub'
+    | 'subScalar'
+    | 'subVectors'
+    | 'transformDirection'
+    | 'unproject'
+  >
 >;
