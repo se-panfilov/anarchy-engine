d223d35e57d7bbac6504c71e06c72e75b761587f
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:50:17 +0100

    [SHOWCASE] Intersections

--- src/App/Levels/Showcase27SaveLoad/spaceIntersections/spaceIntersections.ts
+++ src/App/Levels/Showcase27SaveLoad/spaceIntersections/spaceIntersections.ts
@@ -1,6 +1,6 @@
 import { BehaviorSubject } from 'rxjs';
 
-import type { TSpace, TSpaceConfig } from '@/Engine';
+import type { TIntersectionEvent, TIntersectionsWatcher, TSpace, TSpaceConfig } from '@/Engine';
 
 import type { TSpacesData } from '../ShowcaseTypes';
 import { getContainer } from '../utils';
@@ -14,7 +14,11 @@ export const spaceIntersectionsData: TSpacesData = {
   container: getContainer(config.canvasSelector),
   awaits$: new BehaviorSubject<ReadonlySet<string>>(new Set()),
   onCreate: (space: TSpace): void | never => {
-    //
+    const watcherRed: TIntersectionsWatcher = space.services.intersectionsWatcherService.getRegistry().getByName('watcher_red');
+    const watcherBlue: TIntersectionsWatcher = space.services.intersectionsWatcherService.getRegistry().getByName('watcher_blue');
+
+    watcherRed.value$.subscribe((value: TIntersectionEvent): void => console.log('redWatcher', value));
+    watcherBlue.value$.subscribe((value: TIntersectionEvent): void => console.log('blueWatcher', value));
   },
   onChange: (space: TSpace): void => {
     //
