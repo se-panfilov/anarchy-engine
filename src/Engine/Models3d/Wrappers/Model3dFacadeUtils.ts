4e9e8c2fbf17a8660297a59c11ecd5caf4ae0dbc
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:13:39 +0100

    cleanup

--- src/Engine/Models3d/Wrappers/Model3dFacadeUtils.ts
+++ src/Engine/Models3d/Wrappers/Model3dFacadeUtils.ts
@@ -8,13 +8,5 @@ export function createModels3dEntities(params: TModels3dFacadeParams, animations
   const model: Object3D = SkeletonUtils.clone(params.model);
   const { actions, mixer } = animationsService.createActions(model, params.animations ?? {});
 
-  // TODO do we need to clone animations?
-  // const animations: TAnimationsPack = {};
-  // eslint-disable-next-line functional/immutable-data
-  // Object.entries(params.animations).forEach(([key, value]: [string, AnimationClip]): void => void (animations[key] = value.clone()));
-
-  // TODO debug
-  // if (actions && actions['Run']) console.log(actions['Run'].getMixer());
-  // return { ...params, model, animations, actions, mixer };
   return { ...params, model, actions, mixer };
 }
