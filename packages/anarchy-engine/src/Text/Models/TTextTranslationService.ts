cb29073090c7f31352afda021f14a4bf5fca197c
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:13:49 +0100

    [ENGINE] fixes

--- packages/anarchy-engine/src/Text/Models/TTextTranslationService.ts
+++ packages/anarchy-engine/src/Text/Models/TTextTranslationService.ts
@@ -2,7 +2,7 @@ import type { BehaviorSubject, Observable } from 'rxjs';
 
 export type TTextTranslationService = Readonly<{
   t$: (id: string, params?: Record<string, string> | Observable<Record<string, string>>) => Observable<string>;
-  locale$: BehaviorSubject<Readonly<{ font?: string }>>;
+  locale$: BehaviorSubject<any | Readonly<{ font?: string }>>;
   // n$: (value: number | Observable<number>, options?: FormatNumberOptions | Observable<FormatNumberOptions | undefined>) => Observable<string>;
   // d$: (value: number | Date | Observable<number | Date>, options?: FormatDateOptions | Observable<FormatDateOptions | undefined>) => Observable<string>;
 }>;
