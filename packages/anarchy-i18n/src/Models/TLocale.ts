8fc6c3657a2e3996e55e45a934903c92153455ba
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:03:52 +0100

    [I18N] generate locales script: types

---
+++ packages/anarchy-i18n/src/Models/TLocale.ts
@@ -0,0 +1,10 @@
+export type TLocale = Readonly<{
+  id: string; // BCP47, e.g. 'en-US'
+  languageCode: string; // 'en' (ISO 639-1)
+  regionCode?: string; // 'US' (ISO 3166-1 alpha-2)
+  // eslint-disable-next-line spellcheck/spell-checker
+  scriptCode?: string; // 'Latn' | 'Cyrl' | 'Hans' |
+  englishName: string;
+  nativeName: string;
+  dir: 'ltr' | 'rtl'; // TODO DESKTOP: rename to 'direction'
+}>;
--- packages/anarchy-i18n/src/Models/index.ts
+++ packages/anarchy-i18n/src/Models/index.ts
@@ -1,3 +1,4 @@
+export type * from './TLocale';
 export type * from './TLocalesMapping';
 export type * from './TMessages';
 export type * from './TReactiveTranslationMixin';
