59083e37566178f319eca6f5dfd62f4046d7f6c4
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:00:44 +0100

    fix

--- src/Engine/Controls/Factory/ControlsFactory.ts
+++ src/Engine/Controls/Factory/ControlsFactory.ts
@@ -1,8 +1,13 @@
 import type { IReactiveFactory } from '@/Engine/Abstract';
 import { FactoryType, ReactiveFactory } from '@/Engine/Abstract';
 import { configToParams } from '@/Engine/Controls/Adapter';
-import type { IControlsFactory, IControlsParams, IControlsWrapper } from '@/Engine/Controls/Models';
+import type { IControlsFactory, IControlsParams, IControlsWrapper, IOrbitControlsParams } from '@/Engine/Controls/Models';
 import { OrbitControlsWrapper } from '@/Engine/Controls/Wrapper';
 
-export const controlsWithConfigFactory: IReactiveFactory<IControlsWrapper, IControlsParams> = { ...ReactiveFactory(FactoryType.Controls, OrbitControlsWrapper) };
+function create(params: IOrbitControlsParams): IControlsWrapper {
+  // TODO (S.Panfilov) Add support for other types of controls
+  return OrbitControlsWrapper(params);
+}
+
+export const controlsWithConfigFactory: IReactiveFactory<IControlsWrapper, IControlsParams> = { ...ReactiveFactory(FactoryType.Controls, create) };
 export const ControlsFactory = (): IControlsFactory => ({ ...controlsWithConfigFactory, configToParams });
