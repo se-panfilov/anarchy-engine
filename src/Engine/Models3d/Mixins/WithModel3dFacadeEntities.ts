67d484fba088a9cadcb02812bb12e03b8c4e6ac2
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:13:30 +0100

    [WIP ] clone models3d fix

--- src/Engine/Models3d/Mixins/WithModel3dFacadeEntities.ts
+++ src/Engine/Models3d/Mixins/WithModel3dFacadeEntities.ts
@@ -1,12 +1,13 @@
 import type { TModel3dPack, TWithModel3dFacadeEntities } from '@/Engine/Models3d/Models';
 
-export function withModel3dFacadeEntities({ url, model, animations, actions, mixer, options }: TModel3dPack): TWithModel3dFacadeEntities {
+export function withModel3dFacadeEntities({ url, model, animations, actions, mixer, options, clonedFrom }: TModel3dPack): TWithModel3dFacadeEntities {
   return {
     getUrl: () => url,
     getModel: () => model,
     getAnimations: () => animations,
     getActions: () => actions,
     getMixer: () => mixer,
-    getOptions: () => options
+    getOptions: () => options,
+    getClonedFrom: () => clonedFrom
   };
 }
