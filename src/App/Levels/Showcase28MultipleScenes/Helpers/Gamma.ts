354f27ab54df20c182e3e71b8376882263fecb55
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:37:19 +0100

    Showcases: fixes

--- src/App/Levels/Showcase28MultipleScenes/Helpers/Gamma.ts
+++ src/App/Levels/Showcase28MultipleScenes/Helpers/Gamma.ts
@@ -1,13 +1,38 @@
+import { distinctUntilChanged } from 'rxjs';
 import { Clock } from 'three';
 
+import { initSolder1, initSolder2 } from '@/App/Levels/Showcase24ActorsWithModels/Utils';
 import { moveByCircle } from '@/App/Levels/Utils/MoveUtils';
-import type { TFsmWrapper, TSpace } from '@/Engine';
+import type { TFsmStates, TFsmWrapper, TSpace } from '@/Engine';
+import { KeyCode, KeysExtra } from '@/Engine';
 
 export function runGamma(space: TSpace): void {
+  const { keyboardService } = space.services;
+  const { onKey, isKeyPressed } = keyboardService;
+
   moveByCircle('box_actor', space.services.actorService, space.loops.transformLoop, new Clock());
 
-  // const solder1AnimFsm: TFsmWrapper = initSolder1('solder_actor_1', fadeDuration, space.services);
-  // const solder2AnimFsm: TFsmWrapper = initSolder2('solder_actor_2', fadeDuration, space.services);
+  const fadeDuration = 0.3;
+
+  const solder1AnimFsm: TFsmWrapper = initSolder1('solder_actor_1', fadeDuration, space.services);
+  const solder2AnimFsm: TFsmWrapper = initSolder2('solder_actor_2', fadeDuration, space.services);
+
+  solder1AnimFsm.changed$.pipe(distinctUntilChanged()).subscribe((state: TFsmStates): void => {
+    if (state === 'Idle') {
+      solder2AnimFsm.send$.next('Idle');
+    } else {
+      solder2AnimFsm.send$.next('Dance');
+    }
+  });
+
+  onKey(KeyCode.W).pressing$.subscribe((): void => {
+    const action: 'Run' | 'Walk' = isKeyPressed(KeysExtra.Shift) ? 'Run' : 'Walk';
+    if (solder1AnimFsm.getState() !== action) solder1AnimFsm.send$.next(action);
+  });
+
+  onKey(KeyCode.W).released$.subscribe((): void => {
+    solder1AnimFsm.send$.next('Idle');
+  });
 
   space.start$.next(true);
 }
