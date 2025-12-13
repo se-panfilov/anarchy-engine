ad127f9472d9d71de7162fb454c82fdfa5e5ed72
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:16:10 +0100

    [HEAVY WIP] materials: fixes for types

--- src/Engine/Material/Models/TMaterialPackConfig.ts
+++ src/Engine/Material/Models/TMaterialPackConfig.ts
@@ -1,7 +1,7 @@
 import type { MaterialType } from '@/Engine/Material/Constants';
-import type { TWithNameRequired } from '@/Engine/Mixins';
+import type { TWithNameRequired, TWithReadonlyTags } from '@/Engine/Mixins';
 
 import type { TMaterialConfig } from './TMaterialConfig';
 import type { TMaterialTexturePack } from './TMaterialTexturePack';
 
-export type TMaterialPackConfig<T extends TMaterialTexturePack> = Readonly<{ type: MaterialType; params?: Omit<TMaterialConfig, 'type'>; textures?: T }> & TWithNameRequired;
+export type TMaterialPackConfig<T extends TMaterialTexturePack> = Readonly<{ type: MaterialType; params?: Omit<TMaterialConfig, 'type'>; textures?: T }> & TWithNameRequired & TWithReadonlyTags;
--- src/Engine/Material/Models/TMaterialPackParams.ts
+++ src/Engine/Material/Models/TMaterialPackParams.ts
@@ -1,6 +1,7 @@
 import type { MaterialType } from '@/Engine/Material/Constants';
+import type { TWithNameRequired, TWithReadonlyTags } from '@/Engine/Mixins';
 
 import type { TMaterialProps } from './TMaterialProps';
 import type { TMaterialTexturePack } from './TMaterialTexturePack';
 
-export type TMaterialPackParams<T extends TMaterialTexturePack> = Readonly<{ type: MaterialType; params?: TMaterialProps; textures?: T }>;
+export type TMaterialPackParams<T extends TMaterialTexturePack> = Readonly<{ type: MaterialType; params?: TMaterialProps; textures?: T }> & TWithNameRequired & TWithReadonlyTags;
