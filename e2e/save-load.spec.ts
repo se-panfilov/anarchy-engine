7761f02911651e17aca39920da63db662138344f
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:43:24 +0100

    [E2E] tests updates

--- e2e/save-load.spec.ts
+++ e2e/save-load.spec.ts
@@ -7,38 +7,52 @@ const GAME_URL = `http://localhost:${process.env.PORT}`;
 
 test.use({ viewport: VIEWPORT });
 
-test('SpaceBasic', async ({ page }) => {
-  await page.goto(GAME_URL);
+test.describe('SpaceBasic', () => {
+  test('Normal state', async ({ page }) => {
+    const name = 'SpaceBasic';
+    await page.goto(GAME_URL);
 
-  const spaceSelect: Locator = page.getByLabel('Spaces');
-  await expect(spaceSelect).toBeVisible();
+    const spaceSelect: Locator = page.getByLabel('Spaces');
+    await expect(spaceSelect).toBeVisible();
+    await page.getByLabel('Spaces').selectOption(name);
+    await expect(page.locator('canvas')).toHaveScreenshot(`${name}-default.png`);
+  });
 
-  await page.getByLabel('Spaces').selectOption('SpaceBasic');
+  test('Change, Save and Load with the change', async ({ page }) => {
+    await page.goto(GAME_URL);
+    const name = 'SpaceBasic';
 
-  await expect(page.locator('canvas')).toHaveScreenshot('SpaceBasic-before.png');
+    const spaceSelect: Locator = page.getByLabel('Spaces');
+    await expect(spaceSelect).toBeVisible();
 
-  await page.getByRole('button', { name: 'Change' }).click();
-  await page.getByRole('button', { name: 'Save' }).click();
-  await page.getByRole('button', { name: 'Drop' }).click();
-  await page.getByRole('button', { name: 'Load' }).click();
+    await page.getByLabel('Spaces').selectOption(name);
 
-  await expect(page.locator('canvas')).toHaveScreenshot('SpaceBasic-after.png');
+    await page.getByRole('button', { name: 'Change' }).click();
+
+    await expect(page.locator('canvas')).toHaveScreenshot(`${name}-before.png`);
+
+    await page.getByRole('button', { name: 'Save' }).click();
+    await page.getByRole('button', { name: 'Drop' }).click();
+    await page.getByRole('button', { name: 'Load' }).click();
+
+    await expect(page.locator('canvas')).toHaveScreenshot(`${name}-after.png`);
+  });
 });
 
-test('SpaceCustomModels', async ({ page }) => {
-  await page.goto(GAME_URL);
-
-  const spaceSelect: Locator = page.getByLabel('Spaces');
-  await expect(spaceSelect).toBeVisible();
-
-  await page.getByLabel('Spaces').selectOption('SpaceCustomModels');
-
-  await expect(page.locator('canvas')).toHaveScreenshot('SpaceCustomModels-before.png');
-
-  await page.getByRole('button', { name: 'Change' }).click();
-  await page.getByRole('button', { name: 'Save' }).click();
-  await page.getByRole('button', { name: 'Drop' }).click();
-  await page.getByRole('button', { name: 'Load' }).click();
-
-  await expect(page.locator('canvas')).toHaveScreenshot('SpaceCustomModels-after.png');
-});
+// test('SpaceCustomModels', async ({ page }) => {
+//   await page.goto(GAME_URL);
+//
+//   const spaceSelect: Locator = page.getByLabel('Spaces');
+//   await expect(spaceSelect).toBeVisible();
+//
+//   await page.getByLabel('Spaces').selectOption('SpaceCustomModels');
+//
+//   await expect(page.locator('canvas')).toHaveScreenshot('SpaceCustomModels-before.png');
+//
+//   await page.getByRole('button', { name: 'Change' }).click();
+//   await page.getByRole('button', { name: 'Save' }).click();
+//   await page.getByRole('button', { name: 'Drop' }).click();
+//   await page.getByRole('button', { name: 'Load' }).click();
+//
+//   await expect(page.locator('canvas')).toHaveScreenshot('SpaceCustomModels-after.png');
+// });
