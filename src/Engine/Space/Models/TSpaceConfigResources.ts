1245b841d4a7f4be7f966520ae3236f3c32fcc86
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:15:56 +0100

    [HEAVY WIP] schema: updated schema textures loading

--- src/Engine/Space/Models/TSpaceConfigResources.ts
+++ src/Engine/Space/Models/TSpaceConfigResources.ts
@@ -7,7 +7,6 @@ export type TSpaceConfigResources = Readonly<{
   envMapTextures: ReadonlyArray<TEnvMapResourceConfig>;
   // TODO 9.0.0. RESOURCES: Guess "materials" should use strings to use textures.
   materials: ReadonlyArray<TMaterialConfig>;
-  // TODO 9.0.0. RESOURCES: Would be nice if TModel3dConfig will extends from TAbstractResourceConfig
   models3d: ReadonlyArray<TModel3dResourceConfig>;
   textures: ReadonlyArray<TTextureResourceConfig>;
 }>;
