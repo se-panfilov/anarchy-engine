558678e2313dee6ab115234b56726bb9a8fce998
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:39:49 +0100

    Serialization: color

--- src/Engine/Color/Wrappers/ColorWrapper.ts
+++ src/Engine/Color/Wrappers/ColorWrapper.ts
@@ -6,6 +6,7 @@ import { colorToConfig } from '@/Engine/Color/Adapters';
 import type { TColor, TColorParams, TColorWrapper } from '@/Engine/Color/Models';
 import { isColorWrapper } from '@/Engine/Utils';
 
+// TODO 15-0-0: validate result
 export function ColorWrapper(color: TColorParams): TColorWrapper {
   const entity: TColor = new Color(isColorWrapper(color) ? color.entity : color);
   const result = Object.assign(AbstractWrapper(entity, WrapperType.Color), { entity, serialize: (): ColorRepresentation => colorToConfig(result) });
