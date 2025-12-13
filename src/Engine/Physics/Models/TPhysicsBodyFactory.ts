805029e6f7d6d3e0d57b0baba597adf6df2fd7a6
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:06:35 +0100

    fixes

--- src/Engine/Physics/Models/TPhysicsBodyFactory.ts
+++ src/Engine/Physics/Models/TPhysicsBodyFactory.ts
@@ -2,7 +2,7 @@ import type { TParamsFromConfig, TReactiveFactory } from '@/Engine/Abstract';
 import type { TDestroyable } from '@/Engine/Mixins';
 
 import type { TPhysicsBodyFacade } from './TPhysicsBodyFacade';
-import type { TPhysicsConfig } from './TPhysicsConfig';
+import type { TPhysicsPresetConfig } from './TPhysicsPresetConfig';
 import type { TPhysicsPresetParams } from './TPhysicsPresetParams';
 
-export type TPhysicsBodyFactory = TReactiveFactory<TPhysicsBodyFacade, TPhysicsPresetParams> & TParamsFromConfig<TPhysicsConfig, TPhysicsPresetParams> & TDestroyable;
+export type TPhysicsBodyFactory = TReactiveFactory<TPhysicsBodyFacade, TPhysicsPresetParams> & TParamsFromConfig<TPhysicsPresetConfig, TPhysicsPresetParams> & TDestroyable;
