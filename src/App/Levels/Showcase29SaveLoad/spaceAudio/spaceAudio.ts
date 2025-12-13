dbb11b76d59d479de19854e4b837b9a0e9f6ccca
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:48:31 +0100

    [E2E]: Audio

--- src/App/Levels/Showcase29SaveLoad/spaceAudio/spaceAudio.ts
+++ src/App/Levels/Showcase29SaveLoad/spaceAudio/spaceAudio.ts
@@ -1,6 +1,7 @@
 import { BehaviorSubject } from 'rxjs';
 
-import type { TSpace, TSpaceConfig } from '@/Engine';
+import type { TAudio3dWrapper, TSpace, TSpaceConfig } from '@/Engine';
+import { DebugAudioRenderer } from '@/Engine';
 
 import type { TSpacesData } from '../ShowcaseTypes';
 import { addModel3dToScene, getContainer } from '../utils';
@@ -15,6 +16,8 @@ export const spaceAudioData: TSpacesData = {
   awaits$: new BehaviorSubject<ReadonlySet<string>>(new Set()),
   onCreate: (space: TSpace): void | never => {
     addModel3dToScene(space, 'surface_model');
+    const gunshot1: TAudio3dWrapper = space.services.audioService.getRegistry().getByName('gunshot_1') as TAudio3dWrapper;
+    DebugAudioRenderer(gunshot1, space.services.scenesService.getActive(), space.loops.audioLoop);
   },
   onChange: (space: TSpace): void => {
     // space.services.actorService.getRegistry().getByName('sphere_actor').drive.default.setX(10);
