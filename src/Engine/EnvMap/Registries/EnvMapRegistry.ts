89a8e2b34e74d70280c6f63927332a41905a8f70
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:44:50 +0100

    [Serialization]: EnvMap

--- src/Engine/EnvMap/Registries/EnvMapRegistry.ts
+++ src/Engine/EnvMap/Registries/EnvMapRegistry.ts
@@ -1,19 +1,6 @@
 import { AbstractEntityRegistry, RegistryType } from '@/Engine/Abstract';
-import type { TEnvMapRegistry, TEnvMapResourceConfig, TEnvMapSerializeResourcesDependencies, TEnvMapWrapper } from '@/Engine/EnvMap/Models';
-import { isNotDefined } from '@/Engine/Utils';
+import type { TEnvMapRegistry, TEnvMapWrapper } from '@/Engine/EnvMap/Models';
 
 export function EnvMapRegistry(): TEnvMapRegistry {
-  const registry = Object.assign(AbstractEntityRegistry<TEnvMapWrapper>(RegistryType.EnvMap), {
-    // TODO 15-0-0: could we extract this function as a generic one?
-    serialize: ({ metaInfoRegistry }: TEnvMapSerializeResourcesDependencies): ReadonlyArray<TEnvMapResourceConfig> => {
-      return registry.map((_value: TEnvMapWrapper, key: string | undefined): TEnvMapResourceConfig => {
-        if (isNotDefined(key)) throw new Error(`[EnvMapAsyncRegistry]: Cannot serialize resource: key "${key}" is not found`);
-        const result: TEnvMapResourceConfig | undefined = metaInfoRegistry.findByKey(key);
-        if (isNotDefined(result)) throw new Error(`[EnvMapAsyncRegistry]: Cannot serialize resource: meta info is not found for the resource with name "${key}"`);
-        return result;
-      });
-    }
-  });
-
-  return registry;
+  return AbstractEntityRegistry<TEnvMapWrapper>(RegistryType.EnvMap);
 }
--- src/Engine/EnvMap/Registries/EnvMapTextureAsyncRegistry.ts
+++ src/Engine/EnvMap/Registries/EnvMapTextureAsyncRegistry.ts
@@ -1,4 +1,19 @@
 import { AbstractSimpleAsyncRegistry, RegistryType } from '@/Engine/Abstract';
-import type { TEnvMapTexture, TEnvMapTextureAsyncRegistry } from '@/Engine/EnvMap/Models';
+import type { TEnvMapResourceConfig, TEnvMapSerializeResourcesDependencies, TEnvMapTexture, TEnvMapTextureAsyncRegistry } from '@/Engine/EnvMap/Models';
+import { isNotDefined } from '@/Engine/Utils';
 
-export const EnvMapTextureAsyncRegistry = (): TEnvMapTextureAsyncRegistry => AbstractSimpleAsyncRegistry<TEnvMapTexture>(RegistryType.EnvMapTexture);
+export function EnvMapTextureAsyncRegistry(): TEnvMapTextureAsyncRegistry {
+  const registry = Object.assign(AbstractSimpleAsyncRegistry<TEnvMapTexture>(RegistryType.EnvMapTexture), {
+    // TODO 15-0-0: could we extract this function as a generic one?
+    serialize: ({ metaInfoRegistry }: TEnvMapSerializeResourcesDependencies): ReadonlyArray<TEnvMapResourceConfig> => {
+      return registry.map((_value: TEnvMapTexture, key: string | undefined): TEnvMapResourceConfig => {
+        if (isNotDefined(key)) throw new Error(`[EnvMapAsyncRegistry]: Cannot serialize resource: key "${key}" is not found`);
+        const result: TEnvMapResourceConfig | undefined = metaInfoRegistry.findByKey(key);
+        if (isNotDefined(result)) throw new Error(`[EnvMapAsyncRegistry]: Cannot serialize resource: meta info is not found for the resource with name "${key}"`);
+        return result;
+      });
+    }
+  });
+
+  return registry;
+}
