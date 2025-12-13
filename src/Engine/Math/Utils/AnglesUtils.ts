63ca234780b5bcd15ac80de4199dbc6172cf37e3
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:09:09 +0100

    [WIP] kinematic math fixes

--- src/Engine/Math/Utils/AnglesUtils.ts
+++ src/Engine/Math/Utils/AnglesUtils.ts
@@ -7,20 +7,20 @@ import type { TWithCoordsXYZ, TWithCoordsXYZW, TWithCoordsXZ } from '@/Engine/Mi
 // TODO (S.Panfilov) add unit tests
 export const degToRad = (degrees: number): Decimal => new Decimal(degrees).times(Math.PI).div(180);
 // TODO (S.Panfilov) add unit tests
-export const cos = (value: Decimal): Decimal => new Decimal(Decimal.cos(value));
+export const cos = (value: Decimal): Decimal => Decimal.cos(value);
 // TODO (S.Panfilov) add unit tests
-export const sin = (value: Decimal): Decimal => new Decimal(Decimal.sin(value));
+export const sin = (value: Decimal): Decimal => Decimal.sin(value);
 // TODO (S.Panfilov) add unit tests
 export const radiansToDegrees = (radians: number): Decimal => new Decimal(radians).times(180).div(Math.PI);
 
 // TODO (S.Panfilov) add unit tests
 export function getHorizontalAzimuth(center: TWithCoordsXZ, point: TWithCoordsXZ): number {
-  const dx: Decimal = new Decimal(point.x).minus(new Decimal(center.x));
-  const dz: Decimal = new Decimal(point.z).minus(new Decimal(center.z));
+  const dx: Decimal = new Decimal(point.x).minus(center.x);
+  const dz: Decimal = new Decimal(point.z).minus(center.z);
 
-  let azimuth: Decimal = Decimal.atan2(dz, dx).times(new Decimal(180).div(new Decimal(Math.PI)));
+  let azimuth: Decimal = Decimal.atan2(dz, dx).times(180).div(Math.PI);
 
-  if (azimuth.isNegative()) azimuth = azimuth.plus(new Decimal(360));
+  if (azimuth.isNegative()) azimuth = azimuth.plus(360);
 
   return azimuth.toNumber();
 }
@@ -32,7 +32,7 @@ export const getElevationFromDirection = (direction: Vector3): number => Math.at
 // TODO (S.Panfilov) add unit tests
 export const getDirectionFromLinearVelocity = (linearVelocity: Vector3): Vector3 => linearVelocity.clone().normalize();
 // TODO (S.Panfilov) add unit tests
-export const getSpeedFromLinearVelocity = (linearVelocity: Vector3): number => Math.sqrt(linearVelocity.x ** 2 + linearVelocity.y ** 2 + linearVelocity.z ** 2);
+export const getSpeedFromLinearVelocity = (linearVelocity: Vector3): number => linearVelocity.length();
 // TODO (S.Panfilov) add unit tests
 export const getLinearVelocity = (speed: number, azimuth: number, elevation: number): Vector3 =>
   new Vector3(speed * Math.cos(elevation) * Math.cos(azimuth), speed * Math.sin(elevation), speed * Math.cos(elevation) * Math.sin(azimuth));
@@ -43,17 +43,16 @@ export const getDirectionFromAngularVelocity = (angularVelocity: Vector3): Vecto
 
 // TODO (S.Panfilov) add unit tests
 export function get3DAzimuth(center: TWithCoordsXYZ, point: TWithCoordsXYZ): { azimuth: number; elevation: number } {
-  const dx: Decimal = new Decimal(point.x).minus(new Decimal(center.x));
-  const dy: Decimal = new Decimal(point.y).minus(new Decimal(center.y));
-  const dz: Decimal = new Decimal(point.z).minus(new Decimal(center.z));
+  const dx: Decimal = new Decimal(point.x).minus(center.x);
+  const dy: Decimal = new Decimal(point.y).minus(center.y);
+  const dz: Decimal = new Decimal(point.z).minus(center.z);
 
-  let azimuth: Decimal = Decimal.atan2(dz, dx).times(new Decimal(180).div(new Decimal(Math.PI)));
+  let azimuth: Decimal = Decimal.atan2(dz, dx).times(180).div(Math.PI);
 
-  if (azimuth.isNegative()) azimuth = azimuth.plus(new Decimal(360));
+  if (azimuth.isNegative()) azimuth = azimuth.plus(360);
 
   const horizontalDistance: Decimal = Decimal.sqrt(dx.pow(2).plus(dz.pow(2)));
-
-  const elevation: Decimal = Decimal.atan2(dy, horizontalDistance).times(new Decimal(180).div(new Decimal(Math.PI)));
+  const elevation: Decimal = Decimal.atan2(dy, horizontalDistance).times(180).div(Math.PI);
 
   return {
     azimuth: azimuth.toNumber(),
@@ -63,11 +62,10 @@ export function get3DAzimuth(center: TWithCoordsXYZ, point: TWithCoordsXYZ): { a
 
 // TODO (S.Panfilov) add unit tests
 export function degreesToEuler(degrees: TWithCoordsXYZ): TWithCoordsXYZ {
-  // TODO (S.Panfilov) can I use "degToRad" here?
   const radians = {
-    x: new Decimal(degrees.x).times(Decimal.acos(-1).div(180)),
-    y: new Decimal(degrees.y).times(Decimal.acos(-1).div(180)),
-    z: new Decimal(degrees.z).times(Decimal.acos(-1).div(180))
+    x: degToRad(degrees.x),
+    y: degToRad(degrees.y),
+    z: degToRad(degrees.z)
   };
 
   return {
@@ -79,35 +77,31 @@ export function degreesToEuler(degrees: TWithCoordsXYZ): TWithCoordsXYZ {
 
 // TODO (S.Panfilov) add unit tests
 export function degreesToQuaternion(degrees: TWithCoordsXYZ): TWithCoordsXYZW {
-  // TODO (S.Panfilov) can I use "degToRad" here?
   const radians = {
-    x: new Decimal(degrees.x).times(Decimal.acos(-1).div(180)),
-    y: new Decimal(degrees.y).times(Decimal.acos(-1).div(180)),
-    z: new Decimal(degrees.z).times(Decimal.acos(-1).div(180))
+    x: degToRad(degrees.x),
+    y: degToRad(degrees.y),
+    z: degToRad(degrees.z)
   };
 
   const euler: TEuler = new Euler(radians.x.toNumber(), radians.y.toNumber(), radians.z.toNumber());
   const quaternion: Quaternion = new Quaternion().setFromEuler(euler);
 
   return {
-    w: new Decimal(quaternion.w).toNumber(),
-    x: new Decimal(quaternion.x).toNumber(),
-    y: new Decimal(quaternion.y).toNumber(),
-    z: new Decimal(quaternion.z).toNumber()
+    w: quaternion.w,
+    x: quaternion.x,
+    y: quaternion.y,
+    z: quaternion.z
   };
 }
 
 // TODO (S.Panfilov) add unit tests
 export function quaternionToDegrees(quaternion: TWithCoordsXYZW): TWithCoordsXYZ {
-  const q: Quaternion = new Quaternion(new Decimal(quaternion.x).toNumber(), new Decimal(quaternion.y).toNumber(), new Decimal(quaternion.z).toNumber(), new Decimal(quaternion.w).toNumber());
-
+  const q = new Quaternion(quaternion.x, quaternion.y, quaternion.z, quaternion.w);
   const euler = new Euler().setFromQuaternion(q, 'XYZ');
 
-  const degrees = {
+  return {
     x: radiansToDegrees(euler.x).toNumber(),
     y: radiansToDegrees(euler.y).toNumber(),
     z: radiansToDegrees(euler.z).toNumber()
   };
-
-  return degrees;
 }
