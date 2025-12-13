fbe771b69baa3f0f19860e205e5513a9e54dd5c2
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:11:42 +0100

    [DESKTOP] get safe size

--- apps/showcases-desktop/src/Utils/DisplayUtils.ts
+++ apps/showcases-desktop/src/Utils/DisplayUtils.ts
@@ -1,9 +1,10 @@
+import { clamp } from '@Anarchy/Shared/Utils';
 import type { TResolution } from '@Showcases/Shared';
 import type { Display, Size } from 'electron';
 import { screen } from 'electron';
 
 export function detectResolution(): TResolution {
-  const primaryDisplay = screen.getPrimaryDisplay();
+  const primaryDisplay: Display = screen.getPrimaryDisplay();
   const { width, height } = primaryDisplay.size;
   return { width, height };
 }
@@ -13,12 +14,25 @@ export function getScreenRatio(): number {
   return width / height;
 }
 
-export function getDisplayInfo(display: Display = screen.getPrimaryDisplay()): Readonly<{ width: number; height: number; dpi: number }> {
+export function getDisplayInfo(display: Display = screen.getPrimaryDisplay()): TResolution & Readonly<{ scaleFactor: number }> {
   const workArea: Size = display.workAreaSize;
 
   return {
     width: workArea.width,
     height: workArea.height,
-    dpi: display.scaleFactor
+    scaleFactor: display.scaleFactor
+  };
+}
+
+export function getWindowSizeSafe(display: Display = screen.getPrimaryDisplay(), scale: number = 0.88, min: TResolution = { width: 320, height: 200 }): TResolution {
+  const { width, height } = getDisplayInfo(display);
+  const safeScale = clamp(scale, 0.5, 1.0);
+
+  const targetW: number = Math.floor(width * safeScale);
+  const targetH: number = Math.floor(height * safeScale);
+
+  return {
+    width: Math.min(width, Math.max(min.width, targetW)),
+    height: Math.min(height, Math.max(min.height, targetH))
   };
 }
