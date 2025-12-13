7b47564fd09cdca1f47c43ad229fd73a0799e269
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:45:44 +0100

    [E2E]: Actors with fsm

--- e2e/save-load.spec.ts
+++ e2e/save-load.spec.ts
@@ -14,7 +14,7 @@ test.beforeEach(async ({ page }) => {
   await waitUntilReady(page);
 });
 
-const scenes: ReadonlyArray<string> = ['SpaceBasic', 'SpaceCustomModels', 'SpaceTexts', 'SpaceLight', 'SpaceMaterials', 'SpaceOrbitControls', 'SpaceFpsControls'];
+const scenes: ReadonlyArray<string> = ['SpaceBasic', 'SpaceCustomModels', 'SpaceTexts', 'SpaceLight', 'SpaceMaterials', 'SpaceOrbitControls', 'SpaceFpsControls', 'SpaceActor'];
 
 test.describe('Space save/load persistence', () => {
   const thresholds = {
