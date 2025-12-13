160de1838a77e267806227b81a7367433843bd7b
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:23:42 +0100

    [ENGINE] refactor names

--- packages/anarchy-engine/src/Keyboard/Utils/KeysRxJsUtils.ts
+++ packages/anarchy-engine/src/Keyboard/Utils/KeysRxJsUtils.ts
@@ -5,13 +5,13 @@ import type { Observable, OperatorFunction } from 'rxjs';
 import { filter } from 'rxjs';
 
 export const onKeysCombo = (keys: ReadonlyArray<TGameKey>): OperatorFunction<TKeyEvent, TKeyEvent> => {
-  return (source: Observable<TKeyEvent>): Observable<TKeyEvent> => source.pipe(filter((event: TKeyEvent): boolean => isKeysPressed(keys, event.keys)));
+  return (source: Observable<TKeyEvent>): Observable<TKeyEvent> => source.pipe(filter((keyEvent: TKeyEvent): boolean => isKeysPressed(keys, keyEvent.keys)));
 };
 
 export const onKey = (key: TGameKey): OperatorFunction<TKeyEvent, TKeyEvent> => {
-  return (source: Observable<TKeyEvent>): Observable<TKeyEvent> => source.pipe(filter((event: TKeyEvent): boolean => hasKey(key, event.keys)));
+  return (source: Observable<TKeyEvent>): Observable<TKeyEvent> => source.pipe(filter((keyEvent: TKeyEvent): boolean => hasKey(key, keyEvent.keys)));
 };
 
 export const onKeyReleased = (key: TGameKey): OperatorFunction<TKeyEvent, TKeyEvent> => {
-  return (source: Observable<TKeyEvent>): Observable<TKeyEvent> => source.pipe(filter((event: TKeyEvent): boolean => isKeyInEvent(key, event) && event.released === key));
+  return (source: Observable<TKeyEvent>): Observable<TKeyEvent> => source.pipe(filter((keyEvent: TKeyEvent): boolean => isKeyInEvent(key, keyEvent) && keyEvent.released === key));
 };
--- packages/anarchy-engine/src/Keyboard/Utils/KeysUtils.ts
+++ packages/anarchy-engine/src/Keyboard/Utils/KeysUtils.ts
@@ -3,9 +3,9 @@ import { isDefined } from '@Anarchy/Shared/Utils';
 
 export const hasKey = (key: TGameKey, keys: TKeysCombo): boolean => keys.has(key);
 export const hasNoKey = (key: TGameKey, keys: TKeysCombo): boolean => !hasKey(key, keys);
-export const isKeyInEvent = (key: TGameKey, event: TKeyEvent | undefined): boolean => Boolean(event && event.event?.code === key);
-export const isPressEvent = (event: TKeyEvent): boolean => isDefined(event.pressed);
-export const isReleaseEvent = (event: TKeyEvent): boolean => isDefined(event.released);
+export const isKeyInEvent = (key: TGameKey, keyEvent: TKeyEvent | undefined): boolean => Boolean(keyEvent && keyEvent.event?.code === key);
+export const isPressEvent = (keyEvent: TKeyEvent): boolean => isDefined(keyEvent.pressed);
+export const isReleaseEvent = (keyEvent: TKeyEvent): boolean => isDefined(keyEvent.released);
 
 export function isKeysPressed(keys: ReadonlyArray<TGameKey>, pressedKeys: TKeysCombo): boolean {
   // eslint-disable-next-line functional/no-loop-statements
