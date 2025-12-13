7ded04c30f4fbe8a6b71f68e1627a77a702f4ecb
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:05:20 +0100

    fixes

--- src/Engine/Physics/Utils/PhysicsObjectMoveUtils.ts
+++ src/Engine/Physics/Utils/PhysicsObjectMoveUtils.ts
@@ -1,16 +1,18 @@
 import type { RigidBody } from '@dimforge/rapier3d';
 
+import type { TWithCoordsXYZ } from '@/Engine/Mixins';
 import { VelocityType } from '@/Engine/Physics/Constants';
 import type { TVector3Wrapper } from '@/Engine/Vector';
 
-export function movePhysicsDynamicObjectByVelocity(rigidBody: RigidBody, type: VelocityType, vector3W: TVector3Wrapper): void | never {
+export function movePhysicsDynamicObjectByVelocity(rigidBody: RigidBody, type: VelocityType, vector3W: TVector3Wrapper, shouldWakeUp: boolean = true): void | never {
   if (!rigidBody.isDynamic()) throw new Error(`Cannot move physics object with velocity: rigid body is not dynamic`);
 
+  const coords: TWithCoordsXYZ = vector3W.getCoords();
   switch (type) {
     case VelocityType.Linear:
-      return rigidBody.setLinvel(vector3W.getCoords(), true);
+      return rigidBody.setLinvel(coords, shouldWakeUp);
     case VelocityType.Angular:
-      return rigidBody.setAngvel(vector3W.getCoords(), true);
+      return rigidBody.setAngvel(coords, shouldWakeUp);
     default:
       throw new Error(`Cannot move physics object with velocity: velocity type is invalid: ${type}`);
   }
