b98f1461ea5def70d52df7360e1915127a5b0977
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:10:38 +0100

    [MENU] fixes for MD render

--- packages/showcases-menu/src/components/MdRenderer.vue
+++ packages/showcases-menu/src/components/MdRenderer.vue
@@ -1,19 +1,33 @@
 <script setup lang="ts">
 import { isNotDefined } from '@Anarchy/Shared/Utils';
-// TODO DESKTOP: LEGAL: marked should be lazy loaded
-import { marked } from 'marked';
+import { reactive, watch } from 'vue';
 
-defineProps<{
+const props = defineProps<{
   content: string | undefined;
 }>();
 
-function parseContent(content: string | undefined): string {
+const state = reactive({
+  parsedContent: undefined as string | undefined
+});
+
+async function parseContent(content: string | undefined): Promise<string> {
   if (isNotDefined(content)) return '';
+  const { marked } = await import('marked');
   // const clean = DOMPurify.sanitize(dirty, { USE_PROFILES: { html: true } });
-  return marked.parse(content, { async: false });
+  return marked.parse(content);
 }
+
+watch(
+  () => props.content,
+  async (newContent) => {
+    const parsed = await parseContent(newContent);
+    state.parsedContent = parsed;
+  },
+  { immediate: true }
+);
 </script>
 
 <template>
-  <div class="md-renderer" v-html="parseContent(content)" />
+  <!--  // eslint-disable-next-line vue/no-v-htm-->
+  <div class="md-renderer" v-html="state.parsedContent" />
 </template>
