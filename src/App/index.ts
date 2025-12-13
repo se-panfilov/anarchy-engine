5176dae0fe2cf7cdc902a94847d46a578dfbd1de
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:57:48 +0100

    fix

--- src/App/index.ts
+++ src/App/index.ts
@@ -1,7 +1,7 @@
 import '@/App/style.css';
 
 // import { showcaseLevel } from '@/App/Levels/Showcase1MovingActors'; // Moving actor with intersections & reading data from config
-import { showcaseLevel } from '@/App/Levels/Showcase2TopDown'; // Top-down controls with dynamic actors and camera creation
+// import { showcaseLevel } from '@/App/Levels/Showcase2TopDown'; // Top-down controls with dynamic actors and camera creation
 // import { showcaseLevel } from '@/App/Levels/Showcase3CameraFlying'; // Camera flying around the central actor
 // import { showcaseLevel } from '@/App/Levels/Showcase4AnimejsSimple'; // Anime.js simple animations (easing, etc.)
 // import { showcaseLevel } from '@/App/Levels/Showcase5AnimejsComplex'; // Anime.js animation with complex path and easing
@@ -9,7 +9,7 @@ import { showcaseLevel } from '@/App/Levels/Showcase2TopDown'; // Top-down contr
 // import { showcaseLevel } from '@/App/Levels/Showcase7Text2d'; // Text 2d
 // import { showcaseLevel } from '@/App/Levels/Showcase8Text3d'; // Text 3d
 // import { showcaseLevel } from '@/App/Levels/ShowcaseLevel9TexturesAndMaterials'; // Textures & Materials
-// import { showcaseLevel } from '@/App/Levels/ShowcaseLevel10ComplexMaterials'; // Textures & Materials
+import { showcaseLevel } from '@/App/Levels/ShowcaseLevel10ComplexMaterials'; // Textures & Materials
 import type { IAppCanvas } from '@/Engine';
 import { ambientContext, isNotDefined } from '@/Engine';
 
