3d765c8b7dcd270839b62a3cda02900eddc31bd1
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:37:07 +0100

    Registry: fixed creation

--- src/Engine/Models3d/Registries/Model3dRawToModel3dConnectionRegistry.ts
+++ src/Engine/Models3d/Registries/Model3dRawToModel3dConnectionRegistry.ts
@@ -4,23 +4,25 @@ import type { TRawModel3d } from '@/Engine/Models3d';
 import type { TModel3d, TModel3dRawToModel3dConnectionRegistry, TModel3dRawToModel3dConnectionRegistryExtension } from '@/Engine/Models3d/Models';
 import type { TWriteable } from '@/Engine/Utils';
 
-const registry: TWriteable<TAbstractSimpleRegistry<string>> & TWriteable<TModel3dRawToModel3dConnectionRegistryExtension> = AbstractSimpleRegistry<string>(
-  RegistryType.Models3dRawToModels3dToConnection
-) as TWriteable<TAbstractSimpleRegistry<string>> & TWriteable<TModel3dRawToModel3dConnectionRegistryExtension>;
+export const Model3dRawToModel3dConnectionRegistry = (): TModel3dRawToModel3dConnectionRegistry => {
+  const registry: TWriteable<TAbstractSimpleRegistry<string>> & TWriteable<TModel3dRawToModel3dConnectionRegistryExtension> = AbstractSimpleRegistry<string>(
+    RegistryType.Models3dRawToModels3dToConnection
+  ) as TWriteable<TAbstractSimpleRegistry<string>> & TWriteable<TModel3dRawToModel3dConnectionRegistryExtension>;
 
-// eslint-disable-next-line functional/immutable-data
-registry.addModel3d = (model3dRaw: TRawModel3d, model3d: TModel3d): void => registry.add(model3dRaw.uuid, model3d.id);
-// eslint-disable-next-line functional/immutable-data
-registry.findByModel3d = (model3dRaw: TRawModel3d): string | undefined => registry.findByKey(model3dRaw.uuid);
-// eslint-disable-next-line functional/immutable-data
-registry.setByModel3d = (model3dRaw: TRawModel3d, model3d: TModel3d): void => registry.add(model3dRaw.uuid, model3d.id);
-// eslint-disable-next-line functional/immutable-data
-registry.removeByModel3d = (model3dRaw: TRawModel3d, shouldIgnoreRemoved: boolean): void => {
-  if (shouldIgnoreRemoved) {
-    if (registry.findByModel3d(model3dRaw)) return registry.remove(model3dRaw.uuid);
-    return undefined;
-  }
-  return registry.remove(model3dRaw.uuid);
+  // eslint-disable-next-line functional/immutable-data
+  registry.addModel3d = (model3dRaw: TRawModel3d, model3d: TModel3d): void => registry.add(model3dRaw.uuid, model3d.id);
+  // eslint-disable-next-line functional/immutable-data
+  registry.findByModel3d = (model3dRaw: TRawModel3d): string | undefined => registry.findByKey(model3dRaw.uuid);
+  // eslint-disable-next-line functional/immutable-data
+  registry.setByModel3d = (model3dRaw: TRawModel3d, model3d: TModel3d): void => registry.add(model3dRaw.uuid, model3d.id);
+  // eslint-disable-next-line functional/immutable-data
+  registry.removeByModel3d = (model3dRaw: TRawModel3d, shouldIgnoreRemoved: boolean): void => {
+    if (shouldIgnoreRemoved) {
+      if (registry.findByModel3d(model3dRaw)) return registry.remove(model3dRaw.uuid);
+      return undefined;
+    }
+    return registry.remove(model3dRaw.uuid);
+  };
+
+  return registry;
 };
-
-export const Model3dRawToModel3dConnectionRegistry = (): TModel3dRawToModel3dConnectionRegistry => registry;
