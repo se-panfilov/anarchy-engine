6b15262e8c8bba404c8696fea4760aa852d0aabb
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:19:02 +0100

    removed extra

--- src/Engine/Actor/Models/TActorWrapper.ts
+++ src/Engine/Actor/Models/TActorWrapper.ts
@@ -4,7 +4,7 @@ import type { Euler, Vector3 } from 'three';
 import type { TWrapper } from '@/Engine/Abstract';
 import type { TWithCollisions } from '@/Engine/Collisions';
 import type { TWithKinematic } from '@/Engine/Kinematic';
-import type { TMovable3dXYZ, TRotatable, TScalable, TWithTagsMixin } from '@/Engine/Mixins';
+import type { TMovable3dXYZ, TRotatable, TScalable } from '@/Engine/Mixins';
 import type { TModel3dFacade } from '@/Engine/Models3d';
 import type { TWithOptionalPhysicsBody } from '@/Engine/Physics';
 import type { TWithSpatial, TWithUpdateSpatialCell } from '@/Engine/Spatial/Models';
@@ -18,7 +18,6 @@ export type TActorWrapper = TWrapper<TModel3dFacade> &
   TWithSpatial &
   TWithCollisions &
   TWithUpdateSpatialCell &
-  TWithTagsMixin &
   Readonly<{
     position$: Observable<Vector3>;
     rotation$: Observable<Euler>;
