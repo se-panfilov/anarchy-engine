1c672607a96b3cac89a31d7100d4656644d4e2dc
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:50:40 +0100

    fixes for launcher

--- src/Engine/Launcher/SceneLauncher/SceneLauncher.ts
+++ src/Engine/Launcher/SceneLauncher/SceneLauncher.ts
@@ -1,76 +1,109 @@
 import type { IAppCanvas } from '@Engine/Domains/App';
-import type { ICameraWrapper } from '@Engine/Domains/Camera';
-import { CameraTag } from '@Engine/Domains/Camera';
+import type { ICameraConfig, ICameraFactory, ICameraParams, ICameraRegistry, ICameraWrapper } from '@Engine/Domains/Camera';
+import { CameraFactory, CameraRegistry, CameraTag } from '@Engine/Domains/Camera';
 import type { ILoopWrapper } from '@Engine/Domains/Loop';
-import { LoopTag } from '@Engine/Domains/Loop';
+import { LoopFactory, LoopTag } from '@Engine/Domains/Loop';
 import type { IRendererWrapper } from '@Engine/Domains/Renderer';
-import { RendererTag } from '@Engine/Domains/Renderer';
+import { RendererFactory, RendererTag } from '@Engine/Domains/Renderer';
 import type { ISceneConfig, ISceneWrapper } from '@Engine/Domains/Scene';
+import { SceneFactory } from '@Engine/Domains/Scene';
 import type { ILaunchedScene, ISceneLauncher } from '@Engine/Launcher';
 import { isNotDefined, isValidSceneConfig } from '@Engine/Utils';
 import { BehaviorSubject } from 'rxjs';
+import type { IActorConfig, IActorFactory, IActorParams, IActorRegistry, IActorWrapper } from '@/Engine/Domains/Actor';
+import { ActorFactory, ActorRegistry } from '@/Engine/Domains/Actor';
+import type { ILightConfig, ILightFactory, ILightParams, ILightRegistry, ILightWrapper } from '@/Engine/Domains/Light';
+import { LightFactory, LightRegistry } from '@/Engine/Domains/Light';
+import type { IControlsConfig, IControlsFactory, IControlsParams, IControlsRegistry, IControlsWrapper } from '@/Engine/Domains/Controls';
+import { ControlsFactory, ControlsRegistry } from '@/Engine/Domains/Controls';
 
 // TODO (S.Panfilov) CWP All factories (especially wrapper's) don't care about entity registration, but they should
 // Registries should be created automatically and dynamically added to pools
 
 export function SceneLauncher(): ISceneLauncher {
-  const prepared$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   const launched$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   const destroyed$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-  let registryPool: IRegistryPool;
-  let localFactoriesPool: ILocalFactoryPool;
-  let localFactories: IDestroyableFactories;
-  let registries: IRegistries;
   let scene: ISceneWrapper;
-  let renderer: IRendererWrapper;
-  let loop: ILoopWrapper;
 
-  function prepare(canvas: IAppCanvas): void {
-    registryPool = RegistryPool();
-    registries = registryPool.pool;
-    localFactoriesPool = LocalFactoriesPool({ canvas, cameraRegistry: registries.cameraRegistry });
-    localFactories = localFactoriesPool.pool;
-    prepared$.next(true);
-  }
-
-  function launch(sceneConfig: ISceneConfig, canvas: IAppCanvas, factories: IFactories): ILaunchedScene {
+  // TODO (S.Panfilov) CWP implement new scene launcher
+  function launch(sceneConfig: ISceneConfig, canvas: IAppCanvas): ILaunchedScene {
     if (!isValidSceneConfig(sceneConfig)) throw new Error('Failed to launch a scene: invalid data format');
-    const { name: sceneName, actors, cameras, lights, controls, tags: sceneTags } = sceneConfig;
-    if (!prepared$.value) prepare(canvas);
+    const { name: sceneName, actors, cameras, lights, controls, tags } = sceneConfig;
 
-    const { actorFactory, cameraFactory, lightFactory, rendererFactory, sceneFactory, loopFactory } = factories;
-    const { actorRegistry, cameraRegistry, lightRegistry, controlsRegistry } = registries;
-    const { controlsFactory } = localFactories;
-    scene = sceneFactory.create({ name: sceneName, tags: sceneTags });
+    // TODO (S.Panfilov) launch() should be a method of a scene, extract this outside
+    scene = SceneFactory().create({ name: sceneName, tags });
 
-    registryPool.startAddSubscription(scene);
+    const actorFactory: IActorFactory = ActorFactory();
+    const actorRegistry: IActorRegistry = ActorRegistry();
+    // TODO (S.Panfilov) rewrite with subscribe registry to factory.created?
+    actors.forEach((actor: IActorConfig): void => {
+      const params: IActorParams = actorFactory.getParams(actor);
+      const instance: IActorWrapper = actorFactory.create(params);
+      actorRegistry.add(instance);
+    });
 
-    addFromConfigToRegistry(actors, actorFactory, actorRegistry);
-    addFromConfigToRegistry(cameras, cameraFactory, cameraRegistry);
-    addFromConfigToRegistry(lights, lightFactory, lightRegistry);
-    addFromConfigToRegistry(controls, controlsFactory, controlsRegistry);
+    const cameraFactory: ICameraFactory = CameraFactory();
+    const cameraRegistry: ICameraRegistry = CameraRegistry();
+    cameras.forEach((camera: ICameraConfig): void => {
+      const params: ICameraParams = cameraFactory.getParams(camera);
+      const instance: ICameraWrapper = cameraFactory.create(params);
+      cameraRegistry.add(instance);
+    });
 
-    const renderer: IRendererWrapper = rendererFactory.create({ canvas, tags: [RendererTag.Main] });
+    const lightFactory: ILightFactory = LightFactory();
+    const lightRegistry: ILightRegistry = LightRegistry();
+    lights.forEach((light: ILightConfig): void => {
+      const params: ILightParams = lightFactory.getParams(light);
+      const instance: ILightWrapper = lightFactory.create(params);
+      lightRegistry.add(instance);
+    });
 
-    const loop: ILoopWrapper = loopFactory.create({ tags: [LoopTag.Main] });
+    const controlsFactory: IControlsFactory = ControlsFactory();
+    const controlsRegistry: IControlsRegistry = ControlsRegistry();
+    controls.forEach((control: IControlsConfig): void => {
+      const params: IControlsParams = controlsFactory.getParams(control);
+      const instance: IControlsWrapper = controlsFactory.create(params);
+      controlsRegistry.add(instance);
+    });
+
+    const renderer: IRendererWrapper = RendererFactory().create({ canvas, tags: [RendererTag.Main] });
+
+    const loop: ILoopWrapper = LoopFactory().create({ tags: [LoopTag.Main] });
+
+    destroyed$.subscribe(() => {
+      launched$.complete();
+
+      actorFactory.destroy();
+      actorRegistry.destroy();
+
+      cameraFactory.destroy();
+      cameraRegistry.destroy();
+
+      lightFactory.destroy();
+      lightRegistry.destroy();
+
+      controlsFactory.destroy();
+      controlsRegistry.destroy();
+
+      loop.destroy();
+      renderer.destroy();
+      scene.destroy();
+
+      destroyed$.unsubscribe();
+      destroyed$.complete();
+    });
+
     const initialCamera: ICameraWrapper | undefined = cameraRegistry.getUniqByTag(CameraTag.Initial);
     if (isNotDefined(initialCamera)) throw new Error(`Cannot start the main loop for the scene ${sceneName}: initial camera is not defined`);
     loop.start(renderer, scene, initialCamera);
 
     launched$.next(true);
-    return { loop, renderer, registries, factories: localFactories };
+    return { loop, renderer };
   }
 
   function destroy(): void {
-    prepared$.complete();
-    launched$.complete();
-    destroyed$.complete();
-    registryPool.destroy();
-    localFactoriesPool.destroy();
-    loop.destroy();
-    renderer.destroy();
-    scene.destroy();
+    destroyed$.next(true);
   }
 
   return { prepare, launch, destroy, prepared$, launched$, destroyed$ };
