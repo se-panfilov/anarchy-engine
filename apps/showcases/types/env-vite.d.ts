17de02953e15c6f1abca3bc1bb62da51e03eb598
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:59:54 +0100

    [App] .env fixes

--- apps/showcases/types/env-vite.d.ts
+++ apps/showcases/types/env-vite.d.ts
@@ -7,12 +7,12 @@ interface ViteTypeOptions {
 }
 
 interface ImportMetaEnv {
-  VITE_BUILD_COMPRESSION: boolean;
-  VITE_BUILD_MINIFIED: boolean;
-  VITE_BUILD_SOURCEMAPS: boolean;
+  VITE_BUILD_COMPRESSION: string; // should be cast to boolean
+  VITE_BUILD_MINIFIED: string; // should be cast to boolean
+  VITE_BUILD_SOURCEMAPS: string; // should be cast to boolean
   VITE_BUILD_TARGET_DIR: string;
-  VITE_APP_SHOW_DEBUG_INFO: string;
-  VITE_APP_SHOW_DEV_NAV: string;
+  VITE_APP_SHOW_DEBUG_INFO: string; // should be cast to boolean
+  VITE_APP_SHOW_DEV_NAV: string; // should be cast to boolean
   // VITE_APP_DRACO_DECODER_PATH: string;
 }
 
