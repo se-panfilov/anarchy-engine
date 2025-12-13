cb36b7ba30427a3dbb24ceeedc352eb13cec7a5a
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:32:31 +0100

    Audio: debug renderer

--- src/Engine/Audio/Utils/DebugAudioRenderer.ts
+++ src/Engine/Audio/Utils/DebugAudioRenderer.ts
@@ -10,29 +10,28 @@ import { isDefined } from '@/Engine/Utils';
 
 export function DebugAudioRenderer(source: TAudio3dWrapper, sceneW: TSceneWrapper, loop: TLoop): TDebugAudioRenderer {
   const enabled$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
-  const refDistance: number = source.entity.getRefDistance();
+  // const refDistance: number = source.entity.getRefDistance();
   const maxDistance: number = source.entity.getMaxDistance();
 
-  const refGeometry = new SphereGeometry(refDistance, 16, 16);
-  const refMaterial = new MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-  const refSphere = new Mesh(refGeometry, refMaterial);
-  sceneW.entity.add(refSphere);
+  // const refGeometry = new SphereGeometry(refDistance, 8, 8);
+  // const refMaterial = new MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+  // const refSphere = new Mesh(refGeometry, refMaterial);
+  // sceneW.entity.add(refSphere);
 
-  const maxGeometry = new SphereGeometry(maxDistance, 16, 16);
+  const maxGeometry = new SphereGeometry(maxDistance, 8, 8);
   const maxMaterial = new MeshBasicMaterial({ color: 0xff0000, wireframe: true, opacity: 0.5, transparent: true });
   const maxSphere = new Mesh(maxGeometry, maxMaterial);
   sceneW.entity.add(maxSphere);
 
   enabled$.subscribe((isEnabled: boolean): void => {
-    // eslint-disable-next-line functional/immutable-data
-    refSphere.visible = isEnabled;
+    // refSphere.visible = isEnabled;
     // eslint-disable-next-line functional/immutable-data
     maxSphere.visible = isEnabled;
   });
 
   enabled$.pipe(switchMap((isEnabled: boolean): Observable<TMilliseconds> => (isEnabled ? loop.tick$ : EMPTY))).subscribe((): void => {
     if (isDefined(source.entity.parent)) {
-      refSphere.position.copy(source.entity.parent.position);
+      // refSphere.position.copy(source.entity.parent.position);
       maxSphere.position.copy(source.entity.parent.position);
     }
   });
