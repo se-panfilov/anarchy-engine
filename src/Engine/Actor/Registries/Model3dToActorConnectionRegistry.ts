562244b87d49a4a51c24cf944cd777b4f7f34a9d
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:37:06 +0100

    Registry: fixed creation

--- src/Engine/Actor/Registries/Model3dToActorConnectionRegistry.ts
+++ src/Engine/Actor/Registries/Model3dToActorConnectionRegistry.ts
@@ -4,17 +4,19 @@ import type { TActor, TModel3dToActorConnectionRegistry, TModel3dToActorConnecti
 import type { TModel3d } from '@/Engine/Models3d';
 import type { TWriteable } from '@/Engine/Utils';
 
-const registry: TWriteable<TAbstractSimpleRegistry<string>> & TWriteable<TModel3dToActorConnectionRegistryExtension> = AbstractSimpleRegistry<string>(
-  RegistryType.Models3dToActorConnection
-) as TWriteable<TAbstractSimpleRegistry<string>> & TWriteable<TModel3dToActorConnectionRegistryExtension>;
+export const Model3dToActorConnectionRegistry = (): TModel3dToActorConnectionRegistry => {
+  const registry: TWriteable<TAbstractSimpleRegistry<string>> & TWriteable<TModel3dToActorConnectionRegistryExtension> = AbstractSimpleRegistry<string>(
+    RegistryType.Models3dToActorConnection
+  ) as TWriteable<TAbstractSimpleRegistry<string>> & TWriteable<TModel3dToActorConnectionRegistryExtension>;
 
-// eslint-disable-next-line functional/immutable-data
-registry.addModel3d = (model3d: TModel3d, actor: TActor): void => registry.add(model3d.id, actor.id);
-// eslint-disable-next-line functional/immutable-data
-registry.findByModel3d = (model3d: TModel3d): string | undefined => registry.findByKey(model3d.id);
-// eslint-disable-next-line functional/immutable-data
-registry.setByModel3d = (model3d: TModel3d, actor: TActor): void => registry.add(model3d.id, actor.id);
-// eslint-disable-next-line functional/immutable-data
-registry.removeByModel3d = (model3d: TModel3d): void => registry.remove(model3d.id);
+  // eslint-disable-next-line functional/immutable-data
+  registry.addModel3d = (model3d: TModel3d, actor: TActor): void => registry.add(model3d.id, actor.id);
+  // eslint-disable-next-line functional/immutable-data
+  registry.findByModel3d = (model3d: TModel3d): string | undefined => registry.findByKey(model3d.id);
+  // eslint-disable-next-line functional/immutable-data
+  registry.setByModel3d = (model3d: TModel3d, actor: TActor): void => registry.add(model3d.id, actor.id);
+  // eslint-disable-next-line functional/immutable-data
+  registry.removeByModel3d = (model3d: TModel3d): void => registry.remove(model3d.id);
 
-export const Model3dToActorConnectionRegistry = (): TModel3dToActorConnectionRegistry => registry;
+  return registry;
+};
