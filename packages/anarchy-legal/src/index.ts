a8bb4796974b9b27ab5d6cba679ba565c92d3eb6
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:08:11 +0100

    [LEGAL] todo

--- packages/anarchy-legal/src/index.ts
+++ packages/anarchy-legal/src/index.ts
@@ -3,13 +3,8 @@ export type * from './Models';
 export * from './Services';
 
 // TODO DESKTOP: LEGAL: Finish CRA (all steps)
-// TODO DESKTOP: LEGAL: Adjust templates for legal docs (no extras, all fields are filled)
-// TODO DESKTOP: LEGAL: Add .anarchy-legal.config.json to every package
-// TODO DESKTOP: LEGAL: Generate legal docs for every package
 // TODO DESKTOP: LEGAL: Include legal docs in the build (desktop)
-// TODO DESKTOP: LEGAL: Include legal docs in the npm packages
 // TODO DESKTOP: LEGAL: Display EULA/License, etc via menu
-// TODO DESKTOP: LEGAL: Make template for LICENSE (and update the text)
 
 // TODO DESKTOP: LEGAL: make sure we're matching the following:
 //  - GDPR/UK-GDPR
