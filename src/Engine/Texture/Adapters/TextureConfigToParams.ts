d25b0ed298cfe5bb8484c4dcf974296d36375212
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:15:17 +0100

    [HEAVY WIP] factory for textures

--- src/Engine/Texture/Adapters/TextureConfigToParams.ts
+++ src/Engine/Texture/Adapters/TextureConfigToParams.ts
@@ -1,5 +1,12 @@
-import type { TTexturePackParams, TTextureResourceConfig } from '@/Engine/Texture/Models';
+import type { TTexture, TTextureConfig, TTextureConfigToParamsDependencies, TTextureParams } from '@/Engine/Texture/Models';
+import { isNotDefined } from '@/Engine/Utils';
 
-export function configToParams(config: TTextureResourceConfig): TTexturePackParams {
-  return { ...config };
+export function configToParams(config: TTextureConfig, { resourcesRegistry }: TTextureConfigToParamsDependencies): TTextureParams | never {
+  const texture: TTexture | undefined = resourcesRegistry.findByKey(config.name);
+  if (isNotDefined(texture)) throw new Error(`Texture creation from config: Texture with name "${config.name}" not found`);
+
+  return {
+    ...config,
+    texture
+  };
 }
--- src/Engine/Texture/Factories/TextureFactory.ts
+++ src/Engine/Texture/Factories/TextureFactory.ts
@@ -1,11 +1,11 @@
 import type { TAsyncReactiveFactory, TCreateAsyncEntityFactoryFn } from '@/Engine/Abstract';
 import { AsyncReactiveFactory, FactoryType } from '@/Engine/Abstract';
 import { configToParams } from '@/Engine/Texture/Adapters';
-import type { TTextureDependencies, TTextureFactory, TTextureLoadedPack, TTexturePackParams } from '@/Engine/Texture/Models';
+import type { TTextureDependencies, TTextureFactory, TTextureLoadedPack, TTexturePackParams, TTextureParams, TTextureWrapper } from '@/Engine/Texture/Models';
 
 const createTexture = (pack: TTexturePackParams, { load }: TTextureDependencies): Promise<TTextureLoadedPack> => load(pack);
 
-const factory: TAsyncReactiveFactory<TTextureLoadedPack, TTexturePackParams> = {
-  ...AsyncReactiveFactory(FactoryType.Texture, createTexture as TCreateAsyncEntityFactoryFn<TTextureLoadedPack, TTexturePackParams>)
+const factory: TAsyncReactiveFactory<TTextureWrapper, TTextureParams> = {
+  ...AsyncReactiveFactory(FactoryType.Texture, createTexture as TCreateAsyncEntityFactoryFn<TTextureWrapper, TTextureParams>)
 };
 export const TextureFactory = (): TTextureFactory => ({ ...factory, configToParams });
