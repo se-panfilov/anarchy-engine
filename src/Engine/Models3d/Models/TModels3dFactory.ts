e1d537c02baca76090195fc18f6c4771bf5a60e7
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:16:26 +0100

    [HEAVY WIP] models3d: fixes

--- src/Engine/Models3d/Models/TModels3dFactory.ts
+++ src/Engine/Models3d/Models/TModels3dFactory.ts
@@ -1,4 +1,3 @@
-import type { TModels3dServiceDependencies } from '@/Engine';
 import type { TParamsFromConfigWithDependencies, TReactiveFactoryWithDependencies } from '@/Engine/Abstract';
 import type { TDestroyable } from '@/Engine/Mixins';
 
@@ -6,7 +5,8 @@ import type { TModel3dConfig } from './TModel3dConfig';
 import type { TModel3dConfigToParamsDependencies } from './TModel3dConfigToParamsDependencies';
 import type { TModel3dFacade } from './TModel3dFacade';
 import type { TModel3dParams } from './TModel3dParams';
+import type { TModels3dServiceDependencies } from './TModels3dServiceDependencies';
 
-export type TModels3dFactory = TReactiveFactoryWithDependencies<TModel3dFacade, TModel3dParams, TModels3dServiceDependencies> &
+export type TModels3dFactory = TReactiveFactoryWithDependencies<TModel3dFacade, TModel3dParams, Pick<TModels3dServiceDependencies, 'animationsService'>> &
   TParamsFromConfigWithDependencies<TModel3dConfig, TModel3dParams, TModel3dConfigToParamsDependencies> &
   TDestroyable;
