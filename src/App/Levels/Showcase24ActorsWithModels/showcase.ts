308091fa24444a9097f36160ef734d57c69ea865
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:13:51 +0100

    better scene

--- src/App/Levels/Showcase24ActorsWithModels/showcase.ts
+++ src/App/Levels/Showcase24ActorsWithModels/showcase.ts
@@ -1,5 +1,7 @@
+import { CameraHelper } from 'three';
+
 import type { TShowcase } from '@/App/Levels/Models';
-import type { TAppCanvas, TEngine, TModel3dLoadOptions, TSpace, TSpaceConfig, TVector3Wrapper } from '@/Engine';
+import type { TAppCanvas, TDirectionalLightWrapper, TEngine, TModel3dLoadOptions, TSpace, TSpaceConfig, TVector3Wrapper } from '@/Engine';
 import { buildSpaceFromConfig, Engine, Vector3Wrapper } from '@/Engine';
 
 import spaceConfig from './showcase.json';
@@ -7,7 +9,7 @@ import spaceConfig from './showcase.json';
 export function showcase(canvas: TAppCanvas): TShowcase {
   const space: TSpace = buildSpaceFromConfig(canvas, spaceConfig as TSpaceConfig);
   const engine: TEngine = Engine(space);
-  const { models3dService } = space.services;
+  const { scenesService, models3dService, lightService } = space.services;
 
   async function init(): Promise<void> {
     const scale: TVector3Wrapper = Vector3Wrapper({ x: 1, y: 1, z: 1 });
@@ -15,6 +17,10 @@ export function showcase(canvas: TAppCanvas): TShowcase {
 
     await Promise.all(models3dService.loadAsync([{ url: '/Showcase/Models/Solder/Solder.glb', name: 'actor_1', scale, position: Vector3Wrapper({ x: 0, y: 0, z: 0 }), options }]));
   }
+
+  // TODO debug light
+  const dirLightW: TDirectionalLightWrapper = lightService.getRegistry().findByName('dir_light') as unknown as TDirectionalLightWrapper;
+  scenesService.findActive()?.entity.add(new CameraHelper(dirLightW.entity.shadow.camera));
 
   // TODO debug camera coors
   // setInterval(() => {
