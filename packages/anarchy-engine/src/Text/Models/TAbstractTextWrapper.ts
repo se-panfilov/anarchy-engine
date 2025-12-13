8b3017ecbb0da6c85cbe2864c12b2865c5e10e01
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:13:35 +0100

    [ENGINE]: Text wrapper own translations

--- packages/anarchy-engine/src/Text/Models/TAbstractTextWrapper.ts
+++ packages/anarchy-engine/src/Text/Models/TAbstractTextWrapper.ts
@@ -1,6 +1,15 @@
 import type { TWithObject3d, TWithTags } from '@Anarchy/Engine/Mixins';
+import type { TTextTranslationService } from '@Anarchy/Engine/Text/Models';
 import type { TWithTransformDrive } from '@Anarchy/Engine/TransformDrive';
 
 import type { TTextTransformAgents } from './TTextTransformAgents';
 
-export type TAbstractTextWrapper = Readonly<{ getElement: () => HTMLElement }> & TWithTransformDrive<TTextTransformAgents> & TWithObject3d & TWithTags;
+export type TAbstractTextWrapper = Readonly<{
+  getElement: () => HTMLElement;
+  setTranslationService: (translationService: TTextTranslationService) => void;
+  setTextKey: (textKey: string) => void;
+  getTextKey: () => string | undefined;
+}> &
+  TWithTransformDrive<TTextTransformAgents> &
+  TWithObject3d &
+  TWithTags;
--- packages/anarchy-engine/src/Text/Wrappers/TextWrapper.ts
+++ packages/anarchy-engine/src/Text/Wrappers/TextWrapper.ts
@@ -3,27 +3,66 @@ import { AbstractWrapper } from '@Anarchy/Engine/Abstract';
 import { withObject3d } from '@Anarchy/Engine/Mixins';
 import { textToConfig } from '@Anarchy/Engine/Text/Adapters';
 import { RelatedEntityAttribute, TextCssClass, TextType } from '@Anarchy/Engine/Text/Constants';
-import type { TTextConfig, TTextParams, TTextServiceDependencies, TTextTransformDrive, TTextWrapper } from '@Anarchy/Engine/Text/Models';
+import type { TTextConfig, TTextParams, TTextServiceDependencies, TTextTransformDrive, TTextTranslationService, TTextWrapper } from '@Anarchy/Engine/Text/Models';
 import { TextTransformDrive } from '@Anarchy/Engine/Text/TransformDrive';
 import { getCssAccessors } from '@Anarchy/Engine/Text/Wrappers/Accessors';
 import { applyHtmlElementParams, getWrapperTypeByTextType } from '@Anarchy/Engine/Text/Wrappers/TextWrapperHelper';
 import type { TDriveToTargetConnector } from '@Anarchy/Engine/TransformDrive';
 import { DriveToTargetConnector } from '@Anarchy/Engine/TransformDrive';
 import { applyCenter, applyObject3dParams } from '@Anarchy/Engine/Utils';
-import { isDefined } from '@Anarchy/Shared/Utils';
 import type { Subscription } from 'rxjs';
+import { distinctUntilChanged } from 'rxjs';
+import { isDefined, isNotDefined } from '@Anarchy/Shared/Utils';
 import { CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer';
 import { CSS3DObject } from 'three/examples/jsm/renderers/CSS3DRenderer';
 
 export function createTextWrapper<T extends CSS2DObject | CSS3DObject>(params: TTextParams, type: TextType, dependencies: TTextServiceDependencies): TTextWrapper<T> {
   let element: HTMLElement = document.createElement(params.elementType || 'div');
   // eslint-disable-next-line functional/immutable-data
-  element.textContent = params.text;
+  element.textContent = params.text ?? '';
+  let textKey: string | undefined = params.textKey;
   const entity: T = createText(type, element) as T;
+  let textTranslationService: TTextTranslationService | undefined;
 
   const wrapper: TAbstractWrapper<T> = AbstractWrapper(entity, getWrapperTypeByTextType(type), params);
   const drive: TTextTransformDrive = TextTransformDrive(params, dependencies, wrapper.id);
   const driveToTargetConnector: TDriveToTargetConnector = DriveToTargetConnector(drive, entity);
+
+  let keyChangeSub$: Subscription | undefined;
+
+  function subscribeKeyChange(textKey: string): Subscription {
+    if (isNotDefined(textTranslationService)) throw new Error(`[TextWrapper]: Translation service is not defined. Wrapper Id: ${wrapper.id}, name: "${wrapper.name}".`);
+    return (
+      textTranslationService
+        .t$(textKey)
+        .pipe(distinctUntilChanged())
+        // eslint-disable-next-line functional/immutable-data
+        .subscribe((value: string): void => void (element.textContent = value))
+    );
+  }
+
+  function setTranslationService(translationService: TTextTranslationService): void {
+    textTranslationService = translationService;
+    if (isNotDefined(textKey) || textKey === '') return;
+    keyChangeSub$ = subscribeKeyChange(textKey);
+  }
+
+  function setText(text: string): void {
+    if (isDefined(keyChangeSub$)) keyChangeSub$?.unsubscribe();
+    if (isDefined(textKey)) {
+      textKey = undefined;
+      console.warn(`[TextWrapper]: Text updated, translation disabled. Wrapper Id: ${wrapper.id}, name: "${wrapper.name}".`);
+    }
+
+    // eslint-disable-next-line functional/immutable-data
+    element.textContent = text;
+  }
+
+  function setTextKey(newTextKey: string): void {
+    if (isDefined(keyChangeSub$)) keyChangeSub$?.unsubscribe();
+    textKey = newTextKey;
+    keyChangeSub$ = subscribeKeyChange(textKey);
+  }
 
   // eslint-disable-next-line functional/immutable-data
   const result: TTextWrapper<T> = Object.assign(wrapper, {
@@ -31,8 +70,12 @@ export function createTextWrapper<T extends CSS2DObject | CSS3DObject>(params: T
     drive,
     driveToTargetConnector,
     ...getCssAccessors(element),
+    setText,
+    setTextKey,
+    getTextKey: (): string | undefined => textKey,
     ...withObject3d(entity),
     getElement: (): HTMLElement => element,
+    setTranslationService,
     serialize: (): TTextConfig => textToConfig(result)
   });
 
@@ -40,6 +83,7 @@ export function createTextWrapper<T extends CSS2DObject | CSS3DObject>(params: T
 
   const destroySub$: Subscription = result.destroy$.subscribe((): void => {
     destroySub$.unsubscribe();
+    keyChangeSub$?.unsubscribe();
 
     // eslint-disable-next-line functional/immutable-data
     element.className = '';
@@ -55,7 +99,7 @@ export function createTextWrapper<T extends CSS2DObject | CSS3DObject>(params: T
 
   document.body.appendChild(element);
 
-  result.setText(params.text);
+  if (params.text && isNotDefined(params.textKey)) result.setText(params.text);
   if (isDefined(params.cssProps)) applyHtmlElementParams(result, params.cssProps, type === TextType.Text2d ? TextCssClass.Text2d : TextCssClass.Text3d);
   applyObject3dParams(result, params);
   if (type === TextType.Text2d) applyCenter(entity as CSS2DObject, params.center);
