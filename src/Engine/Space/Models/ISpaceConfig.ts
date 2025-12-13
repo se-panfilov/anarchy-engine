f52c0d080ab935d393b8b9d304ac2698dba78bc3
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:03:14 +0100

    [WIP] added particles to json config

--- src/Engine/Space/Models/ISpaceConfig.ts
+++ src/Engine/Space/Models/ISpaceConfig.ts
@@ -5,6 +5,7 @@ import type { IFogConfig } from '@/Engine/Fog';
 import type { IIntersectionsWatcherConfig } from '@/Engine/Intersections';
 import type { IAnyLightConfig } from '@/Engine/Light';
 import type { IWithReadonlyTags } from '@/Engine/Mixins';
+import type { IParticlesConfig } from '@/Engine/Particles';
 import type { ISceneConfig } from '@/Engine/Scene';
 import type { SpaceSchemaVersion } from '@/Engine/Space/Constants';
 import type { ITextConfig } from '@/Engine/Text';
@@ -17,6 +18,7 @@ export type ISpaceConfig = Readonly<{
   cameras: ReadonlyArray<ICameraConfig>;
   intersections: ReadonlyArray<IIntersectionsWatcherConfig>;
   lights: ReadonlyArray<IAnyLightConfig>;
+  particles: ReadonlyArray<IParticlesConfig>;
   fogs: ReadonlyArray<IFogConfig>;
   texts: ReadonlyArray<ITextConfig>;
   controls: ReadonlyArray<IControlsConfig>;
