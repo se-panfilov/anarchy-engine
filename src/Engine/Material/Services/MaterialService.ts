983749002872fe185634e3196d238bc7ab16c0dc
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:14:48 +0100

    [WIP] fixes

--- src/Engine/Material/Services/MaterialService.ts
+++ src/Engine/Material/Services/MaterialService.ts
@@ -8,11 +8,6 @@ export function MaterialService(factory: TMaterialFactory, registry: TMaterialRe
   factory.entityCreated$.subscribe((wrapper: TMaterialWrapper): void => registry.add(wrapper));
 
   const create = (params: TMaterialParams): TMaterialWrapper => factory.create(params);
-  // TODO 9.0.0. RESOURCES: This could work as is, but let's think of what we want to change:
-  // TODO 9.0.0. RESOURCES: In one hand we could leave "url" here and just try to load texture for material every time
-  // TODO 9.0.0. RESOURCES: (it's not a problem, since we return the loaded texture with that URL)
-  // TODO 9.0.0. RESOURCES: But why we want to call load() from separate place? Could be hard to understand the code and it's a mix of responsibilities.
-  // TODO 9.0.0. RESOURCES: Better, to requre to load all textures before creating materials, and add the texture name to the material config/params, but this requires some refactoring.
   const createFromConfig = (material: ReadonlyArray<TMaterialConfig>): void => material.forEach((config: TMaterialConfig): TMaterialWrapper => factory.create(factory.configToParams(config)));
 
   const destroyable: TDestroyable = destroyableMixin();
--- src/Engine/MaterialTexturePack/Services/MaterialTextureService.ts
+++ src/Engine/MaterialTexturePack/Services/MaterialTextureService.ts
@@ -3,9 +3,11 @@ import type { TMaterialPackParams, TMaterialTexturePack, TMaterialTextureService
 import type { TTextureUploaded } from '@/Engine/Texture';
 
 export function MaterialTextureService(materialService: TMaterialService, { textureService }: TMaterialTextureServiceDependencies): TMaterialTextureService {
+  // TODO 9.0.0. RESOURCES: This method should be removed (prefer) or refactored: textures are loaded before materials,
+  //  and when create a material with a texture, the texture should be taken from a registry
   const createAsync = (pack: TMaterialPackParams<TMaterialTexturePack>): Promise<TMaterialWrapper> => {
     return textureService
-      .load(pack)
+      .loadMaterialPack(pack)
       .all()
       .then((textures: TTextureUploaded) => {
         const params: TMaterialProps = pack.params ?? ({} as TMaterialProps);
