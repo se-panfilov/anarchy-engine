b219483c0b6a45509c44ce006978e5f6116595b8
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:55:46 +0100

    better wrapper

--- src/Engine/Domains/Abstract/Wrapper/AbstractWrapper.ts
+++ src/Engine/Domains/Abstract/Wrapper/AbstractWrapper.ts
@@ -4,10 +4,20 @@ import type { WrapperType } from '@/Engine/Domains/Abstract';
 import type { IWrapper } from '@/Engine/Domains/Abstract/Models';
 import { destroyableMixin } from '@/Engine/Mixins';
 import { withTags } from '@/Engine/Mixins/Generic/WithTags';
+import { isDefined } from '@/Engine/Utils';
 
 export function AbstractWrapper<T>(entity: T, type: WrapperType | string, params?: Readonly<{ tags: ReadonlyArray<string> }>): IWrapper<T> {
+  const id: string = type + '_' + nanoid();
+
+  if (isDefined((entity as any).name)) {
+    // TODO (S.Panfilov) log somehow
+    if ((entity as any).name !== '' || typeof (entity as any).name !== 'string') console.warn('Entity name is not empty or not a string: ', (entity as any).name);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    (entity as any).name = id;
+  }
+
   return {
-    id: type + '_' + nanoid(),
+    id,
     entity,
     ...withTags(params ? params.tags : []),
     ...destroyableMixin()
