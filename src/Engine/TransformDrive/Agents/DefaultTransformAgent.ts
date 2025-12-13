151b30ae7abdea395b44a82928f25bad52d59b05
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:23:25 +0100

    [WIP] DefaultTransformAgent: fixes

--- src/Engine/TransformDrive/Agents/DefaultTransformAgent.ts
+++ src/Engine/TransformDrive/Agents/DefaultTransformAgent.ts
@@ -29,11 +29,129 @@ export function DefaultTransformAgent(params: TTransformAgentParams): TDefaultTr
   const proxyRotationObj: TWithRotationProperty = updateSubjOnChange(rotationObj, 'rotation', abstractTransformAgent.rotation$, (value: TReadonlyEuler): TReadonlyEuler => value.clone());
   const proxyScaleObj: TWithScaleProperty = updateSubjOnChange(scaleObj, 'scale', abstractTransformAgent.scale$, (value: TReadonlyVector3): TReadonlyVector3 => value.clone()) as TWithScaleProperty;
 
+  const { getX, getZ, getY } = withMoveBy3dMixin(proxyPositionObj);
+  const { getScaleX, getScaleY, getScaleZ } = withScaleMixin(proxyScaleObj);
+  const { getRotationX, getRotationY, getRotationZ } = withRotationByXyzMixin(proxyRotationObj);
+
   return {
     ...abstractTransformAgent,
-    ...withMoveBy3dMixin(proxyPositionObj),
-    ...withScaleMixin(proxyScaleObj),
-    ...withRotationByXyzMixin(proxyRotationObj),
+    getX,
+    getY,
+    getZ,
+    getScaleX,
+    getScaleY,
+    getScaleZ,
+    getRotationX,
+    getRotationY,
+    getRotationZ,
+    setX: (x: number): number => {
+      // eslint-disable-next-line functional/immutable-data
+      proxyPositionObj.position.x = x;
+      abstractTransformAgent.position$.next(proxyPositionObj.position);
+      return x;
+    },
+    setY: (y: number): number => {
+      // eslint-disable-next-line functional/immutable-data
+      proxyPositionObj.position.y = y;
+      abstractTransformAgent.position$.next(proxyPositionObj.position);
+      return y;
+    },
+    setZ: (z: number): number => {
+      // eslint-disable-next-line functional/immutable-data
+      proxyPositionObj.position.z = z;
+      abstractTransformAgent.position$.next(proxyPositionObj.position);
+      return z;
+    },
+    addX: (x: number): number => {
+      // eslint-disable-next-line functional/immutable-data
+      proxyPositionObj.position.x += x;
+      abstractTransformAgent.position$.next(proxyPositionObj.position);
+      return proxyPositionObj.position.x;
+    },
+    addY: (y: number): number => {
+      // eslint-disable-next-line functional/immutable-data
+      proxyPositionObj.position.y += y;
+      abstractTransformAgent.position$.next(proxyPositionObj.position);
+      return proxyPositionObj.position.y;
+    },
+    addZ: (z: number): number => {
+      // eslint-disable-next-line functional/immutable-data
+      proxyPositionObj.position.z += z;
+      abstractTransformAgent.position$.next(proxyPositionObj.position);
+      return proxyPositionObj.position.z;
+    },
+    setRotationX: (x: number): number => {
+      // eslint-disable-next-line functional/immutable-data
+      proxyRotationObj.rotation.x = x;
+      abstractTransformAgent.rotation$.next(proxyRotationObj.rotation);
+      return x;
+    },
+    setRotationY: (y: number): number => {
+      // eslint-disable-next-line functional/immutable-data
+      proxyRotationObj.rotation.y = y;
+      abstractTransformAgent.rotation$.next(proxyRotationObj.rotation);
+      return y;
+    },
+    setRotationZ: (z: number): number => {
+      // eslint-disable-next-line functional/immutable-data
+      proxyRotationObj.rotation.z = z;
+      abstractTransformAgent.rotation$.next(proxyRotationObj.rotation);
+      return z;
+    },
+    adjustRotationByX: (x: number): number => {
+      // eslint-disable-next-line functional/immutable-data
+      proxyRotationObj.rotation.x += x;
+      abstractTransformAgent.rotation$.next(proxyRotationObj.rotation);
+      return proxyRotationObj.rotation.x;
+    },
+    adjustRotationByY: (y: number): number => {
+      // eslint-disable-next-line functional/immutable-data
+      proxyRotationObj.rotation.y += y;
+      abstractTransformAgent.rotation$.next(proxyRotationObj.rotation);
+      return proxyRotationObj.rotation.y;
+    },
+    adjustRotationByZ: (z: number): number => {
+      // eslint-disable-next-line functional/immutable-data
+      proxyRotationObj.rotation.z += z;
+      abstractTransformAgent.rotation$.next(proxyRotationObj.rotation);
+      return proxyRotationObj.rotation.z;
+    },
+    setScaleX: (x: number): number => {
+      // eslint-disable-next-line functional/immutable-data
+      proxyScaleObj.scale.x = x;
+      abstractTransformAgent.scale$.next(proxyScaleObj.scale);
+      return x;
+    },
+    setScaleY: (y: number): number => {
+      // eslint-disable-next-line functional/immutable-data
+      proxyScaleObj.scale.y = y;
+      abstractTransformAgent.scale$.next(proxyScaleObj.scale);
+      return y;
+    },
+    setScaleZ: (z: number): number => {
+      // eslint-disable-next-line functional/immutable-data
+      proxyScaleObj.scale.z = z;
+      abstractTransformAgent.scale$.next(proxyScaleObj.scale);
+      return z;
+    },
+    adjustScaleByX: (x: number): number => {
+      // eslint-disable-next-line functional/immutable-data
+      proxyScaleObj.scale.x += x;
+      abstractTransformAgent.scale$.next(proxyScaleObj.scale);
+      return proxyScaleObj.scale.x;
+    },
+    adjustScaleByY: (y: number): number => {
+      // eslint-disable-next-line functional/immutable-data
+      proxyScaleObj.scale.y += y;
+      abstractTransformAgent.scale$.next(proxyScaleObj.scale);
+      return proxyScaleObj.scale.y;
+    },
+    adjustScaleByZ: (z: number): number => {
+      // eslint-disable-next-line functional/immutable-data
+      proxyScaleObj.scale.z += z;
+      abstractTransformAgent.scale$.next(proxyScaleObj.scale);
+      return proxyScaleObj.scale.z;
+    },
     setPosition: (position: Vector3): void => abstractTransformAgent.position$.next(position),
     setRotation: (rotation: Euler): void => abstractTransformAgent.rotation$.next(rotation),
     setScale: (scale: Vector3): void => abstractTransformAgent.scale$.next(scale)
