e15733b9a8e627ae277710f4e52fa9db8737f92c
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:21:32 +0100

    [GUI] added icons

--- packages/showcases-gui/src/views/Bottom.vue
+++ packages/showcases-gui/src/views/Bottom.vue
@@ -2,6 +2,7 @@
 import ActionButton from '@Showcases/GUI/components/ActionButton.vue';
 import ValueBar from '@Showcases/GUI/components/ValueBar.vue';
 import { vueTranslationService } from '@Showcases/i18n';
+import { Backpack, Heart, Map, Settings, Shield, Sword, Zap } from 'lucide-vue-next';
 import type { ShallowRef } from 'vue';
 
 const { $t } = vueTranslationService;
@@ -19,55 +20,34 @@ const buttonSettingsTitle: ShallowRef<string> = $t('gui.bottom.button.settings.t
     <div class="panel -bottom">
       <div class="game-hud">
         <ValueBar :title="valueEnergyTitle" :current="75" :max="100" color="blue">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2" />
-          </svg>
+          <Zap />
         </ValueBar>
 
         <div class="action-buttons">
           <ActionButton :title="buttonAttackTitle" data-key="LMB">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-              <polyline points="14.5 17.5 3 6 3 3 6 3 17.5 14.5" />
-              <line x1="13" x2="19" y1="19" y2="13" />
-              <line x1="16" x2="20" y1="16" y2="20" />
-              <line x1="19" x2="21" y1="21" y2="19" />
-            </svg>
+            <Sword />
           </ActionButton>
 
           <ActionButton :title="buttonDefendTitle" data-key="RMB">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z" />
-            </svg>
+            <Shield />
           </ActionButton>
 
           <ActionButton :title="buttonInventoryTitle" data-key="I">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M20 7h-9" />
-              <path d="M14 17H5" />
-              <circle cx="17" cy="17" r="3" />
-              <circle cx="7" cy="7" r="3" />
-            </svg>
+            <Backpack />
           </ActionButton>
 
           <ActionButton :title="buttonMapTitle" data-key="M">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-              <polygon points="3 6 9 3 15 6 21 3 21 18 15 21 9 18 3 21" />
-              <line x1="9" x2="9" y1="3" y2="18" />
-              <line x1="15" x2="15" y1="6" y2="21" />
-            </svg>
+            <Map />
           </ActionButton>
 
           <ActionButton :title="buttonSettingsTitle" data-key="Esc">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-              <path
-                d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"
-              />
-              <circle cx="12" cy="12" r="3" />
-            </svg>
+            <Settings />
           </ActionButton>
         </div>
 
-        <ValueBar :title="valueHealthTitle" :current="85" :max="100" color="red" />
+        <ValueBar :title="valueHealthTitle" :current="85" :max="100" color="red">
+          <Heart />
+        </ValueBar>
       </div>
     </div>
   </div>
