5964fc71dd774b145e30a359242750f40d0213ac
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:55:09 +0100

    fixes

--- src/Engine/Domains/Texture/Models/IMaterialTexturePack.ts
+++ src/Engine/Domains/Texture/Models/IMaterialTexturePack.ts
@@ -14,16 +14,16 @@ import type {
 } from './IMaterialPackKeys';
 import type { ITexturePackParams } from './ITexturePackParams';
 
-export type IBasicMaterialTexturePack = Readonly<{ [key in IBasicMaterialPackKeys]?: ITexturePackParams }> & Readonly<{ material: MaterialType.Basic }>;
-export type IDepthMaterialTexturePack = Readonly<{ [key in IDepthMaterialPackKeys]?: ITexturePackParams }> & Readonly<{ material: MaterialType.Depth }>;
-export type IDistanceMaterialTexturePack = Readonly<{ [key in IDistanceMaterialPackKeys]?: ITexturePackParams }> & Readonly<{ material: MaterialType.Distance }>;
-export type INormalMaterialTexturePack = Readonly<{ [key in INormalMaterialPackKeys]?: ITexturePackParams }> & Readonly<{ material: MaterialType.Normal }>;
-export type IMatcapMaterialTexturePack = Readonly<{ [key in IMatcapMaterialPackKeys]?: ITexturePackParams }> & Readonly<{ material: MaterialType.Matcap }>;
-export type ILamberMaterialTexturePack = Readonly<{ [key in ILamberMaterialPackKeys]?: ITexturePackParams }> & Readonly<{ material: MaterialType.Lamber }>;
-export type IPhongMaterialTexturePack = Readonly<{ [key in IPhongMaterialPackKeys]?: ITexturePackParams }> & Readonly<{ material: MaterialType.Phong }>;
-export type IPhysicalMaterialTexturePack = Readonly<{ [key in IPhysicalMaterialPackKeys]?: ITexturePackParams }> & Readonly<{ material: MaterialType.Physical }>;
-export type IToonMaterialTexturePack = Readonly<{ [key in IToonMaterialPackKeys]?: ITexturePackParams }> & Readonly<{ material: MaterialType.Toon }>;
-export type IStandardMaterialTexturePack = Readonly<{ [key in IStandardMaterialPackKeys]?: ITexturePackParams }> & Readonly<{ material: MaterialType.Standard }>;
+export type IBasicMaterialTexturePack = Readonly<{ [key in IBasicMaterialPackKeys]?: ITexturePackParams } & { material: MaterialType.Basic }>;
+export type IDepthMaterialTexturePack = Readonly<{ [key in IDepthMaterialPackKeys]?: ITexturePackParams } & { material: MaterialType.Depth }>;
+export type IDistanceMaterialTexturePack = Readonly<{ [key in IDistanceMaterialPackKeys]?: ITexturePackParams } & { material: MaterialType.Distance }>;
+export type INormalMaterialTexturePack = Readonly<{ [key in INormalMaterialPackKeys]?: ITexturePackParams } & { material: MaterialType.Normal }>;
+export type IMatcapMaterialTexturePack = Readonly<{ [key in IMatcapMaterialPackKeys]?: ITexturePackParams } & { material: MaterialType.Matcap }>;
+export type ILamberMaterialTexturePack = Readonly<{ [key in ILamberMaterialPackKeys]?: ITexturePackParams } & { material: MaterialType.Lamber }>;
+export type IPhongMaterialTexturePack = Readonly<{ [key in IPhongMaterialPackKeys]?: ITexturePackParams } & { material: MaterialType.Phong }>;
+export type IPhysicalMaterialTexturePack = Readonly<{ [key in IPhysicalMaterialPackKeys]?: ITexturePackParams } & { material: MaterialType.Physical }>;
+export type IToonMaterialTexturePack = Readonly<{ [key in IToonMaterialPackKeys]?: ITexturePackParams } & { material: MaterialType.Toon }>;
+export type IStandardMaterialTexturePack = Readonly<{ [key in IStandardMaterialPackKeys]?: ITexturePackParams } & { material: MaterialType.Standard }>;
 
 export type IMaterialTexturePack =
   | IBasicMaterialTexturePack
