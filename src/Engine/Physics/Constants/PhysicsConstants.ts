036c7dce7c82f3ad1218ad908109aed770ca8454
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:05:26 +0100

    gravity const

---
+++ src/Engine/Physics/Constants/PhysicsConstants.ts
@@ -0,0 +1,3 @@
+import type { TWithCoordsXYZ } from '@/Engine/Mixins';
+
+export const STANDARD_GRAVITY: TWithCoordsXYZ = { x: 0.0, y: -9.81, z: 0.0 };
--- src/Engine/Physics/Constants/index.ts
+++ src/Engine/Physics/Constants/index.ts
@@ -1,3 +1,4 @@
+export * from './PhysicsConstants';
 export * from './RigidBodyTypeNames';
 export * from './RigidBodyTypesMap';
 export * from './VelocityType';
