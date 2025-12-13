4c3f8351856860686fc91ff26f47076e91f005c1
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:49:32 +0100

    [Serialization]: Physics

--- src/Engine/Physics/Adapters/PhysicsBodyConfigToParams.ts
+++ src/Engine/Physics/Adapters/PhysicsBodyConfigToParams.ts
@@ -4,15 +4,18 @@ import { Vector3 } from 'three/src/math/Vector3';
 import type { TPhysicsBodyConfig, TPhysicsBodyParams } from '@/Engine/Physics/Models';
 import { isPhysicsBodyParamsComplete } from '@/Engine/Physics/Utils';
 import type { TOptional } from '@/Engine/Utils';
+import { isDefined } from '@/Engine/Utils';
 
 export function configToParamsBody(config: TPhysicsBodyConfig): TPhysicsBodyParams | never {
-  const { position, rotation, ...rest } = config;
+  const { position, rotation, angularVelocity, linearVelocity, ...rest } = config;
 
   const result: TPhysicsBodyParams | TOptional<TPhysicsBodyParams> = {
     // type: RigidBodyTypesMap[RigidBodyTypesNames[type]],
     ...rest,
     position: new Vector3(position.x, position.y, position.z),
-    rotation: new Quaternion().setFromEuler(new Euler(rotation.x, rotation.y, rotation.z, rotation.order))
+    rotation: new Quaternion().setFromEuler(new Euler(rotation.x, rotation.y, rotation.z, rotation.order)),
+    angularVelocity: isDefined(angularVelocity) ? new Vector3(angularVelocity.x, angularVelocity.y, angularVelocity.z) : undefined,
+    linearVelocity: isDefined(linearVelocity) ? new Vector3(linearVelocity.x, linearVelocity.y, linearVelocity.z) : undefined
   };
 
   if (!isPhysicsBodyParamsComplete(result)) throw new Error('Cannot create physic body: params are lacking of mandatory fields');
--- src/Engine/Physics/Models/TPhysicsBodyConfig.ts
+++ src/Engine/Physics/Models/TPhysicsBodyConfig.ts
@@ -4,8 +4,10 @@ import type { TEulerLike } from '@/Engine/ThreeLib';
 
 import type { TPhysicsBodyParams } from './TPhysicsBodyParams';
 
-export type TPhysicsBodyConfig = Omit<TPhysicsBodyParams, 'position' | 'rotation'> &
+export type TPhysicsBodyConfig = Omit<TPhysicsBodyParams, 'position' | 'rotation' | 'angularVelocity' | 'linearVelocity'> &
   Readonly<{
     position: Vector3Like;
     rotation: TEulerLike;
+    angularVelocity?: Vector3Like;
+    linearVelocity?: Vector3Like;
   }>;
