4f93fe9eb7f5287d30065299f61c64addd984327
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:16:41 +0100

    [HEAVY WIP] fixes

--- src/Engine/Actor/Adapters/ActrorConfigAdapter.ts
+++ src/Engine/Actor/Adapters/ActrorConfigAdapter.ts
@@ -8,11 +8,8 @@ import { isNotDefined } from '@/Engine/Utils';
 export function configToParams(config: TActorConfig, dependencies: TActorConfigToParamsDependencies): TActorParams {
   const { position, rotation, scale, physics, spatial, model3dEntity, ...rest } = config;
 
-  // TODO 9.0.0. RESOURCES: Failing here, because of primitive models creation.
-  //  But at this moment all models should be created and added to the registry.
-  //  So, check what's inside the registry (and why it's not there).
   const model3d: TModel3dFacade | undefined = dependencies.models3dService.getRegistry().findByName(model3dEntity);
-  if (isNotDefined(model3d)) throw new Error(`Actor. ConfigToParams: Model3d "${model3d}" not found, while actor initialization`);
+  if (isNotDefined(model3d)) throw new Error(`Actor. ConfigToParams: Model3d "${model3dEntity}" not found, while actor initialization`);
 
   return {
     ...rest,
