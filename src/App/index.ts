777c9bdb4f80a0ac6e80c686a8509d9ecbcc6a94
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:49:08 +0100

    fixed intersections

--- src/App/index.ts
+++ src/App/index.ts
@@ -2,31 +2,26 @@ import '@App/style.css';
 
 import sceneConfig from '@App/Scenes/debug-scene.config.json';
 
-import type { IActorWrapper, ICameraWrapper, ILaunchedEngine, ILaunchedScene, IRegistries, IVector3 } from '@/Engine';
-import { ActorTag, ambientContext, CameraTag, isNotDefined, launchEngine, SceneLauncher } from '@/Engine';
-import { IntersectionsWatcher } from '@Engine/Watchers/IntersectionsWatcher';
+import type { IActorWrapper, ICameraWrapper, IIntersectionsWatcher, ILaunchedEngine, ILaunchedScene, IRegistries, IVector3 } from '@/Engine';
+import { ActorTag, ambientContext, CameraTag, IntersectionsWatcherFactory, isNotDefined, launchEngine, SceneLauncher } from '@/Engine';
 
 const { factories, canvas }: ILaunchedEngine = launchEngine('#app');
 const { registries }: ILaunchedScene = SceneLauncher().launch(sceneConfig, canvas, factories);
 const { actorRegistry, cameraRegistry }: IRegistries = registries;
-
-// TODO (S.Panfilov) UNDER CONSTRUCTION: intersections START///////////////////////////////////////////
-const intersectionsService: IIntersectionsService = IntersectionsWatcher();
 
 const clickableActors: ReadonlyArray<IActorWrapper> = actorRegistry.getAllWithEveryTag([ActorTag.Intersectable]);
 const cameraTag: CameraTag = CameraTag.Initial;
 const camera: ICameraWrapper | undefined = cameraRegistry.getUniqByTag(cameraTag);
 
 if (isNotDefined(camera)) throw new Error(`Cannot init intersection service: camera with tag "${cameraTag}" is not defined`);
+const intersectionsWatcher: IIntersectionsWatcher = IntersectionsWatcherFactory().create({ actors: clickableActors, camera, positionWatcher: ambientContext.mousePositionWatcher });
 
-const intersectionsWatcher: IIntersectionsWatcher = intersectionsService.getWatcher(clickableActors, camera, ambientContext.mousePositionWatcher, ambientContext.mouseClicksWatcher);
-
-intersectionsWatcher.value$((obj: IVector3): void => {
+intersectionsWatcher.value$.subscribe((obj: IVector3): void => {
   console.log('intersect obj', obj);
 });
+
+intersectionsWatcher.start();
 
 ambientContext.mouseClicksWatcher.value$.subscribe((): void => {
   console.log('int click:');
 });
-// TODO (S.Panfilov) UNDER CONSTRUCTION: intersections END/////////////////////////////////////////////
-////////////////////////////////////
