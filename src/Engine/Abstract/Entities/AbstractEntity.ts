6d2432e0f82c6b3af6d6d18bd7acf44928cb7f42
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:24:19 +0100

    [WIP] abstract: forced entities to have an id

--- src/Engine/Abstract/Entities/AbstractEntity.ts
+++ src/Engine/Abstract/Entities/AbstractEntity.ts
@@ -2,25 +2,26 @@ import { nanoid } from 'nanoid';
 
 import type { EntityType } from '@/Engine/Abstract';
 import type { TEntity } from '@/Engine/Abstract/Models';
-import type { TDestroyable, TRegistrable, TWithNameAndNameAccessorsMixin, TWithNameOptional, TWithTagsMixin } from '@/Engine/Mixins';
+import type { TDestroyable, TRegistrable, TWithId, TWithNameAndNameAccessorsMixin, TWithNameOptional, TWithTagsMixin } from '@/Engine/Mixins';
 import { destroyableMixin, withNameAndNameAccessorsMixin } from '@/Engine/Mixins';
 import { withTagsMixin } from '@/Engine/Mixins/Generics';
+import type { TOptional } from '@/Engine/Utils';
 import { isDefined } from '@/Engine/Utils';
 
-type TEntityParams = Readonly<{ tags?: ReadonlyArray<string> } & TWithNameOptional>;
+type TEntityParams = Readonly<{ tags?: ReadonlyArray<string> } & TWithNameOptional> & TOptional<TWithId>;
 
 export function AbstractEntity<T extends Record<string, any>>(entities: T, type: EntityType | string, params?: TEntityParams): TEntity<T> {
-  const id: string = type + '_' + nanoid();
+  const id: string = isDefined(params?.id) ? params.id : type + '_' + nanoid();
 
   const withNameAndNameAccessors: TWithNameAndNameAccessorsMixin = withNameAndNameAccessorsMixin();
   const destroyable: TDestroyable = destroyableMixin();
   const withTags: TWithTagsMixin = withTagsMixin(params ? params.tags : []);
 
   const partialResult: T & TRegistrable & TWithTagsMixin & TDestroyable = {
-    id,
-    ...entities,
+    ...destroyable,
     ...withTags,
-    ...destroyable
+    id,
+    ...entities
   };
 
   const result: TEntity<T> = { ...partialResult, ...withNameAndNameAccessors };
--- src/Engine/Abstract/Models/TEntity.ts
+++ src/Engine/Abstract/Models/TEntity.ts
@@ -1,3 +1,3 @@
-import type { TDestroyable, TRegistrable, TWithNameAndNameAccessorsMixin, TWithTagsMixin } from '@/Engine/Mixins';
+import type { TDestroyable, TRegistrable, TWithId, TWithNameAndNameAccessorsMixin, TWithTagsMixin } from '@/Engine/Mixins';
 
-export type TEntity<T extends Record<string, any>> = Readonly<T> & TWithTagsMixin & TDestroyable & TRegistrable & TWithNameAndNameAccessorsMixin;
+export type TEntity<T extends Record<string, any>> = Readonly<T> & TWithTagsMixin & TDestroyable & TRegistrable & TWithNameAndNameAccessorsMixin & TWithId;
