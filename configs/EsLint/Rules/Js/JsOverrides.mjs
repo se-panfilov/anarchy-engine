56f17634c3c43b1c2dbab0ee34097818838b6cbe
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:53:25 +0100

    [ESLINT][TODO]

--- configs/EsLint/Rules/Js/JsOverrides.mjs
+++ configs/EsLint/Rules/Js/JsOverrides.mjs
@@ -1,28 +1,24 @@
+// TODO 18-0-0 Make sure this rules work
+// TODO 18-0-0 The "no-restricted-imports"  rule is only for engine, exclude from other packages configs
 export const JsOverrides = [
   {
-    files: ['src/Engine/**/*'],
+    files: ['packages/engine/src/**/*'],
     rules: {
       'no-restricted-imports': [
         'error',
         {
           paths: [
             {
-              name: '@/Engine',
-              message: "Please import a certain module instead of a whole folder, e.g. '@/Engine/Foo' instead of '@/Engine'"
-            },
-            {
               name: '@Engine',
               message: "Please import a certain module instead of a whole folder, e.g. '@Engine/Foo' instead of '@Engine'"
+            },
+            {
+              name: 'lodash',
+              message: 'Please import lodash-es instead of lodash. This helps to reduce bundle size by importing only the necessary functions.'
             }
           ]
         }
       ]
-    }
-  },
-  {
-    files: ['src/App/**/*'],
-    rules: {
-      'no-restricted-imports': ['off']
     }
   }
 ];
