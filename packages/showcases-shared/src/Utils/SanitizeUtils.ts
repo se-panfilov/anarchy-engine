54ed156370b3bb4a0ca782b3e82456c553a19565
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:16:11 +0100

    [SHARED] fix DOPPurify issue on window-less envs

--- packages/showcases-shared/src/Utils/SanitizeUtils.ts
+++ packages/showcases-shared/src/Utils/SanitizeUtils.ts
@@ -1,6 +1,6 @@
 import type { Config } from 'dompurify';
+import DOMPurify from 'isomorphic-dompurify';
 
-export async function sanitizeMarkDown(content: string, config: Config = { USE_PROFILES: { html: true } }): Promise<string> {
-  const DOMPurify = (await import('dompurify')).default;
+export async function sanitizeMarkDown(content: string, config: Config = { USE_PROFILES: { html: true } }): Promise<string> | never {
   return DOMPurify.sanitize(content, config);
 }
