a17755c2c395abdb77188b4e5362f8b3f652c2fa
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:09:53 +0100

    [LEGAL] todo

--- packages/anarchy-legal/src/index.ts
+++ packages/anarchy-legal/src/index.ts
@@ -6,7 +6,6 @@ export * from './Services';
 // TODO DESKTOP: LEGAL: Include legal docs in the build (desktop)
 // TODO MOBILE: LEGAL: Include legal docs in the build (mobile)
 // TODO DESKTOP: LEGAL: Display EULA/License, etc via menu
-// TODO DESKTOP: LEGAL: add ./legal/** folder to "files" (for npm releases)
 
 // TODO DESKTOP: LEGAL: make sure we're matching the following:
 //  - GDPR/UK-GDPR
@@ -19,12 +18,3 @@ export * from './Services';
 //  - CIRCIA (USA)
 //  - EAA (EU)
 //  - PIPL (China)
-
-// TODO DESKTOP: LEGAL: Add following parts:
-//  - INSTRUCTIONS.md
-//  - SUPPORT.md
-//  - TECHNICAL_DOCUMENTATION
-//  - SBOM
-//  - EU_Declaration_of_Conformity.md
-//  - SECURITY.md + .well-known/security.txt
-//  - INCIDENT_RESPONSE / VULN_HANDLING.md
