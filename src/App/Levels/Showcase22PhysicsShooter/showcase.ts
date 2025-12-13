3d145dcf0b3daf50f9ecfbe68f1805dfc9072e8a
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:25:30 +0100

    showcase: refactoring

--- src/App/Levels/Showcase22PhysicsShooter/showcase.ts
+++ src/App/Levels/Showcase22PhysicsShooter/showcase.ts
@@ -153,10 +153,10 @@ export async function showcase(canvas: TAppCanvas): Promise<TShowcase> {
 
     initGui(mouseLineIntersectionsWatcher, spatialGridService, actorService, shootingParams);
 
-    loopService.tick$.subscribe((delta): void => {
-      cameraFollowingActor(cameraW, hero);
-      updateBullets(bullets, delta.delta, spatialGrid);
+    cameraFollowingActor(cameraW, hero);
 
+    loopService.tick$.subscribe((delta): void => {
+      updateBullets(bullets, delta.delta, spatialGrid);
       // TODO this should be updated only if coords or angle are changed
       if (isDefined(mouseLineIntersections.point)) {
         const heroCoords: Vector3 = hero.drive.getPosition();
--- src/App/Levels/Showcase22PhysicsShooter/utils/Camera.ts
+++ src/App/Levels/Showcase22PhysicsShooter/utils/Camera.ts
@@ -3,9 +3,8 @@ import { Vector3 } from 'three';
 import type { TActor, TCameraWrapper } from '@/Engine';
 
 export function cameraFollowingActor(cameraW: TCameraWrapper, actor: TActor): void {
-  const actorCoords: Vector3 = actor.drive.getPosition();
-  cameraW.drive.position$.next(new Vector3(actorCoords.x, actorCoords.y + 15, actorCoords.z + 20));
-  // cameraW.setPosition(new Vector3(actorCoords.x, actorCoords.y + 25, actorCoords.z));
-  // cameraW.entity.up = new Vector3(1, 0, 0);
-  cameraW.lookAt(actorCoords);
+  actor.drive.position$.subscribe((position: Vector3): void => {
+    cameraW.drive.position$.next(position.clone().add(new Vector3(0, 15, 20)));
+    cameraW.lookAt(position);
+  });
 }
