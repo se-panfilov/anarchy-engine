564e6df1403c5ad673299b7c44861e3c6a30ded2
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:10:11 +0100

    [TODO]

--- packages/showcases-menu/src/views/Legal.vue
+++ packages/showcases-menu/src/views/Legal.vue
@@ -6,6 +6,17 @@ import ViewForm from '@Showcases/Menu/components/ViewForm.vue';
 import { vueTranslationService } from '@Showcases/Menu/services';
 import type { ShallowRef } from 'vue';
 
+// TODO DESKTOP: LEGAL: Display legal info (based on platform) in the menu.
+// The Plan:
+//
+// During legal files generation:
+// - In showcases-core app copy ./legal into ./public/legal
+// - In showcases-desktop app copy ./legal into ./assets/legal
+//
+// Via platform service provide the possibility to lazy-load the docs (params: "name", "lang")
+// Add possibility to menu to receive that lazy-loaded content
+// Render .md files in a scrollable view
+
 const { $t } = vueTranslationService;
 
 const viewTitleText: ShallowRef<string> = $t('main-menu.settings.legal.view.title');
