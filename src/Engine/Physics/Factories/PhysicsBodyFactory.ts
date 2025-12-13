4699d47e04d3897a8a93884b3b918ae1c8f7fc65
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:06:50 +0100

    fixes

--- src/Engine/Physics/Factories/PhysicsBodyFactory.ts
+++ src/Engine/Physics/Factories/PhysicsBodyFactory.ts
@@ -1,8 +1,9 @@
 import type { TReactiveFactory } from '@/Engine/Abstract';
 import { FactoryType, ReactiveFactory } from '@/Engine/Abstract';
-import { configToParamsPreset } from '@/Engine/Physics/Adapters';
+import { configToParamsBody as configToParams } from '@/Engine/Physics/Adapters';
 import type { TPhysicsBodyFacade, TPhysicsBodyFactory, TPhysicsBodyParams } from '@/Engine/Physics/Models';
 import { PhysicsBodyFacade } from '@/Engine/Physics/Wrappers';
 
 const factory: TReactiveFactory<TPhysicsBodyFacade, TPhysicsBodyParams> = { ...ReactiveFactory(FactoryType.PhysicsBody, PhysicsBodyFacade) };
-export const PhysicsBodyFactory = (): TPhysicsBodyFactory => ({ ...factory, configToParams: configToParamsPreset });
+// const factory: TReactiveFactory<TPhysicsBodyFacade, TPhysicsBodyParams> = { ...ReactiveFactory(FactoryType.PhysicsBody, PhysicsBodyFacade as TCreateEntityFactoryFn<TPhysicsBodyFacade, TPhysicsBodyParams>) };
+export const PhysicsBodyFactory = (): TPhysicsBodyFactory => ({ ...factory, configToParams });
