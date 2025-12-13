f8a57501e2154861992d62651b05bfc69f44274f
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:58:12 +0100

    fixes

--- src/App/Levels/ShowcaseLevel10ComplexMaterials/index.ts
+++ src/App/Levels/ShowcaseLevel10ComplexMaterials/index.ts
@@ -1,4 +1,4 @@
-import GUI from 'lil-gui';
+import GUI, { Controller } from 'lil-gui';
 import { BehaviorSubject, Subject } from 'rxjs';
 import type { MeshPhysicalMaterial, MeshStandardMaterial } from 'three';
 
@@ -15,7 +15,7 @@ export function showcaseLevel(canvas: IAppCanvas): IShowcase {
   const level: ILevel = buildLevelFromConfig(canvas, levelConfig as ILevelConfig);
   const { textFactory, actorRegistry, controlsRegistry } = level.entities;
 
-  const materials: ReadonlyArray<string> = ['standard', 'basic', 'phong', 'lambert', 'toon', 'physical', 'matcap'];
+  const materials: ReadonlyArray<string> = ['standard', 'physical', 'basic', 'phong', 'lambert', 'toon', 'matcap'];
   const currentMaterialIndex$: BehaviorSubject<number> = new BehaviorSubject(0);
   const currentMaterial$: Subject<string> = new Subject();
   const currentActor$: Subject<IActorWrapperAsync> = new Subject();
@@ -53,7 +53,14 @@ export function showcaseLevel(canvas: IAppCanvas): IShowcase {
     currentMaterialIndex$.next((currentMaterialIndex$.value - 1 + materials.length) % materials.length);
   });
 
+  const state = {
+    controllers: [] as ReadonlyArray<GUI | Controller>
+  };
+
   function moveCameraToActor(actor: IActorWrapperAsync): void {
+    state.controllers.forEach((controller: GUI | Controller): void => controller.destroy());
+    // eslint-disable-next-line functional/immutable-data
+    state.controllers = addGuiToActor(actor);
     const position: IVector3Wrapper = actor.getPosition();
     const orbitControls: IOrbitControlsWrapper | undefined = controlsRegistry.getUniqByTag('orbit');
     if (isNotDefined(orbitControls)) throw new Error('Orbit controls are not found');
@@ -61,16 +68,12 @@ export function showcaseLevel(canvas: IAppCanvas): IShowcase {
     orbitControls.moveToTargetSmoothly(position);
   }
 
-  void envMapService.load('/Showcase/hdr/urban_alley_01_4k.hdr').then((envMap) => {
-    console.log('envMap', envMap);
-  });
+  void envMapService.load('/Showcase/hdr/urban_alley_01_4k.hdr');
 
-  actorRegistry.added$.subscribe((actor: IActorWrapperAsync) => {
-    addGuiToActor(actor);
-    addTextToActor(actor);
-  });
+  actorRegistry.added$.subscribe((actor: IActorWrapperAsync): void => addTextToActor(actor));
 
-  function addGuiToActor(actor: IActorWrapperAsync): void {
+  function addGuiToActor(actor: IActorWrapperAsync): ReadonlyArray<GUI | Controller> {
+    let controllers: ReadonlyArray<GUI | Controller> = [];
     const isMetalness: boolean = isDefined((actor.entity.material as MeshStandardMaterial).metalness);
     const isRoughness: boolean = isDefined((actor.entity.material as MeshStandardMaterial).roughness);
     const isAoMap: boolean = isDefined((actor.entity.material as MeshStandardMaterial).aoMap);
@@ -81,53 +84,63 @@ export function showcaseLevel(canvas: IAppCanvas): IShowcase {
     const isIridescence: boolean = isDefined((actor.entity.material as MeshPhysicalMaterial).iridescence);
     const isTransmission: boolean = isDefined((actor.entity.material as MeshPhysicalMaterial).transmission);
 
-    gui.addFolder(actor.getTags()[0]);
-    if (isMetalness) gui.add(actor.entity.material, 'metalness').min(0).max(1).step(0.0001);
-    if (isRoughness) gui.add(actor.entity.material, 'roughness').min(0).max(1).step(0.0001);
-    if (isAoMap) gui.add(actor.entity.material, 'aoMapIntensity').min(0).max(1).step(0.0001);
-    if (isDisplacementMap) gui.add(actor.entity.material, 'displacementScale').min(0).max(1).step(0.0001);
+    if (isMetalness) controllers = [...controllers, gui.add(actor.entity.material, 'metalness').min(0).max(1).step(0.0001)];
+    if (isRoughness) controllers = [...controllers, gui.add(actor.entity.material, 'roughness').min(0).max(1).step(0.0001)];
+    if (isAoMap) controllers = [...controllers, gui.add(actor.entity.material, 'aoMapIntensity').min(0).max(1).step(0.0001)];
+    if (isDisplacementMap) controllers = [...controllers, gui.add(actor.entity.material, 'displacementScale').min(0).max(1).step(0.0001)];
     if (isNormalMap) {
       const scale = { normalScale: 1 };
-      gui
-        .add(scale, 'normalScale')
-        .min(0)
-        .max(1)
-        .step(0.0001)
-        .onChange((value: number): void => {
-          (actor.entity.material as MeshStandardMaterial).normalScale.set(value, value);
-        });
+      controllers = [
+        ...controllers,
+        gui
+          .add(scale, 'normalScale')
+          .min(0)
+          .max(1)
+          .step(0.0001)
+          .onChange((value: number): void => {
+            (actor.entity.material as MeshStandardMaterial).normalScale.set(value, value);
+          })
+      ];
     }
     if (isClearCoat) {
-      gui.add(actor.entity.material, 'clearcoat').min(0).max(1).step(0.0001);
-      gui.add(actor.entity.material, 'clearcoatRoughness').min(0).max(1).step(0.0001);
+      controllers = [...controllers, gui.add(actor.entity.material, 'clearcoat').min(0).max(1).step(0.0001)];
+      controllers = [...controllers, gui.add(actor.entity.material, 'clearcoatRoughness').min(0).max(1).step(0.0001)];
     }
 
     if (isSheen) {
-      gui.add(actor.entity.material, 'sheen').min(0).max(1).step(0.0001);
-      gui.add(actor.entity.material, 'sheenRoughness').min(0).max(1).step(0.0001);
-      gui.addColor(actor.entity.material, 'sheenColor');
+      controllers = [...controllers, gui.add(actor.entity.material, 'sheen').min(0).max(1).step(0.0001)];
+      controllers = [...controllers, gui.add(actor.entity.material, 'sheenRoughness').min(0).max(1).step(0.0001)];
+      controllers = [...controllers, gui.addColor(actor.entity.material, 'sheenColor')];
     }
 
     if (isIridescence) {
-      gui.add(actor.entity.material, 'iridescence').min(0).max(1).step(0.0001);
-      gui.add(actor.entity.material, 'iridescenceIOR').min(0).max(2.333).step(0.0001);
-      gui
-        .add((actor.entity.material as MeshPhysicalMaterial).iridescenceThicknessRange, '0')
-        .min(0)
-        .max(1000)
-        .step(1);
-      gui
-        .add((actor.entity.material as MeshPhysicalMaterial).iridescenceThicknessRange, '1')
-        .min(0)
-        .max(1000)
-        .step(1);
+      controllers = [...controllers, gui.add(actor.entity.material, 'iridescence').min(0).max(1).step(0.0001)];
+      controllers = [...controllers, gui.add(actor.entity.material, 'iridescenceIOR').min(0).max(2.333).step(0.0001)];
+      controllers = [
+        ...controllers,
+        gui
+          .add((actor.entity.material as MeshPhysicalMaterial).iridescenceThicknessRange, '0')
+          .min(0)
+          .max(1000)
+          .step(1)
+      ];
+      controllers = [
+        ...controllers,
+        gui
+          .add((actor.entity.material as MeshPhysicalMaterial).iridescenceThicknessRange, '1')
+          .min(0)
+          .max(1000)
+          .step(1)
+      ];
     }
 
     if (isTransmission) {
-      gui.add(actor.entity.material, 'transmission').min(0).max(1).step(0.0001);
-      gui.add(actor.entity.material, 'ior').min(1).max(10).step(0.0001); //diamond ior 2.417, water 1.333, glass 1.5, air 1.0003
-      gui.add(actor.entity.material, 'thickness').min(0).max(1).step(0.0001);
+      controllers = [...controllers, gui.add(actor.entity.material, 'transmission').min(0).max(1).step(0.0001)];
+      controllers = [...controllers, gui.add(actor.entity.material, 'ior').min(1).max(10).step(0.0001)]; //diamond ior 2.417, water 1.333, glass 1.5, air 1.0003
+      controllers = [...controllers, gui.add(actor.entity.material, 'thickness').min(0).max(1).step(0.0001)];
     }
+
+    return controllers;
   }
 
   function start(): void {
