1847dc49eaa3ac060d5c0b7b624708eeb25138f0
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:39:48 +0100

    Serialization: color

--- src/Engine/Color/Adapters/EntityToConfig.ts
+++ src/Engine/Color/Adapters/EntityToConfig.ts
@@ -1,15 +1,8 @@
 import type { ColorRepresentation } from 'three';
 
 import type { TColorWrapper } from '@/Engine/Color/Models';
-import { filterOutEmptyFields } from '@/Engine/Utils';
 
+// TODO 15-0-0: validate result
 export function colorToConfig(entity: TColorWrapper): ColorRepresentation {
-  // TODO 15-0-0: implement
-  console.log('XXX entity', entity);
-
-  const json = entity.entity.toJSON();
-
-  return filterOutEmptyFields({
-    // TODO 15-0-0: fix any
-  }) as any;
+  return entity.entity.toJSON();
 }
--- src/Engine/EnvMap/Adapters/EntityToConfig.ts
+++ src/Engine/EnvMap/Adapters/EntityToConfig.ts
@@ -2,6 +2,7 @@ import type { TEnvMapConfig, TEnvMapConfigToParamsDependencies, TEnvMapWrapper }
 import { extractRegistrableFields } from '@/Engine/Mixins';
 import { filterOutEmptyFields, isNotDefined } from '@/Engine/Utils';
 
+// TODO 15-0-0: validate result
 export function envMapToConfig(entity: TEnvMapWrapper, { resourcesRegistry }: TEnvMapConfigToParamsDependencies): TEnvMapConfig {
   const texture: string | undefined = resourcesRegistry.findKeyByValue(entity.entity);
   if (isNotDefined(texture)) throw new Error(`[Serialization] EnvMap: Texture not found in resourcesRegistry for envMap id: "${entity.id}", name: "${entity.getName()}"`);
