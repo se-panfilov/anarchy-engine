310cfe75bab5f80a7d6a154297ecd0c7efd8b031
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:02:47 +0100

    [MONO][MENU] rename package

--- vite.alias.ts
+++ vite.alias.ts
@@ -3,7 +3,7 @@ import path from 'path';
 export const sharedAliases = {
   '@E2E': path.resolve(__dirname, 'apps/e2e/src'),
   '@Engine': path.resolve(__dirname, 'packages/engine/src'),
-  '@Menu': path.resolve(__dirname, 'packages/showcases-menu/src'),
+  '@Menu': path.resolve(__dirname, 'packages/showcases_menu/src'),
   '@Shared': path.resolve(__dirname, 'packages/shared/src'),
   '@AnarchyI18n': path.resolve(__dirname, 'packages/anarchy_i18n/src'),
   lodash: 'lodash-es'
