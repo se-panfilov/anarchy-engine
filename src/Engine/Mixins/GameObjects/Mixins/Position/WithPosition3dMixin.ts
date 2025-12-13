0caad5abe63d3348430e18e54eb988284cc829ba
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:18:58 +0100

    fixes

--- src/Engine/Mixins/GameObjects/Mixins/Position/WithPosition3dMixin.ts
+++ src/Engine/Mixins/GameObjects/Mixins/Position/WithPosition3dMixin.ts
@@ -1,10 +1,10 @@
-import { Vector3 } from 'three/src/math/Vector3';
+import type { Vector3 } from 'three/src/math/Vector3';
 
 import type { TWithPosition3d, TWithPosition3dProperty } from '@/Engine/Mixins/GameObjects/Models';
 
 export function withPosition3dMixin(entity: TWithPosition3dProperty): TWithPosition3d {
-  const setPosition = (position: Vector3): Vector3 => entity.position.set(position.x, position.y, position.z);
-  const addPosition = (position: Vector3): Vector3 => setPosition(new Vector3(entity.position.x + position.x, entity.position.y + position.y, entity.position.z + position.z));
-  const getPosition = (): Vector3 => new Vector3(entity.position.x, entity.position.y, entity.position.z);
+  const setPosition = ({ x, y, z }: Vector3): Vector3 => entity.position.set(x, y, z);
+  const addPosition = ({ x, y, z }: Vector3): Vector3 => entity.position.add({ x, y, z });
+  const getPosition = (): Vector3 => entity.position;
   return { setPosition, addPosition, getPosition };
 }
--- src/Engine/Mixins/GameObjects/Models/Position/TWithPositionProperty.ts
+++ src/Engine/Mixins/GameObjects/Models/Position/TWithPositionProperty.ts
@@ -2,33 +2,15 @@ import type { Vector2, Vector4 } from 'three';
 import type { Vector3 } from 'three/src/math/Vector3';
 
 export type TWithPosition2dProperty = Readonly<{
-  position: {
-    x: number;
-    y: number;
-    set: (x: number, y: number) => Vector2;
-    // add: (x: number, y: number) =>TVector2;
-  };
+  position: Vector2;
 }>;
 
 export type TWithPosition3dProperty = Readonly<{
-  position: {
-    x: number;
-    y: number;
-    z: number;
-    set: (x: number, y: number, z: number) => Vector3;
-    // add: (x: number, y: number, z: number) => Vector3;
-  };
+  position: Vector3;
 }>;
 
 export type TWithPosition4dProperty = Readonly<{
-  position: {
-    x: number;
-    y: number;
-    z: number;
-    w: number;
-    set: (x: number, y: number, z: number, w: number) => Vector4;
-    // add: (x: number, y: number, z: number, w: number) => Vector4;
-  };
+  position: Vector4;
 }>;
 
 export type TWithPositionProperty = TWithPosition2dProperty | TWithPosition3dProperty | TWithPosition4dProperty;
