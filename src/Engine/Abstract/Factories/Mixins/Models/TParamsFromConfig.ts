7ccf25b2f502fba9cf4d0d69cde9cac56d7acd16
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:14:59 +0100

    [WIP] types fixes

--- src/Engine/Abstract/Factories/Mixins/Models/TParamsFromConfig.ts
+++ src/Engine/Abstract/Factories/Mixins/Models/TParamsFromConfig.ts
@@ -1,15 +1,19 @@
-// export type TParamsFromConfig<C, P, D> = Readonly<{
-//   configToParams: D extends Record<string, any> ? (config: C, dependencies: D) => P : (config: C) => P;
-// }>;
-
-export type TParamsFromConfig<C, P> = Readonly<{
-  configToParams: (config: C, dependencies?: Record<string, any>) => P;
+export type TParamsFromConfigWithDependencies<C, P, D> = Readonly<{
+  configToParams: D extends Record<string, any> ? (config: C, dependencies: D) => P : (config: C) => P;
 }>;
 
-// export type TParamsFromConfigAsync<C, P, D> = Readonly<{
-//   configToParamsAsync: D extends Record<string, any> ? (config: C, dependencies: D) => P : (config: C) => P;
-// }>;
-
-export type TParamsFromConfigAsync<C, P> = Readonly<{
-  configToParamsAsync: (config: C, dependencies?: Record<string, any>) => Promise<P>;
+export type TParamsFromConfigWithoutDependencies<C, P> = Readonly<{
+  configToParams: (config: C) => P;
 }>;
+
+export type TParamsFromConfig<C, P, D = any> = TParamsFromConfigWithDependencies<C, P, D> | TParamsFromConfigWithoutDependencies<C, P>;
+
+export type TParamsFromConfigAsyncWithDependencies<C, P, D> = Readonly<{
+  configToParamsAsync: D extends Record<string, any> ? (config: C, dependencies: D) => P : (config: C) => P;
+}>;
+
+export type TParamsFromConfigAsyncWithoutDependencies<C, P> = Readonly<{
+  configToParamsAsync: (config: C) => Promise<P>;
+}>;
+
+export type TParamsFromConfigAsync<C, P, D = any> = TParamsFromConfigAsyncWithDependencies<C, P, D> | TParamsFromConfigAsyncWithoutDependencies<C, P>;
