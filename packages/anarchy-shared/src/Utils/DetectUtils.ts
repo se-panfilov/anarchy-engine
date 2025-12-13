2627d4672fa240184ec61e7a1a53a54262355980
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:15:12 +0100

    [SHARED] getBrowserInfo

--- packages/anarchy-shared/src/Utils/DetectUtils.ts
+++ packages/anarchy-shared/src/Utils/DetectUtils.ts
@@ -2,7 +2,5 @@ import type { TBrowserInfo } from '@Anarchy/Shared/Models';
 // eslint-disable-next-line spellcheck/spell-checker
 import Bowser from 'bowser';
 
-export function getBrowserInfo(): TBrowserInfo {
-  // eslint-disable-next-line spellcheck/spell-checker
-  return Bowser.parse(window.navigator.userAgent);
-}
+// eslint-disable-next-line spellcheck/spell-checker
+export const getBrowserInfo = (): TBrowserInfo => Bowser.parse(window.navigator.userAgent);
--- packages/showcases-shared/src/Models/TShowcasesDesktopApi.ts
+++ packages/showcases-shared/src/Models/TShowcasesDesktopApi.ts
@@ -1,14 +1,15 @@
 import type { TLocaleId } from '@Anarchy/i18n';
+import type { TBrowserInfo } from '@Anarchy/Shared/Models';
 import type { TLegalDoc, TLoadDocPayload } from '@Showcases/Shared';
 
 import type { TShowcaseGameSettings } from './TShowcaseGameSettings';
 
 export type TShowcasesDesktopApi = Readonly<{
-  chrome: () => string;
   closeApp: () => void;
   desktopAppVersion: () => Promise<string>;
   electron: () => string;
   getAppSettings: () => Promise<TShowcaseGameSettings>;
+  getBrowserInfo: () => TBrowserInfo;
   getLegalDocs: (options: TLoadDocPayload) => Promise<TLegalDoc>;
   getPreferredLocales: () => Promise<ReadonlyArray<TLocaleId>>;
   node: () => string;
