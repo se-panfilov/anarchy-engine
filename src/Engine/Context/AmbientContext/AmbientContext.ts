b7d4d79273c443ce5641cc906baed53118ef307a
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:23:53 +0100

    [WIP] minor fix

--- src/Engine/Context/AmbientContext/AmbientContext.ts
+++ src/Engine/Context/AmbientContext/AmbientContext.ts
@@ -9,7 +9,7 @@ import type { TAmbientContext } from './Models';
 const container: TGlobalContainerDecorator = ContainerDecorator(window);
 const screenSizeWatcherFactory: TScreenSizeWatcherFactory = ScreenSizeWatcherFactory();
 const screenSizeWatcherRegistry: TScreenSizeWatcherRegistry = ScreenSizeWatcherRegistry();
-screenSizeWatcherFactory.entityCreated$.subscribe((watcher: TScreenSizeWatcher) => screenSizeWatcherRegistry.add(watcher));
+screenSizeWatcherFactory.entityCreated$.subscribe((watcher: TScreenSizeWatcher): void => screenSizeWatcherRegistry.add(watcher));
 const screenSizeWatcher: TScreenSizeWatcher = screenSizeWatcherFactory.create({ container, tags: [WatcherTag.Initial, WatcherTag.Global] }).start();
 
 export const ambientContext: TAmbientContext = {
