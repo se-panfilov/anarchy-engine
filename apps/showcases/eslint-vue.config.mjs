3edcabd52f242d6f78e085372a49099cb43655af
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:59:11 +0100

    [App][ESLINT] fixes

--- apps/showcases/eslint-vue.config.mjs
+++
@@ -1,3 +0,0 @@
-import { CustomConfig, FunctionalConfig, ignores, ImportsConfig, JsConfig, languageOptions, plugins, TsConfig, VueConfig } from '../../configs/EsLint/index.mjs';
-
-export default [...JsConfig, ...TsConfig, ...FunctionalConfig, ...ImportsConfig, ...CustomConfig, ...VueConfig, { languageOptions }, { ignores }, { plugins }];
--- apps/showcases/eslint.config.mjs
+++ apps/showcases/eslint.config.mjs
@@ -1,3 +1,3 @@
-import { CustomConfig, FunctionalConfig, ignores, ImportsConfig, JsConfig, languageOptions, plugins, TsConfig } from '../../configs/EsLint/index.mjs';
+import { CustomConfig, FunctionalConfig, ignores, ImportsConfig, JsConfig, languageOptions, plugins, TsConfig, VueConfig } from '../../configs/EsLint/index.mjs';
 
-export default [...JsConfig, ...TsConfig, ...FunctionalConfig, ...ImportsConfig, ...CustomConfig, { languageOptions }, { ignores }, { plugins }];
+export default [...JsConfig, ...TsConfig, ...FunctionalConfig, ...ImportsConfig, ...CustomConfig, ...VueConfig, { languageOptions }, { ignores }, { plugins }];
