eedf107fc2788ff573d2929573fc0964d5c13932
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:39:41 +0100

    Serialization: Camera

--- src/Engine/Camera/Adapters/EntityToConfig.ts
+++ src/Engine/Camera/Adapters/EntityToConfig.ts
@@ -1,21 +1,38 @@
+import type { OrthographicCameraJSONObject, PerspectiveCameraJSONObject } from 'three';
+import { AudioListener } from 'three';
+
 import type { TCameraConfig, TCameraWrapper } from '@/Engine/Camera/Models';
 import { extractRegistrableFields } from '@/Engine/Mixins';
+import { filterOutEmptyFields, isDefined } from '@/Engine/Utils';
 
+// TODO 15-0-0: validate result
 export function cameraToConfig(entity: TCameraWrapper): TCameraConfig {
   const { drive } = entity;
-  // TODO 15-0-0: implement
 
-  return {
-    //  fov?: number;
-    //   near?: number;
-    //   far?: number;
-    //   lookAt?: Vector3;
-    //   audioListener?: AudioListener;
-    //   name: string;
-    //   isActive: boolean
-    //   tags
+  const json: PerspectiveCameraJSONObject | OrthographicCameraJSONObject = entity.entity.toJSON().object;
+
+  let audioListener: string | undefined = undefined;
+  const listener: AudioListener | undefined = entity.entity.children.find((child) => child instanceof AudioListener);
+  if (isDefined(listener)) {
+    audioListener = {} as any;
+  }
+
+  return filterOutEmptyFields({
+    near: json.near,
+    far: json.far,
+    fov: (json as PerspectiveCameraJSONObject).fov ?? undefined,
+    aspect: (json as PerspectiveCameraJSONObject).aspect ?? undefined,
+    filmGauge: (json as PerspectiveCameraJSONObject).filmGauge ?? undefined,
+    filmOffset: (json as PerspectiveCameraJSONObject).filmOffset ?? undefined,
+    focus: (json as PerspectiveCameraJSONObject).focus ?? undefined,
+    layers: json.layers,
+    matrix: json.matrix,
+    up: json.up,
+    zoom: json.zoom,
+    isActive: entity.isActive(),
+    audioListener,
     ...extractRegistrableFields(entity),
     ...drive.serialize()
     // TODO 15-0-0: fix any
-  } as any;
+  });
 }
