a654b97db187754298e34ce80d8845c8e8cf92d0
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:52:26 +0100

    removed extra

--- src/Engine/Domains/Abstract/Pool/AbstractPool.ts
+++
@@ -1,5 +0,0 @@
-import type { IAbstractPool } from '../Models';
-
-export function AbstractPool<T extends Record<string, unknown>>(pool: T): IAbstractPool<T> {
-  return { pool };
-}
--- src/Engine/Domains/Abstract/index.ts
+++ src/Engine/Domains/Abstract/index.ts
@@ -1,7 +1,6 @@
 export * from './Constants';
 export * from './Factory';
 export * from './Models';
-export * from './Pool';
 export * from './Registry';
 export * from './Watcher';
 export * from './Wrapper';
