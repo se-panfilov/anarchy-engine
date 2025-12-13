a525383589d2a1e4a050abc871261fdf3b7b4c5c
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:39:56 +0100

    Serialization: Model3d

--- src/Engine/Models3d/Adapters/EntityToConfig.ts
+++ src/Engine/Models3d/Adapters/EntityToConfig.ts
@@ -2,7 +2,7 @@ import type { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 
 import { extractRegistrableFields } from '@/Engine/Mixins';
 import type { PrimitiveModel3dType } from '@/Engine/Models3d/Constants';
-import type { TModel3d, TModel3dConfig, TModel3dConfigToParamsDependencies, TModel3dParams, TModels3dResourceAsyncRegistry, TRawModel3d } from '@/Engine/Models3d/Models';
+import type { TModel3d, TModel3dConfig, TModel3dConfigToParamsDependencies, TModel3dParams, TModels3dResourceAsyncRegistry } from '@/Engine/Models3d/Models';
 import { isPrimitiveModel3dSource } from '@/Engine/Models3d/Utils';
 import { filterOutEmptyFields, isDefined, isNotDefined } from '@/Engine/Utils';
 
@@ -35,16 +35,7 @@ export function model3dToConfig(entity: TModel3d, { animationsResourceAsyncRegis
 }
 
 function getComplexModel3dSource(entity: TModel3d, model3dResourceAsyncRegistry: TModels3dResourceAsyncRegistry): string | undefined {
-  let model3dSource: string | undefined;
-  if (!entity.forceClone) {
-    //The model is not cloned, so it's in the model3dResourceAsyncRegistry
-    const rawModel3d: TRawModel3d = entity.getRawModel3d();
-    model3dSource = model3dResourceAsyncRegistry.findKey((val: GLTF): boolean => val.scene === rawModel3d);
-    if (isNotDefined(model3dSource)) throw new Error(`[Serialization] Model3d: model3dSource not found for entity with name: "${entity.name}", (id: "${entity.id}")`);
-  } else {
-    //The model is cloned, so it's NOT in the model3dResourceAsyncRegistry
-    // TODO 15-0-0: implement
-    // model3dSource = 'TODO';
-  }
-  return model3dSource;
+  // For cloned models we need to use the ID from the params
+  const modelId: string = !entity.forceClone ? entity.getRawModel3d().uuid : (entity.getParams().model3dSource as GLTF).scene.uuid;
+  return model3dResourceAsyncRegistry.findKey((val: GLTF): boolean => val.scene.uuid === modelId);
 }
