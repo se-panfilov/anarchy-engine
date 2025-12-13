bf5657ad336cfe144f3f5de51ab8ac60a892c4d2
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:59:25 +0100

    [MONO] added aliases

--- vite.alias.ts
+++ vite.alias.ts
@@ -3,6 +3,7 @@ import path from 'path';
 export const sharedAliases = {
   '@Shared': path.resolve(__dirname, 'packages/shared/src'),
   '@Engine': path.resolve(__dirname, 'packages/engine/src'),
+  '@Menu': path.resolve(__dirname, 'packages/ui-menu/src'),
   '@E2E': path.resolve(__dirname, 'apps/e2e/src'),
   lodash: 'lodash-es'
 };
