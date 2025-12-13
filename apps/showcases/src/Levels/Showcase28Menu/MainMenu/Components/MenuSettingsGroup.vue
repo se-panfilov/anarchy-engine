04d0805d2dbb05b78fa85a6e52f58324e8905865
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:58:59 +0100

    [App] refactor

---
+++ apps/showcases/src/Levels/Showcase28Menu/MainMenu/Components/MenuSettingsGroup.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{ title: string }>();
+</script>
+
+<template>
+  <div class="menu-settings-group">
+    <div class="menu-settings-group__title">{{ title }}</div>
+    <slot name="default"></slot>
+  </div>
+</template>
+
+<style scoped></style>
--- apps/showcases/src/Levels/Showcase28Menu/MainMenu/Views/Audio.vue
+++ apps/showcases/src/Levels/Showcase28Menu/MainMenu/Views/Audio.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { reactive } from 'vue';
 import MenuViewActions from '@/Levels/Showcase28Menu/MainMenu/Components/MenuViewActions.vue';
+import MenuSettingsGroup from '@/Levels/Showcase28Menu/MainMenu/Components/MenuSettingsGroup.vue';
 import { useSettingsStore } from '@/Levels/Showcase28Menu/MainMenu/Stores/SettingsStore';
 import type { TAudioSettings } from '@/Levels/Showcase28Menu/Models';
 import type { TWriteable } from '@Engine';
@@ -25,14 +26,13 @@ function save(payload: TAudioSettings) {
 <template>
   <div class="audio main-menu-view">
     <div class="main-menu-view__title">Audio settings</div>
-    <div class="main-menu-view__group">
-      <div class="main-menu-view__group-title">Group 1</div>
+    <MenuSettingsGroup class="main-menu-view__group" title="Group 1">
       <label class="main-menu-view__setting -masterVolume">
         <span class="main-menu-view__setting-description">MasterVolume</span>
         <input type="range" min="0" max="100" v-model="state.masterVolume" class="main-menu-view__setting-value -range" />
         <span class="main-menu-view__setting-helper"> {{ state.masterVolume }}</span>
       </label>
-    </div>
+    </MenuSettingsGroup>
     <MenuViewActions @cancel="cancel()" @save="save(state)" />
   </div>
 </template>
