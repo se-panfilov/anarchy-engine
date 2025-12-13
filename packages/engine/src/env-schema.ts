138c132b3788fee4128d3a57860c91d367ba2d87
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:53:19 +0100

    [MONOREPO] cleanup

--- packages/engine/src/env-schema.ts
+++ packages/engine/src/env-schema.ts
@@ -3,7 +3,6 @@ import { object } from 'valibot';
 
 // TODO 18-0-0 remove all these variables (except CI), adjust env-schema. Params should pe a part of Space options (Space.options.loops)
 export const runtimeSchema = object({
-  VITE_SHOW_DEBUG_INFO: toBool,
   VITE_AUDIO_LOOP_STEP: toInt,
   VITE_AUDIO_LOOP_IS_PARALLEL: toBool,
   VITE_PHYSICS_LOOP_STEP: toInt,
