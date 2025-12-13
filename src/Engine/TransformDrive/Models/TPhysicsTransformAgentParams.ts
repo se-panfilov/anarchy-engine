4c83d2448f19819c41586052f891c327d0d38ca4
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:22:46 +0100

    [HEAVY WIP] physics agent: fixed types

--- src/Engine/TransformDrive/Models/TPhysicsTransformAgentParams.ts
+++ src/Engine/TransformDrive/Models/TPhysicsTransformAgentParams.ts
@@ -2,4 +2,4 @@ import type { TWithPresetNamePhysicsBodyParams } from '@/Engine/Physics';
 
 import type { TTransformAgentParams } from './TTransformAgentParams';
 
-export type TPhysicsTransformAgentParams = TTransformAgentParams & TWithPresetNamePhysicsBodyParams;
+export type TPhysicsTransformAgentParams = TTransformAgentParams & Omit<TWithPresetNamePhysicsBodyParams, 'rotation'>;
--- src/Engine/TransformDrive/Utils/AgentUtils.ts
+++ src/Engine/TransformDrive/Utils/AgentUtils.ts
@@ -12,6 +12,7 @@ import type {
   TPhysicsTransformAgentParams,
   TTransformAgentParams
 } from '@/Engine/TransformDrive/Models';
+import { omitInObjectWithoutMutation } from '@/Engine/Utils';
 
 export function getKinematicTransformAgent(params: TTransformAgentParams, kinematic: TKinematicDataParams | undefined, dependencies: TKinematicAgentDependencies): TKinematicTransformAgent {
   const agentParams: TTransformAgentParams = { position: params.position, rotation: params.rotation, scale: params.scale ?? new Vector3(1, 1, 1) };
@@ -29,8 +30,10 @@ export function getKinematicWithDefaults(kinematic: TKinematicDataParams | undef
   };
 }
 
-export function getPhysicsTransformAgent(params: TTransformAgentParams, physics: TWithPresetNamePhysicsBodyParams | undefined, dependencies: TPhysicsAgentDependencies): TPhysicsTransformAgent {
+export function getPhysicsTransformAgent(params: TTransformAgentParams, physics: TWithPresetNamePhysicsBodyParams, dependencies: TPhysicsAgentDependencies): TPhysicsTransformAgent {
   const agentParams: TTransformAgentParams = { position: params.position, rotation: params.rotation, scale: params.scale ?? new Vector3(1, 1, 1) };
-  const physicalData: TPhysicsTransformAgentParams = { ...agentParams, ...physics };
+  const omittedPhysics: Omit<TWithPresetNamePhysicsBodyParams, 'position' | 'rotation'> = omitInObjectWithoutMutation(physics, ['position', 'rotation']);
+  //We are ignoring physical body's position and rotation, because we are setting them in agentParams (actor)
+  const physicalData: TPhysicsTransformAgentParams = { ...omittedPhysics, ...agentParams };
   return PhysicsTransformAgent(physicalData, dependencies);
 }
