67362b6da5f00116249e2890fc52585fba1600f9
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:17:54 +0100

    [TRACKING] models

--- packages/anarchy-tracking/src/Models/TTrackingService.ts
+++ packages/anarchy-tracking/src/Models/TTrackingService.ts
@@ -1,10 +1,8 @@
-import type { Client } from '@sentry/core';
 import type { ExclusiveEventHintOrCaptureContext } from '@sentry/core/build/types/utils/prepareEvent';
 
 export type TTrackingService = Readonly<{
-  client: Client | undefined;
   captureException: (exception: unknown, hint?: ExclusiveEventHintOrCaptureContext) => string;
-  start: (onErrorHandler: (ev: any) => void, onRejectionHandler: (ev: PromiseRejectionEvent) => void) => void;
-  stop: (onErrorHandler: (ev: any) => void, onRejectionHandler: (ev: PromiseRejectionEvent) => void) => void;
+  start: (onErrorHandler?: (ev: any) => void, onRejectionHandler?: (ev: PromiseRejectionEvent) => void) => void;
+  stop: (onErrorHandler?: (ev: any) => void, onRejectionHandler?: (ev: PromiseRejectionEvent) => void) => void;
   isStarted: () => boolean;
 }>;
