cb893ffee96662c00ab33036753908e4e1098dcc
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:56:26 +0100

    fixes for showcase

--- src/App/Levels/ShowcaseLevel10ComplexMaterials/index.ts
+++ src/App/Levels/ShowcaseLevel10ComplexMaterials/index.ts
@@ -1,34 +1,63 @@
-import { combineLatest } from 'rxjs';
+import { BehaviorSubject, combineLatest } from 'rxjs';
 
 import type { IShowcase } from '@/App/Levels/Models';
-import type { IActorWrapper, IAppCanvas, ICameraWrapper, ILevel, ILevelConfig } from '@/Engine';
-import { ambientContext, buildLevelFromConfig, CameraTag, getRotationByCos, getRotationBySin, isDefined, isNotDefined } from '@/Engine';
+import type { IActorWrapper, IAppCanvas, ICameraWrapper, ILevel, ILevelConfig, ITextAnyWrapper, IVector3Wrapper } from '@/Engine';
+import { ambientContext, buildLevelFromConfig, CameraTag, getRotationByCos, getRotationBySin, isNotDefined, TextType } from '@/Engine';
 
 import levelConfig from './showcase-10-complex-materials.config.json';
+
+const { mousePositionWatcher, screenSizeWatcher, mouseClickWatcher } = ambientContext;
 
 //Showcase 10: Complex Materials
 export function showcaseLevel(canvas: IAppCanvas): IShowcase {
   const level: ILevel = buildLevelFromConfig(canvas, levelConfig as ILevelConfig);
+  const { textFactory, actorRegistry, cameraRegistry } = level.entities;
+  const camera: ICameraWrapper | undefined = cameraRegistry.getUniqByTag(CameraTag.Active);
+  if (isNotDefined(camera)) throw new Error('Camera is not found');
 
-  function start(): void {
-    level.start();
-    const { actorRegistry } = level.entities;
-    const actor: IActorWrapper | undefined = actorRegistry.getUniqByTag('standard_cube');
-    if (isNotDefined(actor)) throw new Error('Actor is not found');
+  const textDescription: ITextAnyWrapper = textFactory.create({
+    type: TextType.Text2d,
+    text: '',
+    cssProps: { fontSize: '2rem', color: 'red' },
+    tags: []
+  });
 
-    initCameraRotation(level, actor);
+  const materials: ReadonlyArray<string> = ['standard', 'basic', 'phong', 'lambert', 'toon', 'physical', 'matcap'];
+  const currentMaterialIndex$: BehaviorSubject<number> = new BehaviorSubject(0);
+  const currentMaterial$: BehaviorSubject<string> = new BehaviorSubject(materials[currentMaterialIndex$.value]);
+  const initialActor: IActorWrapper | undefined = actorRegistry.getUniqByTag(currentMaterial$.value);
+  if (isNotDefined(initialActor)) throw new Error(`Actor with tag "${currentMaterialIndex$.value}" is not found`);
+  const currentActor$: BehaviorSubject<IActorWrapper> = new BehaviorSubject(initialActor);
+  currentMaterialIndex$.subscribe((index: number): void => currentMaterial$.next(materials[index]));
+  currentMaterial$.subscribe((material: string): void => {
+    const actor: IActorWrapper | undefined = actorRegistry.getUniqByTag(material);
+    if (isNotDefined(actor)) throw new Error(`Actor with tag "${material}" is not found`);
+    currentActor$.next(actor);
+  });
+
+  currentActor$.subscribe((actor: IActorWrapper): void => {
+    moveTextToActor(actor);
+    lookAtActor(actor);
+  });
+
+  function lookAtActor(actor: IActorWrapper): void {
+    if (isNotDefined(actor) || isNotDefined(camera)) throw new Error('Actor or camera is not found');
+    camera.lookAt(actor.getPosition());
   }
 
-  return { start, level };
-}
+  function moveTextToActor(actor: IActorWrapper): void {
+    const position: IVector3Wrapper = actor.getPosition();
+    const x: number = position.getX();
+    const y: number = position.getY();
+    const z: number = position.getZ();
+    textDescription.setPosition(x - 1.5, y + 1, z);
+    textDescription.setText(currentMaterial$.value + ' material');
+  }
 
-// This is mostly a copy of Showcase 3 (camera rotation)
-function initCameraRotation(level: ILevel, actor: IActorWrapper | undefined): void {
-  const { cameraRegistry } = level.entities;
+  mouseClickWatcher.value$.subscribe((): void => {
+    currentMaterialIndex$.next((currentMaterialIndex$.value + 1) % materials.length);
+  });
 
-  const camera: ICameraWrapper | undefined = cameraRegistry.getUniqByTag(CameraTag.Active);
-
-  const { mousePositionWatcher, screenSizeWatcher } = ambientContext;
   combineLatest([mousePositionWatcher.value$, screenSizeWatcher.latest$]).subscribe(([{ x, y }, { width, height }]): void => {
     if (isNotDefined(camera)) return;
     const xRatio: number = x / width - 0.5;
@@ -41,6 +70,12 @@ function initCameraRotation(level: ILevel, actor: IActorWrapper | undefined): vo
     camera.setY(yRatio * 10);
     camera.setZ(yRotation);
 
-    if (isDefined(actor)) camera.lookAt(actor.getPosition());
+    camera.lookAt(currentActor$.value.getPosition());
   });
+
+  function start(): void {
+    level.start();
+  }
+
+  return { start, level };
 }
