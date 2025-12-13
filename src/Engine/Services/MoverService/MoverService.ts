d1eb1c72c5567cc6dfecebcf362d9c330e88b28d
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:53:31 +0100

    refactoring

--- src/Engine/Services/MoverService/MoverService.ts
+++ src/Engine/Services/MoverService/MoverService.ts
@@ -15,29 +15,25 @@ export function MoverService(loopService: ILoopService, { suspendWhenDocumentHid
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,functional/immutable-data
   (anime as any).suspendWhenDocumentHidden = suspendWhenDocumentHidden;
 
-  // TODO (S.Panfilov) refactore theese functions to extract the common code
-  function performMove(moveFn: IMoveFn, actor: IActorWrapper, destination: IMoveDestination, animationParams: IAnimationParams): Promise<void> {
+  function performAction<T extends IMoveFn | IMoveByPathFn, P extends Omit<IMoveFnParams, 'complete'> | Omit<IMoveByPathFnParams, 'complete'>>(moveFn: T, params: P): Promise<void> {
     const { promise, resolve } = createDeferredPromise();
-    const params: IMoveFnParams = { actor, destination: prepareDestination(destination, actor), animationParams, complete: resolve };
-    const move = moveFn(params);
+    const move = moveFn({ ...params, complete: resolve } as any);
     const tickSubscription: Subscription = loopService.tick$.subscribe(({ frameTime }: ILoopTimes): void => {
       // TODO (S.Panfilov) CWP what about delta time?
       // TODO (S.Panfilov) I'm not sure if this makes animation independent from frame rate. Need to test.
-      move.tick(frameTime); // frameTime, delta, elapsedTime?
+      move.tick(frameTime);
     });
     return promise.then(() => tickSubscription.unsubscribe());
   }
 
+  function performMove(moveFn: IMoveFn, actor: IActorWrapper, destination: IMoveDestination, animationParams: IAnimationParams): Promise<void> {
+    const params: Omit<IMoveFnParams, 'complete'> = { actor, destination: prepareDestination(destination, actor), animationParams };
+    return performAction(moveFn, params);
+  }
+
   function performMoveByPath(moveFn: IMoveByPathFn, actor: IActorWrapper, path: ReadonlyArray<IKeyframeDestination>, animationParams: IAnimationParams): Promise<void> {
-    const { promise, resolve } = createDeferredPromise();
-    const params: IMoveByPathFnParams = { actor, path: preparePathList(path, actor), animationParams, complete: resolve };
-    const move = moveFn(params);
-    const tickSubscription: Subscription = loopService.tick$.subscribe(({ frameTime }: ILoopTimes): void => {
-      // TODO (S.Panfilov) CWP what about delta time?
-      // TODO (S.Panfilov) I'm not sure if this makes animation independent from frame rate. Need to test.
-      move.tick(frameTime); // frameTime, delta, elapsedTime?
-    });
-    return promise.then(() => tickSubscription.unsubscribe());
+    const params: Omit<IMoveByPathFnParams, 'complete'> = { actor, path: preparePathList(path, actor), animationParams };
+    return performAction(moveFn, params);
   }
 
   return {
