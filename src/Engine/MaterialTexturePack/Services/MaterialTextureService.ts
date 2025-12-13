a9d2c27722adf3e6955adde5ec9761efec2b7429
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:14:35 +0100

    [WIP] todos

--- src/Engine/MaterialTexturePack/Services/MaterialTextureService.ts
+++ src/Engine/MaterialTexturePack/Services/MaterialTextureService.ts
@@ -2,6 +2,7 @@ import type { TMaterialParams, TMaterialProps, TMaterialService, TMaterialWrappe
 import type { TMaterialPackConfig, TMaterialPackParams, TMaterialTexturePack, TMaterialTextureService } from '@/Engine/MaterialTexturePack';
 import type { TTextureService, TTextureUploaded } from '@/Engine/Texture';
 
+// TODO 9.0.0. RESOURCES: add dependencies object
 export function MaterialTextureService(materialService: TMaterialService, textureService: TTextureService): TMaterialTextureService {
   const createAsync = (pack: TMaterialPackParams<TMaterialTexturePack>): Promise<TMaterialWrapper> => {
     return textureService
@@ -13,13 +14,18 @@ export function MaterialTextureService(materialService: TMaterialService, textur
       });
   };
 
-  // const createFromConfigAsync = (packs: ReadonlyArray<TMaterialPackConfig<TMaterialTexturePack>>): void => {
-  //   packs.forEach((pack: TMaterialPackConfig<TMaterialTexturePack>): void => createAsync(pack));
-  // };
-  //
-  // const createFromConfigAsync = (packs: ReadonlyArray<TMaterialPackConfig<TMaterialTexturePack>>): void => {
-  //   packs.forEach((config: TMaterialPackConfig<TMaterialTexturePack>): void => factory.create(factory.configToParams(config)));
-  // };
+  // TODO 9.0.0. RESOURCES: Add factory
+  // TODO 9.0.0. RESOURCES: Add registry (registry will be needed later for create with overrides)
+  // TODO 9.0.0. RESOURCES: Add reactivity
+  const createFromConfigAsync = (packs: ReadonlyArray<TMaterialPackConfig<TMaterialTexturePack>>): void => {
+    packs.forEach((pack: TMaterialPackConfig<TMaterialTexturePack>): void => createAsync(pack));
+  };
+
+  const createFromConfigAsync = (packs: ReadonlyArray<TMaterialPackConfig<TMaterialTexturePack>>): void => {
+    packs.forEach((config: TMaterialPackConfig<TMaterialTexturePack>): void => factory.create(factory.configToParams(config)));
+  };
+
+  // TODO 9.0.0. RESOURCES: add destroyable mixin
 
   return { createAsync, createFromConfigAsync };
 }
