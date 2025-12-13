5497a6e1e2fe8e3de12f8999b4dbd0f0c270565b
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:38:58 +0100

    [WIP] Serialization: Fog adapters

---
+++ src/Engine/Fog/Adapters/EntityToConfig.ts
@@ -0,0 +1,5 @@
+export function QQQQQToConfigSerializer(entity: EEEE): CCCCC {
+  // TODO 15-0-0: implement
+
+  return {};
+}
--- src/Engine/Fog/Adapters/index.ts
+++ src/Engine/Fog/Adapters/index.ts
@@ -1,1 +1,1 @@
-export * from './FogConfigAdapter';
+export * from './ConfigToParams';
