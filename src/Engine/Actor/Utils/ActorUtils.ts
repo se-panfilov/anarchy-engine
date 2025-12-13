c7216e054e9300217d1139bfc465490a1f85fa6e
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:19:16 +0100

    [HEAVY WIP] actor: refactor

--- src/Engine/Actor/Utils/ActorUtils.ts
+++ src/Engine/Actor/Utils/ActorUtils.ts
@@ -1,8 +1,27 @@
-import type { TActor, TActorDependencies, TActorWithPhysicsDependencies, TActorWrapperWithPhysics } from '@/Engine/Actor/Models';
-import { isDefined } from '@/Engine/Utils';
+import type { TActor, TActorDependencies, TActorParams, TActorWithPhysicsDependencies, TActorWrapperWithPhysics } from '@/Engine/Actor/Models';
+import type { TSpatialGridService, TSpatialGridWrapper } from '@/Engine/Spatial';
+import { isDefined, isNotDefined } from '@/Engine/Utils';
 
 export const isActorHasPhysicsBody = (actor: TActor | TActorWrapperWithPhysics): actor is TActorWrapperWithPhysics => isDefined(actor.physicsBody);
 
 export function isBodyServiceDependency(dependencies: TActorDependencies | TActorWithPhysicsDependencies): dependencies is TActorWithPhysicsDependencies {
   return isDefined((dependencies as TActorWithPhysicsDependencies).physicsBodyService);
+}
+
+export function applySpatialGrid(params: TActorParams, actorW: TActor, spatialGridService: TSpatialGridService): void {
+  if (isNotDefined(params.spatial.grid)) return;
+
+  const gridName: string | undefined = params.spatial.grid.getName();
+  if (isNotDefined(gridName)) throw new Error(`Cannot add actor (id: "${actorW.id}") to spatial grid: spatial grid name is not defined`);
+
+  const grid: TSpatialGridWrapper | undefined = spatialGridService.getRegistry().findByName(gridName);
+  if (isNotDefined(grid)) throw new Error(`Cannot add actor (id: "${actorW.id}") to spatial grid: "${gridName}", no such grid found`);
+
+  actorW.spatial.setGrid(grid);
+  grid.addActor(actorW);
+}
+
+export function startCollisions(actorW: TActor): void {
+  if (isNotDefined(actorW.collisions) || !actorW.collisions.isAutoUpdate()) return;
+  actorW.collisions.start(actorW);
 }
