8e98a09fedbaaea696469008a8e5f7554fc6ef95
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:05:27 +0100

    measurmensts fix

--- src/Engine/Measurements/Utils/MeasurementsUtils.spec.ts
+++ src/Engine/Measurements/Utils/MeasurementsUtils.spec.ts
@@ -1,12 +1,12 @@
-import { centimeters, hours, kilometers, meters, minutes, seconds } from './MeasurementsUtils';
+import { centimetersToMeters, hoursToMS, kilometersToMeters, meters, minutesToMS, secondsToMS } from './MeasurementsUtils';
 
 describe('MeasurementsUtils', () => {
-  describe('centimeters', () => {
+  describe('centimetersToMeters', () => {
     it('should convert centimeters to meters', () => {
-      expect(centimeters(1)).toBe(0.01);
-      expect(centimeters(10)).toBe(0.1);
-      expect(centimeters(100)).toBe(1);
-      expect(centimeters(1000)).toBe(10);
+      expect(centimetersToMeters(1)).toBe(0.01);
+      expect(centimetersToMeters(10)).toBe(0.1);
+      expect(centimetersToMeters(100)).toBe(1);
+      expect(centimetersToMeters(1000)).toBe(10);
     });
   });
 
@@ -18,35 +18,35 @@ describe('MeasurementsUtils', () => {
     });
   });
 
-  describe('kilometers', () => {
+  describe('kilometersToMeters', () => {
     it('should convert kilometers to meters', () => {
-      expect(kilometers(1)).toBe(1000);
-      expect(kilometers(100)).toBe(100000);
-      expect(kilometers(999)).toBe(999000);
+      expect(kilometersToMeters(1)).toBe(1000);
+      expect(kilometersToMeters(100)).toBe(100000);
+      expect(kilometersToMeters(999)).toBe(999000);
     });
   });
 
-  describe('Seconds', () => {
+  describe('secondsToMS', () => {
     it('should convert seconds to milliseconds', () => {
-      expect(seconds(1)).toBe(1000);
-      expect(seconds(1000)).toBe(1000000);
-      expect(seconds(999)).toBe(999000);
+      expect(secondsToMS(1)).toBe(1000);
+      expect(secondsToMS(1000)).toBe(1000000);
+      expect(secondsToMS(999)).toBe(999000);
     });
   });
 
-  describe('minutes', () => {
+  describe('minutesToMS', () => {
     it('should convert minutes to milliseconds', () => {
-      expect(minutes(1)).toBe(60000);
-      expect(minutes(100)).toBe(6000000);
-      expect(minutes(999)).toBe(59940000);
+      expect(minutesToMS(1)).toBe(60000);
+      expect(minutesToMS(100)).toBe(6000000);
+      expect(minutesToMS(999)).toBe(59940000);
     });
   });
 
-  describe('hours', () => {
+  describe('hoursToMS', () => {
     it('should convert hours to milliseconds', () => {
-      expect(hours(1)).toBe(3600000);
-      expect(hours(100)).toBe(360000000);
-      expect(hours(999)).toBe(3596400000);
+      expect(hoursToMS(1)).toBe(3600000);
+      expect(hoursToMS(100)).toBe(360000000);
+      expect(hoursToMS(999)).toBe(3596400000);
     });
   });
 });
--- src/Engine/Measurements/Utils/MeasurementsUtils.ts
+++ src/Engine/Measurements/Utils/MeasurementsUtils.ts
@@ -1,9 +1,10 @@
 import { HOUR, METER, MINUTE, SECOND } from '@/Engine/Measurements/Constants';
 
 // TODO (S.Panfilov) MATH: need precision calculations??? (or not? how performant they are?)
-export const centimeters = (cm: number): number => cm / 100;
 export const meters = (meters: number): number => meters * METER;
-export const kilometers = (km: number): number => km * 1000 * METER;
-export const seconds = (seconds: number): number => seconds * SECOND;
-export const minutes = (minutes: number): number => minutes * MINUTE;
-export const hours = (hours: number): number => hours * HOUR;
+export const centimetersToMeters = (cm: number): number => cm / 100;
+export const kilometersToMeters = (km: number): number => km * 1000 * METER;
+
+export const secondsToMS = (seconds: number): number => seconds * SECOND;
+export const minutesToMS = (minutes: number): number => minutes * MINUTE;
+export const hoursToMS = (hours: number): number => hours * HOUR;
