d6c9e758751598e53a3f1654060339f152c02d49
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:13:24 +0100

    [SHOWCASES-I18N]: fix

--- packages/showcases-i18n/src/Services/VueTranslationService.ts
+++ packages/showcases-i18n/src/Services/VueTranslationService.ts
@@ -1,9 +1,10 @@
 import type { TVueTranslationService } from '@Showcases/i18n';
-import { showcasesTranslationService } from '@Showcases/i18n';
 import type { Observable, Subscription } from 'rxjs';
 import { filter } from 'rxjs';
 import type { ShallowRef } from 'vue';
 import { onBeforeUnmount, onMounted, shallowRef } from 'vue';
+
+import { showcasesTranslationService } from './ShowcasesTranslationService';
 
 export function VueTranslationService(): TVueTranslationService {
   const isReadyPromise: Promise<void> = new Promise<void>((resolve, reject): void => {
