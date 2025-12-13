ccd5c51d5d406df1dd6d65a8c02f640783c025d3
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:24:09 +0100

    [WIP] showcase fixes

--- src/App/Levels/Showcase20PhysicsSyncWithModelsTest/showcase.ts
+++ src/App/Levels/Showcase20PhysicsSyncWithModelsTest/showcase.ts
@@ -14,31 +14,33 @@ export async function showcase(canvas: TAppCanvas): Promise<TShowcase> {
   const actorAsyncRegistry = actorService.getRegistry();
 
   function init(): void {
-    const actor1W: TActor | undefined = actorAsyncRegistry.findByName('actor_1');
-    if (isNotDefined(actor1W)) throw new Error(`Cannot find "actor_1" actor`);
+    const actor1: TActor | undefined = actorAsyncRegistry.findByName('actor_1');
+    if (isNotDefined(actor1)) throw new Error(`Cannot find "actor_1" actor`);
 
-    const actor2W: TActor | undefined = actorAsyncRegistry.findByName('actor_2');
-    if (isNotDefined(actor2W)) throw new Error(`Cannot find "actor_2" actor`);
+    const actor2: TActor | undefined = actorAsyncRegistry.findByName('actor_2');
+    if (isNotDefined(actor2)) throw new Error(`Cannot find "actor_2" actor`);
 
-    const actor3W: TActor | undefined = actorAsyncRegistry.findByName('actor_3');
-    if (isNotDefined(actor3W)) throw new Error(`Cannot find "actor_3" actor`);
+    const actor3: TActor | undefined = actorAsyncRegistry.findByName('actor_3');
+    if (isNotDefined(actor3)) throw new Error(`Cannot find "actor_3" actor`);
 
     const cameraW: TCameraWrapper | undefined = cameraService.findActive();
     if (isNotDefined(cameraW)) throw new Error(`Cannot find active camera`);
 
-    actor1W.physicsBody?.getRigidBody()?.addTorque({ x: -0.2, y: 0.5, z: 1 }, true);
-    actor2W.physicsBody?.getRigidBody()?.addTorque({ x: -0.5, y: -0.01, z: 0.05 }, true);
-    actor3W.physicsBody?.getRigidBody()?.addTorque({ x: 0.01, y: 5, z: -0.05 }, true);
+    // TODO CWP: 8.0.0. MODELS: Perhaps, "applyImpulse" (and similar functions) should be available via physical drive
+    actor1.drive.physical.physicsBody$.value?.getRigidBody()?.addTorque({ x: -0.2, y: 0.5, z: 1 }, true);
+    actor2.drive.physical.physicsBody$.value?.getRigidBody()?.addTorque({ x: -0.5, y: -0.01, z: 0.05 }, true);
+    actor3.drive.physical.physicsBody$.value?.getRigidBody()?.addTorque({ x: 0.01, y: 5, z: -0.05 }, true);
 
-    const actor1Position: Vector3 = actor1W.drive.getPosition();
+    const actor1Position: Vector3 = actor1.drive.getPosition();
     cameraW.lookAt(actor1Position);
     cameraW.drive.default.setY(actor1Position.y);
 
     physicsWorldService.getDebugRenderer(loopService).start();
 
     loopService.tick$.subscribe(() => {
-      actor3W.physicsBody?.getRigidBody()?.setAngvel({ x: 0, y: 3, z: 1 }, true);
-      cameraW.drive.default.setY(actor1W.drive.getPosition().y);
+      // TODO CWP: 8.0.0. MODELS: Perhaps, "applyImpulse" (and similar functions) should be available via physical drive
+      actor3.drive.physical.physicsBody$.value?.getRigidBody()?.setAngvel({ x: 0, y: 3, z: 1 }, true);
+      cameraW.drive.default.setY(actor1.drive.getPosition().y);
     });
   }
 
