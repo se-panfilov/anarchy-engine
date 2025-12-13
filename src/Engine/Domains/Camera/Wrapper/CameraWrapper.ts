4fdc38b28fa0ccc8b4db4052a1273a24c3d6b473
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:54:19 +0100

    fixes

--- src/Engine/Domains/Camera/Wrapper/CameraWrapper.ts
+++ src/Engine/Domains/Camera/Wrapper/CameraWrapper.ts
@@ -4,7 +4,7 @@ import { PerspectiveCamera, Vector3 } from 'three';
 import { AbstractWrapper, WrapperType } from '@/Engine/Domains/Abstract';
 import type { ICameraParams, ICameraWrapper, IPerspectiveCamera } from '@/Engine/Domains/Camera/Models';
 import type { IScreenSizeValues, IScreenSizeWatcher } from '@/Engine/Domains/Screen';
-import { withMoveByXyzMixin, withObject3d,withRotationByXyzMixin } from '@/Engine/Mixins';
+import { withMoveByXyzMixin, withObject3d, withRotationByXyzMixin } from '@/Engine/Mixins';
 import { withTags } from '@/Engine/Mixins/Generic/WithTags';
 import type { IWriteable } from '@/Engine/Utils';
 import { applyObject3dParams, applyPosition, applyRotation, isDefined, isNotDefined } from '@/Engine/Utils';
@@ -15,7 +15,6 @@ export function CameraWrapper(params: ICameraParams, screenSizeWatcher: Readonly
   const { fov = 45, near = 1, far = 10000, lookAt, tags }: ICameraParams = params;
   // TODO (S.Panfilov) Test this: aspect is 0 fot now, but should be set by screenSizeWatcher
   const entity: IWriteable<IPerspectiveCamera> = new PerspectiveCamera(fov, 0, near, far);
-  if (isDefined(lookAt)) entity.lookAt(new Vector3(lookAt.entity.x, lookAt.entity.y, lookAt.entity.z));
 
   // eslint-disable-next-line functional/prefer-immutable-types
   function setValues(entity: IWriteable<IPerspectiveCamera>, { width, height }: IScreenSizeValues): void {
@@ -48,6 +47,8 @@ export function CameraWrapper(params: ICameraParams, screenSizeWatcher: Readonly
   applyPosition(result, params.position);
   applyRotation(result, params.rotation);
   applyObject3dParams(result, params);
+
+  if (isDefined(lookAt)) entity.lookAt(new Vector3(lookAt.entity.x, lookAt.entity.y, lookAt.entity.z));
 
   return result;
 }
