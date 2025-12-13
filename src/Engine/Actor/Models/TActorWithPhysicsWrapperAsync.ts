f591e5fc2fcddfbed057fc7e38872299419ea729
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:07:19 +0100

    fixed types

--- src/Engine/Actor/Models/TActorWithPhysicsWrapperAsync.ts
+++ src/Engine/Actor/Models/TActorWithPhysicsWrapperAsync.ts
@@ -1,9 +1,9 @@
-import type { TPhysicsBodyFacade, TWithMaybePhysicsBody } from '@/Engine/Physics';
+import type { TPhysicsBodyFacade, TWithOptionalPhysicsBody } from '@/Engine/Physics';
 
 import type { TActorWrapperAsync } from './TActorWrapperAsync';
 
 export type TActorWithPhysicsWrapperAsync = TActorWrapperAsync &
-  Omit<TWithMaybePhysicsBody, 'physicsBody'> &
+  Omit<TWithOptionalPhysicsBody, 'physicsBody'> &
   Readonly<{
     physicsBody: TPhysicsBodyFacade;
   }>;
--- src/Engine/Actor/Utils/index.ts
+++ src/Engine/Actor/Utils/index.ts
@@ -1,5 +1,6 @@
 import type { TActorWrapperAsync } from '@/Engine/Actor/Models';
-import type { THasPhysicsBody } from '@/Engine/Physics/Models';
+import type { TWithMandatoryPhysicsBody } from '@/Engine/Physics/Models';
 import { isDefined } from '@/Engine/Utils';
 
-export const isActorHasPhysicsBody = (actor: TActorWrapperAsync | THasPhysicsBody<TActorWrapperAsync>): actor is THasPhysicsBody<TActorWrapperAsync> => isDefined(actor.physicsBody);
+export const isActorHasPhysicsBody = (actor: TActorWrapperAsync | TWithMandatoryPhysicsBody<TActorWrapperAsync>): actor is TWithMandatoryPhysicsBody<TActorWrapperAsync> =>
+  isDefined(actor.physicsBody);
