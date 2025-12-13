8c32116306314368d2fa412baf4dd4a13cee6670
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:38:52 +0100

    Serialization: AudioToConfigSerializer

---
+++ src/Engine/Audio/Adapters/AudioToConfigSerializer.ts
@@ -0,0 +1,7 @@
+import type { TAnyAudio, TAnyAudioConfig } from '@/Engine/Audio/Models';
+
+export function AudioToConfigSerializer(entity: TAnyAudio): TAnyAudioConfig {
+  // TODO 15-0-0: implement
+
+  return {};
+}
--- src/Engine/Audio/Adapters/index.ts
+++ src/Engine/Audio/Adapters/index.ts
@@ -1,1 +1,2 @@
 export * from './AudioConfigAdapter';
+export * from './AudioToConfigSerializer';
