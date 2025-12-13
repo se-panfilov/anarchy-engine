b97582faf57c222dfa534f4872b0394a6c28747b
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:59:14 +0100

    [App] todo

--- apps/showcases/src/Levels/Showcase28Menu/MainMenu/MenuMain.ts
+++ apps/showcases/src/Levels/Showcase28Menu/MainMenu/MenuMain.ts
@@ -3,7 +3,6 @@ import { createApp } from 'vue';
 
 import MenuApp from './MenuApp.vue';
 
-// TODO DESKTOP: Make sure lint, ts check and prettier are working with vue files
 const menuApp = createApp(MenuApp);
 
 menuApp.use(createPinia());
