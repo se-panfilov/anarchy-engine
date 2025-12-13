752b3595e91159ff8737f1ba20a1ceb5ff22e9df
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:55:39 +0100

    removed troika

--- src/Engine/Domains/Text/Adapter/TextConfigAdapter.ts
+++ src/Engine/Domains/Text/Adapter/TextConfigAdapter.ts
@@ -1,30 +1,13 @@
-import type { MeshBasicMaterialParameters, MeshStandardMaterialParameters } from 'three';
-import { MeshBasicMaterial, MeshStandardMaterial } from 'three';
-
 import type { ITextConfig, ITextParams } from '@/Engine/Domains/Text/Models';
 import { configToParamsObject3d } from '@/Engine/Domains/ThreeLib';
-import { isDefined } from '@/Engine/Utils';
 
 export function configToParams(config: ITextConfig): ITextParams {
-  const { position, rotation, scale, layers, animations, materialType, materialParams, ...rest } = config;
-  const material: MeshBasicMaterial | MeshStandardMaterial | undefined = materialType ? getTextMaterial(materialType, materialParams) : undefined;
+  const { position, rotation, scale, layers, animations, ...rest } = config;
 
   let result: ITextParams = {
     ...rest,
     ...configToParamsObject3d({ position, rotation, scale, layers, animations })
   };
 
-  if (isDefined(material)) result = { ...result, material };
-
   return result;
-}
-
-function getTextMaterial(
-  materialType: 'MeshBasicMaterial' | 'MeshStandardMaterial',
-  materialParams?: MeshBasicMaterialParameters | MeshStandardMaterialParameters
-): MeshBasicMaterial | MeshStandardMaterial {
-  if (materialType === 'MeshBasicMaterial') {
-    return new MeshBasicMaterial(materialParams);
-  }
-  return new MeshStandardMaterial(materialParams);
 }
