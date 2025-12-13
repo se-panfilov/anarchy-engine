8001e769963f9e0787487b6071fa7d0f6b4835f4
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:49:48 +0100

    pipe

--- src/check4.ts
+++ src/check4.ts
@@ -39,14 +39,31 @@ type PipableFunction<P, T> = (a: P) => T;
 
 type PipeResult<T, Fns extends any[]> = Fns extends [PipableFunction<T, infer U>, ...infer Rest] ? PipeResult<U, Rest> : T;
 
-function pipe<T, Fns extends PipableFunction<any, any>[]>(initial: T, ...fns: Fns): PipeResult<T, Fns> {
-  return fns.reduce((acc: T, fn) => fn(acc), initial) as PipeResult<T, Fns>;
-}
+// function pipe<T, Fns extends PipableFunction<any, any>[]>(initial: T, ...fns: Fns): PipeResult<T, Fns> {
+//   return fns.reduce((acc: T, fn) => fn(acc), initial) as PipeResult<T, Fns>;
+// }
 
 // function pipe<T>(initial: T, ...fns: Array<(arg: T) => T>): T {
 //   return fns.reduce((acc: T, fn) => fn(acc), initial);
 // }
 
-const result: IResult = pipe(getSome(), withName, withCounter);
+type PipeArgs<F extends AnyFunc[], Acc extends AnyFunc[] = []> = F extends [(...args: infer A) => infer B]
+  ? [...Acc, (...args: A) => B]
+  : F extends [(...args: infer A) => any, ...infer Tail]
+  ? Tail extends [(arg: infer B) => any, ...any[]]
+    ? PipeArgs<Tail, [...Acc, (...args: A) => B]>
+    : Acc
+  : Acc;
+
+type AnyFunc = (...arg: any) => any;
+
+type LastFnReturnType<F extends Array<AnyFunc>, Else = never> = F extends [...any[], (...arg: any) => infer R] ? R : Else;
+
+function pipe<FirstFn extends AnyFunc, F extends AnyFunc[]>(arg: Parameters<FirstFn>[0], firstFn: FirstFn, ...fns: PipeArgs<F> extends F ? F : PipeArgs<F>): LastFnReturnType<F, ReturnType<FirstFn>> {
+  return (fns as AnyFunc[]).reduce((acc, fn) => fn(acc), firstFn(arg));
+}
+
+// const result: IResult = pipe(getSome(), withName, withCounter);
+const result: ISome & IWithName & IWithCounter = pipe(getSome(), withName, withCounter);
 
 console.log(result);
