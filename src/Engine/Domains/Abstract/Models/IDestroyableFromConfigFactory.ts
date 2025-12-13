ea92739e6c376851e98b55d4cddf99a1af529f0f
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:49:56 +0100

    fixes

--- src/Engine/Domains/Abstract/Models/IDestroyableFromConfigFactory.ts
+++ src/Engine/Domains/Abstract/Models/IDestroyableFromConfigFactory.ts
@@ -5,6 +5,6 @@ import type { IAbstractConfig } from './IAbstractConfig';
 import type { IAbstractFactory } from './IAbstractFactory';
 import type { IAbstractFromConfigWrapperFactory } from './IAbstractFromConfigWrapperFactory';
 
-export type IDestroyableFromConfigFactory<T extends IWrapper<ENT>, ENT, PRMS, C extends IAbstractConfig, F extends IAbstractFactory<T, PRMS>> = F &
+export type IDestroyableFromConfigFactory<T extends IWrapper<ENT>, ENT, PRMS, C extends IAbstractConfig, F extends IAbstractFactory<T, PRMS>> = INullable<F> &
   INullable<IAbstractFromConfigWrapperFactory<T, ENT, PRMS, C, F>> &
   IDestroyable;
