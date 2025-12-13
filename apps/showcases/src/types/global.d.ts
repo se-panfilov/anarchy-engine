69f1a3874b07ddffaeebcf9694ebf944580bd290
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:57:46 +0100

    [APP] fixes

--- apps/showcases/src/types/global.d.ts
+++ apps/showcases/src/types/global.d.ts
@@ -6,14 +6,14 @@ declare global {
     //Same name as "platformApiName" in desktop/src/Constants/AppToPlatformMessagesConstants.ts
     platformApi: {
       // TODO DESKTOP: Declare all platform API methods here
-      // TODO DESKTOP: any
-      saveAppSettings(settings: any): Promise<void>;
-      // TODO DESKTOP: any
-      loadAppSettings(): Promise<any>;
-      node(): string;
-      chrome(): string;
-      electron(): string;
-      desktopAppVersion(): Promise<string>;
+      // TODO DESKTOP: any (should be TAppSettings)
+      saveAppSettings: (settings: any) => Promise<void>;
+      // TODO DESKTOP: any (should be TAppSettings)
+      loadAppSettings: () => Promise<any>;
+      node: () => string;
+      chrome: () => string;
+      electron: () => string;
+      desktopAppVersion: () => Promise<string>;
     };
   }
 }
