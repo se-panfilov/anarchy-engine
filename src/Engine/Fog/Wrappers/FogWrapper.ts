c861c2bd4cfd913233ded6a4b602fce0b978a976
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:20:10 +0100

    [HEAVY WIP] fog: fix destroy

--- src/Engine/Fog/Wrappers/FogWrapper.ts
+++ src/Engine/Fog/Wrappers/FogWrapper.ts
@@ -1,18 +1,24 @@
+import type { Subscription } from 'rxjs';
 import { Fog } from 'three';
 
 import type { TWrapper } from '@/Engine/Abstract';
 import { AbstractWrapper, WrapperType } from '@/Engine/Abstract';
 import type { TFog, TFogParams, TFogWrapper } from '@/Engine/Fog/Models';
+import type { TDestroyable } from '@/Engine/Mixins';
+import { destroyableMixin } from '@/Engine/Mixins';
 
 export function FogWrapper(params: TFogParams): TFogWrapper {
   const entity: TFog = new Fog(params.color, params.near, params.far);
 
   const wrapper: TWrapper<TFog> = AbstractWrapper(entity, WrapperType.Fog, params);
 
-  function destroy(): void {
+  const destroyable: TDestroyable = destroyableMixin();
+  const destroySub$: Subscription = destroyable.destroy$.subscribe((): void => {
+    destroySub$.unsubscribe();
+
     // TODO DESTROY: implement destroy
     throw new Error('Fog destroy not implemented');
-  }
+  });
 
-  return { ...wrapper, entity, destroy };
+  return { ...wrapper, entity, ...destroyable };
 }
