7e7170f8332f3a4110542ee08ac33a2dee195ac3
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:39:15 +0100

    [WIP] Serialization: fixes

--- src/Engine/Controls/Adapters/EntityToConfig.ts
+++ src/Engine/Controls/Adapters/EntityToConfig.ts
@@ -1,5 +1,9 @@
-export function EntityToConfig(entity: EEEE): CCCCC {
+import type { TControlsConfig, TControlsWrapper } from '@/Engine/Controls/Models';
+
+export function EntityToConfig(entity: TControlsWrapper): TControlsConfig {
   // TODO 15-0-0: implement
+
+  // TODO 15-0-0: implement adapters for FpsControls, OrbitControls
 
   return {};
 }
