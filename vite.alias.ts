cac6be4d9db75eb39f4c6e31ece796422e084c73
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:03:16 +0100

    [E2E] @E2E -> @Showcases/E2E

--- vite.alias.ts
+++ vite.alias.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 
 export const sharedAliases = {
-  '@E2E': path.resolve(__dirname, 'apps/showcases-e2e/src'),
+  '@Showcases/E2E': path.resolve(__dirname, 'apps/showcases-e2e/src'),
   '@Anarchy/Engine': path.resolve(__dirname, 'packages/anarchy-engine/src'),
   '@Menu': path.resolve(__dirname, 'packages/showcases-menu/src'),
   '@Anarchy/Shared': path.resolve(__dirname, 'packages/anarchy-shared/src'),
