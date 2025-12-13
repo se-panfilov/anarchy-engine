83fdb9422b5976fa31c3a7b2369754583426d463
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:59:33 +0100

    fixes for fog

--- src/App/Levels/Showcase13Fog/index.ts
+++ src/App/Levels/Showcase13Fog/index.ts
@@ -1,9 +1,7 @@
 import GUI from 'lil-gui';
-import { CameraHelper, DirectionalLightHelper, HemisphereLightHelper, PointLightHelper, SpotLightHelper } from 'three';
-import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper';
 
 import type { IShowcase } from '@/App/Levels/Models';
-import type { IAppCanvas, IDirectionalLightWrapper, IHemisphereLightWrapper, ILevel, ILevelConfig, IPointLightWrapper, IRectAreaLightWrapper, ISceneWrapper, ISpotLightWrapper } from '@/Engine';
+import type { IAppCanvas, ILevel, ILevelConfig, ISceneWrapper } from '@/Engine';
 import { buildLevelFromConfig, isNotDefined } from '@/Engine';
 
 import levelConfig from './showcase-13-fog.json';
@@ -12,11 +10,15 @@ import levelConfig from './showcase-13-fog.json';
 export function showcaseLevel(canvas: IAppCanvas): IShowcase {
   const gui: GUI = new GUI();
   const level: ILevel = buildLevelFromConfig(canvas, levelConfig as ILevelConfig);
-  const { lightRegistry, scenesRegistry } = level.entities;
+  const { scenesRegistry } = level.entities;
 
   function init(): void {
     const scene: ISceneWrapper | undefined = scenesRegistry.getUniqByTag('current');
     if (isNotDefined(scene)) throw new Error('Scene not found');
+
+    gui.addColor(scene.entity.fog, 'color');
+    gui.add(scene.entity.fog, 'near').min(0).max(1).step(0.1);
+    gui.add(scene.entity.fog, 'far').min(0).max(100).step(1);
   }
 
   function start(): void {
