563703d37f5be174e4ac0635ac596ce30a7351a1
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:21:21 +0100

    [APP] showcase test for keyborad events on menu is on

---
+++ apps/showcases-core/src/Levels/Showcase28Menu/Helpers/InputActors.ts
@@ -0,0 +1,73 @@
+import type {
+  TActor,
+  TActorService,
+  TIntersectionEvent,
+  TIntersectionsCameraWatcher,
+  TIntersectionsWatcherService,
+  TKeyboardPressingEvent,
+  TKeyboardService,
+  TMouseService,
+  TMouseWatcherEvent
+} from '@Anarchy/Engine';
+import { KeyCode, LookUpStrategy, metersPerSecond, mpsSpeed } from '@Anarchy/Engine';
+import { isNotDefined } from '@Anarchy/Shared/Utils';
+import { withLatestFrom } from 'rxjs';
+import { Vector3 } from 'three';
+
+export function initInputActors(actorService: TActorService, keyboardService: TKeyboardService, mouseService: TMouseService, intersectionsWatcherService: TIntersectionsWatcherService): void {
+  const { getByName, getByTags } = actorService.getRegistry();
+  const { onKey } = keyboardService;
+
+  const actorKeyboard: TActor = getByName('sphere_keyboard_actor');
+  const actorMouse: TActor = getByName('sphere_mouse_actor');
+  const actorKeyW: TActor = getByTags(['key', 'W'], LookUpStrategy.Every);
+  const actorKeyA: TActor = getByTags(['key', 'A'], LookUpStrategy.Every);
+  const actorKeyS: TActor = getByTags(['key', 'S'], LookUpStrategy.Every);
+  const actorKeyD: TActor = getByTags(['key', 'D'], LookUpStrategy.Every);
+  const actorMkeyLeft: TActor = getByTags(['mkey', 'Left'], LookUpStrategy.Every);
+  const actorMkeyRight: TActor = getByTags(['mkey', 'Right'], LookUpStrategy.Every);
+  const actorMkeyMiddle: TActor = getByTags(['mkey', 'Middle'], LookUpStrategy.Every);
+  const actorMkeyBack: TActor = getByTags(['mkey', 'Back'], LookUpStrategy.Every);
+  const actorMkeyForward: TActor = getByTags(['mkey', 'Forward'], LookUpStrategy.Every);
+  const actorMkeyExtra: TActor = getByTags(['mkey', 'Extra'], LookUpStrategy.Every);
+
+  onKey(KeyCode.W).pressing$.subscribe(({ delta }: TKeyboardPressingEvent): void => void actorKeyboard.drive.default.addZ(mpsSpeed(metersPerSecond(-10), delta)));
+  onKey(KeyCode.W).pressed$.subscribe((): void => void actorKeyW.drive.default.addY(-0.2));
+  onKey(KeyCode.W).released$.subscribe((): void => void actorKeyW.drive.default.addY(0.2));
+
+  onKey(KeyCode.A).pressing$.subscribe(({ delta }: TKeyboardPressingEvent): void => void actorKeyboard.drive.default.addX(mpsSpeed(metersPerSecond(-10), delta)));
+  onKey(KeyCode.A).pressed$.subscribe((): void => void actorKeyA.drive.default.addY(-0.2));
+  onKey(KeyCode.A).released$.subscribe((): void => void actorKeyA.drive.default.addY(0.2));
+
+  onKey(KeyCode.S).pressing$.subscribe(({ delta }: TKeyboardPressingEvent): void => void actorKeyboard.drive.default.addZ(mpsSpeed(metersPerSecond(10), delta)));
+  onKey(KeyCode.S).pressed$.subscribe((): void => void actorKeyS.drive.default.addY(-0.2));
+  onKey(KeyCode.S).released$.subscribe((): void => void actorKeyS.drive.default.addY(0.2));
+
+  onKey(KeyCode.D).pressing$.subscribe(({ delta }: TKeyboardPressingEvent): void => void actorKeyboard.drive.default.addX(mpsSpeed(metersPerSecond(10), delta)));
+  onKey(KeyCode.D).pressed$.subscribe((): void => void actorKeyD.drive.default.addY(-0.2));
+  onKey(KeyCode.D).released$.subscribe((): void => void actorKeyD.drive.default.addY(0.2));
+
+  const watcherSurface: TIntersectionsCameraWatcher = intersectionsWatcherService.getCameraWatcher('watcher_surface');
+
+  const { clickLeftRelease$, isLeftPressed$, isRightPressed$, isMiddlePressed$, isBackPressed$, isForwardPressed$, isExtraPressed$, doubleLeftClick$, doubleRightClick$, wheelUp$, wheelDown$ } =
+    mouseService;
+
+  clickLeftRelease$.pipe(withLatestFrom(watcherSurface.value$)).subscribe(([, intersection]: [TMouseWatcherEvent, TIntersectionEvent]): void => {
+    if (isNotDefined(intersection)) throw new Error('Intersection not defined');
+    const position: Vector3 = intersection.point.clone().add(new Vector3(0, 1.5, 0));
+    actorMouse.drive.kinematic.moveTo(position, metersPerSecond(15));
+  });
+
+  isLeftPressed$.subscribe((isPressed: boolean): void => void actorMkeyLeft.drive.default.addY(isPressed ? -0.2 : 0.2));
+  isRightPressed$.subscribe((isPressed: boolean): void => void actorMkeyRight.drive.default.addY(isPressed ? -0.2 : 0.2));
+  isMiddlePressed$.subscribe((isPressed: boolean): void => void actorMkeyMiddle.drive.default.addY(isPressed ? -0.2 : 0.2));
+  isBackPressed$.subscribe((isPressed: boolean): void => void actorMkeyBack.drive.default.addY(isPressed ? -0.2 : 0.2));
+  isForwardPressed$.subscribe((isPressed: boolean): void => void actorMkeyForward.drive.default.addY(isPressed ? -0.2 : 0.2));
+  isExtraPressed$.subscribe((isPressed: boolean): void => void actorMkeyExtra.drive.default.addY(isPressed ? -0.2 : 0.2));
+
+  doubleLeftClick$.subscribe((event: TMouseWatcherEvent): void => console.log('double click left', event));
+  doubleRightClick$.subscribe((event: TMouseWatcherEvent): void => console.log('double click right', event));
+
+  wheelUp$.subscribe((): void => actorMkeyMiddle.drive.default.adjustRotationByX(10));
+  wheelDown$.subscribe((): void => actorMkeyMiddle.drive.default.adjustRotationByX(-10));
+}
--- apps/showcases-core/src/Levels/Showcase28Menu/showcase.ts
+++ apps/showcases-core/src/Levels/Showcase28Menu/showcase.ts
@@ -1,11 +1,10 @@
-import type { TActor, TIntersectionEvent, TIntersectionsCameraWatcher, TModel3d, TModels3dRegistry, TMouseWatcherEvent, TSceneWrapper, TSpace, TSpaceConfig } from '@Anarchy/Engine';
-import { metersPerSecond, spaceService } from '@Anarchy/Engine';
+import type { TIntersectionEvent, TIntersectionsCameraWatcher, TModel3d, TModels3dRegistry, TSceneWrapper, TSpace, TSpaceConfig } from '@Anarchy/Engine';
+import { spaceService } from '@Anarchy/Engine';
 import { asRecord, isNotDefined } from '@Anarchy/Shared/Utils';
 import { showcasesTranslationService } from '@Showcases/i18n';
-import { filter, Subject, withLatestFrom } from 'rxjs';
+import { filter, Subject } from 'rxjs';
 import { initGuiApp } from 'showcases-gui/src/main';
 import { initMenuApp } from 'showcases-menu/src/main';
-import { Vector3 } from 'three';
 
 import { runtimeEnv } from '@/env';
 import { fromGuiEventsBus$, fromMenuEventsBus$, toGuiEventsBus$, toMenuEventsBus$ } from '@/Levels/Showcase28Menu/EventsBus';
@@ -15,6 +14,7 @@ import type { TAppSettings } from '@/Models';
 import { addGizmo } from '@/Utils';
 
 import spaceConfigJson from './space.json';
+import { initInputActors } from '@/Levels/Showcase28Menu/Helpers';
 
 const spaceConfig: TSpaceConfig = spaceConfigJson as TSpaceConfig;
 
@@ -27,10 +27,9 @@ export function start(settings: TAppSettings): void {
 }
 
 export function showcase(space: TSpace): void {
-  const { actorService, models3dService, scenesService, textService, intersectionsWatcherService, mouseService } = space.services;
+  const { actorService, models3dService, keyboardService, scenesService, textService, intersectionsWatcherService, mouseService } = space.services;
   const models3dRegistry: TModels3dRegistry = models3dService.getRegistry();
   const { clickLeftRelease$ } = mouseService;
-  const { getByName } = actorService.getRegistry();
   const sceneW: TSceneWrapper = scenesService.getActive();
   const openMenu$: Subject<boolean> = new Subject<boolean>();
 
@@ -62,23 +61,17 @@ export function showcase(space: TSpace): void {
   initGuiApp('#gui', fromGuiEventsBus$, toGuiEventsBus$.asObservable());
 
   const watcherMenuCube: TIntersectionsCameraWatcher = intersectionsWatcherService.getCameraWatcher('watcher_menu_cube');
-  const watcherSurface: TIntersectionsCameraWatcher = intersectionsWatcherService.getCameraWatcher('watcher_surface');
 
   // TODO DESKTOP: Implement also UI (health, ammo, etc.) and make sure it is not under the menu
   let isMouseOverMenuCube: boolean = false;
   watcherMenuCube.value$.subscribe((value: TIntersectionEvent): void => void (isMouseOverMenuCube = !!value));
-
   clickLeftRelease$.pipe(filter((): boolean => isMouseOverMenuCube)).subscribe((): void => openMenu$.next(true));
 
-  const actorMouse: TActor = getByName('sphere_mouse_actor');
-  clickLeftRelease$.pipe(withLatestFrom(watcherSurface.value$)).subscribe(([, intersection]: [TMouseWatcherEvent, TIntersectionEvent]): void => {
-    if (isNotDefined(intersection)) throw new Error('Intersection not defined');
-    const position: Vector3 = intersection.point.clone().add(new Vector3(0, 1.5, 0));
-    actorMouse.drive.kinematic.moveTo(position, metersPerSecond(15));
-  });
   guiService.openGui();
 
   openMenu$.pipe().subscribe(mainMenuService.openMainMenu);
+
+  initInputActors(actorService, keyboardService, mouseService, intersectionsWatcherService);
 
   space.start$.next(true);
 }
