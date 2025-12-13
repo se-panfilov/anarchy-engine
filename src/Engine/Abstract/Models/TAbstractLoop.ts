98564c24f019c3ce2e3edd103e208563d1b4226f
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:30:09 +0100

    Loop: removed extra

--- src/Engine/Abstract/Models/TAbstractLoop.ts
+++ src/Engine/Abstract/Models/TAbstractLoop.ts
@@ -1,12 +1,5 @@
 import type { Subject } from 'rxjs';
 
-// TODO 10.0.0. LOOPS: Check if this needed or required refactoring
 export type TAbstractLoop<T> = Readonly<{
   tick$: Subject<T>;
 }>;
-
-// TODO 10.0.0. LOOPS: Check if this needed or required refactoring
-export type TAbstractReadonlyLoopWith<T> = Omit<TAbstractLoop<T>, 'tick$'> &
-  Readonly<{
-    tick$: Subject<T>;
-  }>;
--- src/Engine/Loop/Models/TLoop.ts
+++ src/Engine/Loop/Models/TLoop.ts
@@ -1,14 +1,13 @@
 import type { BehaviorSubject } from 'rxjs';
 
-import type { TAbstractReadonlyLoopWith } from '@/Engine/Abstract';
+import type { TAbstractLoop } from '@/Engine/Abstract';
 import type { LoopTrigger, LoopType } from '@/Engine/Loop/Constants';
 import type { TDestroyable, TRegistrable, TWithName } from '@/Engine/Mixins';
 
 import type { TDelta } from './TDelta';
 import type { TLoopTriggerFn } from './TLoopTriggerFn';
 
-// TODO 10.0.0. LOOPS: get rid of extra abstract loops (or refactor and fix inheritance)
-export type TLoop = TAbstractReadonlyLoopWith<TDelta> &
+export type TLoop = TAbstractLoop<TDelta> &
   Readonly<{
     type: LoopType;
     triggerMode: LoopTrigger;
