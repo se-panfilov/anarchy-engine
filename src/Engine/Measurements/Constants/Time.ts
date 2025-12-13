0c511b0edffa94279ff29478cfd3463ba725dd8d
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:25:42 +0100

    physics: fixed azimuth

--- src/Engine/Measurements/Constants/Time.ts
+++ src/Engine/Measurements/Constants/Time.ts
@@ -1,4 +1,6 @@
-export const MILLISECOND: number = 1;
-export const SECOND: number = 1000;
-export const MINUTE: number = 60000;
-export const HOUR: number = 3600000;
+import type { TMilliseconds } from '@/Engine/Math/Types';
+
+export const MILLISECOND: TMilliseconds = 1 as TMilliseconds;
+export const SECOND: TMilliseconds = 1000 as TMilliseconds;
+export const MINUTE: TMilliseconds = 60000 as TMilliseconds;
+export const HOUR: TMilliseconds = 3600000 as TMilliseconds;
