6eebb936e9cd078cd313440e56eb5cd6415fce41
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:23:00 +0100

    [ENGINE] removed @ character on key codes

--- packages/anarchy-engine/src/Keyboard/Constants/KeysExtra.ts
+++ packages/anarchy-engine/src/Keyboard/Constants/KeysExtra.ts
@@ -1,16 +1,16 @@
 export enum KeysExtra {
   PauseBreak = 'PauseBreak', //19
-  Space = '@Space', //32
-  TAB = '@TAB', //9
-  Enter = '@Enter', //13
-  Escape = '@Escape', //27
-  Backspace = '@Backspace', //8
-  Delete = '@Delete', //46
-  Insert = '@Insert', //45 ?????????
-  Home = '@Home', //36
-  End = '@End', //35
-  PageUp = '@PageUp', //33
-  PageDown = '@PageDown', //34
+  Space = 'Space', //32
+  TAB = 'TAB', //9
+  Enter = 'Enter', //13
+  Escape = 'Escape', //27
+  Backspace = 'Backspace', //8
+  Delete = 'Delete', //46
+  Insert = 'Insert', //45 ?????????
+  Home = 'Home', //36
+  End = 'End', //35
+  PageUp = 'PageUp', //33
+  PageDown = 'PageDown', //34
   Shift = 'Shift', //16 ?????????
 
   ArrowLeft = 'ArrowLeft', //37
