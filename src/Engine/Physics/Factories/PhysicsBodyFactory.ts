4481e19b794f1447abe0d4d240f461a3a051ab5e
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:06:47 +0100

    fixes

--- src/Engine/Physics/Factories/PhysicsBodyFactory.ts
+++ src/Engine/Physics/Factories/PhysicsBodyFactory.ts
@@ -1,10 +1,8 @@
 import type { TReactiveFactory } from '@/Engine/Abstract';
 import { FactoryType, ReactiveFactory } from '@/Engine/Abstract';
 import { configToParamsPreset } from '@/Engine/Physics/Adapters';
-import type { TPhysicsBody, TPhysicsBodyFactory } from '@/Engine/Physics/Models';
+import type { TPhysicsBody, TPhysicsBodyFactory, TPhysicsBodyParams } from '@/Engine/Physics/Models';
 import { PhysicsBodyFacade } from '@/Engine/Physics/Wrappers';
-
-import type { TPhysicsBodyParams } from './TPhysicsBodyParams';
 
 const factory: TReactiveFactory<TPhysicsBody, TPhysicsBodyParams> = { ...ReactiveFactory(FactoryType.PhysicsBody, PhysicsBodyFacade) };
 export const PhysicsBodyFactory = (): TPhysicsBodyFactory => ({ ...factory, configToParams: configToParamsPreset });
