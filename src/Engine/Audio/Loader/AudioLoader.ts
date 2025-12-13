00a9b8d564f3a3974897621c83ce7e93f24ba036
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:32:43 +0100

    Todoes

--- src/Engine/Audio/Loader/AudioLoader.ts
+++ src/Engine/Audio/Loader/AudioLoader.ts
@@ -4,7 +4,6 @@ import { AbstractLoader, LoaderType } from '@/Engine/Abstract';
 import type { TAudioLoader, TAudioResourceAsyncRegistry } from '@/Engine/Audio/Models';
 
 export function AudioLoader(registry: TAudioResourceAsyncRegistry): TAudioLoader {
-  // TODO 11.0.0: add loading manager
   const audioLoader: ThreeAudioLoader = new ThreeAudioLoader();
   return AbstractLoader(audioLoader, registry, LoaderType.Audio);
 }
