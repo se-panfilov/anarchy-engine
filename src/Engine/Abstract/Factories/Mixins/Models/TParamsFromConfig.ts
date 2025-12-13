9a35b46f56247c84b975af129abe1884bb10f4ad
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:35:02 +0100

    [WIP] TS fixes

--- src/Engine/Abstract/Factories/Mixins/Models/TParamsFromConfig.ts
+++ src/Engine/Abstract/Factories/Mixins/Models/TParamsFromConfig.ts
@@ -1,4 +1,4 @@
-export type TConfigToParamsWithDependenciesFn<C, P, D = undefined> = (config: C, dependencies?: D) => P;
+export type TConfigToParamsWithDependenciesFn<C, P, D = undefined> = (config: C, dependencies: D) => P;
 
 export type TParamsFromConfigWithDependencies<C, P, D> = Readonly<{
   configToParams: TConfigToParamsWithDependenciesFn<C, P, D>;
