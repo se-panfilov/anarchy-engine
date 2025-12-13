58281e18eadfb3bc1a2bfd379c0e013ee106ca0c
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:06:39 +0100

    [LEGAL] Generator: fix

--- packages/anarchy-legal/src/Models/TAnarchyLegalConfigEntry.ts
+++ packages/anarchy-legal/src/Models/TAnarchyLegalConfigEntry.ts
@@ -1,9 +1,8 @@
-import type { LegalDocumentType } from '@Anarchy/Legal/Constants';
-
+import type { TLegalDocumentType } from './TLegalDocumentType.ts';
 import type { TTemplateMessages } from './TTemplateMessages.ts';
 
 export type TAnarchyLegalConfigEntry = Readonly<{
-  type: 'GENERIC' | LegalDocumentType;
+  type: 'GENERIC' | TLegalDocumentType;
   template?: string; // base name without extension, e.g. "DISCLAIMER_COMMERCIAL_TEMPLATE"
   messages?: TTemplateMessages;
 }>;
