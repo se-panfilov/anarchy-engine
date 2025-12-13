d7e77a260d966a0c1380cd1ea085e451623f6913
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:31:05 +0100

    Vectors: safer readonly vector

--- src/Engine/ThreeLib/Models/TReadonlyQuaternion.ts
+++ src/Engine/ThreeLib/Models/TReadonlyQuaternion.ts
@@ -1,3 +1,3 @@
 import type { Quaternion } from 'three';
 
-export type TReadonlyQuaternion = Readonly<Quaternion>;
+export type TReadonlyQuaternion = Readonly<Pick<Quaternion, 'x' | 'y' | 'z' | 'w' | 'clone' | 'equals' | 'dot' | 'toArray'>>;
