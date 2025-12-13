2d938c44b01657e34bf0c17aba98797c21f63d65
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:19:01 +0100

    [APP] removed extra

--- apps/showcases-core/src/Levels/Showcase28Menu/showcase.ts
+++ apps/showcases-core/src/Levels/Showcase28Menu/showcase.ts
@@ -67,8 +67,3 @@ export function showcase(space: TSpace): void {
 
   space.start$.next(true);
 }
-
-// TODO DEBUG
-setTimeout(() => {
-  throw new Error('[DEBUG] test: uncaughtException');
-}, 1000);
