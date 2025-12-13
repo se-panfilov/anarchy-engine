b73371eb00e746fe1b98457ffc274a1a8172ac14
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:16:07 +0100

    [HEAVY WIP] space: fixes

--- src/Engine/Space/Models/TSpaceConfigResources.ts
+++ src/Engine/Space/Models/TSpaceConfigResources.ts
@@ -1,11 +1,11 @@
 import type { TEnvMapResourceConfig } from '@/Engine/EnvMap';
-import type { TMaterialConfig } from '@/Engine/Material';
+import type { TMaterialPackConfig, TMaterialTexturePack } from '@/Engine/Material';
 import type { TModel3dResourceConfig } from '@/Engine/Models3d';
 import type { TTextureResourceConfig } from '@/Engine/Texture';
 
 export type TSpaceConfigResources = Readonly<{
   envMapTextures: ReadonlyArray<TEnvMapResourceConfig>;
-  materials: ReadonlyArray<TMaterialConfig>;
+  materials: ReadonlyArray<TMaterialPackConfig<TMaterialTexturePack>>;
   models3d: ReadonlyArray<TModel3dResourceConfig>;
   textures: ReadonlyArray<TTextureResourceConfig>;
 }>;
