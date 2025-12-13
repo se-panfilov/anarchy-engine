d0aa8f8debb21908a613ac18009e172a7540fde8
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:24:23 +0100

    [WIP] fixed params

--- src/Engine/TransformDrive/Models/TTransformDriveParams.ts
+++ src/Engine/TransformDrive/Models/TTransformDriveParams.ts
@@ -3,6 +3,7 @@ import type { TransformAgent } from '@/Engine/TransformDrive/Constants';
 import type { TTransformDrivePerformanceOptions } from './TTransformDrivePerformanceOptions';
 
 export type TTransformDriveParams = Readonly<{
+  relatedEntityId: string;
   activeAgent?: TransformAgent;
   performance?: TTransformDrivePerformanceOptions;
 }>;
