b8915101a3ae1df2abb36032b688a8c988f0a126
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:29:57 +0100

    removed extra

--- configs/EsLint/Rules/Functional/FunctionalConfig.mjs
+++ configs/EsLint/Rules/Functional/FunctionalConfig.mjs
@@ -1,18 +1,4 @@
 import { FunctionalRules as rules } from './FunctionalRules.mjs';
 import functional from 'eslint-plugin-functional';
-import eslintTs from 'typescript-eslint';
 
-export const FunctionalConfig = [
-  functional.configs.recommended,
-  functional.configs.stylistic,
-  // {
-  //   languageOptions: {
-  //     parserOptions: {
-  //       parser: eslintTs,
-  //       projectService: true,
-  //       tsConfig: import.meta.dirname
-  //     }
-  //   }
-  // },
-  { rules }
-];
+export const FunctionalConfig = [functional.configs.recommended, functional.configs.stylistic, { rules }];
