76d6047d1c6f7dccf8108e457f58322cb0ed0b0a
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:50:25 +0100

    fixes

--- src/Engine/Domains/Abstract/Registry/Mixin/Models/IGenericDestroyableFactory.ts
+++ src/Engine/Domains/Abstract/Registry/Mixin/Models/IGenericDestroyableFactory.ts
@@ -1,4 +1,4 @@
-import type { IAbstractConfig, IAbstractFactory, IWrapper } from '@Engine/Domains/Abstract';
+import type { IAbstractFactory, IWrapper } from '@Engine/Domains/Abstract';
 import type { IDestroyableFactory } from '@Engine/Domains/Mixins';
 
-export type IGenericDestroyableFactory<T extends IWrapper<E>, E, P, F extends IAbstractFactory<T, P>, C extends IAbstractConfig = void> = F & IDestroyableFactory<T, E, P, F>;
+export type IGenericDestroyableFactory<T extends IWrapper<E>, E, P, F extends IAbstractFactory<T, P>> = F & IDestroyableFactory<T, E, P, F>;
--- src/Engine/Utils/DestroyableUtils.ts
+++ src/Engine/Utils/DestroyableUtils.ts
@@ -1,9 +1,7 @@
-import type { IAbstractConfig, IGenericDestroyableFactory, IWrapper } from '@Engine/Domains/Abstract';
+import type { IGenericDestroyableFactory, IWrapper } from '@Engine/Domains/Abstract';
 
 import { isEmptyObject } from './ObjectUtils';
 
-export function isDestroyedFactory<T extends Record<string, unknown>, R extends IGenericDestroyableFactory<W, E, never, never, C>, W extends IWrapper<E>, E, C extends IAbstractConfig = void>(
-  obj: T | R
-): obj is R {
+export function isDestroyedFactory<T extends Record<string, unknown>, R extends IGenericDestroyableFactory<W, E, never, never>, W extends IWrapper<E>, E>(obj: T | R): obj is R {
   return isEmptyObject(obj);
 }
