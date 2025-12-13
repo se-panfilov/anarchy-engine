1d23846e062b311adcba32a971d26140944ecb10
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:13:55 +0100

    [UNIT] enabled unit testing

--- packages/anarchy-shared/src/Utils/ObjectUtils.ts
+++ packages/anarchy-shared/src/Utils/ObjectUtils.ts
@@ -1,4 +1,3 @@
-// TODO DESKTOP: Make sure unit tests are running at the shared code
 export function cleanObject<T extends Record<string, unknown>>(obj: T): void {
   Object.keys(obj).forEach((key: keyof T): void => {
     // eslint-disable-next-line functional/immutable-data
