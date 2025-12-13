b7e0f2df97b65a58b797ce9c36907b9541fc4dbd
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:51:07 +0100

    better actor accessors

--- src/Engine/Domains/Actor/Models/IActorAccessors.ts
+++ src/Engine/Domains/Actor/Models/IActorAccessors.ts
@@ -2,5 +2,8 @@ import type { IVector3 } from '@Engine/Wrappers/Vector3Wrapper';
 
 export type IActorAccessors = Readonly<{
   setPosition: (x: number, y: number, z: number) => IVector3;
+  setX: (x: number) => number;
+  setY: (y: number) => number;
+  setZ: (z: number) => number;
   setCastShadow: (value: boolean) => boolean;
 }>;
--- src/Engine/Domains/Actor/Wrapper/Accessors.ts
+++ src/Engine/Domains/Actor/Wrapper/Accessors.ts
@@ -7,7 +7,13 @@ import type { IActorAccessors, IMesh } from '../Models';
 export function getAccessors(entity: IWriteable<IMesh>): IActorAccessors {
   const setPosition = (x: number, y: number, z: number): IVector3 => entity.position.set(x, y, z);
   // eslint-disable-next-line functional/immutable-data
+  const setX = (x: number): number => (entity.position.x = x);
+  // eslint-disable-next-line functional/immutable-data
+  const setY = (y: number): number => (entity.position.y = y);
+  // eslint-disable-next-line functional/immutable-data
+  const setZ = (z: number): number => (entity.position.z = z);
+  // eslint-disable-next-line functional/immutable-data
   const setCastShadow = (value: boolean): boolean => (entity.castShadow = value);
 
-  return { setPosition, setCastShadow };
+  return { setPosition, setX, setY, setZ, setCastShadow };
 }
