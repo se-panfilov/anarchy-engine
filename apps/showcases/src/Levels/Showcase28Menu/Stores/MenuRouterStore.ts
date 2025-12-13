16c2d8416e1c81c0dff71653bf0c84badc926786
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:58:43 +0100

    [App] fixes for pinia-based router

--- apps/showcases/src/Levels/Showcase28Menu/Stores/MenuRouterStore.ts
+++ apps/showcases/src/Levels/Showcase28Menu/Stores/MenuRouterStore.ts
@@ -1,34 +1,29 @@
 import { defineStore } from 'pinia';
-import { reactive } from 'vue';
+import type { ComputedRef } from 'vue';
+import { computed, reactive } from 'vue';
 
 import { MenuRoutes } from '@/Levels/Showcase28Menu/Constants';
 
 export const useMenuRouterStore = defineStore('menuRouter', () => {
-  const state = reactive({
-    route: MenuRoutes.Home,
-    history: [] as ReadonlyArray<MenuRoutes>
+  const state: { history: ReadonlyArray<MenuRoutes> } = reactive({
+    history: [MenuRoutes.Home]
   });
 
-  function go(value: MenuRoutes): void {
+  const currRoute: ComputedRef<MenuRoutes> = computed(() => state.history[state.history.length - 1]);
+
+  function go(to: MenuRoutes): void {
     // eslint-disable-next-line functional/immutable-data
-    state.route = value;
-    // eslint-disable-next-line functional/immutable-data
-    state.history = [...state.history, value];
+    state.history = [...state.history, to];
   }
 
-  function getPrev(): MenuRoutes {
-    if (state.history.length > 1) return state.history[state.history.length - 2];
-    return MenuRoutes.Home;
-  }
+  const prevRoute = computed(() => {
+    return state.history.length > 1 ? state.history[state.history.length - 2] : MenuRoutes.Home;
+  });
 
   function goBack(): void {
-    if (state.history.length > 1) {
-      // eslint-disable-next-line functional/immutable-data
-      state.route = getPrev();
-      // eslint-disable-next-line functional/immutable-data
-      state.history = state.history.slice(0, -1);
-    }
+    // eslint-disable-next-line functional/immutable-data
+    if (state.history.length > 1) state.history = state.history.slice(0, -1);
   }
 
-  return { go, getPrev, goBack };
+  return { go, prevRoute, currRoute, goBack };
 });
