3e36770486dca2bb4b9a4052dba7641c7e46900a
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:21:08 +0100

    [HEAVY WIP] actor: refactored actor transform drive

--- src/Engine/Actor/TransformDrive/ActorTransfromDrive.ts
+++ src/Engine/Actor/TransformDrive/ActorTransfromDrive.ts
@@ -1,10 +1,11 @@
 import { Vector3 } from 'three';
 
 import type { TActorParams } from '@/Engine/Actor/Models';
-import type { TKinematicLoopService } from '@/Engine/Kinematic';
+import type { TKinematicData, TKinematicDataParams, TKinematicLoopService } from '@/Engine/Kinematic';
 import type {
   TInstantTransformAgent,
   TKinematicTransformAgent,
+  TKinematicTransformAgentParams,
   TPhysicsTransformAgent,
   TTransformAgentParams,
   TTransformAgents,
@@ -17,11 +18,31 @@ import { KinematicTransformAgent } from '@/Engine/TransformDrive/Agents/Kinemati
 import { PhysicsTransformAgent } from '@/Engine/TransformDrive/Agents/PhysicsTransformAgent';
 
 export function ActorTransformDrive(params: TActorParams, kinematicLoopService: TKinematicLoopService): TTransformDrive {
-  const agentParams: TTransformAgentParams = { position: params.position, rotation: params.rotation, scale: params.scale ?? new Vector3(1, 1, 1) };
+  const transformAgents: TTransformAgents = getTransformAgents(params, kinematicLoopService);
   const driveParams: TTransformDriveParams = { activeAgent: params.agent ?? TransformAgent.Instant };
-  const kinematicTransformAgent: TKinematicTransformAgent = KinematicTransformAgent(agentParams, kinematicLoopService);
+  return TransformDrive(driveParams, transformAgents);
+}
+
+function getTransformAgents(params: TActorParams, kinematicLoopService: TKinematicLoopService): TTransformAgents {
+  const agentParams: TTransformAgentParams = { position: params.position, rotation: params.rotation, scale: params.scale ?? new Vector3(1, 1, 1) };
+  const kinematicTransformAgent: TKinematicTransformAgent = getKinematicTransformAgent(agentParams, params.kinematic, kinematicLoopService);
   const physicsTransformAgent: TPhysicsTransformAgent = PhysicsTransformAgent(agentParams);
   const instantTransformAgent: TInstantTransformAgent = InstantTransformAgent(agentParams);
-  const transformAgents: TTransformAgents = { [TransformAgent.Kinematic]: kinematicTransformAgent, [TransformAgent.Physical]: physicsTransformAgent, [TransformAgent.Instant]: instantTransformAgent };
-  return TransformDrive(driveParams, transformAgents);
+  return { [TransformAgent.Kinematic]: kinematicTransformAgent, [TransformAgent.Physical]: physicsTransformAgent, [TransformAgent.Instant]: instantTransformAgent };
+}
+
+function getKinematicTransformAgent(params: TTransformAgentParams, kinematic: TKinematicDataParams | undefined, kinematicLoopService: TKinematicLoopService): TKinematicTransformAgent {
+  const agentParams: TTransformAgentParams = { position: params.position, rotation: params.rotation, scale: params.scale ?? new Vector3(1, 1, 1) };
+  const kinematicData: TKinematicData = getKinematicWithDefaults(kinematic);
+  const kinematicAgentParams: TKinematicTransformAgentParams = { ...agentParams, ...kinematicData, isAutoUpdate: kinematic?.isAutoUpdate ?? true };
+  return KinematicTransformAgent(kinematicAgentParams, kinematicLoopService);
+}
+
+function getKinematicWithDefaults(kinematic: TKinematicDataParams | undefined): TKinematicData {
+  return {
+    linearSpeed: kinematic?.linearSpeed ?? 0,
+    linearDirection: kinematic?.linearDirection ?? new Vector3(),
+    angularSpeed: kinematic?.angularSpeed ?? 0,
+    angularDirection: kinematic?.angularDirection ?? new Vector3()
+  };
 }
