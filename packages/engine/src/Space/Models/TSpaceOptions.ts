4fe110a8d55de19964ae0e7aed413aef4e9b644b
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:54:00 +0100

    [TODO]

--- packages/engine/src/Space/Models/TSpaceOptions.ts
+++ packages/engine/src/Space/Models/TSpaceOptions.ts
@@ -2,6 +2,8 @@ import type { TLoopsSettings } from '@Engine/Loop/Models';
 
 import type { TSpaceDebugFlags } from './TSpaceDebugFlags';
 
+// TODO 20-0-0: SPACE: make use of TSpaceOptions in a Space
+// TODO 20-0-0: SPACE: Save/Load TSpaceOptions from config (serialization)
 export type TSpaceOptions = Readonly<{
   loopsSettings: TLoopsSettings;
   debugFlags: TSpaceDebugFlags;
