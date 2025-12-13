a4313a0442fb017efb56e365a1574a85f031cdc8
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:55:08 +0100

    [DESKTOP][MOBILE][BUILD]

--- packages/engine/src/env-schema.ts
+++ packages/engine/src/env-schema.ts
@@ -2,7 +2,8 @@ import { toBool } from '@Shared/Utils';
 import { object } from 'valibot';
 
 export const runtimeSchema = object({
-  CI: toBool
+  CI: toBool,
+  VITE_BUILD_COMPRESSION: toBool
 });
 
 // export const nodeSchema = object({
--- packages/engine/vite.config.ts
+++ packages/engine/vite.config.ts
@@ -1,96 +1,119 @@
 /// <reference types="vitest" />
 import compression from 'vite-plugin-compression';
-import { defineConfig } from 'vite';
+import { ConfigEnv, defineConfig, loadEnv, UserConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import path from 'path';
 import { sharedAliases } from '../../vite.alias';
 import { visualizer } from 'rollup-plugin-visualizer';
 import wasm from 'vite-plugin-wasm';
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      ...sharedAliases
-    }
-  },
-  plugins: [
-    wasm(),
-    dts({
-      exclude: ['**/*.spec.ts', '**/*.test.ts', 'vite.config.ts']
-    }),
-    compression({
-      ext: '.gz',
-      algorithm: 'gzip',
-      deleteOriginFile: false,
-      filter: /\.(js|mjs|json|css|map|html|glb|gltf|bin|wasm|txt|svg|csv|xml|shader|material|ttf|otf)$/i
-    }),
-    compression({
-      ext: '.br',
-      algorithm: 'brotliCompress',
-      deleteOriginFile: false,
-      filter: /\.(js|mjs|json|css|map|html|glb|gltf|bin|wasm|txt|svg|csv|xml|shader|material|ttf|otf)$/i
-    })
-  ],
-  worker: {
-    format: 'es',
-    //@ts-expect-error
-    plugins: [wasm()]
-  },
-  build: {
-    minify: true, //could have better compression (make sure wasm is not broken)
-    assetsInlineLimit: 0, // Do not inline assets and wasm
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'AnarchyEngine',
-      fileName: (format): string => `anarchy-engine.${format}.js`,
-      formats: ['es']
-    },
-    target: 'esnext',
-    sourcemap: true,
-    rollupOptions: {
-      external: (id: string): boolean => id.endsWith('.spec.ts') || id.endsWith('.test.ts'),
-      //  external: ['three', 'rxjs', '@dimforge/rapier3d'] — If you want to exclude some dependencies from the bundle
-      output: {
-        // manualChunks: {
-        //   'anarchy-rapier3d': ['@dimforge/rapier3d'],
-        //   'anarchy-three': ['three']
-        // }
-        inlineDynamicImports: false, //extract workers to separate bundle
-        manualChunks(id: string): string | undefined {
-          if (id === 'three' || id.includes('node_modules/three/')) return 'anarchy-three';
-          if (id.includes('@dimforge/rapier3d')) return 'anarchy-rapier3d';
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  const root: string = process.cwd();
+  const env: ImportMetaEnv = loadEnv(mode, root) as ImportMetaEnv;
+  const { VITE_BUILD_COMPRESSION } = env;
 
-          return undefined;
-        }
+  const toBool = (v: string): boolean => v === 'true';
+  const buildCompression: boolean = toBool(VITE_BUILD_COMPRESSION as any);
+
+  // console.log('XXX buildCompression', buildCompression); //make it true
+
+  return {
+    base: './',
+    resolve: {
+      alias: {
+        ...sharedAliases
+      }
+    },
+    plugins: [
+      wasm(),
+      dts({
+        exclude: ['**/*.spec.ts', '**/*.test.ts', 'vite.config.ts']
+      }),
+      //Compression is only for web builds (desktop and mobile cannot unpack .br/.gz files)
+      ...(buildCompression
+        ? [
+            compression({
+              ext: '.gz',
+              algorithm: 'gzip',
+              deleteOriginFile: false,
+              filter: /\.(js|mjs|json|css|map|html|glb|gltf|bin|wasm|txt|svg|csv|xml|shader|material|ttf|otf)$/i
+            }),
+            compression({
+              ext: '.br',
+              algorithm: 'brotliCompress',
+              deleteOriginFile: false,
+              filter: /\.(js|mjs|json|css|map|html|glb|gltf|bin|wasm|txt|svg|csv|xml|shader|material|ttf|otf)$/i
+            })
+          ]
+        : [])
+    ],
+    worker: {
+      format: 'es',
+      //@ts-expect-error
+      plugins: [wasm()]
+    },
+    build: {
+      assetsInlineLimit: 0, // Do not inline assets and wasm
+      target: 'esnext',
+      sourcemap: true,
+      // TODO: Minify doesn't work in lib mode (@see: https://github.com/vitejs/vite/issues/6555 )
+      minify: true,
+      lib: {
+        entry: path.resolve(__dirname, 'src/index.ts'),
+        name: 'AnarchyEngine',
+        fileName: (format): string => `anarchy-engine.${format}.js`,
+        formats: ['es']
       },
-      plugins: [visualizer({ open: false })]
+
+      rollupOptions: {
+        external: (id: string): boolean => id.endsWith('.spec.ts') || id.endsWith('.test.ts'),
+        //  external: ['three', 'rxjs', '@dimforge/rapier3d'] — If you want to exclude some dependencies from the bundle
+        output: {
+          inlineDynamicImports: false //extract workers to separate bundle
+
+          // Perhaps it's better not to use manualChunks for a lib build (also could be problems with minify)
+
+          // manualChunks: {
+          //   'anarchy-rapier3d': ['@dimforge/rapier3d'],
+          //   'anarchy-three': ['three']
+          // }
+
+          // manualChunks(id: string): string | undefined {
+          //   if (id === 'three' || id.includes('node_modules/three/')) return 'anarchy-three';
+          //   if (id.includes('@dimforge/rapier3d')) return 'anarchy-rapier3d';
+          //
+          //   return undefined;
+          // }
+        },
+        plugins: [visualizer({ open: false })]
+      },
+      outDir: 'dist',
+      emptyOutDir: true
     },
-    outDir: 'dist',
-    emptyOutDir: true
-  },
-  dedupe: ['three', 'three-mesh-bvh', 'lodash', '@rwh/keystrokes', 'lodash-es', 'rxjs', 'nanoid', 'rbush', 'ts-key-enum', 'typescript-fsm', 'ajv', 'date-fns', '@dimforge/rapier3d'],
-  test: {
-    globals: true,
-    environment: 'jsdom', // TODO try "node", could be faster.
-    alias: {
-      //Since we cannot use wasm version of @dimforge/rapier3d in vitest
-      '@dimforge/rapier3d': path.resolve(__dirname, 'node_modules/@dimforge/rapier3d-compat')
-    },
-    setupFiles: './vitest.setup.js',
-    reporters: ['default', 'html'],
-    outputFile: './reports/html/unit.html',
-    include: ['src/**/*.spec.{js,mjs,cjs,ts,mts,cts}'],
-    coverage: {
-      all: false,
-      reportsDirectory: './reports/coverage',
-      include: ['src/**/*'],
-      exclude: ['src/**/index.ts', 'coverage', 'utils/', 'public', 'vite.config.ts', '.eslintrc.js', 'src/vite-env.d.ts', 'src/App/DeveloperPanel/*', 'e2e'],
-      thresholds: {
-        statements: 15,
-        branches: 60,
-        functions: 30,
-        lines: 15
+    dedupe: ['three', 'three-mesh-bvh', 'lodash', '@rwh/keystrokes', 'lodash-es', 'rxjs', 'nanoid', 'rbush', 'ts-key-enum', 'typescript-fsm', 'ajv', 'date-fns', '@dimforge/rapier3d'],
+    test: {
+      globals: true,
+      environment: 'jsdom', // TODO try "node", could be faster.
+      alias: {
+        //Since we cannot use wasm version of @dimforge/rapier3d in vitest
+        '@dimforge/rapier3d': path.resolve(__dirname, 'node_modules/@dimforge/rapier3d-compat')
+      },
+      setupFiles: './vitest.setup.js',
+      reporters: ['default', 'html'],
+      outputFile: './reports/html/unit.html',
+      include: ['src/**/*.spec.{js,mjs,cjs,ts,mts,cts}'],
+      coverage: {
+        all: false,
+        reportsDirectory: './reports/coverage',
+        include: ['src/**/*'],
+        exclude: ['src/**/index.ts', 'coverage', 'utils/', 'public', 'vite.config.ts', '.eslintrc.js', 'src/vite-env.d.ts', 'src/App/DeveloperPanel/*', 'e2e'],
+        thresholds: {
+          statements: 15,
+          branches: 60,
+          functions: 30,
+          lines: 15
+        }
       }
     }
-  }
+  };
 });
