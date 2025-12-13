ce08fe0e43e4bd8ff61033f38abe21b07cb5302e
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:21:26 +0100

    [GUI] use input shield

--- packages/showcases-gui/src/App.vue
+++ packages/showcases-gui/src/App.vue
@@ -3,17 +3,26 @@ import './assets/style.scss';
 
 import { eventsService } from '@Showcases/GUI/services';
 import Home from '@Showcases/GUI/views/Home.vue';
+import type { TInputShieldService } from '@Showcases/Shared';
+import { InputShieldService } from '@Showcases/Shared';
 import type { Subscription } from 'rxjs';
-import { onMounted, onUnmounted } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
 
 let appEventsSub$: Subscription | undefined;
+
+const root = ref<HTMLElement | null>(null);
+const inputShieldService: TInputShieldService = InputShieldService(() => root.value);
 
 onMounted((): void => {
   appEventsSub$ = eventsService.startListeningAppEvents();
   // eventsService.emitGetCurrentGui();
+  inputShieldService.start();
 });
 
-onUnmounted((): void => appEventsSub$?.unsubscribe());
+onUnmounted((): void => {
+  appEventsSub$?.unsubscribe();
+  inputShieldService.start();
+});
 
 // function some(): void {
 // eventsService.emitSetGuiSettings(useSettingsStore().state);
@@ -21,7 +30,7 @@ onUnmounted((): void => appEventsSub$?.unsubscribe());
 </script>
 
 <template>
-  <div class="gui">
+  <div ref="root" class="gui">
     <!--    <RouterView class="gui__item -view" @save="some" />-->
     <Home class="gui__item -home" />
   </div>
