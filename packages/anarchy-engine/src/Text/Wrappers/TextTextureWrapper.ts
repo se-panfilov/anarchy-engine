128fee224e5b299105a94c288b24fb3e8edc0d20
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:13:46 +0100

    [ENGINE] todo

--- packages/anarchy-engine/src/Text/Wrappers/TextTextureWrapper.ts
+++ packages/anarchy-engine/src/Text/Wrappers/TextTextureWrapper.ts
@@ -52,7 +52,6 @@ export function createTextTextureWrapper(params: TTextParams, type: TextType, de
     const fontSize: string = toPx(params.cssProps?.fontSize);
 
     const fontSizeNoUnits: number = stripUnits(fontSize);
-    // TODO DESKTOP: font-family doesn't apply!
     const fontFamily: string | undefined = params.cssProps?.fontFamily || textTranslationService?.locale$.value.font;
 
     await document.fonts.ready;
