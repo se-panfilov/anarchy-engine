e7742ee06311a0ef716915563e79ddd6b8888f77
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:18:18 +0100

    [HEAVY WIP] material: removed todo

--- src/Engine/Material/Models/TMaterialProps.ts
+++ src/Engine/Material/Models/TMaterialProps.ts
@@ -1,5 +1,5 @@
 import type { TWithNameRequired, TWithReadonlyTags } from '@/Engine/Mixins';
 
 import type { TWithMaterialType } from './TWithMaterialType';
-// TODO 9.0.0. RESOURCES: Check all entities in registries: should have names, tags and etc.
+
 export type TMaterialProps = Readonly<{ options?: Record<string, any> }> & TWithMaterialType & TWithNameRequired & TWithReadonlyTags;
