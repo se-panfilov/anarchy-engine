30a4b167f50369a3e7092793a5336f12030150e7
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:03:44 +0100

    [TODO]

--- apps/showcases-core/src/index.ts
+++ apps/showcases-core/src/index.ts
@@ -18,6 +18,9 @@ const settings: TAppSettings = {
   }
 };
 
+// TODO DESKTOP: (web) Detect user locale in web and send it to the app settings (menu)
+// TODO MOBILE: Detect user locale in mobile app and send it to the app settings (menu)
+
 setBrowserSafeguards(window);
 
 void route(settings).then((): void => {
