71ab880a5d504a83af77c5198191899289197ee9
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:07:17 +0100

    [LEGAL] configs

--- packages/anarchy-engine/anarchy-legal.config.js
+++ packages/anarchy-engine/anarchy-legal.config.js
@@ -1,6 +1,6 @@
 import base from '../../anarchy-legal-base.config.js.js';
 
-//Commercial configuration for legal docs
+//Non-Commercial configuration for legal docs
 export default [
   ...base,
   {
@@ -8,14 +8,14 @@ export default [
   },
   {
     type: 'EULA',
-    template: 'EULA_COMMERCIAL_TEMPLATE'
+    template: 'EULA_NON_COMMERCIAL_TEMPLATE'
   },
   {
     type: 'PRIVACY',
-    template: 'PRIVACY_COMMERCIAL_TEMPLATE'
+    template: 'PRIVACY_NON_COMMERCIAL_TEMPLATE'
   },
   {
     type: 'SECURITY',
-    template: 'SECURITY_COMMERCIAL_TEMPLATE'
+    template: 'SECURITY_NON_COMMERCIAL_TEMPLATE'
   }
 ];
