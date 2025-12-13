4e7e59d00dcddc760be37a4b7268004ef8e6275d
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:54:41 +0100

    text

--- src/App/Levels/ShowcaseLevel7/index.ts
+++ src/App/Levels/ShowcaseLevel7/index.ts
@@ -1,5 +1,5 @@
 import type { IShowcase } from '@/App/Levels/Models';
-import type { IAppCanvas, ILevel, ILevelConfig } from '@/Engine';
+import { EulerWrapper, IAppCanvas, ILevel, ILevelConfig, Vector3Wrapper } from '@/Engine';
 import { buildLevelFromConfig } from '@/Engine';
 
 import levelConfig from './showcase-level-7.config.json';
@@ -8,6 +8,16 @@ import levelConfig from './showcase-level-7.config.json';
 export function showcaseLevel(canvas: IAppCanvas): IShowcase {
   const level: ILevel = buildLevelFromConfig(canvas, levelConfig as ILevelConfig);
   const { textFactory, textRegistry } = level.entities;
+
+  textFactory.create({
+    text: 'Dynamic text',
+    position: Vector3Wrapper({ x: -4, y: 8, z: 0 }),
+    rotation: EulerWrapper({ x: -1.57, y: 0, z: 0 }),
+    color: '#ff0000',
+    fontSize: 3,
+    font: './public/RubikDoodleTriangles-Regular.ttf',
+    tags: []
+  });
 
   function start(): void {
     level.start();
