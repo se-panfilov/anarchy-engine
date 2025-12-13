478faddccba97cd927d1fc8b7377de057ad79fd2
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:28:14 +0100

    animations: adjusted space

--- src/Engine/Space/Models/TSpaceConfigResources.ts
+++ src/Engine/Space/Models/TSpaceConfigResources.ts
@@ -1,9 +1,11 @@
+import type { TAnimationsResourceConfig } from '@/Engine/Animations';
 import type { TEnvMapResourceConfig } from '@/Engine/EnvMap';
 import type { TMaterialConfig } from '@/Engine/Material';
 import type { TModel3dResourceConfig } from '@/Engine/Models3d';
 import type { TTextureResourceConfig } from '@/Engine/Texture';
 
 export type TSpaceConfigResources = Readonly<{
+  animations: ReadonlyArray<TAnimationsResourceConfig>;
   envMaps: ReadonlyArray<TEnvMapResourceConfig>;
   materials: ReadonlyArray<TMaterialConfig>;
   models3d: ReadonlyArray<TModel3dResourceConfig>;
--- src/Engine/Space/Utils/LoadResourcesUtils.ts
+++ src/Engine/Space/Utils/LoadResourcesUtils.ts
@@ -1,7 +1,7 @@
 import type { TEnvMapTexture } from '@/Engine/EnvMap/Models';
 import type { TSpaceConfigResources, TSpaceServices } from '@/Engine/Space/Models';
 
-export async function loadResources(resources: TSpaceConfigResources, { models3dService, envMapService, materialService, textureService }: TSpaceServices): Promise<void> {
+export async function loadResources(resources: TSpaceConfigResources, { animationsService, models3dService, envMapService, materialService, textureService }: TSpaceServices): Promise<void> {
   const { models3d, envMaps, materials, textures } = resources;
 
   // EnvMaps could be loaded async, no need to wait
@@ -14,6 +14,8 @@ export async function loadResources(resources: TSpaceConfigResources, { models3d
   //   They are not loaded from anywhere (should be created instead) and depend on textures.
   //   However, materials (and textures) should be fully ready before models3d.
   materialService.createFromConfig(materials);
+
+  await animationsService.loadFromConfigAsync(models3d);
 
   await Promise.all([
     // Models3d contains of "entities" and "resources". Here only load model resources. And create them lately.
