04becb60ab07cca9c073d403b9fae0b20ef6c272
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:23:07 +0100

    [HEAVY WIP] physics transform agent: fixed getter

--- src/Engine/Physics/Entities/PhysicsBody.ts
+++ src/Engine/Physics/Entities/PhysicsBody.ts
@@ -1,8 +1,11 @@
+import type { RigidBodyType } from '@dimforge/rapier3d';
+
 import { AbstractEntity, EntityType } from '@/Engine/Abstract';
 import type { CollisionShape, RigidBodyTypesNames } from '@/Engine/Physics/Constants';
 import { RigidBodyTypesMap } from '@/Engine/Physics/Constants';
 import { withPhysicsBodyEntities } from '@/Engine/Physics/Mixins';
 import type { TPhysicsBody, TPhysicsBodyEntities, TPhysicsBodyParams, TPhysicsDependencies } from '@/Engine/Physics/Models';
+import { isNotDefined } from '@/Engine/Utils';
 
 import { createPhysicsBodyEntities } from './PhysicsBodyUtils';
 
@@ -17,7 +20,15 @@ export function PhysicsBody(params: TPhysicsBodyParams, { world }: TPhysicsDepen
     setPhysicsBodyType: (type: RigidBodyTypesNames, wakeUp: boolean): void => {
       entities.rigidBody?.setBodyType(RigidBodyTypesMap[type], wakeUp);
     },
-    getPhysicsBodyType: (): RigidBodyTypesNames => params.type,
+    getPhysicsBodyType: (): RigidBodyTypesNames | never => {
+      const bodyType: RigidBodyType | undefined = entities.rigidBody?.bodyType();
+      if (isNotDefined(bodyType)) return params.type;
+      const match = Object.entries(RigidBodyTypesMap).find(([, value]): boolean => value === bodyType);
+      const result: RigidBodyTypesNames | undefined = match?.[0] as RigidBodyTypesNames | undefined;
+
+      if (isNotDefined(result)) throw new Error(`PhysicsBody: RigidBody type is not defined for object with name "${params.name}"`);
+      return result;
+    },
     getPhysicsBodyShape: (): CollisionShape => params.collisionShape
   };
 }
