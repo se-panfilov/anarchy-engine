054c84e4efb0e79168ad062364412069167b4ca6
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:15:11 +0100

    [DESKTOP] getBrowserInfo

--- apps/showcases-desktop/preload.ts
+++ apps/showcases-desktop/preload.ts
@@ -4,17 +4,18 @@ import { platformApiChannel, platformApiName } from '@Showcases/Shared';
 import { PlatformActions } from './src/Constants';
 import type { TLegalDoc } from 'showcases-shared';
 import { TLocaleId } from 'anarchy-i18n';
+import { getBrowserInfo } from 'anarchy-shared/src/Utils';
 
 const { AppExit, AppRestart, GetAppSettings, GetLegalDocs, GetPreferredLocales, SetAppSettings, UpdateAppSettings } = PlatformActions;
 
 declare const __DESKTOP_APP_VERSION__: string;
 
 const mapping: TShowcasesDesktopApi = {
-  chrome: (): string => process.versions.chrome,
   closeApp: (): Promise<void> => ipcRenderer.invoke(platformApiChannel, AppExit),
   desktopAppVersion: async (): Promise<string> => __DESKTOP_APP_VERSION__,
   electron: (): string => process.versions.electron,
   getAppSettings: (): Promise<TShowcaseGameSettings> => ipcRenderer.invoke(platformApiChannel, GetAppSettings),
+  getBrowserInfo,
   getLegalDocs: (options: TLoadDocPayload): Promise<TLegalDoc> => ipcRenderer.invoke(platformApiChannel, GetLegalDocs, options),
   getPreferredLocales: (): Promise<ReadonlyArray<TLocaleId>> => ipcRenderer.invoke(platformApiChannel, GetPreferredLocales),
   node: (): string => process.versions.node,
