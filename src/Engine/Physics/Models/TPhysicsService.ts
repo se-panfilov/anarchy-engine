b682d4c029d213604532a9211826b8c59c2fdfa7
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:05:21 +0100

    gravity

--- src/Engine/Physics/Models/TPhysicsService.ts
+++ src/Engine/Physics/Models/TPhysicsService.ts
@@ -1,5 +1,8 @@
+import type { TVector3Wrapper } from '@/Engine/Vector';
+
 import type { TPhysicsDebugRenderer } from './TPhysicsDebugRenderer';
 
 export type TPhysicsService = {
   getDebugRenderer: () => TPhysicsDebugRenderer;
+  setGravity: (vector: TVector3Wrapper) => void;
 };
--- src/Engine/Physics/Services/PhysicsService.ts
+++ src/Engine/Physics/Services/PhysicsService.ts
@@ -1,12 +1,26 @@
-import type { TPhysicsService } from '@/Engine/Physics/Models';
-import { PhysicsDebugRenderer } from '@/Engine/Physics/Utils';
+import type { World } from '@dimforge/rapier3d';
 
-export function PhysicsService(scene): TPhysicsService {
+import type { TWithCoordsXYZ } from '@/Engine/Mixins';
+import type { TPhysicsDebugRenderer, TPhysicsService } from '@/Engine/Physics/Models';
+import { PhysicsDebugRenderer } from '@/Engine/Physics/Utils';
+import type { TSceneWrapper } from '@/Engine/Scene';
+import type { TVector3Wrapper } from '@/Engine/Vector';
+
+// TODO (S.Panfilov) TWorld instead of World
+export function PhysicsService(scene: TSceneWrapper, world: World): TPhysicsService {
   // TODO (S.Panfilov)
 
-  const getDebugRenderer = PhysicsDebugRenderer(scene, world);
+  const getDebugRenderer = (): TPhysicsDebugRenderer => PhysicsDebugRenderer(scene.entity, world);
+
+  function setGravity(vector: TVector3Wrapper): void {
+    // Earth's gravity is 9.81 m/s^2 or { x: 0.0, y: -9.81, z: 0.0 };
+    const gravity: TWithCoordsXYZ = vector.getCoords();
+    // eslint-disable-next-line functional/immutable-data
+    world.gravity = gravity;
+  }
 
   return {
-    getDebugRenderer
+    getDebugRenderer,
+    setGravity
   };
 }
