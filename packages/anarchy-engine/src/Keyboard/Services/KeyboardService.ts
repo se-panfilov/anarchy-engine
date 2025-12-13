736131ecb8403360aec4ea52dfa7cea294978510
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:23:21 +0100

    [ENGINE] keyboard containers focus fix

--- packages/anarchy-engine/src/Keyboard/Services/KeyboardService.ts
+++ packages/anarchy-engine/src/Keyboard/Services/KeyboardService.ts
@@ -6,11 +6,17 @@ import { KeyWatcherType } from '@Anarchy/Engine/Keyboard';
 import type { TKeyboardService, TKeyWatcher, TKeyWatcherFactory, TKeyWatcherRegistry } from '@Anarchy/Engine/Keyboard/Models';
 import type { TDelta } from '@Anarchy/Engine/Loop';
 import type { TSpaceLoops } from '@Anarchy/Engine/Space';
+import { isDefined } from '@Anarchy/Shared/Utils';
 import type { Subscription } from 'rxjs';
 import { filter, map, Subject } from 'rxjs';
 
 export function KeyboardService(container: TContainerDecorator, keyWatcherFactory: TKeyWatcherFactory, keyWatcherRegistry: TKeyWatcherRegistry, { keyboardLoop }: TSpaceLoops): TKeyboardService {
   keyWatcherFactory.entityCreated$.subscribe((watcher: TKeyWatcher | TKeyComboWatcher): void => keyWatcherRegistry.add(watcher));
+
+  // Only focusable elements can receive keyboard events
+  const element: HTMLElement = container.getElement() as HTMLElement;
+  // eslint-disable-next-line functional/immutable-data
+  if (isDefined(element?.tabIndex) && element?.tabIndex < 0) element.tabIndex = 0;
 
   const tags: ReadonlyArray<WatcherTag | string> = [WatcherTag.Initial, WatcherTag.Global];
   const keyPressWatcher: TKeyWatcher = keyWatcherFactory.create({ type: KeyWatcherType.Press, container, tags }, undefined) as TKeyWatcher;
