24294001119623fdadb188b9e17690461f028abb
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:01:04 +0100

    [DESKTOP] fixes

--- apps/showcases-desktop/preload.ts
+++ apps/showcases-desktop/preload.ts
@@ -6,6 +6,7 @@ const { SaveAppSettings, LoadAppSettings } = PlatformActions;
 
 declare const __DESKTOP_APP_VERSION__: string;
 
+//platformApiName will be available in the main world as `window.platformApi`
 contextBridge.exposeInMainWorld(platformApiName, {
   saveAppSettings: (settings: TGameSettings): Promise<void> => ipcRenderer.invoke(platformApiChannel, SaveAppSettings, settings),
   loadAppSettings: (): Promise<TGameSettings> => ipcRenderer.invoke(platformApiChannel, LoadAppSettings),
