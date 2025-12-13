d62f89179c8e7d6fdb7719d5d316ddcd5da82164
gr-olga <gr-olga@ya.ru>
2025-12-13 01:48:31 +0100

    [WIP] migration threejs 0.150.1 -> 0.151.3

--- src/Engine/Wrappers/LightWrapper/Accessors.ts
+++ src/Engine/Wrappers/LightWrapper/Accessors.ts
@@ -12,12 +12,16 @@ export function getAccessors(entity: Writeable<IAmbientLight | IDirectionalLight
   const setCastShadow = (value: boolean): boolean => (entity.castShadow = value);
   const setControls = (x: number, y: number, z: number): IVector3 => entity.position.set(x, y, z);
   // eslint-disable-next-line functional/immutable-data
+
+  // TODO (S.Panfilov) CWP this function doesn't work yet
   const setFar = (): number => {
     return 43; //((entity.shadow.camera as Writeable<IOrthographicCamera>).far = far);
   };
+  // TODO (S.Panfilov) CWP this function doesn't work yet
   const setShadowMapSize = (): IVector2 => {
     return { x: 3, y: 3 } as any; //entity.shadow.mapSize.set(x, y);
   };
+  // TODO (S.Panfilov) CWP this function doesn't work yet
   // eslint-disable-next-line functional/immutable-data
   const setNormalBias = (): number => {
     return 32; //(entity.shadow.normalBias = val);
