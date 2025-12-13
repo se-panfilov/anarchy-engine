ee2d8807c30d1f52df8c44a72953503995445165
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:10:04 +0100

    [MENU] legal view

---
+++ packages/showcases-menu/src/views/Legal.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Navigation from '@Showcases/Menu/components/Navigation.vue';
+import SettingsGroup from '@Showcases/Menu/components/SettingsGroup.vue';
+import View from '@Showcases/Menu/components/View.vue';
+import ViewForm from '@Showcases/Menu/components/ViewForm.vue';
+import { vueTranslationService } from '@Showcases/Menu/services';
+import type { ShallowRef } from 'vue';
+
+const { $t } = vueTranslationService;
+
+const viewTitleText: ShallowRef<string> = $t('main-menu.settings.legal.view.title');
+const mainSettingsGroupTitleText: ShallowRef<string> = $t('main-menu.settings.legal.group.main-legal-settings.title');
+</script>
+
+<template>
+  <View class="audio" :title="viewTitleText">
+    <ViewForm name="audio" class="audio__view-form">
+      <SettingsGroup :title="mainSettingsGroupTitleText"> 123 </SettingsGroup>
+      <Navigation class="settings__navigation" :back-btn="true" />
+    </ViewForm>
+  </View>
+</template>
