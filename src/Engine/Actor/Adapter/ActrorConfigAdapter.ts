e81ea812f479a620e4cacdeaa880a3a98c6386fb
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:03:55 +0100

    fixes

--- src/Engine/Actor/Adapter/ActrorConfigAdapter.ts
+++ src/Engine/Actor/Adapter/ActrorConfigAdapter.ts
@@ -4,10 +4,11 @@ import { configToParamsObject3d } from '@/Engine/ThreeLib';
 
 export function configToParams(config: IActorConfig): IActorParams {
   const { position, rotation, layers, animations, scale, material, ...rest } = config;
+  const { type: materialType, ...restMaterialParams } = materialConfigToParams({ ...material.params, type: material.type });
 
   return {
     ...rest,
-    material: materialConfigToParams({ ...material.params, type: material.type }),
+    material: { type: materialType, params: { ...restMaterialParams } },
     ...configToParamsObject3d({ position, rotation, scale, layers, animations })
   };
 }
