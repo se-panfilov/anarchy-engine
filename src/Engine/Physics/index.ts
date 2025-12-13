b354c6a7505e1b1c3211cfb9fb724daf08d93855
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:24:01 +0100

    [WIP] physics: removed HOF

--- src/Engine/Physics/index.ts
+++ src/Engine/Physics/index.ts
@@ -2,7 +2,6 @@ export * from './Adapters';
 export * from './Constants';
 export * from './Entities';
 export * from './Factories';
-export * from './HOFs';
 export * from './Mixins';
 export * from './Models';
 export * from './Registries';
