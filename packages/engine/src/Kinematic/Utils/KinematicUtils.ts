a7613f57819a5bb13fcdedef919ad864e1057227
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:53:26 +0100

    [SIZE] fixed lodash size in a bundle

--- packages/engine/src/Kinematic/Utils/KinematicUtils.ts
+++ packages/engine/src/Kinematic/Utils/KinematicUtils.ts
@@ -1,15 +1,15 @@
 import { DefaultIsAutoUpdate, DefaultKinematicConfigState, DefaultKinematicTarget } from '@Engine/Kinematic';
 import type { TKinematicConfig } from '@Engine/Kinematic/Models';
 import { isNotDefined } from '@Engine/Utils';
-import _ from 'lodash';
+import { isEqual } from 'lodash-es';
 
 export function ignoreDefaultStateKinematic(config: TKinematicConfig | undefined): TKinematicConfig | undefined {
   if (isNotDefined(config)) return undefined;
 
   const { state, target, isAutoUpdate } = config;
-  const isDefaultState: boolean = _.isEqual(state, DefaultKinematicConfigState);
-  const isDefaultTarget: boolean = _.isEqual(target, DefaultKinematicTarget);
-  const isDefaultAutoUpdate: boolean = _.isEqual(isAutoUpdate, DefaultIsAutoUpdate);
+  const isDefaultState: boolean = isEqual(state, DefaultKinematicConfigState);
+  const isDefaultTarget: boolean = isEqual(target, DefaultKinematicTarget);
+  const isDefaultAutoUpdate: boolean = isEqual(isAutoUpdate, DefaultIsAutoUpdate);
 
   return isDefaultState && isDefaultTarget && isDefaultAutoUpdate ? undefined : config;
 }
