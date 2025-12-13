e1f4537f501a1c978dcf516e40ff8fb322feb8a4
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:19:09 +0100

    [HEAVY WIP] actor: refactor

--- src/Engine/Actor/Wrappers/Actor.ts
+++ src/Engine/Actor/Wrappers/Actor.ts
@@ -1,90 +1,125 @@
 import type { Subscription } from 'rxjs';
-import type { Group, Mesh, Object3D, Vector3 } from 'three';
+import { combineLatest, Subject } from 'rxjs';
+import { Euler, Vector3 } from 'three';
 
-import { AbstractWrapper, WrapperType } from '@/Engine/Abstract';
-import type { TActorDependencies, TActorParams, TActorWrapper } from '@/Engine/Actor/Models';
+import { FacadeType } from '@/Engine/Abstract';
+import { AbstractFacade } from '@/Engine/Abstract/Wrappers/AbstractFacade';
+import type { ActorDrive } from '@/Engine/Actor/Constants';
+import type { TActor, TActorDependencies, TActorEntities, TActorParams } from '@/Engine/Actor/Models';
 import { applySpatialGrid, startCollisions } from '@/Engine/Actor/Wrappers/ActorWrapperHelper';
 import { withCollisions } from '@/Engine/Collisions';
 import { withKinematic } from '@/Engine/Kinematic';
-import { scalableMixin, withMoveBy3dMixin, withRotationByXyzMixin } from '@/Engine/Mixins';
 import type { TModel3dFacade } from '@/Engine/Models3d';
+import { withModel3dFacade } from '@/Engine/Models3d';
 import type { TSpatialLoopServiceValue } from '@/Engine/Spatial';
 import { withReactivePosition, withReactiveRotation, withSpatial, withUpdateSpatialCell } from '@/Engine/Spatial';
 import { applyPosition, applyRotation, applyScale, isDefined } from '@/Engine/Utils';
 
-// TODO 8.0.0. MODELS: shall we refactor TActorWrapper to TActorFacade?
 export function Actor(
   params: TActorParams,
   { kinematicLoopService, spatialLoopService, spatialGridService, collisionsLoopService, collisionsService, models3dService, model3dFacadeToActorConnectionRegistry }: TActorDependencies
-): TActorWrapper {
+): TActor {
+  // TODO 8.0.0. MODELS: Allow to switch "drive" it in runtime
+  let drive: ActorDrive = params.drive;
+
+  const setDrive = (mode: ActorDrive): string => (drive = mode);
+  const getDrive = (): ActorDrive => drive;
+
+  const position$: Subject<Vector3> = new Subject<Vector3>();
+  const rotation$: Subject<Euler> = new Subject<Euler>();
+  const scale$: Subject<Vector3> = new Subject<Vector3>();
+
+  // TODO 8.0.0. MODELS: position$, rotation$, scale$ should update related model3d values
+
   const isModelAlreadyInUse: boolean = isDefined(model3dFacadeToActorConnectionRegistry.findByModel3dFacade(params.model3dSource));
-  const entity: TModel3dFacade = isModelAlreadyInUse ? models3dService.clone(params.model3dSource) : params.model3dSource;
-  const model3d: Group | Mesh | Object3D = entity.getModel3d();
+  const model3dF: TModel3dFacade = isModelAlreadyInUse ? models3dService.clone(params.model3dSource) : params.model3dSource;
 
-  const { value$: position$, update: updatePosition } = withReactivePosition(model3d);
-  const { value$: rotation$, update: updateRotation } = withReactiveRotation(model3d);
+  // const { value$: position$, update: updatePosition } = withReactivePosition(model3d);
+  // const { value$: rotation$, update: updateRotation } = withReactiveRotation(model3d);
 
-  const actorW: TActorWrapper = {
-    ...AbstractWrapper(entity, WrapperType.Actor, params),
-
-    // TODO 8.0.0. MODELS: perhaps move these mixins to Model3dFacade
-    ...withMoveBy3dMixin(model3d),
-    ...withRotationByXyzMixin(model3d),
-    ...scalableMixin(model3d),
-
-    // TODO CWP Refactor add "withModel3d" mixin and make it facade
-
+  const entities: TActorEntities = {
+    ...withModel3dFacade(model3dF),
+    // TODO 8.0.0. MODELS: Kinematic should update rotation (and position?) (if "drive" is "kinematic")
+    // TODO 8.0.0. MODELS: Physics should update position and rotation (if "drive" is "physics")
     ...withKinematic(params),
     ...withSpatial(params),
     ...withCollisions(params, collisionsService, collisionsLoopService),
-
-    // TODO 8.0.0. MODELS: this mixin should be work properly with Model3dFacade (check if it detects the model's size correctly)
-    ...withUpdateSpatialCell(),
-    position$: position$.asObservable(),
-    rotation$: rotation$.asObservable()
+    ...withUpdateSpatialCell()
   };
 
+  const facade = AbstractFacade(entities, FacadeType.Actor, params);
+
   const kinematicSub$: Subscription = kinematicLoopService.tick$.subscribe((delta: number): void => {
-    if (!actorW.kinematic.isAutoUpdate()) return;
-    actorW.doKinematicMove(delta);
-    actorW.doKinematicRotation(delta);
+    if (!entities.kinematic.isAutoUpdate()) return;
+    entities.doKinematicMove(delta);
+    entities.doKinematicRotation(delta);
   });
 
   const spatialSub$: Subscription = spatialLoopService.tick$.subscribe(({ priority }: TSpatialLoopServiceValue): void => {
-    if (!actorW.spatial.isAutoUpdate()) return;
-    if (actorW.spatial.getSpatialUpdatePriority() >= priority) {
+    if (!entities.spatial.isAutoUpdate()) return;
+    if (entities.spatial.getSpatialUpdatePriority() >= priority) {
       updatePosition();
       updateRotation();
     }
   });
 
-  actorW.destroyed$.subscribe(() => {
+  facade.destroyed$.subscribe(() => {
     kinematicSub$.unsubscribe();
     spatialSub$.unsubscribe();
     position$.unsubscribe();
     position$.complete();
     rotation$.unsubscribe();
     rotation$.complete();
-    actorW.spatial.destroy();
-    actorW.collisions?.destroy();
-    model3dFacadeToActorConnectionRegistry.removeByModel3dFacade(entity);
-    entity.destroy();
+    entities.spatial.destroy();
+    entities.collisions?.destroy();
+    model3dFacadeToActorConnectionRegistry.removeByModel3dFacade(model3dF);
+    model3dF.destroy();
   });
+
+  let oldPosition: Vector3 = new Vector3();
+  let oldRotation: Euler = new Euler();
+  let oldScale: Vector3 = new Vector3();
+
+  combineLatest([position$, rotation$, scale$]).subscribe(([position, rotation, scale]: [Vector3, Euler, Vector3]): void => {
+    if (!position.equals(oldPosition)) {
+      oldPosition = position;
+      applyPosition(entities, position);
+    }
+
+    if (!rotation.equals(oldRotation)) {
+      oldRotation = rotation;
+      applyRotation(entities, rotation);
+    }
+
+    if (!scale.equals(oldScale)) {
+      oldScale = scale;
+      applyScale(entities, scale);
+    }
+
+    // TODO 8.0.0. MODELS: should it take in account scale and rotation? Otherwise trigger it only for position
+    entities.updateSpatialCells(position);
+  });
+
   // TODO 8.0.0. MODELS: check: should be applied both for actor and model3d
-  applyPosition(actorW, params.position);
+  // applyPosition(entities, params.position);
   // TODO 8.0.0. MODELS: check: should be applied both for actor and model3d
-  applyRotation(actorW, params.rotation);
+  // applyRotation(entities, params.rotation);
   // TODO 8.0.0. MODELS: do we need to apply scale for actor?
 
-  if (isDefined(params.scale)) applyScale(actorW, params.scale);
-  applySpatialGrid(params, actorW, spatialGridService);
+  if (isDefined(params.scale)) applyScale(entities, params.scale);
+  applySpatialGrid(params, entities, spatialGridService);
 
   // TODO 8.0.0. MODELS: check how collisions works with the model3d?
-  startCollisions(actorW);
+  startCollisions(entities);
 
-  position$.subscribe((newPosition: Vector3): void => actorW.updateSpatialCells(newPosition));
+  model3dFacadeToActorConnectionRegistry.addModel3dFacade(model3dF, entities);
 
-  model3dFacadeToActorConnectionRegistry.addModel3dFacade(entity, actorW);
-
-  return actorW;
+  return {
+    ...facade,
+    setDrive,
+    getDrive,
+    position$,
+    rotation$,
+    scale$
+  };
 }
