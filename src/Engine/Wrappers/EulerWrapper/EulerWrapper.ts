3cdc3a9694c289079224a17fe3b25ecef4505575
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:52:18 +0100

    fixes

--- src/Engine/Wrappers/EulerWrapper/EulerWrapper.ts
+++ src/Engine/Wrappers/EulerWrapper/EulerWrapper.ts
@@ -4,6 +4,6 @@ import { Euler } from 'three';
 import type { IEuler, IEulerParams, IEulerWrapper } from './Models';
 
 export function EulerWrapper(params: IEulerParams): IEulerWrapper {
-  const entity: IEuler = new Euler(params.x, params.y, params.z);
+  const entity: IEuler = new Euler(params.x, params.y, params.z, params.order || 'XYZ');
   return { ...AbstractWrapper(entity, WrapperType.Euler), entity };
 }
