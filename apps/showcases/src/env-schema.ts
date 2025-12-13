23455f1afdc3a02c795925a3b0c2e4e09746e3e0
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:56:49 +0100

    [BUILD][DESKTOP]

--- apps/showcases/src/env-schema.ts
+++ apps/showcases/src/env-schema.ts
@@ -1,9 +1,9 @@
 import { toBool } from '@Shared/Utils';
-import { object, string } from 'valibot';
+import { object, optional, string } from 'valibot';
 
 export const runtimeSchema = object({
   VITE_APP_SHOW_DEBUG_INFO: toBool,
-  VITE_APP_DRACO_DECODER_PATH: string()
+  VITE_APP_DRACO_DECODER_PATH: optional(string())
 });
 
 // export const nodeSchema = object({
