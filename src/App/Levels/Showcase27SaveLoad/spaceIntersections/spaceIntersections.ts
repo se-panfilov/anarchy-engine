073661e95d9ff2152f3d389b58062940a1082938
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:50:55 +0100

    [Showcase] intersections

--- src/App/Levels/Showcase27SaveLoad/spaceIntersections/spaceIntersections.ts
+++ src/App/Levels/Showcase27SaveLoad/spaceIntersections/spaceIntersections.ts
@@ -1,7 +1,8 @@
 import { BehaviorSubject, skip } from 'rxjs';
 import type { Mesh, MeshStandardMaterial } from 'three';
+import type { Line2 } from 'three/examples/jsm/lines/Line2';
 
-import type { TActor, TIntersectionEvent, TIntersectionsCameraWatcher, TSpace, TSpaceConfig } from '@/Engine';
+import type { TActor, TIntersectionEvent, TIntersectionsCameraWatcher, TIntersectionsDirectionWatcher, TSpace, TSpaceConfig } from '@/Engine';
 
 import type { TSpacesData } from '../ShowcaseTypes';
 import { getContainer } from '../utils';
@@ -10,26 +11,37 @@ import spaceConfig from './spaceIntersections.json';
 const config: TSpaceConfig = spaceConfig as TSpaceConfig;
 
 // TODO 15-0-0: Implement intersections among actors (add source, direction). Camera intersection should be a private case of intersections.
+// TODO 15-0-0: should serialize "shouldReactOnlyOnChange"
+// TODO 15-0-0: should serialize "far"
 export const spaceIntersectionsData: TSpacesData = {
   name: config.name,
   config: config,
   container: getContainer(config.canvasSelector),
   awaits$: new BehaviorSubject<ReadonlySet<string>>(new Set()),
   onCreate: (space: TSpace): void | never => {
-    const watcherRed: TIntersectionsCameraWatcher = space.services.intersectionsWatcherService.getCameraWatcher('watcher_red');
-    const watcherBlue: TIntersectionsCameraWatcher = space.services.intersectionsWatcherService.getCameraWatcher('watcher_blue');
+    const cameraWatcherRed: TIntersectionsCameraWatcher = space.services.intersectionsWatcherService.getCameraWatcher('watcher_red');
+    const directionWatcherBlue: TIntersectionsDirectionWatcher = space.services.intersectionsWatcherService.getDirectionWatcher('watcher_blue');
 
-    watcherRed.value$.pipe(skip(1)).subscribe((value: TIntersectionEvent): void => {
-      console.log('redWatcher', value);
+    const line: Line2 = directionWatcherBlue._debugGetRayVisualizationLine(space.container, 25);
+    space.services.scenesService.getActive().entity.add(line);
+    // const dir = directionWatcherBlue.targetPointToDirection(directionWatcherBlue.origin$.value, { x: 13, y: 0, z: 8 });
+    // console.log('XXX', dir);
+
+    cameraWatcherRed.value$.pipe(skip(1)).subscribe((value: TIntersectionEvent): void => {
+      // console.log('redWatcher', new Date().getMilliseconds(), value.object.name);
       ((value.object as Mesh).material as MeshStandardMaterial).color.set('yellow');
     });
-    watcherBlue.value$.subscribe((value: TIntersectionEvent): void => console.log('blueWatcher', value));
+
+    directionWatcherBlue.value$.subscribe((value: TIntersectionEvent): void => {
+      // console.log('blueWatcher', new Date().getMilliseconds(), value.object.name);
+      ((value.object as Mesh).material as MeshStandardMaterial).color.set('green');
+    });
   },
   onChange: (space: TSpace): void => {
-    const watcherRed: TIntersectionsCameraWatcher = space.services.intersectionsWatcherService.getCameraWatcher('watcher_red');
+    const cameraWatcherRed: TIntersectionsCameraWatcher = space.services.intersectionsWatcherService.getCameraWatcher('watcher_red');
     const sphereRed2Actor: TActor = space.services.actorService.getRegistry().getByName('sphere_red_2_actor');
     // TODO 15-0-0: Prevent adding actors multiple times (same id, same name)
-    watcherRed.addActor(sphereRed2Actor);
-    console.log('XXX actors', watcherRed.getActors());
+    cameraWatcherRed.addActor(sphereRed2Actor);
+    console.log('XXX actors', cameraWatcherRed.getActors());
   }
 };
