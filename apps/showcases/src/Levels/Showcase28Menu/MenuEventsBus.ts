1f90f4becfa9b93bed06e981af6c0b8b087d8fbf
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:00:43 +0100

    [APPS] fix

--- apps/showcases/src/Levels/Showcase28Menu/MenuEventsBus.ts
+++ apps/showcases/src/Levels/Showcase28Menu/MenuEventsBus.ts
@@ -1,4 +1,4 @@
-import type { TMenuEvent } from '@Menu/models';
+import type { TMenuEvent } from '@ShowcasesShared';
 import { Subject } from 'rxjs';
 
 export const menuEventsBus$: Subject<TMenuEvent> = new Subject<TMenuEvent>();
