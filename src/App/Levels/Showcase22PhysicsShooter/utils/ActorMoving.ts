0fa415801107493cd1d1c320f1609cdc5c379a3a
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:09:18 +0100

    [WIP] fixed kinematic

--- src/App/Levels/Showcase22PhysicsShooter/utils/ActorMoving.ts
+++ src/App/Levels/Showcase22PhysicsShooter/utils/ActorMoving.ts
@@ -5,53 +5,45 @@ import { degToRad } from 'three/src/math/MathUtils';
 import type { TActorWrapperAsync, TIntersectionEvent, TIntersectionsWatcher, TKeyboardService, TRadians } from '@/Engine';
 import { getAzimuthRadFromDirection, getElevationRadFromDirection, KeyCode } from '@/Engine';
 
-type TMoveKeys = Readonly<{ Forward: boolean; Left: boolean; Right: boolean; Backward: boolean }>;
+type TMoveKeysState = Readonly<{ Forward: boolean; Left: boolean; Right: boolean; Backward: boolean }>;
+type TActorMovingParams = Readonly<{ speed: number; azimuthAngleDeviationRad: TRadians; toSideAzimuthRad: TRadians; toBackwardAzimuthRad: TRadians }>;
+type TActorMoveState = Readonly<{ currentSpeed: number; azimuthDeviationLeftRad: TRadians; azimuthDeviationRightRad: TRadians; azimuthDeviationBackwardRad: TRadians }>;
 
 export function startMoveActorWithKeyboard(actorW: TActorWrapperAsync, keyboardService: TKeyboardService, mouseLineIntersectionsWatcher: TIntersectionsWatcher): void {
-  const speed: number = 5;
-  let keyStates: TMoveKeys = { Forward: false, Left: false, Right: false, Backward: false };
+  let keyStates: TMoveKeysState = { Forward: false, Left: false, Right: false, Backward: false };
+  let baseAzimuthRad: TRadians = 0;
 
-  const azimuthAngleDeviationRad: TRadians = degToRad(45);
-  let azimuthDeviationLeft: TRadians = 0;
-  let azimuthDeviationRight: TRadians = 0;
-
-  function getAzimuthDeviation(): TRadians {
-    return azimuthDeviationLeft + azimuthDeviationRight;
-  }
+  const actorMovingParams: TActorMovingParams = {
+    speed: 5,
+    azimuthAngleDeviationRad: degToRad(45),
+    toSideAzimuthRad: degToRad(90),
+    toBackwardAzimuthRad: degToRad(180)
+  };
 
   keyboardService.onKey(KeyCode.W).pressed$.subscribe((): void => void (keyStates = { ...keyStates, Forward: true }));
   keyboardService.onKey(KeyCode.A).pressed$.subscribe((): void => void (keyStates = { ...keyStates, Left: true }));
-  keyboardService.onKey(KeyCode.S).pressed$.subscribe((): void => void (keyStates = { ...keyStates, Right: true }));
-  keyboardService.onKey(KeyCode.D).pressed$.subscribe((): void => void (keyStates = { ...keyStates, Backward: true }));
+  keyboardService.onKey(KeyCode.S).pressed$.subscribe((): void => void (keyStates = { ...keyStates, Backward: true }));
+  keyboardService.onKey(KeyCode.D).pressed$.subscribe((): void => void (keyStates = { ...keyStates, Right: true }));
 
   keyboardService.onKey(KeyCode.W).released$.subscribe((): void => void (keyStates = { ...keyStates, Forward: false }));
   keyboardService.onKey(KeyCode.A).released$.subscribe((): void => void (keyStates = { ...keyStates, Left: false }));
-  keyboardService.onKey(KeyCode.S).released$.subscribe((): void => void (keyStates = { ...keyStates, Right: false }));
-  keyboardService.onKey(KeyCode.D).released$.subscribe((): void => void (keyStates = { ...keyStates, Backward: false }));
+  keyboardService.onKey(KeyCode.S).released$.subscribe((): void => void (keyStates = { ...keyStates, Backward: false }));
+  keyboardService.onKey(KeyCode.D).released$.subscribe((): void => void (keyStates = { ...keyStates, Right: false }));
 
   mouseLineIntersectionsWatcher.value$
     .pipe(
       map((intersection: TIntersectionEvent) => getMouseAzimuthAndElevation(intersection.point, actorW.getPosition().entity)),
       distinctUntilChanged()
     )
-    .subscribe(({ azimuth, elevation }: Readonly<{ azimuth: TRadians; elevation: TRadians }>): void => {
-      // actorW.kinematic.setLinearDirectionFromParamsRad(azimuth, elevation);
-      // actorW.kinematic.setLinearAzimuthRad(azimuth);
-      // console.log(`result: ${radToDeg(azimuth + getAzimuthDeviation())}, azimuth: ${radToDeg(azimuth)}, deviation: ${radToDeg(getAzimuthDeviation())}`);
-      actorW.kinematic.setLinearAzimuthRad(azimuth + getAzimuthDeviation());
-    });
+    .subscribe(({ azimuth }: Readonly<{ azimuth: TRadians; elevation: TRadians }>): void => void (baseAzimuthRad = azimuth));
 
-  keyboardService.onKey(KeyCode.W).pressed$.subscribe((): void => actorW.kinematic.setLinearSpeed(speed));
-  keyboardService.onKey(KeyCode.W).released$.subscribe((): void => actorW.kinematic.setLinearSpeed(0));
-
-  keyboardService.onKey(KeyCode.A).pressed$.subscribe((): void => {
-    actorW.kinematic.setLinearSpeed(speed);
-    azimuthDeviationLeft = -azimuthAngleDeviationRad;
-  });
-  keyboardService.onKey(KeyCode.A).released$.subscribe((): void => void (azimuthDeviationLeft = 0));
-
-  keyboardService.onKey(KeyCode.D).pressed$.subscribe((): void => void (azimuthDeviationRight = azimuthAngleDeviationRad));
-  keyboardService.onKey(KeyCode.D).released$.subscribe((): void => void (azimuthDeviationRight = 0));
+  // TODO (S.Panfilov) DEBUG: remove. Testing kinematic movement
+  setInterval(() => {
+    const state: TActorMoveState = getActorMoveState(keyStates, actorMovingParams);
+    actorW.kinematic.setLinearSpeed(state.currentSpeed);
+    actorW.kinematic.setLinearAzimuthRad(baseAzimuthRad + state.azimuthDeviationLeftRad + state.azimuthDeviationRightRad + state.azimuthDeviationBackwardRad);
+    // console.log(state);
+  }, 40);
 }
 
 function getMouseAzimuthAndElevation(mousePosition: Vector3, playerPosition: Vector3): Readonly<{ azimuth: TRadians; elevation: TRadians }> {
@@ -60,4 +52,33 @@ function getMouseAzimuthAndElevation(mousePosition: Vector3, playerPosition: Vec
   const elevation: number = getElevationRadFromDirection(direction);
 
   return { azimuth, elevation };
+}
+
+function getActorMoveState(keyStates: TMoveKeysState, { speed, azimuthAngleDeviationRad, toSideAzimuthRad, toBackwardAzimuthRad }: TActorMovingParams): TActorMoveState {
+  let currentSpeed: number = 0;
+  let azimuthDeviationLeftRad: TRadians = 0;
+  let azimuthDeviationRightRad: TRadians = 0;
+  let azimuthDeviationBackwardRad: TRadians = 0;
+
+  //moving any direction but not forward and backward at the same time
+  if ((keyStates.Forward || keyStates.Left || keyStates.Right || keyStates.Backward) && !(keyStates.Forward && keyStates.Backward)) currentSpeed = speed;
+  //moving forward and left or backward and left
+  if ((keyStates.Forward && keyStates.Left) || (keyStates.Backward && keyStates.Left)) azimuthDeviationLeftRad = -azimuthAngleDeviationRad;
+  //moving forward and right or backward and right
+  if ((keyStates.Forward && keyStates.Right) || (keyStates.Backward && keyStates.Right)) azimuthDeviationRightRad = azimuthAngleDeviationRad;
+  //moving just left (not forward/backward + left)
+  if ((!keyStates.Forward && keyStates.Left) || (!keyStates.Backward && keyStates.Left)) azimuthDeviationLeftRad = -toSideAzimuthRad;
+  //moving just right (not forward/backward + right)
+  if ((!keyStates.Forward && keyStates.Right) || (!keyStates.Backward && keyStates.Right)) azimuthDeviationRightRad = toSideAzimuthRad;
+  //moving just backward (not backward + left/right)
+  if (((keyStates.Backward && !keyStates.Left) || (keyStates.Backward && !keyStates.Right)) && !keyStates.Forward) azimuthDeviationBackwardRad = toBackwardAzimuthRad;
+  //trying to move forward and backward at the same time
+  if (keyStates.Forward && keyStates.Backward) {
+    currentSpeed = 0;
+    azimuthDeviationBackwardRad = 0;
+  }
+
+  console.log(`Forward: ${keyStates.Forward}, Left: ${keyStates.Left}, Right: ${keyStates.Right}, Backward: ${keyStates.Backward}`);
+
+  return { currentSpeed, azimuthDeviationLeftRad, azimuthDeviationRightRad, azimuthDeviationBackwardRad };
 }
