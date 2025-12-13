e2d448b26cd2280b7364d2440160bfe78b3707a3
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:08:50 +0100

    [WIP] Kinematic props for an actor

--- src/Engine/Mixins/GameObjects/Mixins/WithKinematic.ts
+++ src/Engine/Mixins/GameObjects/Mixins/WithKinematic.ts
@@ -5,21 +5,25 @@ import type { TKinematicInfo } from '@/Engine/Physics/Models';
 
 export function withKinematic(params: TActorParams): TWithKinematic {
   return {
-    setKinematicInfo: (kinematic: TKinematicInfo): void => (this.kinematic = kinematic),
-    getKinematicInfo: (): TKinematicInfo => this.kinematic,
+    setKinematicInfo: function (kinematic: TKinematicInfo): void {
+      this.kinematic = kinematic;
+    },
+    getKinematicInfo: function (): TKinematicInfo {
+      return this.kinematic;
+    },
     kinematic: {
       mass: params.physics?.mass ?? 0,
       linearVelocity: { x: 0, y: 0, z: 0 },
       angularVelocity: { x: 0, y: 0, z: 0 },
       principalInertia: { x: 0, y: 0, z: 0 },
       getSpeed: function (): number {
-        return getSpeedByLinearVelocity(this.kinematic.linearVelocity);
+        return getSpeedByLinearVelocity((this as any).kinematic.linearVelocity);
       },
       getAzimuth: function (): number {
-        return getAzimuthByLinearVelocity(this.kinematic.linearVelocity);
+        return getAzimuthByLinearVelocity((this as any).kinematic.linearVelocity);
       },
       getElevation: function (): number {
-        return getElevationByLinearVelocity(this.kinematic.linearVelocity);
+        return getElevationByLinearVelocity((this as any).kinematic.linearVelocity);
       }
     }
   };
