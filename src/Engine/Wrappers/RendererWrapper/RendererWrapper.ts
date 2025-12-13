2597eb8d0b8bfa675d8a836442e45ee5941a1392
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:46:50 +0100

    fixes

--- src/Engine/Wrappers/RendererWrapper/RendererWrapper.ts
+++ src/Engine/Wrappers/RendererWrapper/RendererWrapper.ts
@@ -1,7 +1,7 @@
 import { PCFShadowMap, WebGL1Renderer } from 'three';
 import { isNotDefined, isWebGLAvailable } from '@Engine/Utils';
 import { AbstractWrapper } from '@Engine/Wrappers';
-import type { RendererParams } from '@Engine/Models';
+import type { RendererParams, ScreenParams } from '@Engine/Models';
 import { DeviceWatcher } from '@Engine/Watchers';
 import type { IRendererWrapper } from './Models';
 
@@ -11,23 +11,29 @@ export function RendererWrapper({ canvas }: RendererParams): IRendererWrapper {
   if (!isWebGLAvailable()) throw new Error('WebGL is not supported by this device');
 
   const entity: WebGL1Renderer = new WebGL1Renderer({ canvas });
+  // eslint-disable-next-line functional/immutable-data
   entity.shadowMap.enabled = true;
+  // eslint-disable-next-line functional/immutable-data
   entity.shadowMap.type = PCFShadowMap;
+  // eslint-disable-next-line functional/immutable-data
   entity.physicallyCorrectLights = true;
 
   // TODO (S.Panfilov) DI deviceWatcher instead of a creation of a new entity
   const deviceWatcher: ReturnType<typeof DeviceWatcher> = DeviceWatcher();
 
-  deviceWatcher.value$.subscribe(({ width, height, ratio }) => {
+  deviceWatcher.value$.subscribe(({ width, height, ratio }: ScreenParams): void => {
     if (isNotDefined(entity)) return;
     entity.setSize(width, height);
     entity.setPixelRatio(Math.min(ratio, 2));
   });
 
-  deviceWatcher.destroy$.subscribe(() => deviceWatcher.value$.unsubscribe());
+  deviceWatcher.destroy$.subscribe(() => {
+    deviceWatcher.value$.unsubscribe();
+    deviceWatcher.destroy$.unsubscribe();
+  });
 
   const wrapper = AbstractWrapper(entity);
-  wrapper.destroy$.subscribe(() => {
+  wrapper.destroy$.subscribe((): void => {
     deviceWatcher.value$.unsubscribe();
     wrapper.destroy$.unsubscribe();
   });
