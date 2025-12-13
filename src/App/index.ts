fbb358de5d77653581648d1ed58e5ccbf7fe70ba
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:24:32 +0100

    [WIP] update vitest

--- src/App/index.ts
+++ src/App/index.ts
@@ -18,13 +18,13 @@ import '@/App/style.css';
 // import { showcase } from '@/App/Levels/Showcase16Distance';
 // import { showcase } from '@/App/Levels/Showcase17Particles';
 // import { showcase } from '@/App/Levels/Showcase18ComplexParticles';
-// import { showcase } from '@/App/Levels/Showcase19Physics';
+import { showcase } from '@/App/Levels/Showcase19Physics';
 // import { showcase } from '@/App/Levels/Showcase20PhysicsSyncWithModelsTest';
 // import { showcase } from '@/App/Levels/Showcase21PhysicsManualStep';
 // import { showcase } from '@/App/Levels/Showcase22PhysicsShooter';
 // import { showcase } from '@/App/Levels/Showcase23CustomModels';
 // import { showcase } from '@/App/Levels/Showcase24ActorsWithModels';
-import { showcase } from '@/App/Levels/Showcase25TransformDrive';
+// import { showcase } from '@/App/Levels/Showcase25TransformDrive';
 import type { TAppCanvas } from '@/Engine';
 import { ambientContext, isNotDefined } from '@/Engine';
 
