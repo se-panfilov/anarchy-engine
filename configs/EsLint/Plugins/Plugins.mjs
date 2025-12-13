7647284a9a4a0c60a0634b7bf53c2903cc622f6f
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:33:44 +0100

    Eslint: spellcheck

--- configs/EsLint/Plugins/Plugins.mjs
+++ configs/EsLint/Plugins/Plugins.mjs
@@ -3,12 +3,14 @@ import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 import tsPlugin from '@typescript-eslint/eslint-plugin';
 import functionalPlugin from 'eslint-plugin-functional';
+import spellcheck from 'eslint-plugin-spellcheck';
 
 export const plugins = {
   'simple-import-sort': simpleImportSort,
   '@typescript-eslint': tsPlugin,
   functional: functionalPlugin,
-  'sort-export-all': sortExportAll
+  'sort-export-all': sortExportAll,
+  spellcheck: spellcheck
   // 'custom-rules': {
   //   rules: {}
   // }
