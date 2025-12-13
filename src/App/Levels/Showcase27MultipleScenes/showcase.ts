64caa20cb65a863ebb1bcdef034418dc2d89c474
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:35:45 +0100

    Showcase: fixes

--- src/App/Levels/Showcase27MultipleScenes/showcase.ts
+++ src/App/Levels/Showcase27MultipleScenes/showcase.ts
@@ -3,7 +3,7 @@ import { combineLatest } from 'rxjs';
 import { Clock } from 'three';
 
 import type { TActor, TActorRegistry, TActorService, TMilliseconds, TSpace, TSpaceConfig, TTransformLoop } from '@/Engine';
-import { asRecord, isNotDefined, spaceService } from '@/Engine';
+import { asRecord, isDefined, isNotDefined, spaceService } from '@/Engine';
 
 import spaceAlphaConfigJson from './spaceAlpha.json';
 import spaceBetaConfigJson from './spaceBeta.json';
@@ -19,31 +19,34 @@ export function start(): void {
   if (isNotDefined(spaceBeta)) throw new Error(`Showcase: Space "${spaceBetaConfig.name}" is not defined`);
 
   combineLatest([spaceAlpha.built$, spaceBeta.built$]).subscribe((): void => showcase(spaceAlpha));
+
+  addSpaceSwitcher((): void => nextSpace([spaceAlpha, spaceBeta]));
 }
+
+let currentSpaceId: string | undefined = undefined;
 
 export function showcase(space: TSpace): void {
   switch (space.name) {
     case spaceAlphaConfig.name:
-      showcaseAlpha(space);
+      runAlpha(space);
       break;
     case spaceBetaConfig.name:
-      showcaseBeta(space);
+      runBeta(space);
       break;
     default:
       throw new Error(`Showcase: Space "${space.name}" is not defined`);
   }
+
+  currentSpaceId = space.id;
+  space.start$.next(true);
 }
 
-export function showcaseAlpha(space: TSpace): void {
+export function runAlpha(space: TSpace): void {
   moveCircle('sphere_actor', space.services.actorService, space.loops.transformLoop, new Clock());
-
-  space.start$.next(true);
 }
 
-export function showcaseBeta(space: TSpace): void {
+export function runBeta(space: TSpace): void {
   moveCircle('box_actor', space.services.actorService, space.loops.transformLoop, new Clock());
-
-  space.start$.next(true);
 }
 
 function moveCircle(actorName: string, actorService: TActorService, transformLoop: TTransformLoop, clock: Clock): Subscription {
@@ -56,4 +59,31 @@ function moveCircle(actorName: string, actorService: TActorService, transformLoo
     actor.drive.default.setX(Math.sin(elapsedTime) * 8);
     actor.drive.default.setZ(Math.cos(elapsedTime) * 8);
   });
+}
+
+function addSpaceSwitcher(cb: (...rest: ReadonlyArray<any>) => void): void {
+  const button: HTMLButtonElement = document.createElement('button');
+  // eslint-disable-next-line functional/immutable-data
+  button.textContent = 'Switch Space';
+  // eslint-disable-next-line functional/immutable-data
+  button.style.position = 'absolute';
+  // eslint-disable-next-line functional/immutable-data
+  button.style.top = '10px';
+  // eslint-disable-next-line functional/immutable-data
+  button.style.right = '10px';
+  button.addEventListener('click', cb);
+  document.body.appendChild(button);
+}
+
+function nextSpace(spaces: ReadonlyArray<TSpace>): void {
+  const currIdx: number = isDefined(currentSpaceId) ? spaces.findIndex((s: TSpace): boolean => s.id === currentSpaceId) : 0;
+  const nextIdx: number = currIdx >= spaces.length ? 0 : currIdx + 1;
+
+  console.log('XXX', currIdx, nextIdx);
+
+  const currSpace: TSpace = spaces[currIdx];
+  const nextSpace: TSpace = spaces[nextIdx];
+  currSpace.start$.next(false);
+  nextSpace.start$.next(true);
+  currentSpaceId = nextSpace.id;
 }
