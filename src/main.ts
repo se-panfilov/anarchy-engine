c95a42674223d2268f4fdeea45586c5459c04e21
S. Panfilov <pnf036@gmail.com>
2025-12-13 01:46:55 +0100

    [WIP] POC awillix

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,15 @@ import * as sceneConfig from '@Engine/Launcher/debug-scene.config.json';
 import { launch } from '@Engine/Launcher';
 import type { SceneConfig } from '@Engine/Launcher/Models';
 
+interface ICradle {
+  testService: TestService;
+  depService: DependentService;
+}
+
+// Create the container
+const container = createContainer<ICradle>({
+  injectionMode: InjectionMode.CLASSIC
+});
+
 // TODO (S.Panfilov) for a production we need a runtime validation
 launch(sceneConfig as unknown as SceneConfig);
