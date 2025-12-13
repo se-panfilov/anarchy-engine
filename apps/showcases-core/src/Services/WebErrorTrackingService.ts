45945d8dd2f8bdc3ea52d5a43e976efbc5d21760
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:20:43 +0100

    [APP][TRACKING] send settings

--- apps/showcases-core/src/Services/WebErrorTrackingService.ts
+++ apps/showcases-core/src/Services/WebErrorTrackingService.ts
@@ -1,5 +1,6 @@
-import type { TTrackingService } from '@Anarchy/Tracking';
+import type { TMetaData, TTrackingService } from '@Anarchy/Tracking';
 import { BrowserTrackingService } from '@Anarchy/Tracking/Services/BrowserTrackingService';
+import type { BrowserOptions } from '@sentry/browser';
 
 import { runtimeEnv } from '@/env';
 import type { TWebErrorTrackingService } from '@/Models';
@@ -9,25 +10,28 @@ export function WebErrorTrackingService(): TWebErrorTrackingService {
   async function start(): Promise<TTrackingService | undefined> {
     if (!runtimeEnv.VITE_SENTRY_DSN) return undefined;
 
-    return BrowserTrackingService(
-      {
-        dsn: runtimeEnv.VITE_SENTRY_DSN,
-        environment: __PLATFORM_MODE__,
-        // Release must exact match the platform's release.
-        release: await platformApiService.getReleaseName(),
-        dist: await platformApiService.getDistName()
-      },
-      // TODO DESKTOP: Useful to get app's settings (resolution, locale, etc). But should not break GDPR
-      // TODO DESKTOP: And maybe add some "state in the game" (level, quest, etc). But we have to apply it dynamically guess, not here.
-      {
-        ...__BUILD_META_INFO__,
-        ...(await platformApiService.getPackagesVersions()),
-        webAppVersion: import.meta.env.__APP_VERSION__,
-        platformVersion: platformApiService.getPlatformVersion(),
-        node: platformApiService.getNodeVersion(),
-        wrappedAppVersion: await platformApiService.getWrappedAppVersion()
-      }
-    );
+    const options: BrowserOptions = {
+      dsn: runtimeEnv.VITE_SENTRY_DSN,
+      environment: __PLATFORM_MODE__,
+      // Release must exact match the platform's release.
+      release: await platformApiService.getReleaseName(),
+      dist: await platformApiService.getDistName()
+    };
+
+    const metaData: TMetaData = {
+      ...__BUILD_META_INFO__,
+      ...(await platformApiService.getPackagesVersions()),
+      webAppVersion: import.meta.env.__APP_VERSION__,
+      platformVersion: platformApiService.getPlatformVersion(),
+      node: platformApiService.getNodeVersion(),
+      wrappedAppVersion: await platformApiService.getWrappedAppVersion()
+    };
+
+    const dynamicDataGetters = (): Record<string, any> => ({ settings: platformApiService.getCachedAppSettings() });
+
+    // TODO DESKTOP: Useful to get app's settings (resolution, locale, etc). But should not break GDPR
+    // TODO DESKTOP: And maybe add some "state in the game" (level, quest, etc). But we have to apply it dynamically guess, not here.
+    return BrowserTrackingService(options, metaData, dynamicDataGetters);
   }
 
   return { start };
