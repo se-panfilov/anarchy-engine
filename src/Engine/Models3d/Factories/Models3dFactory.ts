2bddf08fc7adde7fe1468d0340a7e09dd77e65b8
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:16:32 +0100

    [HEAVY WIP] models3d: fixes

--- src/Engine/Models3d/Factories/Models3dFactory.ts
+++ src/Engine/Models3d/Factories/Models3dFactory.ts
@@ -1,13 +1,11 @@
 import type { TReactiveFactoryWithDependencies } from '@/Engine/Abstract';
-import { FactoryType, ReactiveFactory } from '@/Engine/Abstract';
+import { FactoryType, ReactiveFactoryWithDependencies } from '@/Engine/Abstract';
 import { configToParams } from '@/Engine/Models3d/Adapters';
 import type { TModel3dFacade, TModel3dParams, TModels3dFactory, TModels3dServiceDependencies } from '@/Engine/Models3d/Models';
 import { Model3dFacade } from '@/Engine/Models3d/Wrappers';
 
-// TODO 9.0.0. RESOURCES: do we need this create wrapper function?
-// function createModel(params: TModel3dParams, dependencies: TModel3dFacadeDependencies): TModel3dFacade {
-//   return Model3dFacade(params, dependencies);
-// }
-
-const factory: TReactiveFactoryWithDependencies<TModel3dFacade, TModel3dParams, Pick<TModels3dServiceDependencies, 'animationsService'>> = ReactiveFactory(FactoryType.Models3d, Model3dFacade);
+const factory: TReactiveFactoryWithDependencies<TModel3dFacade, TModel3dParams, Pick<TModels3dServiceDependencies, 'animationsService'>> = ReactiveFactoryWithDependencies(
+  FactoryType.Models3d,
+  Model3dFacade
+);
 export const Models3dFactory = (): TModels3dFactory => ({ ...factory, configToParams });
