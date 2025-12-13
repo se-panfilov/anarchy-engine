db935d6a6af03180d60943bd57c6bd0d4f350efc
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:15:41 +0100

    [HEAVY WIP] models3d: removed extras

--- src/Engine/Models3d/Mixins/WithModel3dFacadeEntities.ts
+++ src/Engine/Models3d/Mixins/WithModel3dFacadeEntities.ts
@@ -1,16 +1,14 @@
 import type { AnimationClip, AnimationMixer, Group, Mesh, Object3D } from 'three';
 
 import type { TAnimationActions } from '@/Engine/Animations/Models';
-import type { TModel3dEntities, TModel3dLoadOptions, TWithModel3dFacadeEntities } from '@/Engine/Models3d/Models';
+import type { TModel3dEntities, TWithModel3dFacadeEntities } from '@/Engine/Models3d/Models';
 
-export function withModel3dFacadeEntities({ url, model, animations, options, clonedFrom, mixer, actions }: TModel3dEntities): TWithModel3dFacadeEntities {
+export function withModel3dFacadeEntities({ model, animations, clonedFrom, mixer, actions }: TModel3dEntities): TWithModel3dFacadeEntities {
   return {
-    getUrl: (): string => url,
     getModel: (): Group | Mesh | Object3D => model,
     getAnimations: (): ReadonlyArray<AnimationClip> => animations,
     getMixer: (): AnimationMixer => mixer,
     getActions: (): TAnimationActions => actions,
-    getOptions: (): TModel3dLoadOptions => options,
     getClonedFrom: (): string | undefined => clonedFrom
   };
 }
--- src/Engine/Models3d/Models/TWithModel3dFacadeEntities.ts
+++ src/Engine/Models3d/Models/TWithModel3dFacadeEntities.ts
@@ -2,14 +2,10 @@ import type { AnimationClip, AnimationMixer, Group, Mesh, Object3D } from 'three
 
 import type { TAnimationActions } from '@/Engine/Animations/Models';
 
-import type { TModel3dLoadOptions } from './TModel3dLoadOptions';
-
 export type TWithModel3dFacadeEntities = Readonly<{
-  getUrl: () => string;
   getModel: () => Group | Mesh | Object3D;
   getAnimations: () => ReadonlyArray<AnimationClip>;
   getMixer: () => AnimationMixer;
   getActions: () => TAnimationActions;
-  getOptions: () => TModel3dLoadOptions;
   getClonedFrom: () => string | undefined;
 }>;
