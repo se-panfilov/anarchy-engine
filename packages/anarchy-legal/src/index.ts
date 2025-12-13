6058c492d8cd5ea05b3e7a930f3160f71ea92111
S. Panfilov <pnf036@gmail.com>
2025-12-13 03:05:48 +0100

    [LEGAL] npm scripts

--- packages/anarchy-legal/src/index.ts
+++ packages/anarchy-legal/src/index.ts
@@ -3,6 +3,8 @@ export * from './Services';
 
 // TODO DESKTOP: LEGAL: Finish CRA (notice file, etc.)
 // TODO DESKTOP: LEGAL: Adjust templates for legal docs (no extras, all fields are filled)
+// TODO DESKTOP: LEGAL: Add .anarchy-legal..config.json to every package
+// TODO DESKTOP: LEGAL: Generate legal docs for every package
 // TODO DESKTOP: LEGAL: Include legal docs in the build (desktop)
 // TODO DESKTOP: LEGAL: Include legal docs in the npm packages
 // TODO DESKTOP: LEGAL: Display EULA/License, etc via menu
