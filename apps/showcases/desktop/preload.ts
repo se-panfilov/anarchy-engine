d81806b1b59329c1fbffe5f28f77564a3103f2d3
S. Panfilov <pnf036@gmail.com>
2025-12-13 02:57:31 +0100

    [DESKTOP] api

--- apps/showcases/desktop/preload.ts
+++ apps/showcases/desktop/preload.ts
@@ -1,18 +1,20 @@
 import { contextBridge, ipcRenderer } from 'electron';
+// import { packageJsonVersion } from './src/Generated/versions.js';
+
+// console.log('Module type:', import.meta.url);
 
 // TODO DESKTOP: Const duplications from AppToPlatformMessagesConstants
 const APP_TO_PLATFORM_CHANNEL: string = 'app-to-platform-channel';
 const PLATFORM_API_NAME: string = 'platformAPI';
 
 contextBridge.exposeInMainWorld(PLATFORM_API_NAME, {
-  // TODO DESKTOP: "ping" is just a test api, remove it.
-  ping: (num: number) => ipcRenderer.invoke(APP_TO_PLATFORM_CHANNEL, 'arg1', 'arg2', num),
-  // TODO DESKTOP: "readConfig" is an example of reading file.
-  readConfig: () => ipcRenderer.invoke('read-config'),
-  // TODO DESKTOP: debug
+  // TODO DESKTOP: define actions enum
+  // TODO DESKTOP: any
+  saveAppSettings: (settings: any): Promise<void> => ipcRenderer.invoke(APP_TO_PLATFORM_CHANNEL, 'app:settings:save', settings),
+  // TODO DESKTOP: any
+  loadAppSettings: (): Promise<any> => ipcRenderer.invoke(APP_TO_PLATFORM_CHANNEL, 'app:settings:save'),
   node: (): string => process.versions.node,
-  // TODO DESKTOP: debug
   chrome: (): string => process.versions.chrome,
-  // TODO DESKTOP: debug
   electron: (): string => process.versions.electron
+  // desktopAppVersion: (): string => packageJsonVersion
 });
--- apps/showcases/src/types/global.d.ts
+++ apps/showcases/src/types/global.d.ts
@@ -1,11 +1,18 @@
-// Keep this line, otherwise the file will not be recognized as a script, not as a declaration file
+// Keep this line. Otherwise, the file will not be recognized as a script, not as a declaration file
 export {};
 
 declare global {
   interface Window {
     platformAPI: {
       // TODO DESKTOP: Declare all platform API methods here
-      ping(num: number): Promise<number>;
+      // TODO DESKTOP: any
+      saveAppSettings(settings: any): Promise<void>;
+      // TODO DESKTOP: any
+      loadAppSettings(): Promise<any>;
+      node(): string;
+      chrome(): string;
+      electron(): string;
+      desktopAppVersion(): string;
     };
   }
 }
