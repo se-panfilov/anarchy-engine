36e81332e937419dedec74f0862d092e4a96774c
S Panfilov <994990+se-panfilov@users.noreply.github.com>
2025-12-23 20:55:49 +0100

    [E2E] Desktop win/linux fix (#20)
    * [E2E] fixes for win/linux
    
    * [E2E] eslint
    
    ---------
    
    Co-authored-by: S. Panfilov <pnf036@gmail.com>

--- apps/showcases-e2e/src/desktop/DesktopE2eUtils.ts
+++ apps/showcases-e2e/src/desktop/DesktopE2eUtils.ts
@@ -33,6 +33,9 @@ export async function launchPackagedElectronApp(): Promise<TDesktopAppLaunchResu
 
   const page: Page = await electronApp.firstWindow();
   await page.waitForLoadState('domcontentloaded');
+  // eslint-disable-next-line spellcheck/spell-checker
+  await page.waitForLoadState('networkidle');
+  await page.waitForLoadState('load');
 
   return { electronApp, page };
 }
