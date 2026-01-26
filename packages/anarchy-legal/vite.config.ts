977de86e8356eb8248eebaedee147f619ff4dd22
S. Panfilov <pnf036@gmail.com>
2026-01-26 18:40:33 +0100

    [LEGAL] fixes for npm packaging

--- packages/anarchy-legal/vite.config.ts
+++ packages/anarchy-legal/vite.config.ts
@@ -17,7 +17,9 @@ export default defineConfig((_config: ConfigEnv): UserConfig => {
         entryRoot: 'src',
         outDir: 'dist',
         tsconfigPath: path.resolve(__dirname, 'tsconfig.json'),
-        exclude: ['**/*.spec.ts', '**/*.test.ts', 'vite.config.ts']
+        exclude: ['**/*.spec.ts', '**/*.test.ts', 'vite.config.ts'],
+        // Important: creates dist/index.d.ts automatically when you have src/index.ts
+        insertTypesEntry: true
       })
     ],
     build: {
@@ -37,6 +39,21 @@ export default defineConfig((_config: ConfigEnv): UserConfig => {
           if (nodeBuiltins.includes(id)) return true;
           // externalize runtime deps (and their subpaths)
           return runtimeExternals.some((p) => id === p || id.startsWith(`${p}/`));
+        },
+        output: {
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+          entryFileNames: `[name].js`,
+          chunkFileNames: `chunks/[name]-[hash].js`,
+          assetFileNames: (assetInfo): string => {
+            const ai = assetInfo as unknown as Readonly<{ name?: string; names?: ReadonlyArray<string> }>;
+            const n: string = ai.names?.[0] ?? ai.name ?? '';
+
+            if (n.endsWith('.css')) return 'styles/[name][extname]';
+            if (n.endsWith('.css.map')) return 'styles/[name][extname]';
+            return 'assets/[name]-[hash][extname]';
+          },
+          inlineDynamicImports: false //extract workers to separate bundle
         }
       },
       outDir: 'dist',
