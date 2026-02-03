f67230baa1cbab7ff8487953bd5bcc41abaa522d
S. Panfilov <pnf036@gmail.com>
2026-02-04 00:07:15 +0100

    [ENGINE][FSM] adapters import/export fix

--- packages/anarchy-engine/src/Fsm/Adapters/ConfigToParams.ts
+++ packages/anarchy-engine/src/Fsm/Adapters/ConfigToParams.ts
@@ -1,6 +1,6 @@
 import type { TFsmConfig, TFsmEvents, TFsmParams, TFsmSource } from '@Anarchy/Engine/Fsm/Models';
 
-export function configToParamsFsm(config: TFsmConfig): TFsmParams | never {
+export function fsmConfigToParams(config: TFsmConfig): TFsmParams | never {
   if (hasFunctions(config)) throw new Error('Config must not contains functions');
 
   if (config.transitions.every((t: ReadonlyArray<TFsmSource | TFsmEvents>): boolean => t.length !== 3)) throw new Error('Transitions must be an array of 3 elements');
--- packages/anarchy-engine/src/Fsm/Factories/FsmSourceFactory.ts
+++ packages/anarchy-engine/src/Fsm/Factories/FsmSourceFactory.ts
@@ -1,11 +1,11 @@
 import type { TReactiveFactory } from '@Anarchy/Engine/Abstract';
 import { FactoryType, ReactiveFactory } from '@Anarchy/Engine/Abstract';
-import { configToParamsFsm } from '@Anarchy/Engine/Fsm/Adapters';
+import { fsmConfigToParams } from '@Anarchy/Engine/Fsm/Adapters';
 import { FsmSource } from '@Anarchy/Engine/Fsm/Entities';
 import type { TFsmParams, TFsmSource, TFsmSourceFactory } from '@Anarchy/Engine/Fsm/Models';
 
 export function FsmSourceFactory(): TFsmSourceFactory {
   const factory: TReactiveFactory<TFsmSource, TFsmParams> = ReactiveFactory(FactoryType.FsmSource, FsmSource);
   // eslint-disable-next-line functional/immutable-data
-  return Object.assign(factory, { configToParams: configToParamsFsm });
+  return Object.assign(factory, { configToParams: fsmConfigToParams });
 }
