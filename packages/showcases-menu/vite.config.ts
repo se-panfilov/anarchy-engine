87f6c7142fbccfc3a332ba14d9cf425ed5452da3
S. Panfilov <pnf036@gmail.com>
2026-02-08 01:35:08 +0100

    [MENU] small fix

--- packages/showcases-menu/vite.config.ts
+++ packages/showcases-menu/vite.config.ts
@@ -5,9 +5,7 @@ import { defineConfig, loadEnv } from 'vite';
 import path from 'node:path';
 import { sharedAliases } from '../../vite.alias';
 import { visualizer } from 'rollup-plugin-visualizer';
-// @ts-expect-error: no type declarations
 import vue from '@vitejs/plugin-vue';
-// @ts-expect-error: no type declarations
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import wasm from 'vite-plugin-wasm';
 import { version } from './package.json';
