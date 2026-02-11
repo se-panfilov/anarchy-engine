b4619718a340cbac333f676e7d61784594bc3356
S. Panfilov <pnf036@gmail.com>
2026-02-11 19:35:15 +0100

    [ALIASES][ENGINE] local TS alias fix

--- packages/anarchy-engine/vite.config.ts
+++ packages/anarchy-engine/vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
       alias: {
         //Do not refer inside the package to the alias of the package, otherwise it will cause problems with .d.ts files.
         ...omitInObjectWithoutMutation(sharedAliases, ['@hellpig/anarchy-engine']),
-        '@Anarchy/Engine': path.resolve(__dirname, 'packages/anarchy-engine/src')
+        '@Anarchy/Engine': path.resolve(__dirname, '../../packages/anarchy-engine/src')
       }
     },
     plugins: [
